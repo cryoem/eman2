286f9c9e66c315922823e76342bfd6c05e0102e5
lpeng <lpeng>
2003-11-04 17:46:44 +0000

    updated pyem

--- libpyEM/libpyEM.cpp
+++ libpyEM/libpyEM.cpp
@@ -27,6 +27,22 @@ BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EMAN_EMData_read_image_overloads_1_5, rea
 
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EMAN_EMData_write_image_overloads_1_4, write_image, 1, 4)
 
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EMAN_EMData_align_overloads_2_3, align, 2, 3)
+
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EMAN_EMData_rotate_translate_overloads_0_5, rotate_translate, 0, 5)
+
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EMAN_EMData_fast_translate_overloads_0_1, fast_translate, 0, 1)
+
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EMAN_EMData_dot_overloads_1_2, dot, 1, 2)
+
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EMAN_EMData_calc_ccf_overloads_1_3, calc_ccf, 1, 3)
+
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EMAN_EMData_make_rotational_footprint_overloads_0_2, make_rotational_footprint, 0, 2)
+
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EMAN_EMData_calc_ccfx_overloads_1_4, calc_ccfx, 1, 4)
+
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EMAN_EMData_unwrap_overloads_0_6, unwrap, 0, 6)
+
 struct EMAN_Filter_Wrapper: EMAN::Filter
 {
     EMAN_Filter_Wrapper(PyObject* self_, const EMAN::Filter & p0):
@@ -49,6 +65,14 @@ struct EMAN_Filter_Wrapper: EMAN::Filter
 
     void default_process_list(std::vector<EMAN::EMData*,std::allocator<EMAN::EMData*> > & p0) {
         EMAN::Filter::process_list(p0);
+    }
+
+    EMAN::Dict get_params() const {
+        return call_method< EMAN::Dict >(self, "get_params");
+    }
+
+    EMAN::Dict default_get_params() const {
+        return EMAN::Filter::get_params();
     }
 
     void set_params(const EMAN::Dict & p0) {
@@ -74,17 +98,19 @@ struct EMAN_Filter_Wrapper: EMAN::Filter
 // Module ======================================================================
 BOOST_PYTHON_MODULE(libpyEM)
 {
-        
     EMAN::vector_to_python<EMAN::EMData*>();
     EMAN::vector_from_python<int>();
+    EMAN::vector_from_python<float>();
     EMAN::vector_to_python<std::string>();
- 
+    
     EMAN::map_to_python<EMAN::EMObject>();
     EMAN::map_from_python<EMAN::EMObject>();
 
     EMAN::Dict_to_python();
     EMAN::Dict_from_python();
 
+
+    
     scope* EMAN_EMUtil_scope = new scope(
     class_< EMAN::EMUtil >("EMUtil", init<  >())
         .def(init< const EMAN::EMUtil & >())
@@ -100,6 +126,8 @@ BOOST_PYTHON_MODULE(libpyEM)
         .staticmethod("get_datatype_string")
         .def("dump_dict", &EMAN::EMUtil::dump_dict)
         .staticmethod("dump_dict")
+        .def("is_same_size", &EMAN::EMUtil::is_same_size)
+        .staticmethod("is_same_size")
     );
 
     enum_< EMAN::EMUtil::EMDataType >("EMDataType")
@@ -143,22 +171,6 @@ BOOST_PYTHON_MODULE(libpyEM)
 
     delete EMAN_EMUtil_scope;
 
-    class_< EMAN::EMObject >("EMObject", init<  >())
-        .def(init< const EMAN::EMObject & >())
-        .def(init< int >())
-        .def(init< float >())
-        .def(init< double >())
-        .def(init< std::basic_string<char,std::char_traits<char>,std::allocator<char> > >())
-        .def(init< EMAN::EMData * >())
-        .def("get_int", &EMAN::EMObject::get_int)
-        .def("get_float", &EMAN::EMObject::get_float)
-        .def("get_double", &EMAN::EMObject::get_double)
-        .def("get_string", &EMAN::EMObject::get_string)
-        .def("get_EMData", &EMAN::EMObject::get_EMData, return_internal_reference< 1 >())
-        .def("is_null", &EMAN::EMObject::is_null)
-        .def("to_str", &EMAN::EMObject::to_str)
-    ;
-
     scope* EMAN_EMData_scope = new scope(
     class_< EMAN::EMData >("EMData", init<  >())
         .def(init< const EMAN::EMData & >())
@@ -167,14 +179,43 @@ BOOST_PYTHON_MODULE(libpyEM)
         .def("insert_clip", &EMAN::EMData::insert_clip)
         .def("read_image", &EMAN::EMData::read_image, EMAN_EMData_read_image_overloads_1_5())
         .def("write_image", &EMAN::EMData::write_image, EMAN_EMData_write_image_overloads_1_4())
+        .def("filter", &EMAN::EMData::filter)
+        .def("cmp", &EMAN::EMData::cmp)
+        .def("align", &EMAN::EMData::align, return_value_policy< manage_new_object >(), EMAN_EMData_align_overloads_2_3())
         .def("normalize", &EMAN::EMData::normalize)
         .def("is_complex", &EMAN::EMData::is_complex)
         .def("ri2ap", &EMAN::EMData::ri2ap)
         .def("ap2ri", &EMAN::EMData::ap2ri)
+        .def("get_parent", &EMAN::EMData::get_parent, return_value_policy< manage_new_object >())
+        .def("set_parent", &EMAN::EMData::set_parent)
         .def("do_fft", &EMAN::EMData::do_fft, return_value_policy< manage_new_object >())
         .def("do_ift", &EMAN::EMData::do_ift, return_value_policy< manage_new_object >())
         .def("gimme_fft", &EMAN::EMData::gimme_fft)
+        .def("rotate_x", &EMAN::EMData::rotate_x)
+        .def("rotate_translate", &EMAN::EMData::rotate_translate, EMAN_EMData_rotate_translate_overloads_0_5())
+        .def("dot_rotate_translate", &EMAN::EMData::dot_rotate_translate)
+        .def("fast_translate", &EMAN::EMData::fast_translate, EMAN_EMData_fast_translate_overloads_0_1())
+        .def("rotate_180", &EMAN::EMData::rotate_180)
+        .def("do_radon", &EMAN::EMData::do_radon, return_value_policy< manage_new_object >())
+        .def("vertical_acf", &EMAN::EMData::vertical_acf, return_value_policy< manage_new_object >())
+        .def("vertical_flip", &EMAN::EMData::vertical_flip)
+        .def("horizontal_flip", &EMAN::EMData::horizontal_flip)
+        .def("set_flipped", &EMAN::EMData::set_flipped)
+        .def("dot", &EMAN::EMData::dot, EMAN_EMData_dot_overloads_1_2())
+        .def("calc_ccf", &EMAN::EMData::calc_ccf, return_value_policy< manage_new_object >(), EMAN_EMData_calc_ccf_overloads_1_3())
+        .def("make_rotational_footprint", &EMAN::EMData::make_rotational_footprint, return_value_policy< manage_new_object >(), EMAN_EMData_make_rotational_footprint_overloads_0_2())
+        .def("calc_ccfx", &EMAN::EMData::calc_ccfx, return_value_policy< manage_new_object >(), EMAN_EMData_calc_ccfx_overloads_1_4())
+        .def("unwrap", &EMAN::EMData::unwrap, return_value_policy< manage_new_object >(), EMAN_EMData_unwrap_overloads_0_6())
+        .def("calc_fourier_shell_correlation", &EMAN::EMData::calc_fourier_shell_correlation)
+        .def("mean_shrink", &EMAN::EMData::mean_shrink)
+        .def("median_shrink", &EMAN::EMData::median_shrink)
         .def("apply_radial_func", &EMAN::EMData::apply_radial_func)
+        .def("set_talign_params", (void (EMAN::EMData::*)(float, float) )&EMAN::EMData::set_talign_params)
+        .def("set_talign_params", (void (EMAN::EMData::*)(float, float, float) )&EMAN::EMData::set_talign_params)
+        .def("set_ralign_params", (void (EMAN::EMData::*)(float, float, float) )&EMAN::EMData::set_ralign_params)
+        .def("set_ralign_params", (void (EMAN::EMData::*)(const EMAN::Rotation &) )&EMAN::EMData::set_ralign_params)
+        .def("set_align_score", &EMAN::EMData::set_align_score)
+        .def("get_align_score", &EMAN::EMData::get_align_score)
         .def("add", (int (EMAN::EMData::*)(float) )&EMAN::EMData::add)
         .def("add", (int (EMAN::EMData::*)(const EMAN::EMData &) )&EMAN::EMData::add)
         .def("sub", &EMAN::EMData::sub)
@@ -186,30 +227,38 @@ BOOST_PYTHON_MODULE(libpyEM)
         .def("get_ctf", &EMAN::EMData::get_ctf, return_internal_reference< 1 >())
         .def("set_ctf", &EMAN::EMData::set_ctf)
         .def("set_size", &EMAN::EMData::set_size)
+        .def("get_min_location", &EMAN::EMData::get_min_location)
+        .def("get_max_location", &EMAN::EMData::get_max_location)
+        .def("get_max_index", &EMAN::EMData::get_max_index)
         .def("get_attr_dict", &EMAN::EMData::get_attr_dict)
         .def("get_value_at", (float (EMAN::EMData::*)(int, int, int) const)&EMAN::EMData::get_value_at)
         .def("get_value_at", (float (EMAN::EMData::*)(int, int) const)&EMAN::EMData::get_value_at)
         .def("sget_value_at", (float (EMAN::EMData::*)(int, int, int) const)&EMAN::EMData::sget_value_at)
         .def("sget_value_at", (float (EMAN::EMData::*)(int, int) const)&EMAN::EMData::sget_value_at)
-        .def("get_value_at_interp", &EMAN::EMData::get_value_at_interp)
+        .def("get_value_at_interp", (float (EMAN::EMData::*)(float, float) const)&EMAN::EMData::get_value_at_interp)
+        .def("get_value_at_interp", (float (EMAN::EMData::*)(float, float, float) const)&EMAN::EMData::get_value_at_interp)
         .def("set_value_at", (void (EMAN::EMData::*)(int, int, int, float) )&EMAN::EMData::set_value_at)
         .def("set_value_at", (void (EMAN::EMData::*)(int, int, float) )&EMAN::EMData::set_value_at)
         .def("get_x", &EMAN::EMData::get_x)
         .def("get_y", &EMAN::EMData::get_y)
         .def("get_z", &EMAN::EMData::get_z)
+        .def("get_translation", &EMAN::EMData::get_translation)
+        .def("set_translation", &EMAN::EMData::set_translation)
+        .def("get_rotation", &EMAN::EMData::get_rotation)
+        .def("get_trans_align", &EMAN::EMData::get_trans_align)
         .def("dump_data", &EMAN::EMData::dump_data)
-        .def( self - other< float >() )
-        .def( self + other< float >() )
-        .def( self * other< float >() )
-        .def( self - self )
-        .def( self + self )
-        .def( other< float >() - self )
-        .def( other< float >() * self )
-        .def( self / other< float >() )
-        .def( other< float >() + self )
         .def( self / self )
         .def( self * self )
+        .def( self - self )
+        .def( self + self )
         .def( other< float >() / self )
+        .def( other< float >() * self )
+        .def( other< float >() - self )
+        .def( self - other< float >() )
+        .def( self + other< float >() )
+        .def( other< float >() + self )
+        .def( self / other< float >() )
+        .def( self * other< float >() )
         .def( self += other< float >() )
         .def( self /= other< float >() )
         .def( self += self )
@@ -223,10 +272,43 @@ BOOST_PYTHON_MODULE(libpyEM)
     EMAN_EMData_scope->attr("DATA_READ_WRITE") = EMAN::EMData::DATA_READ_WRITE;
     delete EMAN_EMData_scope;
 
+    scope* EMAN_EMObject_scope = new scope(
+    class_< EMAN::EMObject >("EMObject", init<  >())
+        .def(init< const EMAN::EMObject & >())
+        .def(init< int >())
+        .def(init< float >())
+        .def(init< double >())
+        .def(init< std::basic_string<char,std::char_traits<char>,std::allocator<char> > >())
+        .def(init< EMAN::EMData * >())
+        .def(init< const std::vector<float,std::allocator<float> > & >())
+        .def("get_int", &EMAN::EMObject::get_int)
+        .def("get_float", &EMAN::EMObject::get_float)
+        .def("get_double", &EMAN::EMObject::get_double)
+        .def("get_string", &EMAN::EMObject::get_string)
+        .def("get_EMData", &EMAN::EMObject::get_EMData, return_internal_reference< 1 >())
+        .def("get_farray", &EMAN::EMObject::get_farray)
+        .def("is_null", &EMAN::EMObject::is_null)
+        .def("to_str", &EMAN::EMObject::to_str)
+        .def("get_object_type_name", &EMAN::EMObject::get_object_type_name)
+        .staticmethod("get_object_type_name")
+    );
+
+    enum_< EMAN::EMObject::ObjectType >("ObjectType")
+        .value("FLOATARRAY", EMAN::EMObject::FLOATARRAY)
+        .value("EMDATA", EMAN::EMObject::EMDATA)
+        .value("STRING", EMAN::EMObject::STRING)
+        .value("UNKNOWN", EMAN::EMObject::UNKNOWN)
+        .value("INT", EMAN::EMObject::INT)
+        .value("DOUBLE", EMAN::EMObject::DOUBLE)
+        .value("FLOAT", EMAN::EMObject::FLOAT)
+    ;
+
+    delete EMAN_EMObject_scope;
+
     class_< EMAN::Filter, boost::noncopyable, EMAN_Filter_Wrapper >("Filter", init<  >())
-        .def("get_params", &EMAN::Filter::get_params)
         .def("process", &EMAN::Filter::process, &EMAN_Filter_Wrapper::default_process)
         .def("process_list", &EMAN::Filter::process_list, &EMAN_Filter_Wrapper::default_process_list)
+        .def("get_params", &EMAN::Filter::get_params, &EMAN_Filter_Wrapper::default_get_params)
         .def("set_params", &EMAN::Filter::set_params, &EMAN_Filter_Wrapper::default_set_params)
     ;
 
