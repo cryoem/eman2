bccd2f13c1ba5b98aeb26c79cc70d71a191c058a
lpeng <lpeng>
2004-03-02 02:25:41 +0000

    indent

--- libEM/emcache.h
+++ libEM/emcache.h
@@ -7,35 +7,39 @@
 
 using std::string;
 
-namespace EMAN {
-
-    template <class T>
-    class EMCache {
+namespace EMAN
+{
+    template <class T> class EMCache
+    {
     public:
-	EMCache(int cache_size) {
-	    item_cache = new T*[cache_size];
+	EMCache(int cache_size)
+	{
+	    item_cache = new T *[cache_size];
 	    name_cache = new string[cache_size];
 
 	    size = cache_size;
 	    nitems = 0;
 	}
-	
-	~EMCache() {
+
+	~EMCache()
+	{
 	    for (int i = 0; i < nitems; i++) {
 		delete item_cache[i];
 		item_cache[i] = 0;
 	    }
+	    
 	    delete [] item_cache;
 	    item_cache = 0;
-	    
+
 	    delete [] name_cache;
 	    name_cache = 0;
 	}
-	
 
-	T* get(string itemname) const {
-	    T* result = 0;
-	    
+
+	T *get(string itemname) const
+	{
+	    T *result = 0;
+
 	    for (int i = 0; i < nitems; i++) {
 		if (name_cache[i] == itemname) {
 		    assert(item_cache[i] != 0);
@@ -43,14 +47,16 @@ namespace EMAN {
 		    break;
 		}
 	    }
-	    
+
 	    return result;
 	}
-	
-	void add(string itemname, T* item) {
+
+	void add(string itemname, T * item)
+	{
 	    if (!item) {
 		return;
 	    }
+
 	    if (nitems < size) {
 		item_cache[nitems] = item;
 		name_cache[nitems] = itemname;
@@ -60,47 +66,45 @@ namespace EMAN {
 		int r = (int) (1.0 * size * rand() / (RAND_MAX + 1.0));
 		delete item_cache[r];
 		item_cache[r] = 0;
-		
+
 		item_cache[r] = item;
-		name_cache[r] = itemname;		
-	    }	    
+		name_cache[r] = itemname;
+	    }
 	}
 
-	int get_size() const {
+	int get_size() const
+	{
 	    return size;
 	}
-	
+
     private:
-	T** item_cache;
-	string* name_cache;
+	T ** item_cache;
+	string *name_cache;
 
 	int size;
 	int nitems;
     };
 
     class ImageIO;
-    
-    class GlobalCache {
-    public:
-	static GlobalCache* instance();
 
-	ImageIO* get_imageio(string filename);
-	void add_imageio(string filename, ImageIO* io);
-	
-	
+    class GlobalCache
+    {
+    public:
+	static GlobalCache *instance();
+
+	ImageIO *get_imageio(string filename);
+	void add_imageio(string filename, ImageIO * io);
+
+
     private:
-	EMCache<ImageIO>* imageio_cache;
-	static GlobalCache* global_cache;
-	
+	EMCache<ImageIO> *imageio_cache;
+	static GlobalCache *global_cache;
+
 	GlobalCache();
-	GlobalCache(const GlobalCache& gc);
+	GlobalCache(const GlobalCache & gc);
 	~GlobalCache();
     };
-    
+
 }
 
 #endif
-
-
-
-
