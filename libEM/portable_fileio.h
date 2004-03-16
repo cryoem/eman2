736ec19186958109a4c55b24cb2f0b27edac111d
lpeng <lpeng>
2004-03-16 21:35:54 +0000

    change largefile support to a simplified python-way.

--- libEM/portable_fileio.h
+++ libEM/portable_fileio.h
@@ -1,21 +1,34 @@
-#ifndef eman__portable_fileio_h__
-#define eman__portable_fileio_h__ 1
-
-#define _FILE_OFFSET_BITS 64
-#define _LARGEFILE_SOURCE 1
+#ifndef __portable_fileio_h__
+#define __portable_fileio_h__
 
 #include <stdio.h>
 #include <sys/types.h>
 
 
-inline int portable_fseek(FILE * fp, off_t offset, int whence)
+inline int portable_fseek(FILE *fp, off_t offset, int whence)
 {
+#if defined(HAVE_FSEEKO) && SIZEOF_OFF_T >= 8
     return fseeko(fp, offset, whence);
+#elif defined(HAVE_FSEEK64)
+    return fseek64(fp, offset, whence);
+#elif defined(__BEOS__)
+    return _fseek(fp, offset, whence);
+#else
+    return fseek(fp, offset, whence);
+#endif
 }
 
-inline off_t portable_ftell(FILE * fp)
+inline off_t portable_ftell(FILE* fp)
 {
+#if defined(HAVE_FTELLO) && SIZEOF_OFF_T >= 8
     return ftello(fp);
+#elif defined(HAVE_FTELL64)
+    return ftell64(fp);
+#else
+    return ftell(fp);
+#endif
 }
+
+
 
 #endif
