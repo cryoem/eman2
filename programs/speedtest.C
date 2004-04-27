980e7917a7c74f7285177ed6e5815d5f7e772edc
lpeng <lpeng>
2004-04-27 20:54:16 +0000

    *** empty log message ***

---
+++ programs/speedtest.C
@@ -0,0 +1,373 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include "emdata.h"
+#include "util.h"
+
+using namespace EMAN;
+
+#define CPS	(CLOCKS_PER_SEC)
+
+
+int main(int argc, char *argv[])
+{
+    int SIZE = 96;
+    int NTT = 500;
+ 
+    int slow = 0;
+    int low = 0;
+    int big = 0;
+    int newali = 0;
+
+    if (argc > 1) {
+	if (Util::sstrncmp(argv[1], "slowest"))
+	    slow = 2;
+	else if (Util::sstrncmp(argv[1], "slow") == 0)
+	    slow = 1;
+	else if (Util::sstrncmp(argv[1], "best") == 0)
+	    slow = 3;
+	else if (Util::sstrncmp(argv[1], "low") == 0)
+	    low = 1;
+	else if (Util::sstrncmp(argv[1], "refine") == 0)
+	    newali = 1;
+	else if (Util::sstrncmp(argv[1], "big") == 0)
+	    big = 1;
+    }
+
+    printf("This could take a few minutes. Please be patient.\n");
+    if (big) {
+	NTT = 100;
+	SIZE = 320;
+    }
+
+
+    printf("Initializing\n");
+    EMData pat;
+    pat.set_size(SIZE, SIZE, 1);
+    float *d = pat.get_data();
+
+    for (int i = -SIZE / 2; i < SIZE / 2; i++) {
+	for (int j = -SIZE / 2; j < SIZE / 2; j++) {
+	    d[(i + SIZE / 2) + (j + SIZE / 2) * SIZE] =
+		-3.0 * exp(-Util::square((fabs(i) + fabs(j))) / 10.0) +
+		exp(-Util::square((fabs(i) + fabs(j) / 2.0)) / 100.0) * (abs(i) < 2 ? 2.0 : 1.0);
+	}
+    }
+    pat.done_data();
+    pat.filter("CircleMeanNormalize");
+    pat.filter("SharpMask", Dict("outer_radius", EMObject(pat.get_xsize()/2)));
+
+    EMData *data[5000];
+    
+    for (int i = 0; i < NTT; i++) {
+	data[i] = pat.copy(false, false);
+	float *d = data[i]->get_data();
+
+	for (int j = 0; j < SIZE * SIZE; j++) {
+	    d[j] += Util::get_gauss_rand(0, 1.0);
+	}
+	data[i]->done_data();
+	data[i]->filter("CircleMeanNormalize");
+	data[i]->filter("SharpMask", Dict("outer_radius", EMObject(data[i]->get_xsize()/2)));
+	
+	if (i < 5) {
+	    data[i]->write_image("speed.hed", i, EMUtil::IMAGE_IMAGIC);
+	}
+    }
+
+#if 0
+    if (low) {
+	printf("Low level tests starting. Please note that compiling with optimization may invalidate certain tests. Also note that overhead is difficult to compensate for, so in most cases it is not dealt with.\n");
+	
+	clock_t t1 = clock();
+	for (float fj = 0; fj < 500.0; fj += 1.0) {
+	    for (int i = 0; i < NTT / 2.0; i += 1)
+		r = data[i]->dot(data[i + NTT / 2]);
+	}
+	
+	clock_t t2 = clock();
+	float ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 1: %d, %d x %d dot()s in %1.1f sec  %1.1f/sec-> ~%1.2f mflops\n",
+	       500 * NTT / 2, SIZE, SIZE, ti, 500 * NTT / (2.0 * ti),
+	       SIZE * SIZE * 4 * 500.0 * NTT / (1000000.0 * ti));
+
+	t1 = clock();
+	for (float fj = 0; fj < 500.0; fj += 1.0) {
+	    for (float fi = 0; fi < NTT / 2.0; fi += 1.0)
+		float r = data[1]->dot(data[12]);
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf
+	    ("Baseline 1a: %d, %d x %d optimized cached dot()s in %1.1f sec  %1.1f/sec-> ~%1.2f mflops\n",
+	     500 * NTT / 2, SIZE, SIZE, ti, 500 * NTT / (2.0 * ti),
+	     SIZE * SIZE * 4 * 500.0 * NTT / (1000000.0 * ti));
+	
+	t1 = clock();
+	for (int j = 0; j < 500; j++) {
+	    for (int i = 0; i < NTT / 2; i++)
+		float r = data[i]->lcmp(data[i + NTT / 2], 1);
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 2: %d, %d x %d lcmp()s in %1.1f sec   %1.1f/sec\n", 500 * NTT / 2, SIZE,
+	       SIZE, ti, 500 * NTT / (2.0 * ti));
+
+	t1 = clock();
+	for (j = 0; j < 100; j++) {
+	    for (i = 0; i < NTT / 2; i++)
+		r = data[i]->pcmp(data[i + NTT / 2], NULL);
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 3: %d, %d x %d pcmp()s in %1.1f sec  %1.1f/sec\n", 100 * NTT / 2, SIZE,
+	       SIZE, ti, 100 * NTT / (2.0 * ti));
+
+	t1 = clock();
+	for (j = 0; j < 100; j++) {
+	    for (i = 0; i < NTT / 2; i++)
+		r = data[i]->fscmp(data[i + NTT / 2], NULL);
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 4: %d, %d x %d fscmp()s in %1.1f sec  %1.1f/sec\n", 100 * NTT / 2, SIZE,
+	       SIZE, ti, 100 * NTT / (2.0 * ti));
+
+	t1 = clock();
+	for (j = 0; j < 500; j++) {
+	    for (i = 0; i < NTT / 2; i++)
+		data[i]->realFilter(4);
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 5a: %d, %d x %d fabs in %1.1f sec -> ~%1.2f mfabs/sec\n", 500 * NTT / 2,
+	       SIZE, SIZE, ti, SIZE * SIZE * 500.0 * NTT / (1000000.0 * ti));
+
+	t1 = clock();
+	for (j = 0; j < 100; j++) {
+	    for (i = 0; i < NTT / 2; i++)
+		data[i]->realFilter(10);
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 5b: %d, %d x %d sqrts in %1.1f sec -> ~%1.2f msqrt/sec\n", 100 * NTT / 2,
+	       SIZE, SIZE, ti, SIZE * SIZE * 100.0 * NTT / (1000000.0 * ti));
+
+	d = data[0]->get_data();
+	t1 = clock();
+	for (j = 0; j < 100; j++) {
+	    for (i = 0; i < NTT / 2; i++) {
+		for (k = 0; k < SIZE * SIZE; k++)
+		    f = sqrt(d[k]);
+	    }
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 5c: %d, %d x %d sqrts in %1.1f sec -> ~%1.2f msqrt/sec (cached)\n",
+	       100 * NTT / 2, SIZE, SIZE, ti, SIZE * SIZE * 100.0 * NTT / (1000000.0 * ti));
+	data[0]->done_data();
+
+	d = data[0]->get_data();
+	t1 = clock();
+	for (j = 0; j < 100; j++) {
+	    for (i = 0; i < NTT / 2; i++) {
+		for (k = 0; k < SIZE * SIZE; k++)
+		    f = cos(d[k]);
+	    }
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 5d: %d, %d x %d cos in %1.1f sec -> ~%1.2f mcos/sec (cached)\n",
+	       100 * NTT / 2, SIZE, SIZE, ti, SIZE * SIZE * 100.0 * NTT / (1000000.0 * ti));
+	data[0]->done_data();
+
+	d = data[0]->get_data();
+	t1 = clock();
+	for (j = 0; j < 100; j++) {
+	    for (i = 0; i < NTT / 2; i++) {
+		for (k = 0; k < SIZE * SIZE - 1; k++)
+		    f = hypot(d[k], d[k + 1]);
+	    }
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 5e: %d, %d x %d hypot in %1.1f sec -> ~%1.2f mhypot/sec (cached)\n",
+	       100 * NTT / 2, SIZE, SIZE, ti, SIZE * SIZE * 100.0 * NTT / (1000000.0 * ti));
+	data[0]->done_data();
+
+	d = data[0]->get_data();
+	t1 = clock();
+	for (j = 0; j < 1000; j++) {
+	    for (i = 0; i < NTT / 2; i++) {
+		for (k = 0; k < SIZE * SIZE - 1; k++)
+		    f = d[k] * d[k + 1];
+	    }
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 5f: %d, %d x %d mult in %1.1f sec -> ~%1.2f mmult/sec (cached)\n",
+	       100 * NTT / 2, SIZE, SIZE, ti, SIZE * SIZE * 1000.0 * NTT / (1000000.0 * ti));
+	data[0]->done_data();
+
+	d = data[0]->get_data();
+	t1 = clock();
+	for (j = 0; j < 500; j++) {
+	    for (i = 0; i < NTT / 2; i++) {
+		for (k = 0; k < SIZE * SIZE - 1; k++)
+		    f = d[k] / d[k + 1];
+	    }
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 5g: %d, %d x %d div in %1.1f sec -> ~%1.2f mdiv/sec (cached)\n",
+	       100 * NTT / 2, SIZE, SIZE, ti, SIZE * SIZE * 500.0 * NTT / (1000000.0 * ti));
+	data[0]->done_data();
+
+	d = data[0]->get_data();
+	t1 = clock();
+	for (j = 0; j < 500; j++) {
+	    for (i = 0; i < NTT / 2; i++) {
+		for (k = 0; k < SIZE * SIZE - 1; k++)
+		    f = fabs(d[k]);
+	    }
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 5h: %d, %d x %d fabs in %1.1f sec -> ~%1.2f fabs/sec (cached)\n",
+	       100 * NTT / 2, SIZE, SIZE, ti, SIZE * SIZE * 500.0 * NTT / (1000000.0 * ti));
+	data[0]->done_data();
+
+	d = data[0]->get_data();
+	t1 = clock();
+	for (j = 0; j < 500; j++) {
+	    for (i = 0; i < NTT / 2; i++) {
+		for (k = 0; k < SIZE * SIZE - 1; k++) {
+		    f = atan2(d[k], d[k + 1]);
+		    r = hypot(d[k], d[k + 1]);
+		}
+	    }
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 5i: %d, %d x %d ri2ap in %1.1f sec -> ~%1.2f ri2ap/sec (cached)\n",
+	       100 * NTT / 2, SIZE, SIZE, ti, SIZE * SIZE * 500.0 * NTT / (1000000.0 * ti));
+	data[0]->done_data();
+
+	t1 = clock();
+	r = 0;
+	EMData *cp;
+	for (i = 0; i < NTT * 100; i++) {
+	    cp = data[i % NTT]->copy(0, 0);
+	    cp->meanShrink(2);
+	    delete cp;
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 6:  %1.1f sec %f meanshrink x 2/sec\n", ti, NTT * 100.0 / ti);
+
+	EMData *d1a, *d2a;
+	t1 = clock();
+	r = 0;
+	for (i = 0; i < NTT * 1000; i++) {
+	    EMData *d = d1a->doFFT();
+	    d1a->update();
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 7:  %1.1f sec %f ffts/sec\n", ti, NTT * 1000 / ti);
+	printf("%d\n", d1a->xSize());
+
+	d1a = data[0]->copy(0, 0);
+//      d1a->meanShrink(4);
+	d1a = d1a->copy();
+	d2a = data[1]->copy(0, 0);
+//      d2a->meanShrink(4);
+	t1 = clock();
+	r = 0;
+	for (i = 0; i < NTT * 1000; i++) {
+	    d1a->setTAlign(-1, -3, 0);
+//              d1a->fastTranslate(0);
+	    d1a->rotateAndTranslate();
+	}
+	t2 = clock();
+	ti = (t2 - t1) / (float) CPS;
+	printf("Baseline 8:  %1.1f sec   %f translates/sec\n", ti, NTT * 1000 / ti);
+
+	exit(0);
+
+    }
+#endif
+    
+    EMData *tmp = 0;
+    clock_t t1 = clock();
+    for (int i = 0; i < 3; i++) {
+	for (int j = 5; j < (slow == 2 ? NTT / 10 : NTT); j++) {
+	    if (slow == 2) {
+		tmp = data[i]->align("RTFSlowest", Dict("with", EMObject(data[j]),
+							"flip", EMObject(0),
+							"maxshift", EMObject(SIZE/8)));
+	    }
+	    else if (slow == 3) {
+		tmp = data[i]->align("RTFBest", Dict("with", EMObject(data[j]),
+						     "flip", EMObject(0),
+						     "maxshift", EMObject(SIZE/8)));
+	    }
+	    else if (slow == 1)
+		tmp = data[i]->align("RTFSlow", Dict("with", EMObject(data[j]),
+						     "flip", EMObject(0),
+						     "maxshift", EMObject(SIZE/8)));
+	    else if (newali == 1) {
+		EMData *t1, *t2, *t3;
+		tmp = data[i]->align("RotateTranslateFlip", Dict("with", EMObject(data[j])));
+		tmp->align("Refine", Dict("with", EMObject(data[j])));
+		delete t1;
+		delete t2;
+		delete t3;
+	    }
+	    else {
+		Dict d;
+		d["with"] = EMObject(data[j]);
+		tmp = data[i]->align("RotateTranslateFlip", d);
+	    }
+	    
+	    delete tmp;
+	    if (j % 10 == 0) {
+		putchar('.');
+		fflush(stdout);
+	    }
+	}
+	putchar('\n');
+    }
+    clock_t t2 = clock();
+
+    float ti = (t2 - t1) / (float) CPS;
+    if (slow == 2)
+	ti *= 10.0;
+    if (!big && !slow && !newali) {
+	printf
+	    ("\nFor comparison (note these numbers may change from release to release)\n* - denotes old values which may be 25% higher than the current version\n");
+	printf("A  250Mhz R10k Octane has a SF of 180\n");
+	printf("A  PIII 800 has a sf of ~390\n");
+	printf("An Athlon 1.4ghz has a sf of 580\n");
+	printf
+	    ("A Xeon 2600mhz on a 4-way shared memory machine with all processors active has a sf of 380 *\n");
+	printf
+	    ("A Xeon 2600mhz on a 4-way shared memory machine with 2 processors active has a sf of 400 *\n");
+	printf("A P4 3.2 Ghz with dual channel pc2700 ram has a sf of 800 (*?)\n");
+	printf("An Athlon XP2800+ (2133mhz) with gcc3.2 and FFTWGEL has a sf of 1100\n");
+	printf("An Athlon64 3000 (2000mhz) with gcc3.2, FFTWGEL and 2.4 kernel has a sf of 1050\n");
+	printf("An Athlon64 3200 (2000mhz) 32-bit with FFTWGEL and 2.6 kernel has a sf of 1350\n");
+	printf("An Athlon64 3200 (2000mhz) 64-bit has a sf of 1520\n");
+	printf("\nYour machines speed factor = %1.1f\n", 25000.0 / ti);
+	printf("\n\nThis repesents %1.2f RTFAligns/sec\n",
+	       3.0 * ((slow == 2 ? NTT / 10 : NTT) - 5) / ti);
+    }
+    else if (big && !slow) {
+//printf("An Athlon XP2400+ (2000mhz) with gcc3.2 and FFTWGEL has a BIG sf of 970\n");
+	printf("\nYour machines speed factor = %1.1f\n", 72000.0 / ti);
+    }
+    else {
+	printf("\nYour machines speed factor on this test = %1.1f\n", 25000.0 / ti);
+    }
+
+}
