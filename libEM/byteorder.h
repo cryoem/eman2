70667ae5bcfc63a7b55feb663f48887fa479c29b
lpeng <lpeng>
2004-04-29 17:02:17 +0000

    documentation

--- libEM/byteorder.h
+++ libEM/byteorder.h
@@ -9,12 +9,36 @@
 
 namespace EMAN
 {
-
+    /** ByteOrder defines functions to work on big/little endian byte orders.
+     *
+     * The byte order is the order in which bytes are stored to
+     * create larger data types such as the int and long values.
+     * Different kinds of computers use different byte order conventions.
+     * 
+     * There are 2 major byte orders: big-endian and little-endian.
+     *
+     * big-endian (like SGI) store the most significant bytes (i.e. the
+     * bytes that hold the largest part of the value) first. little-endian
+     * (like x86) store the most significant byte last. 
+     *
+     * The machine byte order is the byte order used on the current machine.
+     *
+     * ByteOrder class defines functions for
+     *    checking running-machine byte-order,
+     *    checking data byte-order,
+     *    convert data from one byte-order to another byte-order.
+     */
     class ByteOrder
     {
     public:
 	static bool is_machine_big_endian();
 
+	/** given a pointer to a reasonable small integer number,
+	 * return whether the number is big endian or not.
+	 * For a n-bit integer, the number should < (2 ^ (n/2)).
+	 * e.g., for 'int', number should < 65535;
+	 * for 'short', number should < 255.
+	 */
 	template <class T> static bool is_data_big_endian(T * small_num_addr)
 	{
 	    if (!small_num_addr) {
@@ -47,20 +71,26 @@ namespace EMAN
 	    return data_big_endian;
 	}
 
-
+	/** convert data from machine byte order to big endian order.
+	 * 'n' is the number of elements of type T.
+	 */
 	template <class T> static void become_big_endian(T * data, size_t n = 1) {
 	    if (!is_machine_big_endian()) {
 		swap_bytes<T>(data, n);
 	    }
 	}
 
+	/** convert data from machine byte order to little endian order.
+	 * 'n' is the number of elements of type T.
+	 */
 	template <class T> static void become_little_endian(T * data, size_t n = 1) {
 	    if (is_machine_big_endian()) {
 		swap_bytes<T>(data, n);
 	    }
 	}
 
-
+	/** swap the byte order of data with 'n' T-type elements.
+	 */
 	template <class T> static void swap_bytes(T * data, size_t n = 1) {
 	    unsigned char s;
 	    size_t p = sizeof(T);
