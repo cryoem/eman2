649d58b621edfde619adbcc15ed3b832576b87dd
lpeng <lpeng>
2004-07-23 02:18:16 +0000

    add more EMData filter tests

--- rt/pyem/exception.py
+++ rt/pyem/exception.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
 
 from EMAN2 import *
+import os
 
 e = EMData()
 e.set_size(100, 100, 1)
+
 try:
 	e.filter("GoodMorning")
 except RuntimeError, detail:
-	print type(detail)
-except:
-	print "nothing"
-	
+	print detail
+
+fake_img = os.environ['HOME'] + "/images/fake.mrc"
+try:
+	e.read_image(fake_img)
+except RuntimeError, detail:
+	print detail
 	
