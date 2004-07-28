9b625ddace9a8d14252b1080492797706de1661f
stevel <stevel>
2004-07-28 23:58:45 +0000

    Working on boxer

--- libpyEM/EMAN2.py
+++ libpyEM/EMAN2.py
@@ -2,6 +2,7 @@ from libpyEMData2 import *
 from libpyFactory2 import *
 from libpyGeometry2 import *
 from libpyUtils2 import *
+from bisect import bisect_left
 
 EMANVERSION="EMAN2 v1.90"
 
@@ -47,3 +48,20 @@ def get_optionlist(argv):
             argname = arg1.split("=")
             optionlist.append(argname[0].lstrip("-"))
     return optionlist
+
+# These are particle box sizes which have only 2,3,5 and 7 as prime factors, 
+# and are all divisible by 4
+good_box_sizes=[24, 28, 32, 36, 40, 48, 56, 60, 64, 72, 80, 84, 96, 100, 108, 112, 120, 128, 
+	140, 144, 160, 168, 180, 192, 196, 200, 216, 224, 240, 252, 256, 280, 288, 300, 320, 324, 
+	336, 360, 384, 392, 400, 420, 432, 448, 480, 500, 504, 512, 540, 560, 576, 588, 600, 640, 
+	648, 672, 700, 720, 756, 768, 784, 800, 840, 864, 896, 900, 960, 972, 980, 1000, 1008, 1024, 
+	1080, 1120, 1152, 1176, 1200, 1260, 1280, 1296, 1344, 1372, 1400, 1440, 1500, 1512, 1536, 
+	1568, 1600, 1620, 1680, 1728, 1764, 1792, 1800, 1920, 1944, 1960, 2000, 2016, 2048, 2100, 
+	2160, 2240, 2268, 2304, 2352, 2400, 2500, 2520, 2560, 2592, 2688, 2700, 2744, 2800, 2880, 
+	2916, 2940, 3000, 3024, 3072, 3136, 3200, 3240, 3360, 3456, 3500, 3528, 3584, 3600, 3780, 
+	3840, 3888, 3920, 4000, 4032, 4096]
+
+def good_boxsize(val):
+	"This will find the next largest 'good' boxsize"
+	if val > 4096: return int(2**ceil(log(val)/log(2.0)))
+	return good_box_sizes[bisect_left(good_box_sizes,val)]
--- programs/e2boxer.py
+++ programs/e2boxer.py
@@ -29,11 +29,46 @@ for single particle analysis."""
 	image=EMData()
 	image.read_image(args[0])
 	
+	if options.refptcl :
+		refptcl=EMData.read_images(options.refptcl)
+		print "%d reference particles read"%len(refptcl)
+	
+	if options.box<5 :
+		if options.refptcl : options.box=refptcl.get_xsize()
+		elif options.ptclsize : 
+			options.box=good_boxsize(options.ptclsize*1.2)
+		else : parser.error("Please specify a box size")
+	else:
+		if not options.box in good_box_sizes:
+			print "Note: EMAN2 processing would be more efficient with a boxsize of %d"%good_boxsize(options.box)
+			
+	shrinkfactor=int(ceil(image.get_ysize()/1024.0))
+	if options.box/shrinkfactor<12 : shrinkfactor/=2
+	
+	shrink=image.mean_shrink(shrinkfactor)		# shrunken original image
+	
 	if "circle" in options.auto:
+		shrinksq=shrink.copy(0)
+		shrinksq*=shrinksq			# shrunken original image squared
 		
-	
-	
-	
+		# outer and inner ring mask
+		outer=EMData()
+		sbox=int(options.box/shrinkfactor)
+		outer.set_size(shrink.get_xsize(),shrink.get_ysize(),1)
+		outer.to_one()
+		inner=outer.copy(0)
+		
+		outer.filter("SharpMask",{"inner_radius":sbox*2/5,"outer_radius":sbox/2})
+		inner.filter("SharpMask",{"outer_radius":sbox*2/5})
+		
+		outer.write_image("b_outer.mrc")
+		inner.write_image("b_inner.mrc")
+
+		ccf1=  shrink.calc_ccf(outer,1)
+		ccf2=shrinksq.calc_ccf(outer,1)
+		
+		ccf1.write_image("b_ccf1.mrc")
+		ccf2.write_image("b_ccf2.mrc")
 	
 if __name__ == "__main__":
 	main()
