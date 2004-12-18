a6620dc8fd318b9ce0e2ab64264a4a907391d7fa
lpeng <lpeng>
2004-12-18 13:44:30 +0000

    add more std::map related type converters

--- libpyEM/libpyTypeConverter2.cpp
+++ libpyEM/libpyTypeConverter2.cpp
@@ -27,6 +27,7 @@ BOOST_PYTHON_MODULE(libpyTypeConverter2)
 
 
 	EMAN::vector_to_python<int>();
+	EMAN::vector_from_python<long>();
 	EMAN::vector_to_python<float>();
 	EMAN::vector_to_python<double>();
 	EMAN::vector_to_python<std::string>();
@@ -34,15 +35,28 @@ BOOST_PYTHON_MODULE(libpyTypeConverter2)
 	EMAN::vector_to_python<EMAN::Pixel>();
 
 	EMAN::vector_from_python<int>();
+	EMAN::vector_from_python<long>();
 	EMAN::vector_from_python<float>();
 	EMAN::vector_from_python<double>();
 	EMAN::vector_from_python<std::string>();
 	EMAN::vector_from_python<EMAN::EMData*>();
 	EMAN::vector_from_python<EMAN::Pixel>();
 
-	EMAN::map_to_python<EMAN::EMObject>();
-	EMAN::map_from_python<EMAN::EMObject>();
+	EMAN::map_to_python<int>();
+	EMAN::map_to_python<long>();
+	EMAN::map_to_python<float>();
+	EMAN::map_to_python<double>();
+	EMAN::map_to_python<std::string>();
 	EMAN::map_to_python<vector<string> >();
+	EMAN::map_to_python<EMAN::EMObject>();
+
+	EMAN::map_from_python<int>();
+	EMAN::map_from_python<long>();
+	EMAN::map_from_python<float>();
+	EMAN::map_from_python<double>();
+	EMAN::map_from_python<std::string>();
+	EMAN::map_from_python<vector<string> >();
+	EMAN::map_from_python<EMAN::EMObject>();
 
 	EMAN::Dict_to_python();
 	EMAN::Dict_from_python();
