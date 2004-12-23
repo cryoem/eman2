11642455a21fbf3bc6ebf3823b3aab9243ba57b8
lpeng <lpeng>
2004-12-23 03:14:26 +0000

    simple dummy reconstruction

---
+++ rt/demo/reconstructor.py
@@ -0,0 +1,19 @@
+#!/bin/env python
+
+from EMAN2 import *
+import math
+
+e1 = EMData()
+e1.read_image(TestUtil.get_debug_image("samesize1.mrc"))
+
+e2 = EMData()
+e2.read_image(TestUtil.get_debug_image("samesize2.mrc"))
+
+r = Reconstructors.get("BackProjection")
+r.set_params({"size":100, "weight":1})
+r.setup()
+r.insert_slice(e1, Transform(EULER_EMAN, 0,0,0))
+r.insert_slice(e2, Transform(EULER_EMAN, math.pi/2,0,0))
+
+result = r.finish()
+result.write_image("reconstructor.mrc")
