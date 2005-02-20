cb2fe32a260948e3bda3c390eba867cc8edad041
lpeng <lpeng>
2005-02-20 01:35:18 +0000

    add more filter tests

--- rt/pyem/test_filter.py
+++ rt/pyem/test_filter.py
@@ -4,12 +4,20 @@ from EMAN2 import *
 import unittest,os,sys
 from test import test_support
 import testlib
+from pyemtbx.exceptions import *
 
 class TestFilter(unittest.TestCase):
 
     def test_get_filter_list(self):
         filternames = Filters.get_list()
         self.assertEqual(len(filternames), 78)
+
+        try:
+            f2 = Filters.get("_nosuchfilter___")
+        except RuntimeError, runtime_err:
+            err_type = exception_type(runtime_err)
+            self.assertEqual(err_type, "NotExistingObjectException")
+
 
 
     def test_BinarizeFilter(self):
@@ -24,10 +32,48 @@ class TestFilter(unittest.TestCase):
         f1.process(e)
         testlib.check_emdata(e, sys.argv[0])
         os.unlink(imgfile1)
+
+
+    def test_RangeThreshold(self):
+        imgfile1 = "test_RangeThreshold.mrc"
+        TestUtil.make_image_file(imgfile1, MRC)
+        e = EMData()
+        e.read_image(imgfile1)
+        low = 10
+        high = 20
+        f1 = Filters.get("threshold.binaryrange", {"low":low, "high":high});
+        d1 = f1.get_params()
+        self.assertEqual(d1["low"], low)
+        self.assertEqual(d1["high"], high)
+
+        xc = 12
+        rw = 12.5
+        f2 = Filters.get("mask.ringmean", {"xc":xc, "ring_width":rw})
+        d2 = f2.get_params()
+        self.assertEqual(d2["xc"], xc)
+        self.assertEqual(d2["ring_width"], rw)
+
+        e.filter("threshold.binary", {"value": 200})
+        e.write_image("test_RangeThreshold_out.mrc")
+
+
+
+class TestCmp(unittest.TestCase):
+    def test_variance(self):
+        imgfile1 = "test_variance_1.mrc"
+        TestUtil.make_image_file(imgfile1, MRC)
+        e1 = EMData()
+        e1.read_image(imgfile1)
+
+        e2 = e1.copy()
+        score = e2.cmp("Variance", e1, {"keepzero": 0})
+        self.assertEqual(score, 0)
+        os.unlink(imgfile1)
+        
         
 
 def test_main():
-    test_support.run_unittest(TestFilter)
+    test_support.run_unittest(TestCmp, TestFilter)
 
 if __name__ == '__main__':
     test_main()
