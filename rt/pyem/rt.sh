76b3c1bff31aaffc86942917b1553054d7cad118
lpeng <lpeng>
2005-02-24 04:05:26 +0000

    to run all programs

---
+++ rt/pyem/rt.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+python test_emdata.py
+python test_filter.py
+python test_imageio.py
+python test_misc.py
+python test_transform.py
+python test_typeconverter.py
+python test_utils.py
+python test_vec.py
+
