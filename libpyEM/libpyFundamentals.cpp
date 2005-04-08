9bd81c1cf0f10edecf071557b8fe46114e00fad0
grant <grant>
2005-04-08 18:40:58 +0000

    Adding for now so that we can do testing.  May be unceremoniously yanked when we shuffle around this code to place it appropriately into EMAN2.

---
+++ libpyEM/libpyFundamentals.cpp
@@ -0,0 +1,20 @@
+#include <boost/python.hpp>
+#include <emdata.h>
+#include <fundamentals.h>
+
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(libpyFundamentals) {
+	enum_<EMAN::fp_flag>("fp_flag")
+		.value("CIRCULANT", EMAN::CIRCULANT)
+		.value("CIRCULANT_NORMALIZED", EMAN::CIRCULANT_NORMALIZED)
+		.value("PADDED", EMAN::PADDED)
+		.value("PADDED_LAG", EMAN::PADDED_LAG)
+		.value("PADDED_NORMALIZED_LAG", EMAN::PADDED_NORMALIZED_LAG)
+		;
+	def("correlation", &EMAN::correlation, return_value_policy< manage_new_object >() );
+	def("convolution", &EMAN::convolution, return_value_policy< manage_new_object >() );
+	def("autocorrelation", &EMAN::autocorrelation, return_value_policy< manage_new_object >() );
+	def("self_correlation", &EMAN::self_correlation, return_value_policy< manage_new_object >() );
+	def("periodogram", &EMAN::periodogram, return_value_policy< manage_new_object >() );
+}
