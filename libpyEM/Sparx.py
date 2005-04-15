488bec1c45daeaaec540cc18d87d84a46360ef31
ppenczek <ppenczek>
2005-04-15 20:26:40 +0000

    Yet another version

--- libpyEM/Sparx.py
+++ libpyEM/Sparx.py
@@ -4,19 +4,22 @@
 from EMAN2 import *
 
 def descriptive_statistics(image):
-	"""Calculate the statistics of an image.
+	"""Calculate the descriptive statistics of an image.
 
-	Usage: [mean,sigma,xmin,xmax =] descriptive_statistics(image object)
+	Usage: [mean, sigma, xmin, xmax, nx, ny, nz =] descriptive_statistics(image object)
 	       or
-	       [mean,sigma,xmin,xmax =] descriptive_statistics(image filename)
+	       [mean, sigma, xmin, xmax, nx, ny, nz =] descriptive_statistics(image filename)
 
-	Purpose: calculate basic statistical characteristics of an image
+	Purpose: calculate basic statistical characteristics of an image.
 	"""
 	try:
 		mean = image.get_attr("mean")
 		sigma = image.get_attr("sigma")
 		imin = image.get_attr("minimum")
 		imax = image.get_attr("maximum")
+		nx = image.get_xsize()
+		ny = image.get_xsize()
+		nz = image.get_xsize()
 	except:
 		# hopefully the "image" is actually a filename
 		e = EMData()
@@ -25,6 +28,10 @@ def descriptive_statistics(image):
 		sigma = e.get_attr("sigma")
 		imin = e.get_attr("minimum")
 		imax = e.get_attr("maximum")
+		nx = e.get_xsize()
+		ny = e.get_xsize()
+		nz = e.get_xsize()
+	print "Image size: nx = %i, ny = %i, nz = %i" % (nx, ny, nz)
 	print "avg = %g, std dev = %g, min = %g, max = %g" % (mean, sigma, imin, imax)
-	return mean,sigma,imin,imax
+	return mean,sigma,imin,imax, nx, ny, nz
 	
