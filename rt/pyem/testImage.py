26bec69cfe79d83531a1ea9e68c83da2f194f122
gtang <gtang>
2005-05-10 19:30:15 +0000

    if specify input image, apply test image to input.

--- rt/pyem/testImage.py
+++ rt/pyem/testImage.py
@@ -76,7 +76,91 @@ def create_test_image( img ):
     img.process("testimage.noise.gauss", params)
     img.write_image("img_noi_gau.mrc")
     print "Write test image img_noi_gau.mrc..."
-
+    
+def get_img( file_name ):
+    img = EMData()
+    img.read_image( file_name )
+    return img
+    
+def apply_gaussian( input_file_name, file_name, file_ext ):
+    #apply a gaussian blob to input image
+    print "Apply gaussian blob to input image", input_file_name, "..."
+    img = get_img( input_file_name )
+    params = create_param_gau()
+    img.process("testimage.gaussian", params)
+    img2 = get_img( input_file_name )
+    img3 = img * img2
+    output_filename = file_name+'_gau.'+file_ext
+    img3.write_image( output_filename )
+    print "Write processed image to--", output_filename
+    
+def apply_sinewave( input_file_name, file_name, file_ext ):
+    #apply a sine wave to input image
+    print "Apply sine wave to input image", input_file_name, "..."
+    img = get_img( input_file_name )
+    params = create_param_sin()
+    img.process("testimage.sinewave", params)
+    img2 = get_img( input_file_name )
+    img3 = img * img2
+    output_filename = file_name+'_sin.'+file_ext
+    img3.write_image( output_filename )
+    print "Write processed image to--", output_filename
+    
+def apply_square( input_file_name, file_name, file_ext ):
+    #apply a square/cube to input image
+    print "Apply square/cube to input image", input_file_name, "..."
+    img = get_img( input_file_name )
+    params = create_param_squ_cube()
+    img.process("testimage.squarecube", params)
+    img2 = get_img( input_file_name )
+    img3 = img * img2
+    output_filename = file_name+'_squ.'+file_ext
+    img3.write_image( output_filename )
+    print "Write processed image to--", output_filename
+    
+def apply_circle( input_file_name, file_name, file_ext ):
+    print "Apply circle/sphere to input image", input_file_name, "..."
+    img = get_img( input_file_name )
+    params = create_param_cir_sph()
+    img.process("testimage.circlesphere", params)
+    img2 = get_img( input_file_name )
+    img3 = img * img2
+    output_filename = file_name+'_cir.'+file_ext
+    img3.write_image( output_filename )
+    print "Write processed image to--", output_filename
+    
+def apply_uniform_noise( input_file_name, file_name, file_ext ):
+    print "Apply uniform noise to input image", input_file_name, "..."
+    img = get_img( input_file_name )
+    img.process("testimage.noise.uniform.rand")
+    img2 = get_img( input_file_name )
+    img3 = 0.1 * img2.get_attr("mean") * img + img2
+    output_filename = file_name+'_noiuni.'+file_ext
+    img3.write_image( output_filename )
+    print "Write processed image to--", output_filename
+    
+def apply_gaussian_noise( input_file_name, file_name, file_ext ):
+    print "Apply gaussian noise to input image", input_file_name, "..."
+    img = get_img( input_file_name )
+    params = create_param_noi_gau()
+    img.process("testimage.noise.gauss", params)
+    img2 = get_img( input_file_name )
+    img3 = 0.1 * img2.get_attr("mean") * img + img2
+    output_filename = file_name+'_noigau.'+file_ext
+    img3.write_image( output_filename )
+    print "Write processed image to--", output_filename
+    
+def apply_test_image( input_file_name ):
+    file_ext = input_file_name[-3:]
+    file_name = input_file_name[0:-4]
+    
+    apply_gaussian( input_file_name, file_name, file_ext )
+    apply_sinewave( input_file_name, file_name, file_ext )
+    apply_square( input_file_name, file_name, file_ext )
+    apply_circle( input_file_name, file_name, file_ext )
+    apply_uniform_noise( input_file_name, file_name, file_ext )
+    apply_gaussian_noise( input_file_name, file_name, file_ext )
+    
 def testImage_main():
     from optparse import OptionParser
     parser = OptionParser()
@@ -84,10 +168,9 @@ def testImage_main():
     
     from sys import argv
     (options, argv) = parser.parse_args(argv)
-    if options.input_filename != None:
-        img = EMData()
-        img.read_image( options.input_filename )
-        create_test_image( img )
+    fname = options.input_filename
+    if fname != None:
+        apply_test_image( fname )
     else:
         img = EMData()
         img.set_size(1024,1024,1)
