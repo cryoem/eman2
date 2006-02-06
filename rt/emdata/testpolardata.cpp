1ed84f6f74db62cff0d904ccd8ab50cf30444d66
gtang <gtang>
2006-02-06 22:59:24 +0000

    c++ testing code for PolarData class.

---
+++ rt/emdata/testpolardata.cpp
@@ -0,0 +1,33 @@
+#include "emdata.h"
+#include "polardata.h"
+
+#include <iostream>
+
+using namespace std;
+using namespace EMAN;
+
+void test_polar_data() {
+	cout << "Enter test_polar_data() function..." << endl;
+	
+	PolarData * pd = new PolarData();
+	pd->print_polar();
+	
+	cout << "Leave test_polar_data() function..." << endl;
+}
+
+int main()
+{
+	cout << "--- Starting to test PolarData ---" << endl;
+	
+	try {
+		test_polar_data();
+	}
+	catch (E2Exception & e)	{
+		cout << e.what();
+	}
+	catch (...) {
+		cout << "Unknown exception ???" << endl;
+	}
+	
+	return 0;
+}
