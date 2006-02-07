ab37b46abe4c6266f7844423934432db9cd87c52
gtang <gtang>
2006-02-07 23:22:51 +0000

    PolarData implementation.

--- libpyEM/libpyPolarData2.cpp
+++ libpyEM/libpyPolarData2.cpp
@@ -13,7 +13,15 @@ using namespace boost::python;
 // Module ======================================================================
 BOOST_PYTHON_MODULE(libpyPolarData2)
 {
-    class_< EMAN::PolarData >("PolarData", init<  >())
+    class_< EMAN::UnevenMatrix >("UnevenMatrix", init<  >())
+        .def(init< const EMAN::UnevenMatrix& >())
+        .def("get_xsize", &EMAN::UnevenMatrix::get_xsize)
+        .def("get_xmin", &EMAN::UnevenMatrix::get_xmin)
+        .def("get_xmax", &EMAN::UnevenMatrix::get_xmax)
+        .def("print_UnevenMatrix", &EMAN::UnevenMatrix::print_UnevenMatrix)
+    ;
+
+    class_< EMAN::PolarData, bases< EMAN::UnevenMatrix >  >("PolarData", init<  >())
         .def(init< const EMAN::PolarData& >())
         .def(init< EMAN::EMData*, int, int >())
         .def("print_polar", &EMAN::PolarData::print_polar)
