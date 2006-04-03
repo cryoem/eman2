7cc7b06086ed78c8257516c138b10b9586e5bcd9
zhuang <zhuang>
2006-04-03 15:41:33 +0000

    *** empty log message ***

---
+++ libEM/sparx/util.h
@@ -0,0 +1,24 @@
+#ifndef eman_util_h
+#define eman_util_h
+#include <cstddef>
+
+/** @file util.h
+ *  utilities of image proccessing -- 
+ *  @author P. A. Penczek <Pawel.A.Penczek@uth.tmc.edu>.
+ *  The University of Texas.
+ *  Please do not modify the contents of this document
+ *  without written consent of the author.
+ *
+ *  @date $Date: 2006/04/03
+ *
+ *  
+ *  
+ *
+ */
+ namespace EMAN
+{
+	class EMData;
+	/** Search specified number peaks in 1D, 2D, or 3D real images.
+	  * and output the peaks in descendent order */
+	  vector<float> EMData::peak_search(int ml, float invert)
+}	 
