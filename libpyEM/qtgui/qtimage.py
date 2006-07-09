3f95cab7385e116a4b60ebc05a17bbbad08b4566
stevel <stevel>
2006-07-09 03:51:41 +0000

    Add valslider

--- libpyEM/qtgui/qtimage.py
+++ libpyEM/qtgui/qtimage.py
@@ -2,17 +2,47 @@ from PyQt4 import QtCore, QtGui, QtOpenGL
 from OpenGL import GL
 from math import *
 from EMAN2 import *
+from valslider import ValSlider
 import sys
 
-class QtGLImage(QtOpenGL.QGLWidget):
+class EMImage(QtOpenGL.QGLWidget):
 	def __init__(self, parent=None):
 		fmt=QtOpenGL.QGLFormat()
 		fmt.setDoubleBuffer(True);
 		QtOpenGL.QGLWidget.__init__(self,fmt, parent)
+		
 		self.data=None
+		self.scale=1.0
+		self.minden=0
+		self.maxden=1.0
+		self.origin=(0,0)
 	
+	def setData(data):
+		self.data=data
+		mean=data.get_attr("mean")
+		sigma=data.get_attr("sigma")
+		m0=data.get_attr("minimum")
+		m1=data.get_attr("maximum")
+		
+		if data==None:
+			self.updateGL()
+			return
+		
+		self.minden=max(m0,mean-3.0*sigma)
+		self.maxden=min(m1,mean+3.0*sigma)
+		self.updateGL()
+		
+	def setrange(x0,x1):
+		self.minden=x0
+		self.maxden=x1
+		self.updateGL()
+	
+	def setorigin(x,y):
+		self.origin=(x,y)
+		self.updateGL()
+		
 	def initializeGL(self):
-		GL.glClearColor(.5,0.,.5,0.)
+		GL.glClearColor(0,0,0)
 	
 	def paintGL(self):
 		GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
@@ -20,7 +50,7 @@ class QtGLImage(QtOpenGL.QGLWidget):
 		GL.glTranslated(0.0, 0.0, -10.0)
 		
 		if self.data :
-			a=self.data.render_amp8(0,0,self.width(),self.height(),(self.width()-1)/4*4+4,1.0,1,254,self.data.get_attr("minimum"),self.data.get_attr("maximum"),0)
+			a=self.data.render_amp8(origin[0],origin[1],self.width(),self.height(),(self.width()-1)/4*4+4,self.scale,1,254,self.minden,self.maxden,0)
 #			print "%d %d   %d"%(self.width(),self.height(),len(a))
 			GL.glDrawPixels(self.width(),self.height(),GL.GL_LUMINANCE,GL.GL_BYTE,a)
 	
@@ -39,11 +69,21 @@ class QtGLImage(QtOpenGL.QGLWidget):
 	def mouseMoveEvent(self, event):
 		pass
 	
+	def mouseReleaseEvent(self, event):
+		pass
 
 if __name__ == '__main__':
 	app = QtGui.QApplication(sys.argv)
 	window = QtGLImage()
 	window.data=test_image()
 	window.show()
+	
+	w2=QtGui.QWidget()
+	w2.resize(256,128)
+	
+	w3=ValSlider(w2)
+	w3.resize(256,24)
+	w2.show()
+	
 	sys.exit(app.exec_())
 	
---
+++ libpyEM/qtgui/valslider.py
@@ -0,0 +1,98 @@
+import sys
+from PyQt4 import QtCore, QtGui
+
+def clamp(x0,val,x1):
+	return max(min(val,x1),x0)
+
+
+class ValSlider(QtGui.QWidget):
+	"""The valslider class represents a connected text widget and horizontal slider.
+	setValue(float) - to programatically change the value
+	emit valueChanged(float)
+	"""
+	def __init__(self, parent):
+		if not parent: raise Exception,"ValSliders must have parents"
+		QtGui.QWidget.__init__(self,parent)
+		
+		self.range=[0,1.0]
+		self.value=0.0
+		self.ignore=0
+		
+		self.hboxlayout = QtGui.QHBoxLayout(self)
+		self.hboxlayout.setMargin(0)
+		self.hboxlayout.setSpacing(6)
+		self.hboxlayout.setObjectName("hboxlayout")
+		
+		self.text = QtGui.QLineEdit(self)
+		
+		sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Policy(7),QtGui.QSizePolicy.Policy(0))
+		sizePolicy.setHorizontalStretch(1)
+		sizePolicy.setVerticalStretch(0)
+		sizePolicy.setHeightForWidth(self.text.sizePolicy().hasHeightForWidth())
+		self.text.setSizePolicy(sizePolicy)
+		self.text.setMinimumSize(QtCore.QSize(80,0))
+		self.text.setObjectName("text")
+		self.hboxlayout.addWidget(self.text)
+		
+		self.slider = QtGui.QSlider(self)
+		
+		sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Policy(7),QtGui.QSizePolicy.Policy(0))
+		sizePolicy.setHorizontalStretch(7)
+		sizePolicy.setVerticalStretch(0)
+		sizePolicy.setHeightForWidth(self.slider.sizePolicy().hasHeightForWidth())
+		self.slider.setSizePolicy(sizePolicy)
+		self.slider.setMinimumSize(QtCore.QSize(100,0))
+		self.slider.setMaximum(4095)
+		self.slider.setSingleStep(16)
+		self.slider.setPageStep(256)
+		self.slider.setOrientation(QtCore.Qt.Horizontal)
+		self.slider.setObjectName("slider")
+		self.hboxlayout.addWidget(self.slider)
+		
+		QtCore.QObject.connect(self.text, QtCore.SIGNAL("editingFinished()"), self.textchange)
+		QtCore.QObject.connect(self.slider, QtCore.SIGNAL("valueChanged(int)"), self.sliderchange)
+
+	def setvalue(val):
+		self.value=val
+		self.updateboth()
+		self.emit(QtCore.SIGNAL("valueChanged"),self.value)
+		
+	def textchange(self):
+		if self.ignore : return
+		x=self.text.text()
+		if len(x)==0 : return
+		if x[0]=='<' : 
+			try: self.range[1]=float(x[1:])
+			except: pass
+			self.updateboth()
+		elif x[0]=='>' : 
+			try: self.range[0]=float(x[1:])
+			except: pass
+			self.updateboth()
+		else:
+			try:
+				self.value=float(x)
+				self.updates()
+				self.emit(QtCore.SIGNAL("valueChanged"),self.value)
+			except:
+				self.updateboth()
+				
+	def sliderchange(self,x):
+		if self.ignore : return
+		self.value=(self.slider.value()/4095.0)*(self.range[1]-self.range[0])+self.range[0]
+		self.updatet()
+		self.emit(QtCore.SIGNAL("valueChanged"),self.value)
+		
+		
+	def updates(self):
+		self.ignore=1
+		self.slider.setValue(clamp(0,(self.value-self.range[0])/(self.range[1]-self.range[0])*4095.0,4095.0))
+		self.ignore=0
+
+	def updatet(self):
+		self.text.setText(str(self.value)[:self.text.width()/10-1])
+		
+	def updateboth(self):
+		self.updates()
+		self.updatet()
+		
