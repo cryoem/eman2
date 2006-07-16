8c206e21fb68fd08c2db652695c09b6a70c676c5
stevel <stevel>
2006-07-16 07:53:49 +0000

    More improvements to QT image viewer.

--- libpyEM/qtgui/emimage.py
+++ libpyEM/qtgui/emimage.py
@@ -17,12 +17,14 @@ class EMImage(QtOpenGL.QGLWidget):
 		QtOpenGL.QGLWidget.__init__(self,fmt, parent)
 		
 		self.data=None
+		self.datasize=(1,1)
 		self.scale=1.0
 		self.minden=0
 		self.maxden=1.0
 		self.mindeng=0
 		self.maxdeng=1.0
 		self.origin=(0,0)
+		self.mousedrag=None
 		
 		self.inspector=None
 	
@@ -67,6 +69,8 @@ class EMImage(QtOpenGL.QGLWidget):
 			self.mindeng=max(m0,mean-5.0*sigma)
 			self.maxdeng=min(m1,mean+5.0*sigma)
 
+			self.datasize=(data.get_xsize(),data.get_ysize())
+
 			if self.inspector: self.inspector.setLimits(self.mindeng,self.maxdeng,self.minden,self.maxden)
 		# if we have a single 3D image
 		elif data.get_zsize()>1 :
@@ -80,16 +84,20 @@ class EMImage(QtOpenGL.QGLWidget):
 		self.updateGL()
 		
 	def setDenRange(self,x0,x1):
+		"""Set the range of densities to be mapped to the 0-255 pixel value range"""
 		self.minden=x0
 		self.maxden=x1
 		self.updateGL()
 	
 	def setOrigin(self,x,y):
+		"""Set the display origin within the image"""
 		self.origin=(x,y)
 		self.updateGL()
 		
-	def setScale(self,val):
-		self.scale=val
+	def setScale(self,newscale):
+		"""Adjusts the scale of the display. Tries to maintain the center of the image at the center"""
+		self.origin=(newscale/self.scale*(self.width()/2+self.origin[0])-self.width()/2,newscale/self.scale*(self.height()/2+self.origin[1])-self.height()/2)
+		self.scale=newscale
 		self.updateGL()
 		
 	def setDenMin(self,val):
@@ -109,7 +117,7 @@ class EMImage(QtOpenGL.QGLWidget):
 		GL.glTranslated(0.0, 0.0, -10.0)
 		
 		if self.data :
-			a=self.data.render_amp8(self.origin[0],self.origin[1],self.width(),self.height(),(self.width()-1)/4*4+4,self.scale,0,255,self.minden,self.maxden,2)
+			a=self.data.render_amp8(int(self.origin[0]/self.scale),int(self.origin[1]/self.scale),self.width(),self.height(),(self.width()-1)/4*4+4,self.scale,0,255,self.minden,self.maxden,2)
 #			a=self.data.render_amp8(self.origin[0],self.origin[1],self.width(),self.height(),(self.width()-1)/4*4+4,1.0,1,254,self.minden,self.maxden,0)
 #			GL.glPixelZoom(self.scale,self.scale)
 			GL.glDrawPixels(self.width(),self.height(),GL.GL_LUMINANCE,GL.GL_UNSIGNED_BYTE,a)
@@ -129,15 +137,21 @@ class EMImage(QtOpenGL.QGLWidget):
 	
 	def mousePressEvent(self, event):
 		if event.button()==Qt.MidButton:
-			if not self.inspector : self.inspector=EMImageInspector(self)
+			if not self.inspector : self.inspector=EMImageInspector2D(self)
 			self.inspector.setLimits(self.mindeng,self.maxdeng,self.minden,self.maxden)
 			self.inspector.show()
+		elif event.button()==Qt.RightButton:
+			self.mousedrag=(event.x(),event.y())
 	
 	def mouseMoveEvent(self, event):
-		pass
-	
+		if self.mousedrag:
+			self.origin=(self.origin[0]+self.mousedrag[0]-event.x(),self.origin[1]+self.mousedrag[1]-event.y())
+			self.mousedrag=(event.x(),event.y())
+			self.update()
+		
 	def mouseReleaseEvent(self, event):
-		pass
+		if event.button()==Qt.RightButton:
+			self.mousedrag=None
 
 class ImgHistogram(QtGui.QWidget):
 	def __init__(self,parent):
@@ -219,6 +233,7 @@ class EMImageInspector2D(QtGui.QWidget):
 		
 		self.scale = ValSlider(self,(0.1,5.0),"Mag:")
 		self.scale.setObjectName("scale")
+		self.scale.setValue(1.0)
 		self.vboxlayout.addWidget(self.scale)
 		
 		self.mins = ValSlider(self,label="Min:")
@@ -237,6 +252,10 @@ class EMImageInspector2D(QtGui.QWidget):
 		self.conts.setObjectName("conts")
 		self.vboxlayout.addWidget(self.conts)
 		
+		self.lowlim=0
+		self.highlim=1.0
+		self.busy=0
+		
 		QtCore.QObject.connect(self.scale, QtCore.SIGNAL("valueChanged"), target.setScale)
 		QtCore.QObject.connect(self.mins, QtCore.SIGNAL("valueChanged"), self.newMin)
 		QtCore.QObject.connect(self.maxs, QtCore.SIGNAL("valueChanged"), self.newMax)
@@ -244,22 +263,51 @@ class EMImageInspector2D(QtGui.QWidget):
 		QtCore.QObject.connect(self.conts, QtCore.SIGNAL("valueChanged"), self.newCont)
 
 	def newMin(self,val):
+		if self.busy : return
+		self.busy=1
 		self.target.setDenMin(val)
+
+		self.updBC()
+		self.busy=0
 		
 	def newMax(self,val):
+		if self.busy : return
+		self.busy=1
 		self.target.setDenMax(val)
+		self.updBC()
+		self.busy=0
 	
 	def newBrt(self,val):
-		pass
+		if self.busy : return
+		self.busy=1
+		self.updMM()
+		self.busy=0
 		
 	def newCont(self,val):
-		pass
+		if self.busy : return
+		self.busy=1
+		self.updMM()
+		self.busy=0
 
+	def updBC(self):
+		b=0.5*(self.mins.value+self.maxs.value-(self.lowlim+self.highlim))
+		c=(self.mins.value-self.maxs.value)/(2.0*(self.lowlim-self.highlim))
+		self.brts.setValue(-b)
+		self.conts.setValue(1.0-c)
+		
+	def updMM(self):
+		x0=((self.lowlim+self.highlim)/2.0-(self.highlim-self.lowlim)*(1.0-self.conts.value+self.brts.value))
+		x1=((self.lowlim+self.highlim)/2.0+(self.highlim-self.lowlim)*(1.0-self.conts.value-self.brts.value))
+		self.mins.setValue(x0)
+		self.maxs.setValue(x1)
+		self.target.setDenRange(x0,x1)
+		
 	def setHist(self,hist,minden,maxden):
 		self.hist.setData(hist,minden,maxden)
 
 	def setLimits(self,lowlim,highlim,curmin,curmax):
-		print lowlim,highlim,curmin,curmax
+		self.lowlim=lowlim
+		self.highlim=highlim
 		self.mins.setRange(lowlim,highlim)
 		self.maxs.setRange(lowlim,highlim)
 		self.mins.setValue(curmin)
