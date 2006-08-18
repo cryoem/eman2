70dcbbf93df6dd10566a09bc2a8d48ea12da48a3
stevel <stevel>
2006-08-18 17:41:48 +0000

    New e2 using qt

---
+++ programs/e2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from optparse import OptionParser
+from EMAN2 import *
+from emimagemx import *
+from emimage2d import *
+import pyshed
+from PyQt4 import QtCore, QtGui, QtOpenGL
+from PyQt4.QtCore import Qt
+	
+def imgupd():
+	for i in EMImage2D.allim.keys():
+		if i.data.get_attr("changecount")!=i.changec :
+			i.setData(i.rdata)
+
+if __name__ == "__main__":
+	app = QtGui.QApplication([])
+	window = pyshed.Shell()
+	window.show()
+	
+	tmr=QtCore.QTimer()
+	tmr.setInterval(300)
+	tmr.connect(tmr,QtCore.SIGNAL("timeout()"), imgupd)
+	tmr.start()
+	
+	sys.exit(app.exec_())
