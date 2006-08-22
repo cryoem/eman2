ccb28c77b34ea29154368e4305a18e224a216cfa
cyang <cyang>
2006-08-22 22:56:02 +0000

    put dopca under PCA class

--- libEM/sparx/pca.cpp
+++ libEM/sparx/pca.cpp
@@ -1,9 +1,46 @@
 #include "emdata.h"
 #include "util.h"
+#include "emutil.h"
+
+#include "pca.h"
 
 using namespace EMAN;
 
-vector <EMData*> dopca(vector <EMData*> imgstack, EMData *mask, int nvec)
+// return all right singular vectors
+vector <EMData*> PCA::dopca(vector <EMData*> imgstack, EMData *mask)
+{
+   // performs PCA on a list of images (each under a mask)
+   // returns a list of eigenimages
+
+   int i;
+
+   vector<EMData*> img1dlst;
+   vector<EMData*> eigvecs;
+   vector<EMData*> eigimages;
+
+   int nimgs = imgstack.size();
+   // printf("number of images in the stack = %d\n", nimgs);
+
+   for (i=0; i<nimgs; i++) {
+      img1dlst.push_back(Util::compress_image_mask(imgstack[i],mask));
+   }
+
+   // for right now, compute a full SVD
+   eigvecs = Util::svdcmp(img1dlst, 0);
+
+   img1dlst.clear();
+
+   for (i=0; i<nimgs; i++) {
+      eigimages.push_back(Util::reconstitute_image_mask(eigvecs[i],mask));
+   }
+
+   eigvecs.clear();
+
+   return eigimages;
+}
+
+// return a subset of right singular vectors
+vector <EMData*> PCA::dopca(vector <EMData*> imgstack, EMData *mask, int nvec)
 {
    // performs PCA on a list of images (each under a mask)
    // returns a list of eigenimages
@@ -35,5 +72,21 @@ vector <EMData*> dopca(vector <EMData*> imgstack, EMData *mask, int nvec)
    eigvecs.clear();
 
    return eigimages;
+}
+
+char* PCA::dopca_ooc(const string &filename_in, EMData *mask, int nvec)
+{
+   char *filename_out = NULL;
+   EMData *image_raw = new EMData();
+   EMData *image_masked;
+
+   int nimgs = EMUtil::get_image_count(filename_in);
+   for (int i=0; i<nimgs; i++) {
+       image_raw->read_image(filename_in, i);      
+       image_masked=Util::compress_image_mask(image_raw,mask);
+       image_masked->write_image("temp_masked_imaged.img",i); 
+   }
+
+   return filename_out;
 }
 
--- libEM/sparx/pca.h
+++ libEM/sparx/pca.h
@@ -1,6 +1,21 @@
-#ifndef sparx__pca_h__
-#define sparx__pca_h__
+//#ifndef sparx__pca_h__
+//#define sparx__pca_h__
 
-vector <EMData*> dopca(vector <EMData*> imgstack, EMData *mask, int nvec);
+#include "emdata.h"
 
-#endif //sparx__pca_h__
+namespace EMAN {
+   class PCA {
+      public: 
+         float *svalue; // singular values
+
+         // pca in core, returns all singular vectors
+         vector <EMData*> dopca(vector <EMData*> imgstack, EMData *mask);
+
+         // pca in core, returns a subset of right singular vectors
+         vector <EMData*> dopca(vector <EMData*> imgstack, EMData *mask, int nvec);
+         // pca out of core
+         char *dopca_ooc(const string &filename, EMData *mask, int nvec);
+   };
+}
+
+//#endif //sparx__pca_h__
