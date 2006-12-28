6322f24f48005638450492b6032f39d326172e19
ppenczek <ppenczek>
2006-12-28 04:14:17 +0000

    *** empty log message ***

--- libpyEM/util_wrapitems.h
+++ libpyEM/util_wrapitems.h
@@ -34,13 +34,4 @@
 
 #ifndef UTIL_WRAPPER_H
 #define UTIL_WRAPPER_H
-using EMAN::EMData;
-boost::python::tuple 
-util_Crosrng_e(EMData* circ1, EMData* circ2, 
-               vector<int> numr, int neg){
-    double qn = 0.;
-    float tot = 0.f;
-    boost::tie(qn, tot, neg) = EMAN::Util::Crosrng_e(circ1, circ2, numr, neg);
-    return boost::python::make_tuple(qn, tot, neg);
-}
 #endif // UTIL_WRAPPER_H
