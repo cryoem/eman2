8ad36ce8eb9c8fa96ff28852830284292369c3d7
ppenczek <ppenczek>
2007-01-14 22:24:13 +0000

    *** empty log message ***

--- libEM/processor.cpp
+++ libEM/processor.cpp
@@ -1661,11 +1661,28 @@ float NormalizeMaskProcessor::calc_sigma(EMData * image) const
 		LOGWARN("NULL Image");
 		return 0;
 	}
-	float sigma = 1;
-	if ((int) params["no_sigma"] == 0) {
-		sigma = image->get_attr("sigma");
+	EMData *mask = params["mask"];
+
+	if (!EMUtil::is_same_size(mask, image)) {
+		LOGERR("normalize.maskProcessor: mask and image must be the same size");
+		return 0;
 	}
-	return sigma;
+
+	float *data = image->get_data();
+	float *mask_data = mask->get_data();
+	size_t size = image->get_xsize() * image->get_ysize() * image->get_zsize();
+	double sum = 0;
+	double sq2 = 0;
+	size_t n_norm = 0;
+
+	for (size_t i = 0; i < size; i++) {
+		if (mask_data[i] && data[i]) {
+			sum += data[i];
+			sq2 += data[i]*double (data[i]);
+			n_norm++;
+		}
+	}
+	return sqrt((sq2 - sum * sum /n_norm)/float(n_norm -1)) ;
 }
 
 float NormalizeMaskProcessor::calc_mean(EMData * image) const
