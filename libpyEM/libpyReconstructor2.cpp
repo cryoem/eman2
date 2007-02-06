464764b68eb223cf6db172d03a17478a63ac1220
zhuang <zhuang>
2007-02-06 19:12:05 +0000

    *** empty log message ***

--- libpyEM/libpyReconstructor2.cpp
+++ libpyEM/libpyReconstructor2.cpp
@@ -93,11 +93,5 @@ BOOST_PYTHON_MODULE(libpyReconstructor2)
         .staticmethod("get")
     ;
 
-    class_< EMAN::file_store >( "file_store", init< const string&, int, int >() )
-        .def( "add_image", &EMAN::file_store::add_image )
-        .def( "get_image", &EMAN::file_store::get_image )
-        .def( "restart",   &EMAN::file_store::restart )
-    ;
-
 }
 
