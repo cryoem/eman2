a58e96fb51b3b6534b8a5387a7f270bdc2a6ddd1
gtang <gtang>
2007-03-02 22:50:07 +0000

    unit test update

--- rt/pyem/test_processor.py
+++ rt/pyem/test_processor.py
@@ -44,7 +44,7 @@ class TestProcessor(unittest.TestCase):
     def test_get_processor_list(self):
         """test get processor list .........................."""
         processor_names = Processors.get_list()
-        self.assertEqual(len(processor_names), 121)
+        self.assertEqual(len(processor_names), 122)
 
         try:
             f2 = Processors.get("_nosuchfilter___")
