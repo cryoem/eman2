40f557c0142a1ccfde26c6cb2f369a5317ca7a31
sternberg <sternberg>
2007-04-24 18:21:32 +0000

    fixed indexing bug in converting angleshift back to degrees; serialized fcalc()

--- libEM/sparx/temp/ali3d_unified_mpi.cpp
+++ libEM/sparx/temp/ali3d_unified_mpi.cpp
@@ -226,11 +226,11 @@ int  unified(MPI_Comm comm, EMData *volume, EMData **projdata,
     }
     // and convert back to ali3d_d's format
     for ( int i = 0 ; i < nangloc ; ++i ) {
-	angleshift[0] *= (180.0/PI);
-	angleshift[1] *= (180.0/PI);
-	angleshift[2] *= (180.0/PI);
-	angleshift[3] *= -1.0;
-	angleshift[4] *= -1.0;		
+	angleshift[5*i + 0] *= (180.0/PI);
+	angleshift[5*i + 1] *= (180.0/PI);
+	angleshift[5*i + 2] *= (180.0/PI);
+	angleshift[5*i + 3] *= -1.0;
+	angleshift[5*i + 4] *= -1.0;		
     }
 
     EMDeleteArray(rhs);
@@ -892,97 +892,68 @@ int fgcalc(MPI_Comm comm, float *volsph, Vec3i volsize,
 }
 
  
-int fcalc(MPI_Comm comm, float *volsph, Vec3i volsize, 
+int fcalc(float *volsph, Vec3i volsize, 
            int nnz, int nrays, Vec3i origin, int ri, 
            int *ptrs, int *cord, float *angtrs, int nang, 
            float *rhs, NUMBER *fval)
 {
-	int mypid, ncpus, nvars;
-	int *psize, *nbase;
+    int	   nx, ny, nz, jglb, ierr;
+    float  phi, theta, psi, sx, sy;
+    NUMBER cphi, sphi, cthe, sthe, cpsi, spsi;
+    float  dm[8];
+    float * rvec;
+    *fval = 0.0;
 
-	int	   nangloc, nx, ny, nz, jglb, ierr, ibeg;
-	float  phi, theta, psi, sx, sy;
-	NUMBER cphi, sphi, cthe, sthe, cpsi, spsi;
-	float  dm[8];
-	float * rvec;
-	NUMBER fvalloc= 0.0;
-	
-	MPI_Comm_rank(comm,&mypid);
-	MPI_Comm_size(comm,&ncpus);
-
-	nvars = nnz + nang*5;
-	*fval = 0.0;
-
-	nx = volsize[0];
-	ny = volsize[1];
-	nz = volsize[2];
+    nx = volsize[0];
+    ny = volsize[1];
+    nz = volsize[2];
  
-	psize = new int[ncpus];
-	nbase = new int[ncpus];
- 	rvec  = new float[nx*ny];
+    rvec  = new float[nx*ny];
 
-	nangloc = setpart(comm, nang, psize, nbase);
-	float * img_res = new float[nangloc]; // here we'll store the residuals for each data image
-	
-	dm[6] = 0.0;
-	dm[7] = 0.0;
-	
-	for (int j = 0; j < nangloc; j++) {
-	    img_res[j] = 0.0; // initialize; in the inner loop we accumulate the residual at each pixel
-	    for (int i = 0; i<nx*ny; i++) { 
-		rvec[i] = 0.0;
-	    }
-	    // rvec <-- P(theta)*f
-	    jglb  = nbase[mypid] + j;
-		 
-	    phi	  = angtrs[5*jglb+0];
-	    theta = angtrs[5*jglb+1];
-	    psi	  = angtrs[5*jglb+2];
-	    sx	  = angtrs[5*jglb+3];
-	    sy	  = angtrs[5*jglb+4];
-
-	    cphi=cos(phi);
-	    sphi=sin(phi);
-	    cthe=cos(theta);
-	    sthe=sin(theta);
-	    cpsi=cos(psi);
-	    spsi=sin(psi);
-
-	    dm[0]=cphi*cthe*cpsi-sphi*spsi;
-	    dm[1]=sphi*cthe*cpsi+cphi*spsi;
-	    dm[2]=-sthe*cpsi;
-	    dm[3]=-cphi*cthe*spsi-sphi*cpsi;
-	    dm[4]=-sphi*cthe*spsi+cphi*cpsi;
-	    dm[5]=sthe*spsi;
-	    dm[6]=sx;
-	    dm[7]=sy;
-
-	    ierr = fwdpj3(volsize, nrays, nnz, dm, origin, ri, ptrs, cord, volsph, rvec);
-
-	    // rvec <--- rvec - rhs
-	    for (int i = 1; i <= nx*ny; i++) {
-		rvec(i) = rvec(i) - rhs(i,j+1);
-	    }
-
-	    // fval <--- norm(rvec)^2/2 
-	    ibeg = nnz + 5*(jglb) - 1;
-	    for (int i = 0; i < nx*ny; i++) {
-
-		fvalloc += rvec[i]*rvec[i]; 
-		img_res[j] += rvec[i] * rvec[i];
-	    }
-	    // 
-
+    for (int j = 0; j < nang; j++) {
+	for (int i = 0; i<nx*ny; i++) { 
+	    rvec[i] = 0.0;
 	}
-	
-	ierr = MPI_Allreduce(&fvalloc, fval, 1, MPI_FLOAT, MPI_SUM, comm);
+	// rvec <-- P(theta)*f
 
-	EMDeleteArray(psize);
-	EMDeleteArray(nbase);
-	EMDeleteArray(rvec);
-	EMDeleteArray(img_res);
+	phi	  = angtrs[5*j+0];
+	theta = angtrs[5*j+1];
+	psi	  = angtrs[5*j+2];
+	sx	  = angtrs[5*j+3];
+	sy	  = angtrs[5*j+4];
+
+	cphi=cos(phi);
+	sphi=sin(phi);
+	cthe=cos(theta);
+	sthe=sin(theta);
+	cpsi=cos(psi);
+	spsi=sin(psi);
+
+	dm[0]=cphi*cthe*cpsi-sphi*spsi;
+	dm[1]=sphi*cthe*cpsi+cphi*spsi;
+	dm[2]=-sthe*cpsi;
+	dm[3]=-cphi*cthe*spsi-sphi*cpsi;
+	dm[4]=-sphi*cthe*spsi+cphi*cpsi;
+	dm[5]=sthe*spsi;
+	dm[6]=sx;
+	dm[7]=sy;
+
+	ierr = fwdpj3(volsize, nrays, nnz, dm, origin, ri, ptrs, cord, volsph, rvec);
+
+	// rvec <--- rvec - rhs
+	for (int i = 1; i <= nx*ny; i++) {
+	    rvec(i) = rvec(i) - rhs(i,j+1);
+	}
+
+	// fval <--- norm(rvec)^2/2 
+	for (int i = 0; i < nx*ny; i++) {
+	    *fval += rvec[i]*rvec[i]; 
+	}
+    }
 	
-	return 0;
+    EMDeleteArray(rvec);
+	
+    return 0;
 }
 
 // This might be a convenient function to use in fgcalc, rather than repeating the same code six times
--- libEM/sparx/temp/ali3d_unified_mpi.h
+++ libEM/sparx/temp/ali3d_unified_mpi.h
@@ -11,7 +11,7 @@ int fgcalc(MPI_Comm comm, float *volsph, Vec3i volsize, int nnz,
            int nrays    , Vec3i  origin, int        ri, int *ptrs, 
            int *cord    , float *angtrs, int      nang, float *rhs, 
            float     aba, NUMBER  *fval, float   *grad, char *fname_base);
-int fcalc(MPI_Comm comm, float *volsph, Vec3i volsize, 
+int fcalc(float *volsph, Vec3i volsize, 
            int nnz, int nrays, Vec3i origin, int ri, 
            int *ptrs, int *cord, float *angtrs, int nang, 
 	  float *rhs, NUMBER *fval);
