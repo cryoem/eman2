88c1f962ae6bc722b133116eb1aebe74fab8f72a
sternberg <sternberg>
2007-06-20 00:55:12 +0000

    Add function ParseAlignOptions to utilcomm.cpp; remove option parsing from runali3d
    New function that handles parsing the input file with options for projection
    matching.  Reads options in on the master node and then broadcasts them.

--- libEM/sparx/temp/runali3d_d.cpp
+++ libEM/sparx/temp/runali3d_d.cpp
@@ -38,7 +38,6 @@ int main(int argc, char *argv[])
       exit(1);
     }
     int ia=0;
-    bool have_options = false;
     while (ia < argc) {
        if ( !strncmp(argv[ia],"-data",5) ) {
           strcpy(stackfname,&argv[ia][6]);
@@ -51,7 +50,6 @@ int main(int argc, char *argv[])
        }
        else if ( !strncmp(argv[ia],"-options",8) ) {
 	   strcpy(optionsfname,&argv[ia][9]);
-	   have_options = true;
        }
        ia++;
     }
@@ -93,190 +91,28 @@ int main(int argc, char *argv[])
     ierr = CleanStack(comm, cleanimages, nloc, ri, volsize, origin);
 
     float *angleshift = new float[5*nloc];
+    int maxiter = 1; // need to be able to set this, either on command line or in AlignOptions object
     
-    AlignOptions options(volsize);
-
-    std::ifstream option_stream;
-    std::string current_option;
-    char option_buffer[100];
-    int int_option;
-    float float_option;
-
-    int maxiter = 5;
     EMData * mask3D = NULL;
-    float * mask_data;
-    if ( have_options ) {
-	if ( mypid == 0 ) { // read data from the options file
-	    option_stream.open(optionsfname);
-	    while ( option_stream >> current_option ) {
-		if ( current_option == "maskfile" ) {
-		    option_stream >> current_option;
-		    mask3D = new EMData();
-		    mask3D->read_image(current_option);
-		    options.set_mask3D(mask3D);
-		} 
-		else if ( current_option == "inner_ring" ) {
-		    option_stream >> int_option;
-		    options.set_first_ring(int_option);
-		    std::cout << "first_ring = " << int_option << std::endl;
-		} 
-		else if ( current_option == "outer_ring" ) {
-		    option_stream >> int_option;
-		    options.set_last_ring(int_option);
-		    std::cout << "last_ring = " << int_option << std::endl;
-		} 
-		else if ( current_option == "rstep" ) {
-		    option_stream >> int_option;
-		    options.set_rstep(int_option);
-		    std::cout << "rstep = " << int_option << std::endl;
-		} 
-		else if ( current_option == "radius" ) { // perhaps this is the same as outer_ring?
-		    option_stream >> int_option;	
-		    options.set_ri(int_option);
-		    std::cout << "radius = " << int_option << std::endl;
-		} 
-		else if ( current_option == "x_range" ) {
-		    option_stream >> float_option;
-		    options.set_xrng(float_option);
-		    std::cout << "x_range = " << float_option << std::endl;
-		} 
-		else if ( current_option == "y_range" ) {
-		    option_stream >> float_option;
-		    options.set_yrng(float_option);
-		    std::cout << "y_range = " << float_option << std::endl;
-		}
-		else if ( current_option == "translation_step" ) {
-		    option_stream >> float_option;
-		    options.set_step(float_option);
-		    std::cout << "step = " << float_option << std::endl;
-		}
-		else if ( current_option == "theta_step" ) {
-		    option_stream >> float_option;
-		    options.set_dtheta(float_option);
-		    std::cout << "theta_step = " << float_option << std::endl;
-		}
-		else if ( current_option == "maxit" ) {
-		    option_stream >> int_option;
-		    maxiter = int_option;
-		    std::cout << "maxit = " << int_option << std::endl;
-		}
-		else if ( current_option == "CTF" ) {
-		    option_stream >> current_option;
-		    if ( current_option == "true" ) {
-			options.set_CTF(true);
-			std::cout << "CTF = true" << std::endl;
-		    } 
-		    else { // anything else sets it to false
-			options.set_CTF(false);
-			std::cout << "CTF = false" << std::endl;
-		    }
-		}
-		else if ( current_option == "snr" ) {
-		    option_stream >> float_option;
-		    options.set_snr(float_option);
-		    std::cout << "snr = " << float_option << std::endl;
-		}
-		else if ( current_option == "ref_a" ) {
-		    option_stream >> current_option;
-		    if ( current_option == "P" ) {
-			options.set_ref_angle_type("P");
-		    } 
-		    else if ( current_option == "S" ){ // should add support for this
-			std::cerr << "Currently only support Penczek-type reference angles..." << std::endl;
-			options.set_ref_angle_type("P");
-		    }
-		    else { // Currently default to "P", will eventually default to "S"
-		    }
-		}
-		else if ( current_option == "symmetry" ) {
-		    option_stream >> current_option;
-		    options.set_symmetry(current_option);
-		    std::cout << "symmetry = " << current_option << std::endl;
-		}
-		else { // catch-all
-		    std::cerr << "Unsupported option " << current_option << "..." << std::endl;
-		}
-	    }
-	    option_stream.close();
-	}
-	// Then broadcast all the data that was read
-	ierr = MPI_Bcast(&mask3D, 1, MPI_INT, 0, comm); // if it's not NULL, need to allocate and bcast its data
-	// NOTE: this is sending over the master's address for mask3D ONLY as a test to see if it's NULL or not.
-	// DO NOT USE THIS POINTER ON ANY OTHER NODES EXCEPT FOR THIS TEST!
-	if ( mask3D != NULL ) {
-	    if ( mypid != 0 ) {
-		mask3D = new EMData();
-	    }
-	    mask_data = mask3D->get_data();
-	    ierr = MPI_Bcast(mask_data, volsize[0]*volsize[1]*volsize[2], MPI_FLOAT, 0, comm);
-	    options.set_mask3D(mask3D);
-	}
-
-	int_option = options.get_first_ring();
-	ierr = MPI_Bcast(&int_option, 1, MPI_INT, 0, comm);
-	options.set_first_ring(int_option);
-
-	int_option = options.get_last_ring();
-	ierr = MPI_Bcast(&int_option, 1, MPI_INT, 0, comm);
-	options.set_last_ring(int_option);
-
-	int_option = options.get_rstep();
-	ierr = MPI_Bcast(&int_option, 1, MPI_INT, 0, comm);
-	options.set_rstep(int_option);
-
-	int_option = options.get_ri();
-	ierr = MPI_Bcast(&int_option, 1, MPI_INT, 0, comm);
-	options.set_ri(int_option);
-
-	float_option = options.get_xrng();
-	ierr = MPI_Bcast(&float_option, 1, MPI_FLOAT, 0, comm);
-	options.set_xrng(float_option);
-
-	float_option = options.get_yrng();
-	ierr = MPI_Bcast(&float_option, 1, MPI_FLOAT, 0, comm);
-	options.set_yrng(float_option);
-
-	float_option = options.get_step();
-	ierr = MPI_Bcast(&float_option, 1, MPI_FLOAT, 0, comm);
-	options.set_step(float_option);
-
-	float_option = options.get_dtheta();
-	ierr = MPI_Bcast(&float_option, 1, MPI_FLOAT, 0, comm);
-	options.set_dtheta(float_option);
-
-	int_option = options.get_CTF();
-	ierr = MPI_Bcast(&int_option, 1, MPI_INT, 0, comm);
-	options.set_CTF(int_option);
-
-	float_option = options.get_snr();
-	ierr = MPI_Bcast(&float_option, 1, MPI_FLOAT, 0, comm);
-	options.set_snr(float_option);
-
-	current_option = options.get_ref_angle_type();
-	strcpy(option_buffer, current_option.c_str());
-	ierr = MPI_Bcast(option_buffer, current_option.size(), MPI_CHAR, 0, comm);
-	options.set_ref_angle_type(option_buffer);
-
-	current_option = options.get_symmetry();
-	strcpy(option_buffer, current_option.c_str());
-	ierr = MPI_Bcast(option_buffer, current_option.size(), MPI_CHAR, 0, comm);
-	options.set_symmetry(option_buffer);
+    AlignOptions options(volsize);
+    if ( optionsfname[0] ){
+	ParseAlignOptions(comm, options, optionsfname, volsize[0]*volsize[1]*volsize[2], mask3D);
+    } else {
+	if ( mypid == 0 ) printf("Using default alignment options\n");
     }
+
     options.set_have_angles(false);
 
     try {
-       ali3d_d(comm, volume, expimages, cleanimages, angleshift, nloc, options, 
-               maxiter, voutfname);
+       ali3d_d(comm, volume, expimages, cleanimages, angleshift, nloc, options, voutfname);
     }
     catch (std::exception const& e) {
 	printf("%s\n", e.what());
     }
-
     EMDeletePtr(volume);
     for ( int i = 0 ; i < nloc; ++i ) {
 	EMDeletePtr(expimages[i]);
     }
-    EMDeleteArray(expimages);
     EMDeleteArray(expimages);
     for ( int i = 0 ; i < nloc; ++i ) {
 	EMDeletePtr(cleanimages[i]);
--- libEM/sparx/temp/utilcomm.h
+++ libEM/sparx/temp/utilcomm.h
@@ -4,6 +4,7 @@
 #include "mpi.h"
 #include "stdlib.h"
 #include "emdata.h"
+#include "alignoptions.h"
 
 using namespace EMAN;
 
@@ -11,5 +12,6 @@ int ReadVandBcast(MPI_Comm comm, EMData *volume, char *volfname);
 int ReadStackandDist(MPI_Comm comm, EMData ***images2D, char *stackfname);
 int CleanStack(MPI_Comm comm, EMData ** image_stack, int nloc, int ri, Vec3i volsize, Vec3i origin);
 int setpart(MPI_Comm comm, int nima, int *psize, int *nbase);
+int ParseAlignOptions(MPI_Comm comm, AlignOptions& options, char* optionsfname, int nvoxels, EMData*& mask3D);
 
 #endif // UTILCOMM_H
