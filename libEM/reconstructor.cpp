ea3d6179be7f5fa80442911c3955e73780e51662
zyang <zyang>
2007-07-30 23:18:45 +0000

    *** empty log message ***

--- libEM/reconstructor.cpp
+++ libEM/reconstructor.cpp
@@ -2711,7 +2711,7 @@ EMData* nnSSNR_ctfReconstructor::finish()
 						Assert( r >=0 && r < (int)pow_b.size() );
 						wght = pow_b[r] / ( 1.0 - alpha * sum );
 					} // end of ( m_weighting == ESTIMATE )
-					float nominator   = std::norm(m_volume->cmplx(ix,iy,iz))/(*m_wptr)(ix,iy,iz);
+					float nominator   = std::norm(m_volume->cmplx(ix,iy,iz))*(*m_wptr)(ix,iy,iz);
 					float denominator = ((*m_wptr2)(ix,iy,iz)-std::norm(m_volume->cmplx(ix,iy,iz))/(*m_wptr)(ix,iy,iz))/((Kn-1.0f));
 					// Skip Friedel related values
 					if( (ix>0 || (kz>=0 && (ky>=0 || kz!=0)))) {
