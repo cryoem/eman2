26631a87045c72f73eeec0112176c4161e692d26
cyang <cyang>
2007-08-10 21:09:49 +0000

    do not change the sign of shift in SIRT

--- libEM/sparx/temp/sirt_Cart.cpp
+++ libEM/sparx/temp/sirt_Cart.cpp
@@ -147,8 +147,8 @@ int recons3d_sirt_mpi_Cart(MPI_Comm comm_2d, MPI_Comm comm_row, MPI_Comm comm_co
 		    phi   = angleshift[5*i + 0];
 		    theta = angleshift[5*i + 1];
 		    psi   = angleshift[5*i + 2];
-		    dm[6] = angleshift[5*i + 3] * -1.0;
-		    dm[7] = angleshift[5*i + 4] * -1.0;
+		    dm[6] = angleshift[5*i + 3];
+		    dm[7] = angleshift[5*i + 4];
 		    // make an instance of Transform3D with the angles
 		    RA = Transform3D(EULER_SPIDER, phi, theta, psi);
 		    for ( int ns = 1 ; ns < nsym + 1 ; ++ns ) {
@@ -192,8 +192,8 @@ int recons3d_sirt_mpi_Cart(MPI_Comm comm_2d, MPI_Comm comm_row, MPI_Comm comm_co
 	    for ( int i = 0 ; i < nangloc ; ++i ) {
 		// retrieve the angles and shifts from angleshift
 		RA = Transform3D(EULER_SPIDER, angleshift[5*i + 0], angleshift[5*i + 1], angleshift[5*i + 2]);
-		dm[6] = angleshift[5*i + 3] * -1.0;
-		dm[7] = angleshift[5*i + 4] * -1.0;
+		dm[6] = angleshift[5*i + 3];
+		dm[7] = angleshift[5*i + 4];
 		for ( int ns = 1 ; ns < nsym + 1 ; ++ns ) {
 		    // iterate over symmetries
 		    Tf = Tf.get_sym(symmetry, ns) * RA;
@@ -214,8 +214,8 @@ int recons3d_sirt_mpi_Cart(MPI_Comm comm_2d, MPI_Comm comm_row, MPI_Comm comm_co
 	    for ( int i = 0 ; i < nangloc ; ++i ) {
 		// retrieve the angles and shifts from angleshift
 		RA = Transform3D(EULER_SPIDER, angleshift[5*i + 0], angleshift[5*i + 1], angleshift[5*i + 2]);
-		dm[6] = angleshift[5*i + 3] * -1.0;
-		dm[7] = angleshift[5*i + 4] * -1.0;
+		dm[6] = angleshift[5*i + 3];
+		dm[7] = angleshift[5*i + 4];
 		for ( int ns = 1 ; ns < nsym + 1 ; ++ns ) {
 		    // iterate over symmetries
 		    Tf = Tf.get_sym(symmetry, ns) * RA;
