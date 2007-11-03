451f1a4694f55cbdf79d268e0fc4428e0111111d
gtang <gtang>
2007-11-03 18:04:17 +0000

    *** empty log message ***

--- rt/pyem/test_processor.py
+++ rt/pyem/test_processor.py
@@ -44,7 +44,7 @@ class TestProcessor(unittest.TestCase):
     def test_get_processor_list(self):
         """test get processor list .........................."""
         processor_names = Processors.get_list()
-        self.assertEqual(len(processor_names), 131)
+        self.assertEqual(len(processor_names), 133)
 
         try:
             f2 = Processors.get("_nosuchfilter___")
