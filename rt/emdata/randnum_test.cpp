a9ec5d8d34daed5ecf1ecec57830f5965ada9032
gtang <gtang>
2008-03-28 17:22:28 +0000

    *** empty log message ***

--- rt/emdata/randnum_test.cpp
+++ rt/emdata/randnum_test.cpp
@@ -8,10 +8,12 @@ int main()
 {
 	cout << "Start random number generator..." << endl;
 	
-	Randnum r = Randnum(gsl_rng_rand);
+	Randnum *r = Randnum::Instance(gsl_rng_rand);
 	for(int i=0; i<100; ++i) {
-		cout << r.get_irand(0,99) << "\t" << r.get_frand() <<  "\t" << r.get_gauss_rand(1.0, 1.0) << endl;
+		cout << r->get_irand(0,99) << "\t" << r->get_frand() <<  "\t" << r->get_gauss_rand(1.0, 1.0) << endl;
 	}
+	
+	r->print_generator_type();
 	
 	return 0;
 }
