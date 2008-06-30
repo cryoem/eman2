2ba452578deacddc6d264765f5ab52d7545bbd08
stevel <stevel>
2008-06-30 02:49:27 +0000

    Working on database.

--- libpyEM/EMAN2db.py
+++ libpyEM/EMAN2db.py
@@ -43,7 +43,7 @@ except:
 	
 
 envopenflags=db.DB_CREATE|db.DB_INIT_MPOOL|db.DB_INIT_LOCK|db.DB_INIT_LOG|db.DB_THREAD
-dbopenflags=db.DB_CREATE
+#dbopenflags=db.DB_CREATE
 cachesize=10000000
 
 class EMAN2DB:
@@ -102,7 +102,11 @@ class DBDict:
 		self.txn=None	# current transaction used for all database operations
 		self.bdb=db.DB(dbenv)
 		if file==None : file=name+".bdb"
-		self.bdb.open(file,name,db.DB_BTREE,dbopenflags)
+		try: self.bdb.open(file,name,db.DB_BTREE,0)
+		except: 
+			self.bdb.open(file,name,db.DB_BTREE,db.DB_CREATE)
+			self.bdb.put(dumps("_freebinary",-1),dumps([],-1),txn=self.txn)
+			print "new db created"
 #		self.bdb.open(file,name,db.DB_HASH,dbopenflags)
 
 	def __str__(self): return "<EMAN2db DBHash instance: %s>" % self.name
@@ -143,7 +147,8 @@ class DBDict:
 
 	def __setitem__(self,key,val):
 		if (val==None) :
-			self.__delitem__(key)
+			try: self.__delitem__(key)
+			except: return
 		elif isinstance(val,EMData) : 
 			# decide where to put the binary data
 			ad=val.get_attr_dict()
@@ -169,7 +174,8 @@ class DBDict:
 			self.bdb.put(dumps(key,-1),dumps(val,-1),txn=self.txn)
 				
 	def __getitem__(self,key):
-		r=loads(self.bdb.get(dumps(key,-1),txn=self.txn))
+		try: r=loads(self.bdb.get(dumps(key,-1),txn=self.txn))
+		except: return None
 		if isinstance(r,dict) and r.has_key("binary") :
 			fkey="%s/%s%dx%dx%d"%(self.path,self.name,r["nx"],r["ny"],r["nz"])
 #			print "r",fkey
@@ -200,11 +206,15 @@ class DBDict:
 		return self.bdb.has_key(dumps(key,-1))
 
 	def get(self,key,txn=None):
-		r=loads(self.bdb.get(dumps(key,-1),txn=txn))
-		if isinstance(r,dict) and r.has_key("nx") :
+		try: r=loads(self.bdb.get(dumps(key,-1),txn=txn))
+		except: return None
+		if isinstance(r,dict) and r.has_key("binary") :
+			fkey="%s/%s%dx%dx%d"%(self.path,self.name,r["nx"],r["ny"],r["nz"])
+#			print "r",fkey
 			ret=EMData(r["nx"],r["ny"],r["nz"])
+			ret.read_data(fkey,r["binary"]*4*r["nx"]*r["ny"]*r["nz"])
 			k=set(r.keys())
-			k-=DBHashMeta.fixedkeys
+			k-=DBDict.fixedkeys
 			for i in k: ret.set_attr(i,r[i])
 			return ret
 		return r
@@ -212,146 +222,31 @@ class DBDict:
 	def set(self,key,val,txn=None):
 		"Alternative to x[key]=val with transaction set"
 		if (val==None) :
-			self.__delitem__(key)
+			try: self.__delitem__(key)
+			except: return
 		elif isinstance(val,EMData) : 
-			self.bdb.put(dumps(key,-1),dumps(val.get_attr_dict(),-1),txn=txn)
+			# decide where to put the binary data
+			ad=val.get_attr_dict()
+			fkey="%s/%s%dx%dx%d"%(self.path,self.name,ad["nx"],ad["ny"],ad["nz"])
+#			print "w",fkey
+			try :
+				o=loads(self.bdb.get(dumps(key,-1),txn=txn))
+				n=o["binary"]
+			except:
+				if not self.has_key(fkey) : self[fkey]=0
+				else: self[fkey]+=1 
+				n=self[fkey]
+			ad["binary"]=n
+			
+			# write the metadata
+			self.bdb.put(dumps(key,-1),dumps(ad,-1),txn=txn)
 			if not self.has_key("maxrec") or key>self["maxrec"] : self["maxrec"]=key
+			
+			# write the binary data
+			val.write_data(fkey,n*4*ad["nx"]*ad["ny"]*ad["nz"])
+			
 		else :
-			self.bdb.put(dumps(key,-1),dumps(val,-1),txn=self.txn)
-
-	def update(self,dict):
-		for i,j in dict.items(): self[i]=j
-
-
-class DBHash:
-	"""This class uses BerkeleyDB to create an object much like a persistent Python Dictionary,
-	keys and data may be arbitrary pickleable types, however, additional functionality is provided
-	for EMData objects. Specifically, if integer keys are used, set_attr and get_attr may be used
-	to efficiently get and set attributes for images with reduced i/o requirements (in certain cases)."""
-	
-	allhashes=weakref.WeakKeyDictionary()
-	def __init__(self,name,file=None,dbenv=None,nelem=0):
-		"""This is a persistent dictionary implemented as a BerkeleyDB Hash
-		name is required, and will also be used as a filename if none is
-		specified. """
-		
-		global dbopenflags
-		DBHash.allhashes[self]=1		# we keep a running list of all trees so we can close everything properly
-		self.name = name
-		self.txn=None	# current transaction used for all database operations
-		self.bdb=db.DB(dbenv)
-		if file==None : file=name+".bdb"
-#		self.bdb.open(file,name,db.DB_BTREE,dbopenflags)
-		self.bdb.open(file,name,db.DB_HASH,dbopenflags)
-
-	def __str__(self): return "<EMAN2db DBHash instance: %s>" % self.name
-
-	def __del__(self):
-		self.close()
-
-	def close(self):
-		if self.bdb == None: return
-		self.bdb.close()
-		self.bdb=None
-	
-	def sync(self):
-		if self.bdb : self.bdb.sync()
-		
-	def set_txn(self,txn):
-		"""sets the current transaction. Note that other python threads will not be able to use this
-		Hash until it is 'released' by setting the txn back to None"""
-		if txn==None: 
-			self.txn=None
-			return
-		
-		while self.txn :
-			time.sleep(.1)
-		self.txn=txn
-
-	def get_attr(self,n,attr):
-		"""This works only for values which are EMData objects and integer keys. It allows attributes to be 
-		read without reading the full image in some cases"""
-		try:
-			a=self["%d_attr"%n]
-			return a[attr]
-		except:
-			im=self[n]
-			return im.get_attr(attr)
-		
-	def set_attr(self,n,attr,val):
-		"""This works only for values which are EMData objects and integer keys. It allows attributes to be 
-		awr without reading/writing the full image"""
-		try:
-			a=self["%d_attr"%n]
-			a[attr]=val
-			self["%d_attr"%n]=a
-		except:
-			a={attr:val}
-			self["%d_attr"%n]=a
-
-	def __len__(self):
-		return self.bdb.stat(db.DB_FAST_STAT)["nkeys"]
-#		return len(self.bdb)
-
-	def __setitem__(self,key,val):
-		if (val==None) :
-			self.__delitem__(key)
-		else : 
-			self.bdb.put(dumps(key,-1),compress(dumps(val,-1),1),txn=self.txn)
-			if type(key)==int :
-				if not self.has_key("maxrec") or key>self["maxrec"] : self["maxrec"]=key
-				try: del(self["%d_attr"%key])
-				except: pass
-				
-	def __getitem__(self,key):
-		ret=loads(decompress(self.bdb.get(dumps(key,-1),txn=self.txn)))
-		if isinstance(ret,EMData) and type(key)==int :
-			try: 
-				a=self["%d_attr"%key]
-				for k,v in a.items():
-					ret.set_attr(k,v)
-			except: pass
-		
-		return ret
-
-	def __delitem__(self,key):
-		self.bdb.delete(dumps(key,-1),txn=self.txn)
-
-	def __contains__(self,key):
-		return self.bdb.has_key(dumps(key,-1),txn=self.txn)
-
-	def keys(self):
-		return map(lambda x:loads(x),self.bdb.keys())
-
-	def values(self):
-		return map(lambda x:loads(decompress(x)),self.bdb.values())
-
-	def items(self):
-		return map(lambda x:(loads(x[0]),loads(decompress(x[1]))),self.bdb.items())
-
-	def has_key(self,key):
-		return self.bdb.has_key(dumps(key,-1))
-
-	def get(self,key,txn=None):
-		ret=loads(decompress(self.bdb.get(dumps(key,-1),txn=txn)))
-		if isinstance(ret,EMData) and type(key)==int :
-			try: 
-				a=self["%d_attr"%key]
-				for k,v in a.items():
-					ret.set_attr(k,v)
-			except: pass
-		return ret
-
-	def set(self,key,val,txn=None):
-		"Alternative to x[key]=val with transaction set"
-		if (val==None) :
-			self.bdb.delete(dumps(key,-1),txn=txn)
-		else : 
-			self.bdb.put(dumps(key,-1),compress(dumps(val,-1),1),txn=txn)
-			if type(key)==int :
-				if not self.has_key("maxrec") or key>self["maxrec"] : self["maxrec"]=key
-				try: del(self["%d_attr"%key])
-				except: pass
+			self.bdb.put(dumps(key,-1),dumps(val,-1),txn=txn)
 
 	def update(self,dict):
 		for i,j in dict.items(): self[i]=j
