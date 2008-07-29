229249e19ed828a03c685584dbae0ec77374af3b
zweig <zweig>
2008-07-29 14:26:13 +0000

    add python binding for newfile_store

--- libpyEM/libpyReconstructor2.cpp
+++ libpyEM/libpyReconstructor2.cpp
@@ -118,5 +118,13 @@ BOOST_PYTHON_MODULE(libpyReconstructor2)
         .def( "restart",   &EMAN::file_store::restart )
     ;
 
+
+    class_< EMAN::newfile_store >( "newfile_store", init< const string&, int >() )
+        .def( "add_image", &EMAN::newfile_store::add_image )
+        .def( "get_image", &EMAN::newfile_store::get_image )
+        .def( "restart",   &EMAN::newfile_store::restart )
+    ;
+
+
 }
 
