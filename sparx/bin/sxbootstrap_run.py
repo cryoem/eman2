7a1b8c7679a4a48f0fffe948770cb395c5684f2b
zweig <zweig>
2008-11-10 20:37:19 +0000

    add option CTF

--- sparx/bin/sxbootstrap_run.py
+++ sparx/bin/sxbootstrap_run.py
@@ -12,10 +12,11 @@ for arg in sys.argv:
 	arglist.append( arg )
 
 progname = os.path.basename( arglist[0] )
-usage = progname + "prj_stack buf_prefix vol_prefix nvol --snr=signal_noise_ratio --sym=symmetry -verbose=(0|1) --MPI"
+usage = progname + " prj_stack buf_prefix vol_prefix nvol --CTF --snr=signal_noise_ratio --sym=symmetry -verbose=(0|1) --MPI"
 parser = OptionParser(usage, version=SPARXVERSION)
 
-parser.add_option("--snr", type="float", default=10.0, help="Signal-to-Noise Ratio of input projection" )
+parser.add_option("--CTF", action="store_true", default=False, help="  Consider CTF correction during the alignment" )
+parser.add_option("--snr", type="float", default=1.0, help="Signal-to-Noise Ratio of input projection" )
 parser.add_option("--sym", type="string", default="c1", help="Symmetry of input projection" )
 parser.add_option("--verbose", type="int", default=0, help="verbosity" )
 parser.add_option("--MPI", action="store_true", default=False,     help="  whether using MPI version ")
@@ -36,6 +37,6 @@ if options.MPI:
 	from mpi import mpi_init
 	sys.argv = mpi_init(len(sys.argv), sys.argv)
 global_def.BATCH = True
-bootstrap_run(prj_stack, buf_prefix, vol_prefix, nvol, options.snr, options.sym, options.verbose, options.MPI)
+bootstrap_run(prj_stack, buf_prefix, vol_prefix, nvol, options.CTF, options.snr, options.sym, options.verbose, options.MPI)
 global_def.BATCH = False
 
