ff346f72de98a7ce6907c134a2d685d466d34e35
zyang <zyang>
2008-11-20 17:08:00 +0000

    Change order of the parameters

--- sparx/bin/sxali3d_d.py
+++ sparx/bin/sxali3d_d.py
@@ -41,15 +41,15 @@ def main():
         for arg in sys.argv:
         	arglist.append( arg )
 	progname = os.path.basename(arglist[0])
-	usage = progname + " stack ref_vol outdir <maskfile> --ir=inner_radius --ou=outer_radius --rs=ring_step --xr=x_range --yr=y_range  --ts=translational_search_step  --delta=angular_step --an=angular_neighborhood --center=1 --maxit=max_iter --CTF --snr=1.0  --ref_a=S --sym=c1 --function=user_function --MPI"
+	usage = progname + " stack ref_vol outdir <maskfile> --ir=inner_radius --ou=outer_radius --rs=ring_step --xr=x_range --yr=y_range  --ts=translational_search_step  --delta=angular_step --an=angular_neighborhood --center=center_type --maxit=max_iter --CTF --snr=SNR  --ref_a=S --sym=c1 --function=user_function --debug --MPI"
 	parser = OptionParser(usage,version=SPARXVERSION)
 	parser.add_option("--ir",       type= "int",   default= 1,                  help="  inner radius for rotational correlation > 0 (set to 1)")
 	parser.add_option("--ou",       type= "int",   default= -1,                 help="  outer radius for rotational correlation < int(nx/2)-1 (set to the radius of the particle)")
 	parser.add_option("--rs",       type= "int",   default= 1,                  help="  step between rings in rotational correlation >0  (set to 1)" ) 
-	parser.add_option("--xr",       type="string", default= "4  2 1  1   1",    help="  range for translation search in x direction, search is +/xr ")
+	parser.add_option("--xr",       type="string", default= "4 2 1 1 1",        help="  range for translation search in x direction, search is +/xr ")
 	parser.add_option("--yr",       type="string", default= "-1",               help="  range for translation search in y direction, search is +/yr (default = same as xr)")
 	parser.add_option("--ts", 	type="string", default= "1 1 1 0.5 0.25",   help="  step size of the translation search in both directions, search is -xr, -xr+ts, 0, xr-ts, xr ")
-	parser.add_option("--delta",    type="string", default= "10 6 4  3   2",    help="  angular step of reference projections")
+	parser.add_option("--delta",    type="string", default= "10 6 4 3 2",       help="  angular step of reference projections")
 	parser.add_option("--an",       type="string", default= "-1",               help="  angular neighborhood for local searches")
 	parser.add_option("--center",   type="float",  default= -1,                 help="  -1. average center method 0. if you do not want the volume to be centered, 1. center the volume using cog (default=1)")
 	parser.add_option("--maxit",    type="float",  default= 5,                  help="  maximum number of iterations performed for each angular step (set to 5) ")
@@ -58,8 +58,8 @@ def main():
 	parser.add_option("--ref_a",    type="string", default= "S",                help="  method for generating the quasi-uniformly distributed projection directions (default S) ")
 	parser.add_option("--sym",      type="string", default= "c1",               help="  symmetry of the structure ")
 	parser.add_option("--function", type="string", default="ref_ali3d",         help="  name of the reference preparation function")
-	parser.add_option("--MPI",      action="store_true",default=False,          help="  whether to use MPI version ")
 	parser.add_option("--debug",    action="store_true", default=False,         help="  Berlin dataset ")
+	parser.add_option("--MPI",      action="store_true", default=False,         help="  whether to use MPI version ")
 	(options, args) = parser.parse_args(arglist[1:])
 	if len(args) < 3 or len(args) > 4:
     		print "usage: " + usage
@@ -74,7 +74,7 @@ def main():
 			sys.argv = mpi_init(len(sys.argv), sys.argv)
 		from applications import ali3d_d
 		global_def.BATCH = True
-		ali3d_d(args[0], args[1], args[2], mask, options.ir, options.ou, options.rs, options.xr, options.yr, options.ts, options.delta, options.an, options.center, options.maxit, options.CTF, options.snr, options.ref_a, options.sym, options.function, options.MPI, options.debug)
+		ali3d_d(args[0], args[1], args[2], mask, options.ir, options.ou, options.rs, options.xr, options.yr, options.ts, options.delta, options.an, options.center, options.maxit, options.CTF, options.snr, options.ref_a, options.sym, options.function, options.debug, options.MPI)
 		global_def.BATCH = False
 
 if __name__ == "__main__":
