c6d343e1bac1578e7d42d6a211e1b2742ea28715
woolford <woolford>
2009-02-21 06:05:30 +0000

    cuda read only memory cache works properly

--- libEM/cuda/cuda_util.cu
+++ libEM/cuda/cuda_util.cu
@@ -32,14 +32,14 @@ void copy_array_data(CudaEMDataArray* to, CudaEMDataArray* from) {
 	to->emdata_pointer = from->emdata_pointer;
 }
 
-void set_arry_data_null(CudaEMDataArray* p)
+void set_array_data_null(CudaEMDataArray* p)
 {
 	p->array = 0;
 	p->data = 0;
 	p->emdata_pointer = 0;
 }
 
-const int max_cuda_arrays = 100;
+const int max_cuda_arrays = 2;
 int num_cuda_arrays = 0;
 CudaEMDataArray cuda_arrays[max_cuda_arrays];
 
@@ -50,19 +50,58 @@ void init_cuda_emdata_arrays() {
 	}
 }
 
+
+void debug_arrays()
+{
+	printf("debug\n");
+	for(int i=0; i < max_cuda_arrays; ++i) 
+	{
+		CudaEMDataArray t = cuda_arrays[i];
+		printf("%d %d %d\n",t.array,t.data,t.emdata_pointer);
+	}
+	printf("end debug\n");
+}
+
+int make_cuda_array_space_0_free() {
+	//printf("Freeing space 0\n");
+	//debug_arrays();
+	int n = num_cuda_arrays-1;
+	set_emdata_cuda_array_handle(-1,cuda_arrays[n].emdata_pointer);
+	CUDA_SAFE_CALL(cudaFree(cuda_arrays[n].array));
+	cuda_arrays[n].array = 0;
+	
+	for (int i = 0; i < num_cuda_arrays-1; ++i ) {
+		CudaEMDataArray* to = &cuda_arrays[i+1];
+		CudaEMDataArray* from = &cuda_arrays[i];
+		copy_array_data(to,from);
+		set_emdata_cuda_array_handle(i+1,to->emdata_pointer);
+	}
+	set_array_data_null(&cuda_arrays[0]);
+	
+	//debug_arrays();
+	return 0;
+}
+
+
 int get_cuda_array_handle(const float * data,const int nx, const int ny, const int nz, void* emdata_pointer) {
 	
+	//printf("Get cuda array %d\n", emdata_pointer);
+	//debug_arrays();
 	for(int i = 0; i < num_cuda_arrays; ++i ) {
-		if (cuda_arrays[i].data == data ) {
+		if (cuda_arrays[i].emdata_pointer == emdata_pointer ) {
 			//printf("Found that cuda arrary\n");
 			return i;
 		}
 	}
-	
+	int idx = num_cuda_arrays;
+	if (num_cuda_arrays == max_cuda_arrays) {
+		make_cuda_array_space_0_free();
+		idx = 0;
+	}
 	//printf("Making a new cuda array\n");
 	// If we make it here then it doesn't exist
-	cuda_arrays[num_cuda_arrays].data = data;
-	cuda_arrays[num_cuda_arrays].emdata_pointer = emdata_pointer;
+	cuda_arrays[idx].data = data;
+	cuda_arrays[idx].emdata_pointer = emdata_pointer;
 	cudaExtent VS = make_cudaExtent(nx,ny,nz);
 	
 	cudaArray *array = 0;
@@ -77,27 +116,30 @@ int get_cuda_array_handle(const float * data,const int nx, const int ny, const i
 	cudaMemcpy3D(&copyParams);
 	
 	//cudaBindTexture(tex,array);
-	cuda_arrays[num_cuda_arrays].array = array;
-	cuda_arrays[num_cuda_arrays].emdata_pointer = emdata_pointer;
-	num_cuda_arrays++;
-	return num_cuda_arrays-1;
+	cuda_arrays[idx].array = array;
+	if (num_cuda_arrays != max_cuda_arrays) num_cuda_arrays++;
+	return idx;
 }
 
 int delete_cuda_array(const int idx) {
+	printf("Deleting a cuda array\n");
 	CUDA_SAFE_CALL(cudaFree(cuda_arrays[idx].array));
 	cuda_arrays[idx].array = 0;
 	
 	for (int i = idx; i < num_cuda_arrays-1; ++i ) {
-		CudaEMDataArray* to = &cuda_arrays[idx];
-		CudaEMDataArray* from = &cuda_arrays[idx+1];
+		CudaEMDataArray* to = &cuda_arrays[i];
+		CudaEMDataArray* from = &cuda_arrays[i+1];
 		copy_array_data(to,from);
-		set_emdata_cuda_array_handle(idx,to->emdata_pointer);
+		set_emdata_cuda_array_handle(i,to->emdata_pointer);
 	}
-	set_arry_data_null(&cuda_arrays[num_cuda_arrays-1]);
+	set_array_data_null(&cuda_arrays[num_cuda_arrays-1]);
 	num_cuda_arrays--;
 	
 	return 0;
 }
+
+// highly specialized, use cautiously
+
 
 int delete_cuda_memory(float*p) {
 	CUDA_SAFE_CALL(cudaFree(p));
