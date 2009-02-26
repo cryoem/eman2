b9f2063bf37e76482402ff45dae464bfe8bbb904
ppenczek <ppenczek>
2009-02-26 20:35:41 +0000

    activated FourTruncate

--- libpyEM/libpyEMData2.cpp
+++ libpyEM/libpyEMData2.cpp
@@ -121,6 +121,8 @@ BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EMAN_EMData_pad_fft_overloads_0_1, pad_ff
 
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EMAN_EMData_FourInterpol_overloads_1_4, FourInterpol, 1, 4)
 
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EMAN_EMData_FourTruncate_overloads_1_4, FourTruncate, 1, 4)
+
 //BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EMAN_EMData_FourInterpol_i_overloads_1_4, FourInterpol_i, 1, 4)
 
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EMAN_EMData_Four_ds_overloads_1_4, Four_ds, 1, 4)
@@ -409,6 +411,7 @@ BOOST_PYTHON_MODULE(libpyEMData2)
 	.def("depad", &EMAN::EMData::depad)
 	.def("depad_corner", &EMAN::EMData::depad_corner)
 	.def("FourInterpol", &EMAN::EMData::FourInterpol, EMAN_EMData_FourInterpol_overloads_1_4()[ return_value_policy< manage_new_object >() ])
+	.def("FourTruncate", &EMAN::EMData::FourTruncate, EMAN_EMData_FourTruncate_overloads_1_4()[ return_value_policy< manage_new_object >() ])
 //       .def("FourInterpol_i", &EMAN::EMData::FourInterpol_i, EMAN_EMData_FourInterpol_i_overloads_1_4()[ return_value_policy< manage_new_object >() ])
 	.def("Four_ds", &EMAN::EMData::Four_ds, EMAN_EMData_Four_ds_overloads_1_4()[ return_value_policy< manage_new_object >() ])
 	.def("Four_shuf_ds_cen_us", &EMAN::EMData::Four_shuf_ds_cen_us, EMAN_EMData_Four_shuf_ds_cen_us_overloads_1_4()[ return_value_policy< manage_new_object >() ])
