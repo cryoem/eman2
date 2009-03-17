f0711deafcd4f3f89c73a6b8b7cddb35ec5a64ea
zyang <zyang>
2009-03-17 21:45:37 +0000

    Fix a small bug

--- sparx/libpy/development.py
+++ sparx/libpy/development.py
@@ -7559,6 +7559,7 @@ def ali_SSNR_MPI(stack, maskfile=None, ou=-1, maxit=10, CTF=False, opti_method="
 	from scipy.optimize.optimize import fmin_cg
 	from mpi import mpi_comm_size, mpi_comm_rank, mpi_bcast, MPI_COMM_WORLD
 	from utilities import get_image, set_params2D, get_params2D, print_begin_msg, print_end_msg, print_msg
+	from utilities import model_circle
 	
 	if CTF:
 		from utilities import get_arb_params
