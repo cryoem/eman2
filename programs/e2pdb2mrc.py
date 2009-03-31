59399753b758340caa285dc523cc10c9897aed73
woolford <woolford>
2009-03-31 21:33:57 +0000

    minor alterations

--- programs/e2pdb2mrc.py
+++ programs/e2pdb2mrc.py
@@ -80,117 +80,6 @@ using 1/2 width of Gaussian in Fourier space."""
 	except: box=None
 	outmap = pdb_2_mrc(args[0],options.apix,options.res,options.het,box,chains,options.quiet)
 	outmap.write_image(args[1])
-
-	#try : infile=open(args[0],"r")
-	#except : parser.error("Cannot open input file")
-	
-	#if options.res<=options.apix : print "Warning: res<=apix. Generally res should be 2x apix or more"
-	
-	#aavg=[0,0,0]	# calculate atomic center
-	#amin=[1.0e20,1.0e20,1.0e20]		# coords of lower left front corner of bounding box
-	#amax=[-1.0e20,-1.0e20,-1.0e20]	# coords
-	#natm=0
-	#atoms=[]		# we store a list of atoms to process to avoid multiple passes
-	#nelec=0
-	#mass=0
-
-	## parse the pdb file and pull out relevant atoms
-	#for line in infile:
-		#if (line[:4]=='ATOM' or (line[:6]=='HETATM' and options.het)) :
-			#if chains and not (line[21] in chains) : continue
-			
-			#try:
-				#a=line[12:14].strip()
-				#aseq=int(line[6:11].strip())
-				#res=int(line[22:26].strip())
-	
-				#x=float(line[30:38])
-				#y=float(line[38:46])
-				#z=float(line[46:54])
-			#except:
-				#print "PDB Parse error:\n%s\n'%s','%s','%s'  '%s','%s','%s'\n"%(
-					#line,line[12:14],line[6:11],line[22:26],line[30:38],line[38:46],line[46:54])
-				#print a,aseq,res,x,y,z
-
-			#atoms.append((a,x,y,z))
-						
-			#aavg[0]+=x
-			#aavg[1]+=y
-			#aavg[2]+=z
-			#natm+=1
-			
-			#amin[0]=min(x,amin[0])
-			#amin[1]=min(y,amin[1])
-			#amin[2]=min(z,amin[2])
-			#amax[0]=max(x,amax[0])
-			#amax[1]=max(y,amax[1])
-			#amax[2]=max(z,amax[2])
-
-			#try:
-				#nelec+=atomdefs[a.upper()][0]
-				#mass+=atomdefs[a.upper()][1]
-			#except:
-				#print("Unknown atom %s ignored at %d"%(a,aseq))
-							
-	#infile.close()
-	
-	#if not options.quiet:
-		#print "%d atoms used with a total charge of %d e- and a mass of %d kDa"%(natm,nelec,mass/1000)
-		#print "atomic center at %1.1f,%1.1f,%1.1f (center of volume at 0,0,0)"%(aavg[0]/natm,aavg[1]/natm,aavg[2]/natm)
-		#print "Bounding box: x: %7.2f - %7.2f"%(amin[0],amax[0])
-		#print "              y: %7.2f - %7.2f"%(amin[1],amax[1])
-		#print "              z: %7.2f - %7.2f"%(amin[2],amax[2])
-
-	## precalculate a prototypical Gaussian to resample
-	## 64^3 box with a real-space 1/2 width of 12 pixels
-	#gaus=EMData()
-	#gaus.set_size(64,64,64)
-	#gaus.to_one()
-	#gaus.process_inplace("mask.gaussian",{"outer_radius":12.0})
-
-	## find the output box size, either user specified or from bounding box
-	#box=[0,0,0]
-	#try:
-		#spl=options.box.split(',')
-		#if len(spl)==1 : box[0]=box[1]=box[2]=int(spl[0])
-		#else :
-			#box[0]=int(spl[0])
-			#box[1]=int(spl[1])
-			#box[2]=int(spl[2])
-	#except:
-		#print "EXCEPTION"
-		#print amin,amax
-		#pad=int(2.0*options.res/options.apix)
-		#print pad
-		#box[0]=int(2*max(fabs(amin[0]),fabs(amax[0]))/options.apix)+pad
-		#box[1]=int(2*max(fabs(amin[1]),fabs(amax[1]))/options.apix)+pad
-		#box[2]=int(2*max(fabs(amin[2]),fabs(amax[2]))/options.apix)+pad
-		#box[0]+=box[0]%2
-		#box[1]+=box[1]%2
-		#box[2]+=box[2]%2
-		
-	#if not options.quiet: print "Box size: %d x %d x %d"%(box[0],box[1],box[2])
-	
-	## initialize the final output volume
-	#outmap=EMData()
-	#outmap.set_size(box[0],box[1],box[2])
-	#outmap.to_zero()
-	
-	## fill in the atom gaussians
-	#for i,a in enumerate(atoms):
-		#if not options.quiet and i%1000==0 : 
-			#print '\r   %d'%i,
-			#sys.stdout.flush()
-		#try:
-			#elec=atomdefs[a[0].upper()][0]
-			#outmap.insert_scaled_sum(gaus,(a[1]/options.apix+box[0]/2,a[2]/options.apix+box[1]/2,a[3]/options.apix+box[2]/2),options.res/(pi*12.0*options.apix),elec)
-		#except: print "Skipping %d '%s'"%(i,a[0])
-		
-	#if not options.quiet: print '\r   %d\nConversion complete'%len(atoms)
-	#outmap.set_attr("apix_x",options.apix)
-	#outmap.set_attr("apix_y",options.apix)
-	#outmap.set_attr("apix_z",options.apix)
-	#outmap.write_image(args[1])
 	
 def pdb_2_mrc(file_name,apix=1.0,res=2.8,het=False,box=None,chains=None,quiet=False):
 	
@@ -264,21 +153,34 @@ def pdb_2_mrc(file_name,apix=1.0,res=2.8,het=False,box=None,chains=None,quiet=Fa
 	# find the output box size, either user specified or from bounding box
 	outbox=[0,0,0]
 	try:
-		spl=box.split(',')
-		if len(spl)==1 : outbox[0]=outbox[1]=outbox[2]=int(spl[0])
-		else :
-			outbox[0]=int(spl[0])
-			outbox[1]=int(spl[1])
-			outbox[2]=int(spl[2])
+		if isinstance(box,int):
+			outbox[0]=outbox[1]=outbox[2]=box
+		elif isinstance(box,list):
+			outbox[0]=box[0]
+			outbox[1]=box[1]
+			outbox[2]=box[2]
+		else:
+			spl=box.split(',')
+			if len(spl)==1 : outbox[0]=outbox[1]=outbox[2]=int(spl[0])
+			else :
+				outbox[0]=int(spl[0])
+				outbox[1]=int(spl[1])
+				outbox[2]=int(spl[2])
 	except:
 		pad=int(2.0*res/apix)
-		outbox[0]=int(2*max(fabs(amin[0]),fabs(amax[0]))/apix)+pad
-		outbox[1]=int(2*max(fabs(amin[1]),fabs(amax[1]))/apix)+pad
-		outbox[2]=int(2*max(fabs(amin[2]),fabs(amax[2]))/apix)+pad
+		#outbox[0]=int(2*max(fabs(amin[0]),fabs(amax[0]))/apix)+pad
+		#outbox[1]=int(2*max(fabs(amin[1]),fabs(amax[1]))/apix)+pad
+		#outbox[2]=int(2*max(fabs(amin[2]),fabs(amax[2]))/apix)+pad
+		outbox[0]=int((amax[0]-amin[0])/apix)+pad
+		outbox[1]=int((amax[1]-amin[1])/apix)+pad
+		outbox[2]=int((amax[2]-amin[2])/apix)+pad
+		#outbox[1]=int(2*max(fabs(amin[1]),fabs(amax[1]))/apix)+pad
+		#outbox[2]=int(2*max(fabs(amin[2]),fabs(amax[2]))/apix)+pad
 		outbox[0]+=outbox[0]%2
 		outbox[1]+=outbox[1]%2
 		outbox[2]+=outbox[2]%2
 		
+	print amax,amin
 	if not quiet: print "Box size: %d x %d x %d"%(outbox[0],outbox[1],outbox[2])
 	
 	# initialize the final output volume
@@ -289,13 +191,17 @@ def pdb_2_mrc(file_name,apix=1.0,res=2.8,het=False,box=None,chains=None,quiet=Fa
 	for i in range(len(aavg)): aavg[i] = aavg[i]/float(natm)
 	
 	# fill in the atom gaussians
+	xt = outbox[0]/2 - (amax[0]-amin[0])/(2*apix)
+	yt = outbox[1]/2 - (amax[1]-amin[1])/(2*apix)
+	zt = outbox[2]/2 - (amax[2]-amin[2])/(2*apix)
+	print xt,yt,zt
 	for i,a in enumerate(atoms):
 		if not quiet and i%1000==0 : 
 			print '\r   %d'%i,
 			sys.stdout.flush()
 		try:
 			elec=atomdefs[a[0].upper()][0]
-			outmap.insert_scaled_sum(gaus,(a[1]/apix+outbox[0]/2-aavg[0],a[2]/apix+outbox[1]/2-aavg[1],a[3]/apix+outbox[2]/2-aavg[2]),res/(pi*12.0*apix),elec)
+			outmap.insert_scaled_sum(gaus,(a[1]/apix+xt-amin[0]/apix,a[2]/apix+yt-amin[1]/apix,a[3]/apix+zt-amin[2]/apix),res/(pi*12.0*apix),elec)
 		except: print "Skipping %d '%s'"%(i,a[0])
 		
 	if not quiet: print '\r   %d\nConversion complete'%len(atoms)
