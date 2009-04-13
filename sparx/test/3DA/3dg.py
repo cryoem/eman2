d3f8da71f8f6b0a11f6f787f81f3884fa470a41f
ppenczek <ppenczek>
2009-04-13 17:21:05 +0000

    modernized

--- sparx/test/3DA/3dg.py
+++ sparx/test/3DA/3dg.py
@@ -5,41 +5,6 @@ from sparx  import *
 
 from sys import exit
 
-s2x = 10
-s2y = -21
-phi = 130
-theta = 25
-psi = 73
-model = test_image_3d()
-t = Transform({"type":"spider","phi":phi,"theta":theta,"psi":psi})
-t.set_trans(Vec2f(s2x,s2y))
-print  t.get_trans()
-
-#t.set_scale(2.0) works but probably won't be used
-#t.set_mirror(1) works but probably won't be used
-proj = model.project("standard",t)
-proj_2 = model.project("gauss_fft",t)
-projl = project(model, [phi, theta, psi, s2x, s2y],44.0)
-projg = prg(model, [phi, theta, psi, s2x, s2y])
-#prl = prl(model, [[phi, theta, psi, -s2x, -s2y]],22.0)
-dropImage(proj,"p1.hdf")
-dropImage(proj_2,"p2.hdf")
-dropImage(projl,"p3.hdf")
-dropImage(projg,"p4.hdf")
-dropImage(proj-projg,"p5.hdf")
-dropImage(proj_2-projg,"p6.hdf")
-#dropImage(prl,"p5.hdf")
-print  ccc(proj,proj_2),ccc(proj_2,projg),ccc(proj,projg),ccc(projl,projg)
-
-# test of shifting in-plane
-
-projt = prg(model, [phi, theta, psi, 0, 0])
-
-#  This works and proves that the shift stored in the header (s2x, s2y) can be
-#  applied to the projection to have it centered.
-print ccc(projt, fshift(projg, -s2x, -s2y))
-
-exit()
 vol = EMData()
 vol.read_image("../model001.tcp")
 #vol.read_image("model.spi")
@@ -82,21 +47,21 @@ for i in xrange(nangles):
 	#proj += model_gauss_noise(st[1]*3.,nx,nx)
 	# Set all parameters for the new 2D image
 	# three angles and two shifts to zero
-	proj.set_attr_dict({'phi':angles[i][0], 'theta':angles[i][1], 'psi':angles[i][2], 's2x':s2x, 's2y':s2y})
+	set_params_proj(proj, [angles[i][0], angles[i][1], angles[i][2], s2x, s2y])
 	# CTF parameters, if defocus zero, they are undetermined
-	proj.set_attr_dict({'defocus':defocus, 'amp_contrast':0.1, 'voltage':300, 'Cs':2.0, 'Pixel_size':2.5, 'B_factor':0.})
+	set_ctf(proj, [defocus, 2.0, 300.0, 2.5, 0.0, 0.1])
 	# flags describing the status of the image (1 = true, 0 = false)
 	proj.set_attr_dict({'active':1, 'ctf_applied':0})
 	proj.write_image(stack_data, i)
 del volft
-dropSpiderDoc("params.doc",ppp)
+write_text_file(ppp, "params.txt")
 #exit()
 del ppp
 nprojdata = EMUtil.get_image_count(stack_data)
 snr = 1.0e20
 vol1   = recons3d_4nn_ctf(stack_data, range(0,nprojdata,2), snr)
 #vol1   = recons3d_4nn(stack_data, range(0,nprojdata,2))
-dropImage(vol1,"v1.spi","s")
+vol1.write_image("v1.hdf", 0)
 
 vol2   = recons3d_4nn_ctf(stack_data, range(1,nprojdata,2), snr)
 #vol2   = recons3d_4nn(stack_data, range(1,nprojdata,2))
@@ -107,10 +72,10 @@ vol2   = recons3d_4nn_ctf(stack_data, range(1,nprojdata,2), snr)
 #Util.mul_img(vol2, mask3d)
 #del mask3d
 
-dropImage(vol2,"v2.spi","s")
+vol2.write_image("v2.hdf", 0)
 # THIS HAS TO BE INDEXED BY THE LOOP
 fsc(vol1,vol2,1.0,"tdt.txt")
-exit()
+#exit()
 #vol = recons3d_wbp(stack_data, range(nprojdata),"general", const=1.0e4)
 #dropImage(vol,"newvolwp.spi","s")
 #vol = recons3d_4nn(stack_data, range(nprojdata))
@@ -118,7 +83,7 @@ exit()
 #snr = 1.0
 #vol = recons3d_4nn_ctf(stack_data, range(nprojdata), snr)
 #dropImage(vol,"newvolp.spi","s")
-exit()
+#exit()
 mask3D = []
 # do the projection matching
 first_ring = 1
@@ -129,32 +94,19 @@ yrng  = 1
 step  = 1
 dtheta= 15
 # begin a refinement loop, slowly decrease dtheta inside the loop
-snr = 100.0
+snr = 1.0
 for iter in xrange(1):
 	print " ITERATION #",iter
 	#proj_ali(vol, mask3D, stack_data, first_ring, last_ring, rstep, xrng, yrng, step, dtheta)
 
-	# now have to retrieve all angles, it is only to print them!!
-	cang = []
-	for i in xrange(nangles):
-		proj.read_image(stack_data, i)
-		phi = proj.get_attr('phi')
-		theta = proj.get_attr('theta')
-		psi = proj.get_attr('psi')
-		s2x = proj.get_attr('s2x')
-		s2y = proj.get_attr('s2y')
-		print  i,phi,theta,psi,s2x,s2y
-		cang.append([phi, theta, psi, s2x, s2y])
-	out="anglesn"
-	dropSpiderDoc(out,cang)
 	#calculate new and improved 3D
 	stack_data = "data.hdf"
 	nprojdata = EMUtil.get_image_count(stack_data)
 	list_p = range(nprojdata)
 	vol = recons3d_4nn_ctf(stack_data, list_p, snr, 1, "c2")
-	dropImage(vol,"newvol.spi","s")
+	vol.write_image("newvol.hdf",0)
 	del  vol
-	exit()
+	#exit()
 	#and now the resolution
 
 	list_p = range(0,nprojdata,2)
