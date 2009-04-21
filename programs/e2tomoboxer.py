58355854532671360bf12077db26e9c0d4381232
woolford <woolford>
2009-04-21 22:54:24 +0000

    tomo boxer

--- programs/e2tomoboxer.py
+++ programs/e2tomoboxer.py
@@ -33,7 +33,7 @@
 from optparse import OptionParser
 import sys
 import os
-from EMAN2 import file_exists, gimme_image_dimensions3D,EMANVERSION,EMData,Region,Transform
+from EMAN2 import file_exists, gimme_image_dimensions3D,EMANVERSION,EMData,Region,Transform,get_image_directory
 from pyemtbx.boxertools import Cache,get_idd_image_entry,set_idd_image_entry, Box
 from emapplication import get_application, EMStandAloneApplication,EMQtWidgetModule
 from emimage2d import EMImage2DModule
@@ -98,9 +98,27 @@ class TomogramZProjectionImage:
 		if self.image == None:
 			tmp = EMData()
 			tmp.read_image(self.file_name,0)
-			self.image = tmp.project("standard",Transform())
-			self.image.set_attr("z_height",tmp.get_zsize())
-#			nx,ny,nz = gimme_image_dimensions3D(self.file_name)
+		
+			nx,ny,nz = gimme_image_dimensions3D(self.file_name)
+			if ny < nz:
+				self.image = tmp.process("misc.directional_sum",{"direction":"y"})
+				self.image.set_size(nx,nz,1)
+				self.image.set_attr("sum_pixels",tmp.get_ysize())
+				self.image.set_attr("sum_direction","y")
+				
+			elif nx < nz:
+				self.image = tmp.process("misc.directional_sum",{"direction":"x"})
+				self.image.set_size(ny,nz,1)
+				self.image.set_attr("sum_pixels",tmp.get_xsize())
+				self.image.set_attr("sum_direction","x")
+			else:
+				self.image = tmp.process("misc.directional_sum",{"direction":"z"})
+				self.image.set_size(nx,ny,1)
+				self.image.set_attr("sum_pixels",tmp.get_zsize())
+				self.image.set_attr("sum_direction","z")
+				
+			
+			
 #			self.image = EMData(nx,ny)
 #			self.image.to_zero()
 #			tmp = EMData()
@@ -162,12 +180,12 @@ class EMTBBoxManipulations():
 			if event.modifiers() & Qt.ShiftModifier : return # the user tried to delete nothing
 			
 			# If we get here, we need to add a new box
-			box_size = self.target().get_box_size()
-			lbx = coord_data[0]-box_size/2
-			lby = coord_data[1]-box_size/2
-			box = [lbx,lby,lbx+box_size,lby+box_size]
+#			box_size = self.target().get_box_size()
+#			lbx = coord_data[0]-box_size/2
+#			lby = coord_data[1]-box_size/2
+#			box = [lbx,lby,lbx+box_size,lby+box_size]
 			
-			self.target().add_box(box)
+			self.target().add_box(coord_data)
 			self.moving=[coord_data,box_num]
 		
 		elif event.modifiers()&Qt.ShiftModifier : # box removal
@@ -217,17 +235,97 @@ class EMTBBoxManipulations():
 		'''
 		pass
 	
-class EMBoxList:
+class EMTBPositionBoxManipulations():
+	'''
+	A class that responds to added, moved and removed box signals emitted
+	by the EMImage2DModule
+	'''
+	def __init__(self,target,main_2d_window):
+		self.target = weakref.ref(target) # prevent a strong cycle
+		self.main_2d_window = main_2d_window
+		self.moving = None
+		
+		self.__connect_signals_to_slots()
+	def __connect_signals_to_slots(self):
+		
+		'''
+		connects the signals of the main 2D window to the slots of this object
+		'''
+		qt_target = get_application().get_qt_emitter(self.main_2d_window)
+		
+		QtCore.QObject.connect(qt_target,QtCore.SIGNAL("mousedown"),self.mouse_down)
+		QtCore.QObject.connect(qt_target,QtCore.SIGNAL("mousedrag"),self.mouse_drag)
+		QtCore.QObject.connect(qt_target,QtCore.SIGNAL("mouseup")  ,self.mouse_up  )
+		QtCore.QObject.connect(qt_target,QtCore.SIGNAL("keypress"),self.key_press)
+		QtCore.QObject.connect(qt_target,QtCore.SIGNAL("mousewheel"),self.mouse_wheel)
+
+	def __box_collision_detect(self,coord_data):
+		'''
+		A wrapper function that could easily disappear
+		@param coord_data a coordinate interms of the main image's pixel coordinate system
+		'''
+		return self.target().detect_side_view_box_collision(coord_data)
+	
+	def mouse_down(self,event) :
+		'''
+		@param a QtGui.QMouseEvent sent from the EMImage2DModule
+		'''
+		coord_data = list(self.main_2d_window.scr_to_img((event.x(),event.y())))
+		coord_data[0] = int(coord_data[0])
+		coord_data[1] = int(coord_data[1])
+		box_num =  self.__box_collision_detect(coord_data)
+		if box_num != -1:
+
+#			# if we make it here than the we're moving a box
+			box = self.target().get_box(box_num)
+			self.moving=[coord_data,box_num]
+#			self.target().set_active_box(box_num)
+							
+	def mouse_drag(self,event) :
+		'''
+		@param a QtGui.QMouseEvent sent from the EMImage2DModule
+		'''
+		
+		coord_data = list(self.main_2d_window.scr_to_img((event.x(),event.y())))
+		coord_data[0] = int(coord_data[0])
+		coord_data[1] = int(coord_data[1])
+#		print "mouse drag"#
+		if self.moving != None:
+			old_coord = self.moving[0]
+			
+			self.target().move_box(self.moving[1],coord_data[0]-old_coord[0],0,coord_data[1]-old_coord[1])
+			self.moving[0] = coord_data
+#	
+	def mouse_up(self,event) :
+		'''
+		@param a QtGui.QMouseEvent sent from the EMImage2DModule
+		'''
+		if self.moving != None:
+			self.moving=None
+		
+	def key_press(self,event):
+		'''
+		@param a QtGui.QKeyEvent sent from the EMImage2DModule
+		'''
+		pass 
+	def mouse_wheel(self,event):
+		'''
+		@param a QtGui.QMouseEvent sent from the EMImage2DModule
+		'''
+		pass
+	
+class EMCoordList:
 	'''
 	Meant to store the primary data associated with a box, namely its bottom left and top right
-	coordinates. Stores coordinates as a list of length 4, e.g.
+	coordinates. Stores central coordinates as a list of length 4, e.g.
 	[bottom left x, bottom left y, top right x, top right y]
 	Provides additional collision detection functionality
 	Provides additional box size changing and box moving functionality
 	Supports list-style iterator support 
 	'''
 	def __init__(self):
-		self.boxes = [] # main data container
+		self.coords = [] # main data container, entries are lists of length 4
+		self.z_boxes = [] # z coordinate data boxes
 		self.current_iter = 0 # iterator support
 		self.max_idx = 0 # iterator support
 		
@@ -235,14 +333,17 @@ class EMBoxList:
 		'''
 		@param box_coords should be a list like [bottom left x, bottom left y, top right x, top right y]
 		@exception RuntimeError raised if box_coords is not a list
-		@exception RuntimeError raised if box_coords is not of length 4
+		@exception RuntimeError raised if box_coords is not of length 2 or 3
 		'''
 		if not isinstance(box_coords,list): raise RuntimeError("box_coords should be a list")
-		if len(box_coords) != 4: raise RuntimeError("box_coords should have precisely 4 entries")
+		if len(box_coords) not in [2,3]: raise RuntimeError("box_coords should have 2 or 3 entries")
 		
-		self.boxes.append(box_coords)
+		if len(box_coords) == 2:
+			box_coords.append(-1)
+			
+		self.coords.append(box_coords)
 	
-	def detect_collision(self,coords):
+	def detect_collision(self,coords,box_size):
 		'''
 		@param coords a coordinate list, the first two values are the coordinates
 		@exception RuntimeError raised if coords is not a list
@@ -254,72 +355,51 @@ class EMBoxList:
 		if not isinstance(coords,list) and not isinstance(coords,tuple): raise RuntimeError("coords should be a list")
 		if len(coords) < 2: raise RuntimeError("coords should have 2 entries")
 		
-		for box_num,box in enumerate(self.boxes):
-			if coords[0]<box[0] or coords[0]>box[2] or coords[1]<box[1] or coords[1]>box[3]:
+		for box_num,box in enumerate(self.coords):
+			lbx = box[0]-box_size/2
+			lby = box[1]-box_size/2
+			rtx = lbx+box_size
+			rty = lby+box_size
+			if coords[0]<lbx or coords[0]>rtx or coords[1]<lby or coords[1]>rty:
 				# no collision
 				continue
 			# if we make it here there has been a collision, the box already exists
 			return box_num
 		
-		return -1
-	
-	def change_box_size(self,new_box_size):
-		'''
-		@param the box size - this is much more like diameter than it is radius
-		@exception RuntimeError raised if the box_size is less than 1
-		#exception RuntimeError raised if the first internally stored box is not a square - to alert developers if they have changed this
-		'''
-		if new_box_size < 1: raise RuntimeError("The boxsize must be at least 1")
-		if len(self.boxes) == 0: return # this is fine, the calling program might not know and there is not box_size attribute
+		return -1			
 		
-		box_0 = self.boxes[0]
-		old_box_size = box_0[2]-box_0[0] # note no consideration 
-		check_box_size = box_0[3]-box_0[1]
-		if old_box_size != check_box_size:
-			print box_0,old_box_size,check_box_size
-			raise NotImplementedError("This code does not currently handle uneven box dimensions - developer alert") 
-	
-		box_shift = (new_box_size - old_box_size)/2 # this should be fine for odd sizes
-		new_boxes = []
-		for box in self.boxes:
-			lbx = box[0]-box_shift
-			lby = box[1]-box_shift
-			new_boxes.append([lbx,lby,lbx+new_box_size,lby+new_box_size])
-		
-		self.boxes = new_boxes
-			
-	def move_box(self,idx,dx,dy):
+	def move_box(self,idx,dx,dy,dz=0):
 		'''
 		move box at idx in the x and y direction
 		@param idx the index of the object to be moved
 		@param dx the amount by which to move in the x direction
 		@param dy the amount to move in the y direction
 		'''
-		box = self.boxes[idx]
+		box = self.coords[idx]
 		box[0] += dx
 		box[1] += dy
-		box[2] += dx
-		box[3] += dy
+		box[2] += dz
+#		box[3] += dy
 	
 	def __getitem__(self,idx):
 		'''
 		Support for [] syntax
 		@idx the index of the object which is to be retrieved
 		'''
-		return self.boxes[idx]
+		return self.coords[idx]
 	
 	def __len__(self):
 		'''
 		support for standard len
 		'''
-		return len(self.boxes)
+		return len(self.coords)
 	
 	def __iter__(self):
 		''' 
 		Iteration support - initialization 
 		'''
 		self.current_iter = 0
-		self.max_idx = len(self.boxes)
+		self.max_idx = len(self.coords)
 		return self
 	
 	def next(self):
@@ -332,28 +412,32 @@ class EMBoxList:
 			raise StopIteration
 		else:
 			self.current_iter += 1
-			return self.boxes[self.current_iter-1]
+			return self.coords[self.current_iter-1]
 		
 	def pop(self,idx):
 		'''
 		@param idx the index of object that is to be popped
 		@return the popped object
 		'''
-		return self.boxes.pop(idx)
+		return self.coords.pop(idx)
 	
 class EMBoxDisplayShapes:
 	def __init__(self): pass
 		
-	def gen_shapes(box_list,shape_string="rectpoint"):
+	def gen_shapes(box_list,box_size,shape_string="rectpoint"):
 		'''
-		@param box_list should be an instance of an EMBoxList
-		@exception RuntimeError raise if box_list if not an EMBoxList
+		@param box_list should be an instance of an EMCoordList
+		@exception RuntimeError raise if box_list if not an EMCoordList
 		
 		'''
 		shapes = {}
 		
 		for i,box in enumerate(box_list):
-			shape = EMShape([shape_string,0,0.9,0.4,box[0],box[1],box[2],box[3],2.0])
+			lbx = box[0]-box_size/2
+			lby = box[1]-box_size/2
+			rtx = lbx+box_size
+			rty = lby+box_size
+			shape = EMShape([shape_string,0,0.9,0.4,lbx,lby,rtx,rty,2.0])
 			shapes[i] = shape
 			
 		return shapes
@@ -372,8 +456,8 @@ class EMTomoBoxerModule:
 		'''
 		if not file_exists(file_name): raise RuntimeError("The file (%s) does not exist" %file_name)
 		self.file_name = file_name
-		self.box_size = 256
-		self.box_list = EMBoxList()
+		self.box_size = 64
+		self.coord_list = EMCoordList()
 		self.active_box = -1 # an index storing which box the user had clicked on
 		self.positioning_2d_window = None # will eventually be used to position boxed images in the z direction
 		
@@ -416,7 +500,7 @@ class EMTomoBoxerModule:
 		self.positioning_2d_window = EMImage2DModule()
 		self.positioning_2d_window.setWindowTitle("Z positioning")
 		
-		self.signal_slot_handlers["position_manipulation"] = EMTBBoxManipulations(self,self.positioning_2d_window)
+		self.signal_slot_handlers["position_manipulation"] = EMTBPositionBoxManipulations(self,self.positioning_2d_window)
 		
 	def show_guis(self):
 		'''
@@ -440,7 +524,6 @@ class EMTomoBoxerModule:
 		if box_size > image.get_xsize() or box_size > image.get_ysize():
 			raise RuntimeError("The box size can not be larger than any of the image's dimensions")
 	
-		self.box_list.change_box_size(box_size)
 		self.box_size = box_size
 		self.__refresh_box_display()
 	
@@ -456,37 +539,48 @@ class EMTomoBoxerModule:
 		
 	def detect_box_collision(self,coords):
 		'''
-		See EMBoxList.detect_collision help
+		See EMCoordList.detect_collision help
 		'''
-		return self.box_list.detect_collision(coords)
+		return self.coord_list.detect_collision(coords,self.box_size)
 	
-	def add_box(self,box):
+	def detect_side_view_box_collision(self,coords):
+		if self.active_box == None: return
+		
+		active_box =  self.coord_list[self.active_box]
+		
+		by = active_box[2]-self.box_size/2
+		ty = by+self.box_size
+		if coords[1]<by or coords[1]>ty: return -1
+		else: return self.active_box
+
+	
+	def add_box(self,coord):
 		'''
-		See EMBoxList.append help
+		See EMCoordList.append help
 		'''
-		self.box_list.append(box)
-		self.active_box = len(self.box_list)-1
+		self.coord_list.append(coord)
+		self.active_box = len(self.coord_list)-1
 		self.__refresh_box_display()
 	
 	def remove_box(self,box_idx):
 		'''
-		See EMBoxList.pop help
+		See EMCoordList.pop help
 		'''
-		self.box_list.pop(box_idx)
+		self.coord_list.pop(box_idx)
 		self.__refresh_box_display()
 		
-	def move_box(self,box_idx,dx,dy):
+	def move_box(self,box_idx,dx,dy,dz=0):
 		'''
-		See EMBoxList.move_box for help
+		See EMCoordList.move_box for help
 		'''
-		self.box_list.move_box(box_idx,dx,dy)
+		self.coord_list.move_box(box_idx,dx,dy,dz)
 		self.__refresh_box_display()
 		
 	def get_box(self,box_idx):
 		'''
-		See EMBoxList.__getitem__ for help
+		See EMCoordList.__getitem__ for help
 		'''
-		return self.box_list[box_idx]
+		return self.coord_list[box_idx]
 	
 	def set_active_box(self,box_idx):
 		self.active_box = box_idx
@@ -500,7 +594,7 @@ class EMTomoBoxerModule:
 		Refreshes the display of boxes. Typically called when a box has been added or moved.
 		Tells concerned interfaces to update their displays
 		'''
-		shapes = EMBoxDisplayShapes.gen_shapes(self.box_list,self.get_shape_string())
+		shapes = EMBoxDisplayShapes.gen_shapes(self.coord_list,self.box_size,self.get_shape_string())
 		self.main_2d_window.set_shapes(shapes,1.0)
 		self.main_2d_window.set_active(self.active_box,.9,.9,.1)
 		self.main_2d_window.updateGL()
@@ -509,16 +603,37 @@ class EMTomoBoxerModule:
 		
 	def __refresh_positioner(self):
 		if self.active_box != -1:
-			box = self.box_list[self.active_box]
-			nz = ZProjectionCache.get_image_directly(self.file_name).get_attr("z_height")
-			nx = box[2]-box[0]
-			ny = box[3]-box[1]
-			
-			region = Region(box[0],box[1],0,nx,ny,nz)
+			coord = self.coord_list[self.active_box]
+			image = ZProjectionCache.get_image_directly(self.file_name)
 			a = EMData()
-			a.read_image(self.file_name,0,False,region)
-			b = a.process("misc.directional_sum",{"direction":"y"})
-			b.set_size(int(nx),int(nz),1)
+			direction = image.get_attr("sum_direction")
+			half_box = self.box_size/2
+			if direction == "z":
+				nz = image.get_attr("sum_pixels")
+				nx = self.box_size
+				ny = self.box_size
+				region = Region(coord[0]-half_box,coord[1]-half_box,0,nx,ny,nz)
+				
+				a.read_image(self.file_name,0,False,region)
+				b = a.process("misc.directional_sum",{"direction":"y"})
+				b.set_size(int(nx),int(nz),1)
+			elif direction == "y":
+				ny = image.get_attr("sum_pixels")
+				nx = self.box_size
+				nz = self.box_size
+				region = Region(coord[0]-half_box,0,coord[1]-half_box,nx,ny,nz)
+				a.read_image(self.file_name,0,False,region)
+				b = a.process("misc.directional_sum",{"direction":"z"})
+				b.set_size(int(nx),int(ny),1)
+			elif direction == "x":
+				nx = image.get_attr("sum_pixels")
+				ny = self.box_size
+				nz = self.box_size
+				region = Region(0,coord[0]-half_box,coord[1]-half_box,nx,ny,nz)
+				a.read_image(self.file_name,0,False,region)
+				b = a.process("misc.directional_sum",{"direction":"z"})
+				b.set_size(int(nx),int(ny),1)
+			
 			if self.positioning_2d_window == None:
 				self.__init_positioning_2d_window()
 				self.positioning_2d_window.set_data(b)
@@ -526,8 +641,21 @@ class EMTomoBoxerModule:
 				self.positioning_2d_window.optimally_resize()
 			else:
 				self.positioning_2d_window.set_data(b)
+			
+			
+			if coord[2] == -1:
+				com = b.calc_center_of_mass()
+				coord[2] = b.get_ysize() - com[1]
 				
+			l = coord[2]  - self.box_size/2
+			r = l + self.box_size
+			shape = EMShape(["rectpoint",0,0.9,0.4,0,l,self.box_size,r,2.0])
+			shapes = {0:shape}
+			self.positioning_2d_window.set_shapes(shapes,1)
 			self.positioning_2d_window.updateGL()
+	
+	def run_output_dialog(self):
+		pass
 			
 
 class EMTomoBoxerInspectorModule(EMQtWidgetModule):
@@ -548,6 +676,7 @@ class EMTomoBoxerInspector(QtGui.QWidget):
 	def __init__(self,target) :
 		
 		QtGui.QWidget.__init__(self,None)
+		self.setWindowIcon(QtGui.QIcon(get_image_directory() +"eman.png"))
 		self.target=weakref.ref(target)
 		
 		self.vbl = QtGui.QVBoxLayout(self)
@@ -569,8 +698,13 @@ class EMTomoBoxerInspector(QtGui.QWidget):
 		
 		self.vbl.addLayout(box_size_hbl)
 		
-		self.connect(self.box_size,QtCore.SIGNAL("editingFinished()"),self.new_box_size)
+		self.gen_output_but=QtGui.QPushButton("Write output")
+		self.vbl.addWidget(self.gen_output_but)
 		
+		
+		self.connect(self.box_size,QtCore.SIGNAL("editingFinished()"),self.new_box_size)
+		self.connect(self.gen_output_but,QtCore.SIGNAL("clicked(bool)"),self.target().run_output_dialog)
+
 	def new_box_size(self):
 		box_size=int(self.box_size.text())
 		self.target().set_box_size(box_size)
