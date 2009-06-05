15c29fb0549e41b91f24478a9c6b9b1ae28b7466
woolford <woolford>
2009-06-05 16:12:57 +0000

    get rid of print stmts

--- libpyEM/EMAN2db.py
+++ libpyEM/EMAN2db.py
@@ -182,7 +182,6 @@ def db_parse_path(url):
 			exc_set = set(ddb.select[u2[0][8:]])
 			all_set = set(range(0,EMUtil.get_image_count("bdb:"+url[0]+"#"+url[1])))
 			rem_set = all_set - exc_set
-			
 			return (url[0],url[1],list(rem_set))		# bdb:path/to#dict?select/name
 		return url											# bdb:path/to#dict?onekey
 	# make sure integer string keys are integers
@@ -961,8 +960,6 @@ class DBDict:
 		if isinstance(r,dict) and r.has_key("is_complex_x") :
 			pkey="%s/%s_"%(self.path,self.name)
 			fkey="%dx%dx%d"%(r["nx"],r["ny"],r["nz"])
-			print fkey,dumps(key,-1)
-			print self.bdb.get(fkey+dumps(key,-1))
 			n=loads(self.bdb.get(fkey+dumps(key,-1)))
 			if r.has_key("data_path"): return r["data_path"]
 			else : return "%s*%d"%(pkey+fkey,n*4*r["nx"]*r["ny"]*r["nz"])
