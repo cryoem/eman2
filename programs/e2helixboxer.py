0bf3cc2e467a132a149a4cae7584bad003f65e09
ross <ross>
2009-09-30 17:02:31 +0000

    Moving entire boxes or just an endpoint of a box is supported, also EMHelixBoxerModule is now in use. For some reason, creating an Img2DModEventsHandler in EMHelixBoxerModule causes mouse bugs, so it is created in main().

--- programs/e2helixboxer.py
+++ programs/e2helixboxer.py
@@ -2,9 +2,10 @@
 
 #Author: Ross Coleman
 
-from EMAN2 import test_image, get_image_directory #,EMData
-from emapplication import EMQtWidgetModule, EMStandAloneApplication#, get_application
+from EMAN2 import test_image, get_image_directory, Transform #,EMData
+from emapplication import EMQtWidgetModule, EMStandAloneApplication, get_application
 from emimage2d import EMImage2DModule
+from emimagemx import EMImageMXModule
 from emshape import EMShape
 
 from math import *
@@ -22,26 +23,27 @@ def counterGen():
 		i += 1
 		yield i
 
-class Im2DModEventsHandler:
+class Img2DModEventsHandler:
 	"""Adds helix-boxing specific mouse interactivity to
 	an EMImage2DModule object"""
 	def __init__(self, target, boxwidth=50):
 		"""The target should be an EMImage2DModule instance and the
 		boxwidth is the width to draw the boxes."""
 		self.counter = counterGen() #Used to make keys for the helix objects
-		self.startPoint = None #This is the location of the first click when making a box
-		self.endPoint = None #This is the location of the other endpoint for the box
-		self.currentKey = None #This is the key for the "rectline" EMShape we're working on
+		self.start_point = None #This is the location of the first click when making a box
+		self.end_point = None #This is the location of the other endpoint for the box
+		self.current_box_key = None #This is the key for the "rectline" EMShape we're working on
 		#The current key is created during a "mousedrag" if one doesn't exist and is set to None upon "mouseup"
 		self.boxwidth = boxwidth
 		self.editmode = None #Can be None, 'n' for new, 'm' for move, 'f' for move first point, or 's' for move second point
-		self.old_box_key = None #Gives the key in EMImage2DModule's dictionary for the "rectline" EMShape to edit
 		self.old_box_list = None #Gives the list used to create the old EMShape box
 		self.color = (1, 1, 1)
 		self.target = weakref.ref(target)
 		QtCore.QObject.connect( self.target().emitter(), QtCore.SIGNAL("mousedown"), self.mouse_down)
 		QtCore.QObject.connect( self.target().emitter(), QtCore.SIGNAL("mousedrag"), self.mouse_drag)
 		QtCore.QObject.connect( self.target().emitter(), QtCore.SIGNAL("mouseup"), self.mouse_up)
+
+		self.particles=None
 
 	def get_helix_keys(self):
 		"""This returns the keys for the "rectline" EMShape objects
@@ -97,16 +99,18 @@ class Im2DModEventsHandler:
 		Clicking on a point betwen 3/8 L and 5/8 L from the shorter axis of symmetry
 		results in moving that end of the box while keeping the midpoint of the other end fixed.
 		"""
-		self.startPoint = loc
+		
+		self.start_point = loc
+		
 		if self.get_helix_keys(): #helix boxes already exist
-			closest_helix_key = self.get_nearest_helix_key(loc)
+			nearest_helix_key = self.get_nearest_helix_key(loc)
 
-			box = self.target().get_shapes()[closest_helix_key].getShape() #list used to create the "rectline" EMShape
+			box = self.target().get_shapes()[nearest_helix_key].getShape() #list used to create the "rectline" EMShape
 			mid = ( (box[4]+box[6])/2.0, (box[5]+box[7])/2.0 ) #midpoint
 			length = sqrt( (box[6]-box[4])**2 + (box[7]-box[5])**2 )
 			#Converting to a coordinate system with origin at the centroid and axes along the width and length
 			#Unit vector along the length pointing from x0,y0 to x1,y1
-			l_uvect = ( (box[6]-box[4])/length, (box[7]-box[5])/length )
+			l_uvect = ( float(box[6]-box[4])/length, float(box[7]-box[5])/length )
 			#Unit vector along the width such that w_uvect (cross product) l_uvect = k_hat
 			w_uvect = ( l_uvect[1], -1*l_uvect[0] )
 			click_vect = ( loc[0]-mid[0], loc[1] - mid[1] ) #vector from midpoint to click location
@@ -117,35 +121,27 @@ class Im2DModEventsHandler:
 			if abs(w_coord) > half_width: #outside box
 				if event.modifiers()&QtCore.Qt.ShiftModifier: #nothing to delete
 					self.editmode = None
-					self.old_box_key = None
 				else: #new box
 					self.editmode = 'n' #new box
-					self.old_box_key = None
 			else:
 				#dot product of click_vect on l_uvect
 				l_coord = click_vect[0]*l_uvect[0]+click_vect[1]*l_uvect[1]
 				if event.modifiers()&QtCore.Qt.ShiftModifier: #Shift key means delete
 					self.editmode = None
-					self.old_box_key = None
 					if abs(l_coord) <= length /2.0: #inside box, so delete it
-						self.target().del_shape(closest_helix_key)
+						self.target().del_shape(nearest_helix_key)
 						self.target().updateGL()
 				else: #either new box or edit current box
 					if l_coord > 5*length/8.0:
 						self.editmode = 'n' #new box
-						self.old_box_key = None
 					elif l_coord >= 3*length/8.0:
 						self.editmode = 's' #second point
-						self.old_box_key = closest_helix_key
 					elif l_coord > -3*length/8.0:
 						self.editmode = 'm' #move entire box
-						self.old_box_key = closest_helix_key
 					elif l_coord >= -5*length/8.0:
 						self.editmode = 'f' #first point
-						self.old_box_key = closest_helix_key
 					elif l_coord < -5*length/8.0:
 						self.editmode = 'n' #new box
-						self.old_box_key = None
 					else:
 						raise ValueError
 
@@ -154,36 +150,38 @@ class Im2DModEventsHandler:
 				self.editmode = None
 			else:
 				self.editmode = 'n' #new box
-			self.old_box_key = None #there is no old box
-
-		if self.old_box_key:
-			self.old_box_list = self.target().get_shapes()[self.old_box_key].getShape() #The list used to make the EMShape
-		else:
+		
+		if self.editmode == 'n' or not self.editmode:
+			self.current_box_key = None
 			self.old_box_list = None
+		else:
+			self.current_box_key = nearest_helix_key
+			self.old_box_list = self.target().get_shapes()[self.current_box_key].getShape() #The list used to make the EMShape
 
 	def mouse_drag(self, event, location):
 		"""
 		Boxes are deleted in mouse_down, and the decision of how to edit is made there.
 		However, new boxes are made and existing boxes are edited here.
 		"""
-		if self.startPoint and self.editmode: #self.startPoint and self.editmode are set in mouse_down
+
+		if self.start_point and self.editmode: #self.start_point and self.editmode are set in mouse_down
 			if self.editmode == 'n': #new
 				box = EMShape(["rectline", self.color[0], self.color[1], self.color[2],
-					self.startPoint[0], self.startPoint[1],
+					self.start_point[0], self.start_point[1],
 					location[0], location[1], self.boxwidth])
-				if not self.currentKey: 
-					self.currentKey = self.counter.next() #We only make boxes if there is a mouse drag, so we create it here
+				if not self.current_box_key: 
+					self.current_box_key = self.counter.next() #We only make boxes if there is a mouse drag, so we create it here
 
-				self.target().add_shape(self.currentKey, box)
+				self.target().add_shape(self.current_box_key, box)
 			else:
 				color = self.old_box_list[1:4]
 				first = self.old_box_list[4:6]
 				second = self.old_box_list[6:8]
 				width = self.old_box_list[8]
-				move = (location[0] - self.startPoint[0], location[1]-self.startPoint[1])
+				move = (location[0] - self.start_point[0], location[1]-self.start_point[1])
 
-				if self.old_box_key in self.target().get_shapes().keys():
-					self.target().del_shape(self.old_box_key)
+				if self.current_box_key in self.target().get_shapes().keys():
+					self.target().del_shape(self.current_box_key)
 				if self.editmode == 'm': #move entire box
 					newbox = EMShape(["rectline", color[0], color[1], color[2],
 						move[0]+first[0], move[1]+first[1],
@@ -195,7 +193,7 @@ class Im2DModEventsHandler:
 					newbox = EMShape(["rectline", color[0], color[1], color[2],
 						first[0], first[1], move[0]+second[0], move[1]+second[1], width])
 
-				self.target().add_shape(self.old_box_key, newbox) #replaces the previous box
+				self.target().add_shape(self.current_box_key, newbox) #replaces the previous box
 				
 			self.target().updateGL()
 
@@ -205,11 +203,31 @@ class Im2DModEventsHandler:
 		an existing box is complete, so we need only clear variables relevant
 		to creating or editing boxes.
 		"""
-		self.startPoint = None
-		self.endPoint = None
-		self.currentKey = None
+
+		if self.current_box_key:
+			boxList = self.target().get_shapes()[self.current_box_key].getShape() #The list used to create the box
+			x_min = min(boxList[4], boxList[6])
+			y_min = min(boxList[5], boxList[7])
+			length = abs( (boxList[6]-boxList[4])**2 + (boxList[7]-boxList[5])**2 )
+			width = boxList[8]
+			l_uvect = ( float((boxList[6]-boxList[4]))/length, float(boxList[7]-boxList[5])/length )
+			rot_angle = acos( l_uvect[1] ) #To rotate so the length is parallel to the y axis: l_uvect (dot) j_hat = cos (rot_angle)
+			tr = Transform()
+			tr.set_rotation({"type":"2d", "alpha":rot_angle})
+			#i_size = IntSize(round(width), round(length))
+			em_image = self.target().get_data()
+			boxed_em_data = em_image.get_rotated_clip( tr, [int(round(width)),int(round(length)),1] )
+			print (boxed_em_data["nx"], boxed_em_data["ny"])
+			#TODO: much more flexible creation and updating of the particles window
+			if not self.particles:
+				self.particles = EMImageMXModule(data=boxed_em_data, application=get_application())
+				self.particles.show()
+				self.particles.updateGL()
+				#TODO: figure out why nothing displays!
+		self.start_point = None
+		self.end_point = None
+		self.current_box_key = None
 		self.editmode = None
-		self.old_box_key = None
 		self.old_box_list = None
 
 	def set_boxwidth(self, boxwidth):
@@ -281,7 +299,7 @@ class EMHelixBoxerModule(QtCore.QObject):
 	def __init__(self, app):
 		QtCore.QObject.__init__(self)
 		self.img2d = EMImage2DModule(test_image(), app)
-		self.img2d_events_handler = Im2DModEventsHandler(self.img2d)
+		#self.img2d_handler = Img2DModEventsHandler(self.img2d) #For some reason this doesn't work if placed here
 		self.inspector = EMHelixBoxerInspectorModule(self.img2d)
 		#TODO: Should there be a window for image thumbnails???
 		#TODO: Should there be a window for picked paricles???
@@ -297,25 +315,8 @@ class EMHelixBoxerModule(QtCore.QObject):
 
 def main():
 	app = EMStandAloneApplication()
-	#EMHelixBoxerModule(app) #FIXME: mouse doesn't work!
-
-#Code below should be handled in EMHelixBoxerModule
-	img2dmodule = EMImage2DModule(test_image(), app)
-	eventsHandler = Im2DModEventsHandler(img2dmodule)
-
-	from OpenGL import GL
-	print str(GL.glGetIntegerv(GL.GL_LINE_WIDTH_RANGE))
-	#The line width setting seems to have no effect for "rectpoint" shapes
-	rect1 = EMShape(["rectpoint", 1, 0, 0, 10, 10, 30, 30, 1])
-	rect2 = EMShape(["rectpoint", 0, 0, 1, 10, 35, 30, 55, 10])
-	img2dmodule.add_shapes({"first": rect1, "second": rect2})
-
-	inspector = EMHelixBoxerInspectorModule(img2dmodule)
-
-	img2dmodule.show()
-	inspector.show()
-#Code above should be handled in EMHelixBoxerModule
-	
+	helixboxer = EMHelixBoxerModule(app)
+	handler = Img2DModEventsHandler(helixboxer.img2d) #For some reason, this doesn't work if placed in EMHelixBoxerModule
 	sys.exit(app.exec_())
 
 if __name__ == '__main__':
