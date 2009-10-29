7d57a4d3cc468562420b6fe0f8579163682bdde5
gtang <gtang>
2009-10-29 04:00:59 +0000

    fix comment.

--- libEM/emdata_modular.cpp
+++ libEM/emdata_modular.cpp
@@ -1,38 +1,38 @@
 /**
  * $Id$
  */
- 
+
 /*
  * Author: Steven Ludtke, 04/10/2003 (sludtke@bcm.edu)
  * Copyright (c) 2000-2006 Baylor College of Medicine
- * 
+ *
  * This software is issued under a joint BSD/GNU license. You may use the
  * source code in this file under either license. However, note that the
  * complete EMAN2 and SPARX software packages have some GPL dependencies,
  * so you are responsible for compliance with the licenses of these packages
  * if you opt to use BSD licensing. The warranty disclaimer below holds
  * in either instance.
- * 
+ *
  * This complete copyright notice must be included in any revised version of the
  * source code. Additional authorship citations may be added, but existing
  * author citations must be preserved.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- * 
+ *
  * */
- 
+
 #include "emdata.h"
 #include "processor.h"
 #include "cmp.h"
@@ -138,14 +138,14 @@ EMData *EMData::align(const string & aligner_name, EMData * to_img,
 }
 
 vector<Dict> EMData::xform_align_nbest(const string & aligner_name, EMData * to_img,
-								const Dict & params, const unsigned int nsoln, const string & comp_name, 
+								const Dict & params, const unsigned int nsoln, const string & cmp_name,
 										const Dict& cmp_params)
 {
 	ENTERFUNC;
 	Aligner *a = Factory < Aligner >::get(aligner_name, params);
 	vector<Dict> result;
 	if (a) {
-		result = a->xform_align_nbest(this,to_img,nsoln,comp_name,cmp_params);
+		result = a->xform_align_nbest(this,to_img,nsoln,cmp_name,cmp_params);
 	}
 
 	return result;
