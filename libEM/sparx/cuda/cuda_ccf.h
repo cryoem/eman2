e180440cd0731565ec16c29fb07e56317497ed7a
zyang <zyang>
2009-11-19 20:56:07 +0000

    Add image filteration and summation funtion for 2d alignment

--- libEM/sparx/cuda/cuda_ccf.h
+++ libEM/sparx/cuda/cuda_ccf.h
@@ -1,5 +1,5 @@
 void calculate_ccf(float *subject_image, float *ref_image, float *ccf, int NIAMGE, int NX, int NY, int RING_LENGTH, int NRING, int OU, float STEP, int KX, int KY, float *sx, float *sy, int id, int silent); 
 
-void filter_image(float *image_in, float *image_out, int NIMA, int NX, int NY, float *params);
+void filter_image(float *image_in, float *image_out, int NIMA, int NX, int NY, float *params, int id);
 
-void rot_filt_sum(float *image, int NIMA, int NX, int NY, int CTF, float *ctf_params, float *ali_params, float *ave1, float *ave2);
+void rot_filt_sum(float *image, int NIMA, int NX, int NY, int CTF, float *ctf_params, float *ali_params, float *ave1, float *ave2, int id);
