67e9e63c93ad2f7ea2f9369a619f08698e85e70f
ppenczek <ppenczek>
2010-07-18 16:01:40 +0000

    *** empty log message ***

--- sparx/libpy/utilities.py
+++ sparx/libpy/utilities.py
@@ -2028,32 +2028,17 @@ def reduce_EMData_to_root(data, myid, main_node = 0, comm = -1):
 		if myid == main_node:
 			array1d[block_begin:block_end] = tmpsum[0:block_size]
 
-'''
-def bcast_array_to_all(data, myid, main_node = 0, comm = -1):
-	from numpy import array, shape, reshape
-	from mpi import mpi_bcast, MPI_FLOAT, MPI_COMM_WORLD
-
-	if comm == -1: comm = MPI_COMM_WORLD
-	n = shape(data)
-	ntot = 1
-	for i in n: ntot *= i
-	tmp = mpi_bcast(data, ntot, MPI_FLOAT, main_node, comm)
-	if myid != main_node:
-		data1d = reshape(data, (ntot, ))
-		data1d[0:ntot] = tmp[0:ntot]
-'''
-
-def bcast_EMData_to_all(tavg, myid, main_node = 0, comm = -1):
+def bcast_EMData_to_all(tavg, myid, source_node = 0, comm = -1):
 	from numpy import array, shape, reshape
 	from mpi   import mpi_bcast, MPI_FLOAT, MPI_COMM_WORLD
-	
+
 	if comm == -1: comm = MPI_COMM_WORLD
 	tavg_data = EMNumPy.em2numpy(tavg)
 	n = shape(tavg_data)
 	ntot = 1
 	for i in n: ntot *= i
-	tavg_tmp = mpi_bcast(tavg_data, ntot, MPI_FLOAT, main_node, comm)
-	if(myid != main_node):
+	tavg_tmp = mpi_bcast(tavg_data, ntot, MPI_FLOAT, source_node, comm)
+	if(myid != source_node):
 		tavg_data1d = reshape(tavg_data,(ntot,))
 		tavg_data1d[0:ntot] = tavg_tmp[0:ntot]
 	
