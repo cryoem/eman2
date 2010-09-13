20d0809254e97508d43ab11344eea69ee331dd47
jkovacs <jkovacs>
2010-09-13 21:37:07 +0000

    When quiet was set, centering was not being done. Moved tr0 after centering, so as not to loose its translation. Added a centering option to substract a given vector from all PDB coordinates.

--- sparx/bin/sxpdb2em.py
+++ sparx/bin/sxpdb2em.py
@@ -62,11 +62,11 @@ map to the center of the volume."""
 
 	parser = OptionParser(usage=usage,version=EMANVERSION)
 
-	parser.add_option("--apix", "-A", type="float",        help="A/voxel", default=1.0)
-	parser.add_option("--box",  "-B", type="string",       help="Box size in pixels, <xyz> or <x>,<y>,<z>")
+	parser.add_option("--apix", "-A", type="float",        help="Angstrom/voxel", default=1.0)
+	parser.add_option("--box",  "-B", type="string",       help="Box size in pixels, <xyz> or <x,y,z>")
 	parser.add_option("--het",        action="store_true", help="Include HET atoms in the map", default=False)
 	#parser.add_option("--chains",type="string",help="String list of chain identifiers to include, eg 'ABEFG'")
-	parser.add_option("--center",     type="string",       default="n", help="center: c - coordinates, a - center of gravity, default: n - no" )
+	parser.add_option("--center",     type="string",       default="n", help="center: c - coordinates; a - center of gravity; <x,y,z> - a vector (in Angstrom) to substract from all coordinates; default: n - no" )
 	parser.add_option("--O",          action="store_true", default=False, help="use O system of coordinates")
 	parser.add_option("--quiet",      action="store_true", default=False, help="Verbose is the default")
 	parser.add_option("--tr0",        type="string",       default="none", help="Filename of initial 3x4 transformation matrix")
@@ -80,12 +80,12 @@ map to the center of the volume."""
 		disable_bdb_cache()
 	chains=None
 	
+	print "************ Version of 9/13 ************"
+	
 	try : infile=open(args[0],"r")
 	except : parser.error("Cannot open input file")
 	
 	aavg=[0,0,0]	# calculate atomic center
-	amin=[1.0e20,1.0e20,1.0e20]		# coords of lower left front corner of bounding box
-	amax=[-1.0e20,-1.0e20,-1.0e20]	# coords
 	natm=0
 	atoms=[]		# we store a list of atoms to process to avoid multiple passes
 	nelec=0
@@ -130,14 +130,6 @@ map to the center of the volume."""
 			except:
 				print("Unknown atom %s ignored at %d"%(a,aseq))
 
-			# apply initial transformation:
-			if(options.tr0 != "none"):
-				atom_coords = Vec3f(x,y,z)
-				new_atom_coords = tr0*atom_coords
-				x = new_atom_coords[0]
-				y = new_atom_coords[1]
-				z = new_atom_coords[2]
-
 			atoms.append([a,x,y,z])
 
 			if(options.center == "a"):
@@ -150,37 +142,61 @@ map to the center of the volume."""
 				aavg[2] += z
 			natm += 1
 			
-			amin[0]=min(x,amin[0])
-			amin[1]=min(y,amin[1])
-			amin[2]=min(z,amin[2])
-			amax[0]=max(x,amax[0])
-			amax[1]=max(y,amax[1])
-			amax[2]=max(z,amax[2])
-
-							
 	infile.close()
 	
 	if not options.quiet:
 		print "%d atoms used with a total charge of %d e- and a mass of %d kDa"%(natm,nelec,mass/1000)
-		if(options.center == "a"):
+
+	# center PDB according to option:
+	if(options.center == "a"):
+		if not options.quiet:
 			print "center of gravity at %1.1f,%1.1f,%1.1f (center of volume at 0,0,0)"%(aavg[0]/mass,aavg[1]/mass,aavg[2]/mass)
-			for i in xrange( len(atoms) ) :
-				atoms[i][1] -= aavg[0]/mass
-				atoms[i][2] -= aavg[1]/mass
-				atoms[i][3] -= aavg[2]/mass
-			for i in xrange(3):
-				amin[i] -= aavg[i]/mass
-				amax[i] -= aavg[i]/mass
-		else:
+		for i in xrange( len(atoms) ) :
+			atoms[i][1] -= aavg[0]/mass
+			atoms[i][2] -= aavg[1]/mass
+			atoms[i][3] -= aavg[2]/mass
+	if(options.center == "c"):
+		if not options.quiet:
 			print "atomic center at %1.1f,%1.1f,%1.1f (center of volume at 0,0,0)"%(aavg[0]/natm,aavg[1]/natm,aavg[2]/natm)
-			if(options.center == "c"):
-				for i in xrange( len(atoms) ) :
-					atoms[i][1] -= aavg[0]/natm
-					atoms[i][2] -= aavg[1]/natm
-					atoms[i][3] -= aavg[2]/natm
-				for i in xrange(3):
-					amin[i] -= aavg[i]/natm
-					amax[i] -= aavg[i]/natm
+		for i in xrange( len(atoms) ) :
+			atoms[i][1] -= aavg[0]/natm
+			atoms[i][2] -= aavg[1]/natm
+			atoms[i][3] -= aavg[2]/natm
+	spl = options.center.split(',')
+	if len(spl)==3:   # substract the given vector from all coordinates
+		if not options.quiet:
+			print "vector to substract: %1.1f,%1.1f,%1.1f (center of volume at 0,0,0)"%(float(spl[0]),float(spl[1]),float(spl[2]))
+		for i in xrange( len(atoms) ) :
+			atoms[i][1] -= float(spl[0])
+			atoms[i][2] -= float(spl[1])
+			atoms[i][3] -= float(spl[2])
+
+	# apply given initial transformation (this used to be done before centering,
+	# thereby loosing the translation. This is the right place to apply tr0):
+	if(options.tr0 != "none"):
+		if not options.quiet:
+			print "Applying initial transformation to PDB coordinates... "
+		for i in xrange(len(atoms)):
+			atom_coords = Vec3f(atoms[i][1],atoms[i][2],atoms[i][3])
+			new_atom_coords = tr0*atom_coords
+			atoms[i][1] = new_atom_coords[0]
+			atoms[i][2] = new_atom_coords[1]
+			atoms[i][3] = new_atom_coords[2]
+		if not options.quiet:
+			print "done.\n"
+
+	# bounding box:
+	amin=[atoms[0][1],atoms[0][2],atoms[0][3]]
+	amax=[atoms[0][1],atoms[0][2],atoms[0][3]]
+	for i in xrange(len(atoms)):
+		amin[0]=min(atoms[i][1],amin[0])
+		amin[1]=min(atoms[i][2],amin[1])
+		amin[2]=min(atoms[i][3],amin[2])
+		amax[0]=max(atoms[i][1],amax[0])
+		amax[1]=max(atoms[i][2],amax[1])
+		amax[2]=max(atoms[i][3],amax[2])
+
+	if not options.quiet:
 		print "Bounding box: x: %7.2f - %7.2f"%(amin[0],amax[0])
 		print "              y: %7.2f - %7.2f"%(amin[1],amax[1])
 		print "              z: %7.2f - %7.2f"%(amin[2],amax[2])
