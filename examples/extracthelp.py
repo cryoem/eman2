5c0be9e2c6735a5feee327060bb3e12c94d83e72
stevel <stevel>
2010-09-22 22:43:35 +0000

    New program to help making entries in wiki

--- examples/extracthelp.py
+++ examples/extracthelp.py
@@ -5,7 +5,7 @@
 # This program will extract the command-line help options from another EMAN2 program and output
 # them in a wiki-friendly format
 
-from sys import argv
+import sys
 from optparse import OptionParser
 
 parser = OptionParser(usage="doesn't matter",version="1.0")
@@ -13,4 +13,24 @@ parser = OptionParser(usage="doesn't matter",version="1.0")
 lines=[i for i in file(sys.argv[1],"r") if "parser.add_option" in i]
 
 for i in lines: 
-
+	exec i.strip()
+	
+#print parser.__dict__.keys()
+for i in parser.option_list:
+	try: o_long=i._long_opts[0]
+	except: o_long=" "
+	
+	try: o_short=i._short_opts[0]
+	except: o_short=" "
+	
+	try: 
+		o_help=i.help
+		if o_help==None : raise Exception
+	except: o_help=" "
+	
+	try: 
+		o_type=i.type
+		if o_type==None : raise Exception
+	except: o_type="bool"
+	
+	print "||%s||%s||%s||%s||"%(o_short,o_long,o_type,o_help)
