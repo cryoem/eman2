1bd17df42c5402c722e8efe28b50600aa969a8f3
ppenczek <ppenczek>
2010-10-06 19:06:57 +0000

    *** empty log message ***

--- libEM/sparx/fourierfilter.cpp
+++ libEM/sparx/fourierfilter.cpp
@@ -181,6 +181,7 @@ EMData* Processor::EMFourierFilterFunc(EMData * fimage, Dict params, bool doInPl
 		case GAUSS_HIGH_PASS:
 		case GAUSS_INVERSE:
 			omega = params["cutoff_abs"];
+			cout << omega<<endl;
 			omega = 0.5f/omega/omega;
 			break;
 		case GAUSS_BAND_PASS:
