0437d5ef39c6c268d2cb11ab0982e33647d15219
ross <ross>
2011-01-16 19:47:09 +0000

    2D plots are cleared before new data is plotted when plot2d() is called repeatedly

--- programs/e2fhstat.py
+++ programs/e2fhstat.py
@@ -125,52 +125,58 @@ class E2ValidateMed():
 		the target probe fares relative to the random transformations. 
 		'''
 		from pylab import scatter, subplot, show, figure, colorbar
-		########  Graphing 1  - graph of 2 of the 3 scoring functiosn plotted against each other
+		########  Graphing 1  - graph of 2 of the 3 scoring functions plotted against each other
 		
 		g1 = figure(1)
-		g1 = subplot(311)
-		g1.scatter(s1,s2, marker ="o", color ="green")
-		g1.scatter(s1[:1],s2[:1],color="red")
-		g1.set_xlabel("Real space correlation")
-		g1.set_ylabel("Atom Inclusion Percentage")
+		g1a = subplot(311)
+		g1a.clear()
+		g1a.scatter(s1,s2, marker ="o", color ="green")
+		g1a.scatter(s1[:1],s2[:1],color="red")
+		g1a.set_xlabel("Real space correlation")
+		g1a.set_ylabel("Atom Inclusion Percentage")
 		
-		g1 = subplot(312)
-		g1.scatter(s3,s2, marker = "o", color="green")
-		g1.scatter(s3[:1],s2[:1],color="red")
-		g1.set_xlabel("Volume Overlap")
-		g1.set_ylabel("Atom Inclusion Percentage")
-		g1=subplot(313)
+		g1b = subplot(312)
+		g1b.clear()
+		g1b.scatter(s3,s2, marker = "o", color="green")
+		g1b.scatter(s3[:1],s2[:1],color="red")
+		g1b.set_xlabel("Volume Overlap")
+		g1b.set_ylabel("Atom Inclusion Percentage")
 		
-		g1.scatter(s3,s1, marker = "o", color="green")
-		g1.scatter(s3[:1],s1[:1],color="red")
-		g1.set_xlabel("Volume Overlap")
-		g1.set_ylabel("Real space correlation")
+		g1c=subplot(313)
+		g1c.clear()
+		g1c.scatter(s3,s1, marker = "o", color="green")
+		g1c.scatter(s3[:1],s1[:1],color="red")
+		g1c.set_xlabel("Volume Overlap")
+		g1c.set_ylabel("Real space correlation")
 #		show()
 		####################################################
 		
 		######### Graphing 2 - individual line grpahs for each scoring function
 		
 		g2 = figure(2)
-		g2 = subplot(311)
+		g2a = subplot(311)
+		g2a.clear()
 		blank = []
 		for i in range(0,len(s1)):
 			blank.append(0)
-		g2.scatter(s1,blank, marker ="o", color ="green")
-		g2.scatter(s1[:1],blank[:1],color="red")
-		g2.set_xlabel("Real space correlation")
-		g2.set_ylabel(" ")
+		g2a.scatter(s1,blank, marker ="o", color ="green")
+		g2a.scatter(s1[:1],blank[:1],color="red")
+		g2a.set_xlabel("Real space correlation")
+		g2a.set_ylabel(" ")
 		
-		g2 = subplot(312)
-		g2.scatter(s2,blank, marker = "o", color="green")
-		g2.scatter(s2[:1],blank[:1],color="red")
-		g2.set_xlabel("Atom Inclusion Percentage")
-		g2.set_ylabel(" ")
+		g2b = subplot(312)
+		g2b.clear()
+		g2b.scatter(s2,blank, marker = "o", color="green")
+		g2b.scatter(s2[:1],blank[:1],color="red")
+		g2b.set_xlabel("Atom Inclusion Percentage")
+		g2b.set_ylabel(" ")
 		
-		g2=subplot(313)
-		g2.scatter(s3,blank, marker = "o", color="green")
-		g2.scatter(s3[:1],blank[:1],color="red")
-		g2.set_xlabel("Volume Overlap")
-		g2.set_ylabel(" ")
+		g2c=subplot(313)
+		g2c.clear()
+		g2c.scatter(s3,blank, marker = "o", color="green")
+		g2c.scatter(s3[:1],blank[:1],color="red")
+		g2c.set_xlabel("Volume Overlap")
+		g2c.set_ylabel(" ")
 #		show()
 		
 		###################################################
@@ -178,7 +184,8 @@ class E2ValidateMed():
 		########## Graphing 3 - line graph for cumulative z-score
 		
 		g3 = figure(3)
-		g3 = subplot(111)
+		g3a = subplot(111)
+		g3a.clear()
 		
 		blank = []
 		s4 =[]
@@ -195,10 +202,10 @@ class E2ValidateMed():
 				val = i
 		valT = Transform(rotList[val].get_params("eman"))
 		
-		g3.scatter(s4,blank, marker ="o", color ="green")
-		g3.scatter(s4[:1],blank[:1],color="red")
-		g3.set_xlabel("Total z score")
-		g3.set_ylabel(" ")
+		g3a.scatter(s4,blank, marker ="o", color ="green")
+		g3a.scatter(s4[:1],blank[:1],color="red")
+		g3a.set_xlabel("Total z score")
+		g3a.set_ylabel(" ")
 #		show()
 		
 		##################################################
@@ -207,20 +214,23 @@ class E2ValidateMed():
 		######## Graphimg 4 - histogram for each scoring function
 		
 		g4 = figure(4)
-		g4 = subplot(311)
-		g4.hist(s1,50, histtype='bar')
-		g4.scatter(s1[:1],blank[:1],color="red")
-		g4.set_xlabel("Real space correlation")
+		g4a = subplot(311)
+		g4a.clear()
+		g4a.hist(s1,50, histtype='bar')
+		g4a.scatter(s1[:1],blank[:1],color="red")
+		g4a.set_xlabel("Real space correlation")
 		
-		g4 = subplot(312)
-		g4.hist(s2,50, histtype='bar')
-		g4.scatter(s2[:1], blank[:1],color="red")
-		g4.set_xlabel("Atom Inclusion Percentage")
+		g4b = subplot(312)
+		g4b.clear()
+		g4b.hist(s2,50, histtype='bar')
+		g4b.scatter(s2[:1], blank[:1],color="red")
+		g4b.set_xlabel("Atom Inclusion Percentage")
 		
-		g4 = subplot(313)
-		g4.hist(s3,50, histtype='bar')
-		g4.scatter(s3[:1], blank[:1],color="red")
-		g4.set_xlabel("Volume inclusion")
+		g4c = subplot(313)
+		g4c.clear()
+		g4c.hist(s3,50, histtype='bar')
+		g4c.scatter(s3[:1], blank[:1],color="red")
+		g4c.set_xlabel("Volume inclusion")
 		show()
 		#################################################
 
