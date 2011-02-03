5f4c70745a07056a230fa9e568d1ffc0654c1881
john <john>
2011-02-03 15:48:06 +0000

    Bug fix for CUDA envvar

--- libEM/emdata_cuda.cpp
+++ libEM/emdata_cuda.cpp
@@ -50,8 +50,7 @@ int EMData::fudgemem = 1.024E7; //let's leave 10 MB of 'fudge' memory on the dev
 int EMData::mempoolused = -1;
 int EMData::mempoolarraysize = 0;
 bool EMData::usemempoolswitch = false;
-//bool EMData::usecuda = atoi(getenv("USECUDA"));
-bool EMData::usecuda = 1;
+bool EMData::usecuda = (getenv("USECUDA") != NULL) ? 1 : 0;
 float* EMData::mempool[] = {0};
 
 bool EMData::copy_to_cuda() const
