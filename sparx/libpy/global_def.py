61e930b22046276f1cae928dfd7348ecd3751a41
gtang <gtang>
2011-04-19 21:19:00 +0000

    *** empty log message ***

--- sparx/libpy/global_def.py
+++ sparx/libpy/global_def.py
@@ -47,6 +47,7 @@ Eulerian_Angles = "SPIDER"
 # We read the global seed here. If the user wish to repeat the random results twice,
 # he/she should first set the rand_seed to a fixed number and then run the program twice.
 from   EMAN2   import Util
+from   e2version import CVSDATESTAMP
 from   EMAN2db import *
 import EMAN2db
 from random  import seed
