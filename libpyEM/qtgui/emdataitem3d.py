9a1a8d1ab6d08a7af5160bd6487a7f3643e5913c
ross <ross>
2011-08-04 18:10:59 +0000

    changed the way updates happen in data and isosurface nodes when new EMData is set for the data node

--- libpyEM/qtgui/emdataitem3d.py
+++ libpyEM/qtgui/emdataitem3d.py
@@ -29,6 +29,9 @@ class EMDataItem3D(EMItem3D):
 			self.item_inspector = EMDataItem3DInspector("DATA", self)
 		return self.item_inspector
 	
+	def getData(self):
+		return self.data
+	
 	def setData(self, data):
 		if isinstance(data, EMData):
 			self.data = data
@@ -38,7 +41,10 @@ class EMDataItem3D(EMItem3D):
 				self.path = None 
 		else:
 			self.data = EMData(str(data))
-			self.path = str(data)		
+			self.path = str(data)
+		
+		for child in self.getChildren():
+			child.dataChanged()
 	
 class EMDataItem3DInspector(EMItem3DInspector):
 	def __init__(self, name, item3d):
@@ -47,12 +53,13 @@ class EMDataItem3DInspector(EMItem3DInspector):
 	def updateItemControls(self):
 		super(EMDataItem3DInspector, self).updateItemControls()
 		# Anything that needs to be updated when the scene is rendered goes here.....
+		self.file_path_label.setText(self.item3d().path)
 		
 	def addControls(self, gridbox):
 		super(EMDataItem3DInspector, self).addControls(gridbox)
 		hblbrowse = QtGui.QHBoxLayout()
-		self.file_line_edit = QtGui.QLineEdit()
-		hblbrowse.addWidget(self.file_line_edit)
+		self.file_path_label = QtGui.QLabel()
+		hblbrowse.addWidget(self.file_path_label)
 		self.file_browse_button = QtGui.QPushButton("Browse")
 		hblbrowse.addWidget(self.file_browse_button)
 		gridbox.addLayout(hblbrowse, 3, 0, 1, 1)
@@ -65,25 +72,32 @@ class EMDataItem3DInspector(EMItem3DInspector):
 	def onFileBrowse(self):
 		#TODO: replace this with an EMAN2 browser window once we re-write it
 		file_path = QtGui.QFileDialog.getOpenFileName(self, "Open 3D Volume Map")
-		self.file_line_edit.setText(file_path)
+		self.file_path_label.setText(file_path)
 		self.item3d().setData(file_path)
-		for child in self.item3d().getChildren():
-			child.getItemInspector().dataChanged()
 		self.inspector.updateSceneGraph()
 
 class EMIsosurfaceInspector(EMInspectorControlShape):
 	def __init__(self, name, item3d):
 		EMItem3DInspector.__init__(self, name, item3d, numgridcols=2)	# for the iso inspector we need two grid cols for extra space....
+		self.updateItemControls()
 		
 		QtCore.QObject.connect(self.thr, QtCore.SIGNAL("valueChanged"), self.onThresholdSlider)
 		self.cullbackface.toggled.connect(self.onCullFaces)
 		self.wireframe.toggled.connect(self.onWireframe)
 		self.sampling_spinbox.valueChanged[int].connect(self.onSampling)
-		self.dataChanged()
 	
 	def updateItemControls(self):
 		super(EMIsosurfaceInspector, self).updateItemControls()
 		# Anything that needs to be updated when the scene is rendered goes here.....
+		
+		minden = self.item3d().minden
+		maxden = self.item3d().maxden
+		
+		self.thr.setRange(minden, maxden)
+		self.thr.setValue(self.item3d().isothr, True)
+		
+		self.histogram_widget.set_data(self.item3d().histogram_data,minden,maxden)
+		self.setSamplingRange(self.item3d().isorender.get_sampling_range())
 		
 	def addControls(self, gridbox):
 		super(EMIsosurfaceInspector, self).addControls(gridbox)
@@ -121,25 +135,6 @@ class EMIsosurfaceInspector(EMInspectorControlShape):
 		# Set to default, but run only once and not in each base class
 		if type(self) == EMIsosurfaceInspector: self.updateItemControls()
 	
-	def dataChanged(self):
-		data = self.item3d().parent.data
-		
-		minden=data.get_attr("minimum")
-		maxden=data.get_attr("maximum")
-		mean=data.get_attr("mean")
-		sigma=data.get_attr("sigma")
-		iso_threshold = mean+3.0*sigma
-		
-		self.thr.setRange(minden,maxden)
-		self.thr.setValue(iso_threshold, True)
-		
-		histogram_data = data.calc_hist(256,minden,maxden)
-		self.histogram_widget.set_data(histogram_data,minden,maxden) 
-		
-		self.item3d().force_update = True
-		self.item3d().isorender = MarchingCubes(data)
-		self.setSamplingRange(self.item3d().isorender.get_sampling_range())
-
 	def onCullFaces(self):
 		self.item3d().cullbackfaces = self.cullbackface.isChecked()
 		self.inspector.updateSceneGraph()
@@ -167,7 +162,7 @@ class EMIsosurface(EMItem3D):
 	def __init__(self, parent=None, children = set(), transform = None):
 		EMItem3D.__init__(self, parent, children, transform)
 		
-		self.isothr=0.5
+		self.isothr = None #Will be set in self.dataChanged()
 		self.isodl = 0
 		self.smpval=-1
 		self.cube = False
@@ -189,6 +184,8 @@ class EMIsosurface(EMItem3D):
 		self.ambient = self.colors[self.isocolor]["ambient"]		
 		self.shininess = self.colors[self.isocolor]["shininess"]
 		
+		self.dataChanged()
+		
 	def setParent(self, parent):
 		"""
 		For use in session restore
@@ -201,6 +198,28 @@ class EMIsosurface(EMItem3D):
 		
 	def getEvalString(self):
 		return "EMIsosurface()"
+	
+	def dataChanged(self):
+		data = self.getParent().getData()
+		
+		if self.isothr: #there was data previously
+			normalized_threshold = (self.isothr - self.mean)/self.sigma
+		else:
+			normalized_threshold = 3.0
+		
+		self.minden = data.get_attr("minimum")
+		self.maxden = data.get_attr("maximum")
+		self.mean   = data.get_attr("mean")
+		self.sigma  = data.get_attr("sigma")
+		
+		self.isothr = self.mean+normalized_threshold*self.sigma
+		self.histogram_data = data.calc_hist(256,self.minden, self.maxden)
+		
+		self.force_update = True
+		self.isorender = MarchingCubes(data)
+		
+		self.getItemInspector().updateItemControls()
+	
 		
 	# I have added these methods so the inspector can set the color John Flanagan
 	def setAmbientColor(self, red, green, blue, alpha=1.0):
