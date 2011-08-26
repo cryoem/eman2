596823f5c9aa029cec2a48d7dd2b4484507e5b40
gtang <gtang>
2011-08-26 22:32:07 +0000

    *** empty log message ***

--- rt/demo/scene3d_test.py
+++ rt/demo/scene3d_test.py
@@ -10,48 +10,45 @@ class GLdemo(QtGui.QWidget):
 	def __init__(self):
 		QtGui.QWidget.__init__(self)
 		self.widget = EMScene3D()
-		#self.widget.camera.useprespective(50, 0.5)
-#		self.cube1 = glCube(50.0)
+
 		self.cube1 = EMCube(50.0)
-		self.widget.addChild(self.cube1)    # Something to Render something..... an EMItem3D
-		#self.widget.activatenode(cube1)
-#		self.cube2 = glCube(50.0)
+		self.widget.insertNewNode("Cube1", self.cube1)    # Something to Render something..... an EMItem3D
+
 		self.cube2 = EMCube(50.0)
-		self.widget.addChild(self.cube2)
-		#self.widget.activatenode(cube2)
+		self.widget.insertNewNode("Cube2", self.cube2)
 		
 		self.sphere = EMSphere(50.0)
-		self.widget.addChild(self.sphere)
+		self.widget.insertNewNode("Sphere", self.sphere)
 		
 		self.cylinder = EMCylinder(50, 300)
-		self.widget.addChild(self.cylinder)
+		self.widget.insertNewNode("Cylinder", self.cylinder)
 		
 		self.cone = EMCone(50, 300)
-		self.widget.addChild(self.cone)
+		self.widget.insertNewNode("Cone", self.cone)
 		
 		self.line = EMLine(10,10,10,200,200,200, 16)
-		self.widget.addChild(self.line)
+		self.widget.insertNewNode("Line", self.line)
 		
 		self.text1 = EM3DText('3D text in extrude', 75)
-		self.widget.addChild(self.text1)
+		self.widget.insertNewNode("Text1", self.text1)
 		
 		self.text2 = EM3DText('3D text in texture', 75, FTGLFontMode.TEXTURE)
-		self.widget.addChild(self.text2)
+		self.widget.insertNewNode("Text2", self.text2)
 		
 #		self.text3 = EM3DText('3D text in bitmap', 75, FTGLFontMode.BITMAP)
 #		self.widget.addChild(self.text3)
 		
 		self.text4 = EM3DText('3D text in polygon', 75, FTGLFontMode.POLYGON)
-		self.widget.addChild(self.text4)
+		self.widget.insertNewNode("Text3", self.text4)
 		
 		self.text5 = EM3DText('3D text in outline', 75, FTGLFontMode.OUTLINE)
-		self.widget.addChild(self.text5)
+		self.widget.insertNewNode("Text4", self.text5)
 		
 		self.text6 = EM3DText('3D text in pixmap', 75, FTGLFontMode.PIXMAP)
-		self.widget.addChild(self.text6)
+		self.widget.insertNewNode("Text5", self.text6)
 		
 		#Show inspector
-		self.widget.showInspector()
+		#self.widget.showInspector()
 		
 		# QT stuff to display the widget
 		vbox = QtGui.QVBoxLayout()
