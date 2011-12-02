e8f975ffb9db2acc453095d6369354fb2652efaa
john <john>
2011-12-02 22:37:49 +0000

    GUI updates

---
+++ libpyEM/qtgui/empmtabwidgets.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python
+#
+# Author: Steven Ludtke, 04/10/2003 (sludtke@bcm.edu)
+# Copyright (c) 2000-2006 Baylor College of Medicine
+#
+# This software is issued under a joint BSD/GNU license. You may use the
+# source code in this file under either license. However, note that the
+# complete EMAN2 and SPARX software packages have some GPL dependencies,
+# so you are responsible for compliance with the licenses of these packages
+# if you opt to use BSD licensing. The warranty disclaimer below holds
+# in either instance.
+#
+# This complete copyright notice must be included in any revised version of the
+# source code. Additional authorship citations may be added, but existing
+# author citations must be preserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston MA 02111-1307 USA
+#
+#
+
+from EMAN2 import *
+import os
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import Qt
+from embrowser import EMBrowserWidget, EMFileItemModel, EMDirEntry, nonone
+
+class EMBoxesTable(EMBrowserWidget):
+	""" Widget to display junk from e2boxercache """
+	def __init__(self, withmodal=False, multiselect=False):
+		EMBrowserWidget.__init__(self, withmodal=withmodal, multiselect=multiselect, startpath="./micrographs")
+	
+	def setPath(self,path,silent=False):
+		super(EMBoxesTable, self).setPath(path,silent=False,inimodel=EMBoxesModel)
+
+
+class EMBoxesModel(EMFileItemModel):
+	""" Item model for the raw data """
+	
+	headers=("Raw Data Files","Stored Boxes", "Quality")
+	
+	def __init__(self,startpath=None):
+		EMFileItemModel.__init__(self, startpath=startpath, direntryclass=EMBoxesEntry)
+		
+	def columnCount(self,parent):
+		"Always 3 columns"
+		#print "EMFileItemModel.columnCount()=6"
+		return 3
+		
+	def data(self,index,role):
+		"Returns the data for a specific location as a string"
+		
+		if not index.isValid() : return None
+		if role!=Qt.DisplayRole : return None
+		
+		data=index.internalPointer()
+		if data==None : 
+			print "Error with index ",index.row(),index.column()
+			return "XXX"
+		#if index.column()==0 : print "EMFileItemModel.data(%d %d %s)=%s"%(index.row(),index.column(),index.parent(),str(data.__dict__))
+
+		col=index.column()
+		if col==0 : 
+			if data.isbdb : return "bdb:"+data.name
+			return nonone(data.name)
+		elif col==1 :
+			if data.boxcount==0 : return "-"
+			return nonone(data.boxcount)
+		elif col==2 :
+			if data.quality==0 : return "-"
+			return nonone(data.quality)
+
+	def headerData(self,sec,orient,role):
+		if orient==Qt.Horizontal:
+			if role==Qt.DisplayRole :
+				return EMBoxesModel.headers[sec]
+			elif role==Qt.ToolTipRole:
+				return None								# May fill this in later
+			
+		return None
+
+class EMBoxesEntry(EMDirEntry):
+	""" Subclassing of EMDirEntry to provide functionality"""
+	
+	col=(lambda x:x.name,lambda x:x.boxcount, lambda x:x.quality)
+	
+	def __init__(self,root,name,parent=None,hidedot=True):
+		EMDirEntry.__init__(self,root,name,parent=parent,hidedot=hidedot)
+		self.boxcount=None
+		self.quality=None
+	
+	def __del__(self):
+		db_close_dict(self.db)
+		
+	def fillDetails(self):
+		"""Fills in the expensive metadata about this entry. Returns False if no update was necessary."""
+		if self.filetype!=None : return False		# must all ready be filled in
+		
+		# get quality and box numbers
+		if db_check_dict('bdb:e2boxercache#quality'):
+			qdb = db_open_dict('bdb:e2boxercache#quality')
+			self.quality = qdb[os.path.basename(self.path())]
+			
+		pathname = os.path.basename(self.path())
+		if db_check_dict('bdb:e2boxercache#boxes'):
+			db = db_open_dict('bdb:e2boxercache#boxes')
+			bc = db[os.path.basename(self.path())]
+			if bc:
+				self.boxcount = len(bc)
+				return True
+			
+		return True
+
+#############################################################################################################################
+
+class EMRCTBoxesTable(EMBrowserWidget):
+	""" Widget to display junk from e2boxercache """
+	def __init__(self, withmodal=False, multiselect=False):
+		EMBrowserWidget.__init__(self, withmodal=withmodal, multiselect=multiselect, startpath="./micrographs")
+	
+	def setPath(self,path,silent=False):
+		super(EMRCTBoxesTable, self).setPath(path,silent=False,inimodel=EMRCTBoxesModel)
+
+
+class EMRCTBoxesModel(EMFileItemModel):
+	""" Item model for the raw data """
+	
+	headers=("Raw Data Files","Stored Boxes", "Quality")
+	
+	def __init__(self,startpath=None):
+		EMFileItemModel.__init__(self, startpath=startpath, direntryclass=EMRCTBoxesEntry)
+		
+	def columnCount(self,parent):
+		"Always 3 columns"
+		#print "EMFileItemModel.columnCount()=6"
+		return 3
+		
+	def data(self,index,role):
+		"Returns the data for a specific location as a string"
+		
+		if not index.isValid() : return None
+		if role!=Qt.DisplayRole : return None
+		
+		data=index.internalPointer()
+		if data==None : 
+			print "Error with index ",index.row(),index.column()
+			return "XXX"
+		#if index.column()==0 : print "EMFileItemModel.data(%d %d %s)=%s"%(index.row(),index.column(),index.parent(),str(data.__dict__))
+
+		col=index.column()
+		if col==0 : 
+			if data.isbdb : return "bdb:"+data.name
+			return nonone(data.name)
+		elif col==1 :
+			if data.boxcount==0 : return "-"
+			return nonone(data.boxcount)
+		elif col==2 :
+			if data.quality==0 : return "-"
+			return nonone(data.quality)
+
+	def headerData(self,sec,orient,role):
+		if orient==Qt.Horizontal:
+			if role==Qt.DisplayRole :
+				return EMRCTBoxesModel.headers[sec]
+			elif role==Qt.ToolTipRole:
+				return None								# May fill this in later
+			
+		return None
+
+class EMRCTBoxesEntry(EMDirEntry):
+	""" Subclassing of EMDirEntry to provide functionality"""
+	
+	col=(lambda x:x.name,lambda x:x.boxcount, lambda x:x.quality)
+	
+	def __init__(self,root,name,parent=None,hidedot=True):
+		EMDirEntry.__init__(self,root,name,parent=parent,hidedot=hidedot)
+		self.boxcount=None
+		self.quality=None
+	
+	def __del__(self):
+		db_close_dict(self.db)
+		
+	def fillDetails(self):
+		"""Fills in the expensive metadata about this entry. Returns False if no update was necessary."""
+		if self.filetype!=None : return False		# must all ready be filled in
+		
+		# get quality and box numbers
+		if db_check_dict('bdb:e2boxercache#quality'):
+			qdb = db_open_dict('bdb:e2boxercache#quality')
+			self.quality = qdb[os.path.basename(self.path())]
+			
+		pathname = os.path.basename(self.path())
+		if db_check_dict('bdb:e2boxercache#boxestilted'):
+			db = db_open_dict('bdb:e2boxercache#boxestilted')
+			bc = db[os.path.basename(self.path())]
+			if bc:
+				self.boxcount = len(bc)
+				return True
+			
+		if db_check_dict('bdb:e2boxercache#boxesuntilted'):
+			db = db_open_dict('bdb:e2boxercache#boxesuntilted')
+			bc = db[os.path.basename(self.path())]
+			if bc:
+				self.boxcount = len(bc)
+				return True
+			
+		return True
+		
+#################################################################################################################################
+
+class EMRawDataTable(EMBrowserWidget):
+	""" Widget to display Raw Data """
+	def __init__(self, withmodal=False, multiselect=False):
+		EMBrowserWidget.__init__(self, withmodal=withmodal, multiselect=multiselect, startpath="./micrographs")
+	
+	def setPath(self,path,silent=False):
+		super(EMBoxesTable, self).setPath(path,silent=False,inimodel=EMRawDataModel)
+
+			
+class EMRawDataModel(EMFileItemModel):
+	""" Item model for the raw data """
+	
+	headers=("Raw Data Files","Dimensions")
+	
+	def __init__(self,startpath=None):
+		EMFileItemModel.__init__(self, startpath=startpath, direntryclass=EMRawDataEntry)
+		
+	def columnCount(self,parent):
+		"Always 2 columns"
+		#print "EMFileItemModel.columnCount()=6"
+		return 2
+		
+	def data(self,index,role):
+		"Returns the data for a specific location as a string"
+		
+		if not index.isValid() : return None
+		if role!=Qt.DisplayRole : return None
+		
+		data=index.internalPointer()
+		if data==None : 
+			print "Error with index ",index.row(),index.column()
+			return "XXX"
+		#if index.column()==0 : print "EMFileItemModel.data(%d %d %s)=%s"%(index.row(),index.column(),index.parent(),str(data.__dict__))
+
+		col=index.column()
+		if col==0 : 
+			if data.isbdb : return "bdb:"+data.name
+			return nonone(data.name)
+		elif col==1 :
+			if data.dim==0 : return "-"
+			return nonone(data.dim)
+
+	def headerData(self,sec,orient,role):
+		if orient==Qt.Horizontal:
+			if role==Qt.DisplayRole :
+				return EMRawDataModel.headers[sec]
+			elif role==Qt.ToolTipRole:
+				return None								# May fill this in later
+			
+		return None
+		
+class EMRawDataEntry(EMDirEntry):
+	""" Subclassing of EMDirEntry to provide functionality"""
+	
+	col=(lambda x:x.name,lambda x:x.dim)
+	
+	def __init__(self,root,name,parent=None,hidedot=True):
+		EMDirEntry.__init__(self,root,name,parent=parent,hidedot=hidedot)
+	
+	def __del__(self):
+		db_close_dict(self.db)
+		
+	def fillDetails(self):
+		# Maybe add code to cache results.....
+		super(EMRawDataEntry, self).fillDetails()
+		
+		
