35b1130f6afb0719a13b5750d4b742c9e2bb51c3
fjia <fjia>
2011-12-13 16:20:37 +0000

    create and fill database for e2boxer line in run through example

---
+++ sparx/demo/filldb_demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from EMAN2    import *
+from sparx    import *
+from random   import random, seed, randint
+from sys      import argv, exit
+from optparse import OptionParser
+from EMAN2db import db_check_dict
+
+dbkey  = sys.argv[1]
+print dbkey
+gbdbname = 'bdb:e2boxercache#gauss_box_DB'
+
+gbdb = db_open_dict(gbdbname)
+gbdb[dbkey] = {'gauss_width':1.0,'pixel_input':5.2,'pixel_output':5.2,'thr_low':1.0,'thr_hi':30.0,"invert_contrast":False,"use_variance":True,"boxsize":64,"ctf_cs":2.0,"ctf_fstart":0.02,"ctf_fstop":0.5,"ctf_ampcont":10,"ctf_volt":120,"ctf_window":512,"ctf_edge":0,"ctf_overlap":50}
