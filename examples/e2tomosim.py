50a08652493d36ac50842e559ebcc63cf1b98672
jgalaz <jgalaz>
2012-02-21 12:06:01 +0000

    Program pretty much finished (re-written). Minor issues/options to add.

--- examples/e2tomosim.py
+++ examples/e2tomosim.py
@@ -54,10 +54,9 @@ def main():
 							If the input file is PDB or MRC, a version of the supplied model will be written out in HDF format.
 							If the input file is a stack, simulatd subvolumes will be generated from each model in the stack and written to different output stacks.
 							For example, if the input file contains models A and B, two output stacks with simulated subvolumes will be generated.""", default=None)
-	
-	#parser.add_option("--output", type="string", help="The name of the simulated stack. MUST be since volume stack support is required.", default=None)
-			
-	parser.add_option("--filter",type="string",help="A processor (as in e2proc3d.py) to be applied to each volume prior to alignment. Not applied to aligned particles before averaging.",default=None)
+				
+	parser.add_option("--filter",type="string",help="""A filter (as in a processor from e2proc3d.py) apply to the model before generating simulated particles from it.
+							Type 'e2help.py processors' at the command line and find the options availbale from the processors list)""",default=None)
 	
 	parser.add_option("--shrink", type="int",default=1,help="Optionally shrink the input volume before the simulation if you want binned/down-sampled subtomograms.")
 	parser.add_option("--verbose", "-v", dest="verbose", action="store", metavar="n",type="int", default=0, help="verbose level [0-9], higner number means higher level of verboseness")
@@ -73,25 +72,46 @@ def main():
 	parser.add_option("--transrange", type="int",default=4,help="""Maximum number of pixels to randomly translate each subtomogram in all X, Y and Z. 
 									The random translation will be picked between -transrage and +transrange; --txrange, --tyrange and --tzrange overwrite --transrange for each specified direction.""")
 	
-	parser.add_option("--tiltstep", type="int",default=10,help="Degrees between each image in the simulated tilt series for each subtomogram.")
-	parser.add_option("--tiltrange", type="int",default=10,help="""Maximum angular value at which the highest tilt picture will be simulated. Projections will be simulated from -tiltrange to +titlrange. 
+	parser.add_option("--tiltstep", type="int",default=5,help="Degrees between each image in the simulated tilt series for each subtomogram.")
+	parser.add_option("--tiltrange", type="int",default=60,help="""Maximum angular value at which the highest tilt picture will be simulated. Projections will be simulated from -tiltrange to +titlrange. 
 									For example, if simulating a tilt series collected from -60 to 60 degrees, enter a --tiltrange value of 60. 
 									Note that this parameter will determine the size of the missing wedge.""")
-	parser.add_option("--nptcls", type="int",default=10,help="Number of simulated subtomograms tu generate per referece.")
-	
+	parser.add_option("--defocus", type="int",default=3,help="Intended defocus at the tilt axis (in microns) for the simulated tilt series.")
+	parser.add_option("--voltage", type="int",default=200,help="Voltage of the microscope, used to simulate the ctf added to the subtomograms.")
+	parser.add_option("--cs", type="int",default=2,help="Cs of the microscope, used to simulate the ctf added to the subtomograms.")
+
+	parser.add_option("--gridholesize", type="int",default=2,help="""Size of the carbon hole for the simulated grid (this will determine shifts in defocus for each particle at 
+									each tilt step, depending on the position of the particle respect to the tilt axis, which is assigned randomly.""")
+	parser.add_option("--saverandstack", action="store_true",default=False,help="Save the stack of randomly oriented particles, before subtomogram simulation (before the missing wedge and noise are added).")
+	parser.add_option("--saveprjs", action="store_true",default=False,help="Save the projections (the 'tilt series') for each simulated subtomogram.")
+
+	parser.add_option("--reconstructor", type="string",default="fourier",help="""The reconstructor to use to reconstruct the tilt series into a tomogram. Type 'e2help.py reconstructors' at the command line
+											to see all options and parameters available.""")
+	parser.add_option("--pad", action="store_true",default=False,help="""If on, it will double the box size of the model for 3D reconstruction purposes, but the final
+									simulated tilt series and subtomograms will be clipped back to the original unpadded box size.""")								
+	parser.add_option("--noiseproc",type="string",help="A noise processor to be applied to the individual projections of the simulated tilt series",default=None)
+	parser.add_option("--snr",type="int",help="Weighing factor for gaussian noise added to the image",default=10)
+
 	(options, args) = parser.parse_args()	
 	
 	if options.filter:
 		options.filter = parsemodopt(options.filter)
+		
+	if options.noiseproc:
+		options.noiseproc= parsemodopt(options.noiseproc)
+		
+	if options.reconstructor:
+		options.reconstructor= parsemodopt(options.reconstructor)
 	
-	if options.shrink:
-		options.shrink= parsemodopt(options.shrink)
+	if '.pdb' in options.input:
+		pdbmodel = options.input
+		mrcmodel = pdbmodel.replace('.pdb','.mrc').split('/')[-1]
+		os.system('e2pdb2mrc.py ' + pdbmodel + ' ' + mrcmodel)
+		options.input = mrcmodel
 	
-	if '.pdb' in options.input or '.mrc' in options.input:
-		wrongmodel = options.input
-		hdfmodel = wrongmodel.replace('.pdb','.hdf')
-		hdfmodel = wrongmodel.replace('.mrc','.hdf')
-		os.system('e2pdb2mrc.py ' + pdbmodel + ' ' + hdfmodel)
+	if '.mrc' in options.input:
+		hdfmodel = options.input.replace('.mrc','.hdf').split('/')[-1]
+		os.system('e2proc3d.py ' + options.input + ' ' + hdfmodel)
 		options.input = hdfmodel
 	
 	nrefs = EMUtil.get_image_count(options.input)
@@ -105,29 +125,38 @@ def main():
 			tag = str(i).zfill(len(str(nrefs)))
 	
 		model = EMData(options.input,i)
-		if model['nx'] != model['ny'] or model['nx'] != model['nz'] or model['ny'] != model['nz']:
 		
+		newsize = model['nx']
+		oldx = model['nx']
+	
+		if model['nx'] != model['ny'] or model['nx'] != model['nz'] or model['ny'] != model['nz']:
 			newsize = max(model['nx'], model['ny'], model['nz'])
+			
+		if options.shrink != None and options.shrink > 1:
+			newsize = newsize/options.shrink	
 			if newsize % 2:
 				newsize += 1
-						
-			os.system('e2proc3d.py ' + options.input + ' ' + options.input + ' --clip=' + str(mewsize) + ' --first=' + str(i) + ' --last=' + str(i))	
+	
+			os.system('e2proc3d.py ' + options.input + ' ' + options.input + ' --process=math.meanshrink:n=' + str(options.shrink))
+	
+		if options.pad:
+			newsize *= 3
+			print "I am padding the model. The original size in X was", oldx				
+			print "Whereas the new one (thrice as much as the largest dimension) is", newsize
+							
+		if newsize != oldx:
+			os.system('e2proc3d.py ' + options.input + ' ' + options.input + ' --clip=' + str(newsize) + ' --first=' + str(i) + ' --last=' + str(i))	
 	
 		model = EMData(options.input,i)
 
 		if options.filter != None:
 			model.process_inplace(options.filter[0],options.filter[1])
 		
-		if options.shrink != None and options.shrink > 1 :
-			model = model.process("math.meanshrink",{"n":options["shrink"]})
-		else:
-			model = model
-		
 		model.process_inplace('normalize')
 			
 		randptcls = randomizer(options, model, tag)
 		
-		subtomosim(options,randptcls)
+		subtomosim(options,randptcls, tag)
 					
 	return()
 	
@@ -149,33 +178,32 @@ def angdist(t1,t2):
 
 '''
 ====================
-RANDOMIZER - Takes a file in .hdf format and generates a set of particles randomly rotated and translated.
+RANDOMIZER - Takes a file in .hdf format and generates a set of n particles randomly rotated and translated, where n is
+the size of the set and is defined by the user
 ====================
 '''
 def randomizer(options, model, tag):
 	
-	stackname = options.input.replace('.hdf','_st' + tag + '_n' + str(options.n).zfill(len(str(options.n))) + '.hdf')
-
 	print "I am inside the RANDOMIZER"
 	
-	if parameters['verbose']:
-		print "You have requested to generate %d particles with random orientations and translations" %(options.n)
+	if options.verbose:
+		print "You have requested to generate %d particles with random orientations and translations" %(options.nptcls)
 	
 	randptcls = []
-	for i in range(options.n):
-		if parameters['verbose']:
+	for i in range(options.nptcls):
+		if options.verbose:
 			print "I will generate particle #", i
 		
 		b = model.copy()
-		b['origin_x'] = 0							#Make sure the origin is set to zero, to avoid display issues with Chimera
+		b['origin_x'] = 0									#Make sure the origin is set to zero, to avoid display issues with Chimera
 		b['origin_y'] = 0
 		b['origin_z'] = 0
 
-		rand_orient = OrientGens.get("rand",{"n":1, "phitoo":1})		#Generate a random orientation (randomizes all 3 euler angles)
+		rand_orient = OrientGens.get("rand",{"n":1, "phitoo":1})				#Generate a random orientation (randomizes all 3 euler angles)
 		c1_sym = Symmetries.get("c1")
 		random_transform = rand_orient.gen_orientations(c1_sym)[0]
 
-		randtx = random.randrange(-1 * options.transrange, options.transrange)	#Generate random translations
+		randtx = random.randrange(-1 * options.transrange, options.transrange)			#Generate random translations
 		randty = random.randrange(-1 * options.transrange, options.transrange)
 		randtz = random.randrange(-1 * options.transrange, options.transrange)
 		
@@ -191,14 +219,16 @@ def randomizer(options, model, tag):
 		b.transform(random_transform)		
 		
 		b['spt_randT'] = random_transform
-		b['xform.align3d'] = Transform()					#This parameter should be set to the identity transform since it can be used later to determine whether
-											#alignment programs can "undo" the random rotation in spt_randT accurately or not
-		#b.write_image(stackname,i)
+		b['xform.align3d'] = Transform()							#This parameter should be set to the identity transform since it can be used later to determine whether
+													#alignment programs can "undo" the random rotation in spt_randT accurately or not
+		if options.saverandstack:	
+			stackname = options.input.replace('.hdf','_randst' + tag + '_n' + str(options.nptcls).zfill(len(str(options.nptcls))) + '.hdf')
+			b.write_image(stackname.split('/')[-1],i)
 		
 		randptcls.append(b)
 		
-		if parameters['verbose']:
-			print "The random transform applied to it was", rand 
+		if options.verbose:
+			print "The random transform applied to it was", random_transform
 	
 	return(randptcls)
 	
@@ -206,64 +236,92 @@ def randomizer(options, model, tag):
 ====================
 SUBTOMOSIM takes a set of particles in .hdf format and generates a simulated sub-tomogram for each, using user-defined parameters for tomographic simulation.
 The function generates projections for each particle with a user-defined tilt step and missing wedge size (or data collection range),
-adds noise and ctf to each projection, and recounstructs a new 3D volume from the simulated tilt series.
+adds noise and ctf to each projection, randomizes the position of each particle "in the ice" and changes the defocus for each picture in the tilt series accordingly,
+and recounstructs a new 3D volume from the simulated tilt series.
 ====================
 '''	
-def subtomosim(options,ptcls):
+def subtomosim(options,ptcls,tag):
+	
+	stackname = options.input.replace('.hdf','_randst' + tag + '_n' + str(options.nptcls).zfill(len(str(options.nptcls))) + '.hdf').split('/')[-1]
 		
 	lower_bound = -1 * options.tiltrange
 	upper_bound = options.tiltrange
 	
 	nslices = int(round((upper_bound - lower_bound)/ options.tiltstep))
 	
-	if parameters['verbose']:
+	if options.verbose:
 		print "There are these many particles in the set", len(ptcls)
 		print "And these many slices to simulate each subtomogram", nslices
 	
+	outname = stackname.replace('.hdf','_subtomos.hdf')
+	
 	for i in range(len(ptcls)):
-		if parameters['verbose']:
+		if options.verbose:
 			print "Projecting and adding noise to particle #", i
 
 		apix = ptcls[i]['apix_x']
 		
-		px = random.uniform(-1*parameters['hole_diameter']/2, parameters['hole_diameter']/2)		#random distance of the particle center from the tilt axis
+		px = random.uniform(-1* options.gridholesize/2, options.gridholesize/2)			#random distance of the particle's center from the tilt axis
 			
 		alt = lower_bound
 		raw_projections = []
 		ctfed_projections = []
-		for j in range(slices):
-			t = Transform({'type':'eman','az':0,'alt':alt,'phi':0})
+		for j in range(nslices):
+			t = Transform({'type':'eman','az':0,'alt':alt,'phi':0})				#Generate the projection orientation for each picture in the tilt series
 			
-			dz = -1 * px * numpy.sin(alt)			#For particles left of the tilt axis, px is negative. With negative alt [left end of the ice down, right end up], dz should be negative.
-			defocus = parameters['defocus'] + dz
+			dz = -1 * px * numpy.sin(alt)							#Calculate the defocus shift per picture, per particle, depending on the 
+													#particle's position relative to the tilt axis. For particles left of the tilt axis,
+													#px is negative. With negative alt [left end of the ice down, right end up], 
+													#dz should be negative.
+			defocus = options.defocus + dz
 						
-			prj = particles[i].project("standard",t)
+			prj = ptcls[i].project("standard",t)
 			prj.set_attr('xform.projection',t)
-			prj.write_image('prj_' + parameters['ID'] + '.hdf',j)		#Write projections stack
+			
+			if options.saveprjs:
+				prj.write_image( stackname.replace('.hdf', '_prjs' + '_ptcl' + str(i).zfill(len(str(nslices))) + '.hdf') , j)					#Write projections stack for particle i
 			raw_projections.append(prj)
 			
 			ctf = EMAN2Ctf()
-			ctf.from_dict({'defocus':defocus,'bfactor':100,'ampcont':0.05,'apix':apix,'voltage':parameters['voltage'],'cs':parameters['cs']})
+			ctf.from_dict({'defocus':options.defocus,'bfactor':100,'ampcont':0.05,'apix':apix,'voltage':options.voltage,'cs':options.cs})
 			
 			prj_fft = prj.do_fft()
 			prj_ctf = prj_fft.copy()
 			
 			ctf.compute_2d_complex(prj_ctf,Ctf.CtfType.CTF_AMP)
-			prj_ctf.mult(-1)			#Reverse the contrast, as in "authentic" cryoEM data		
+			prj_ctf.mult(-1)								#Reverse the contrast, as in "authentic" cryoEM data		
 			
 			prj_fft.mult(prj_ctf)
 			
-			prj_r = prj_fft.do_ift()	#Go back to real space
+			prj_r = prj_fft.do_ift()							#Go back to real space
 			
-			prj_n = prj.process('math.addnoise',{'noise':10})
-			prj_n.write_image('prj_ctfed' + parameters['ID'] + '.hdf',j)	
+			#prj_n = prj.process('math.addnoise',{'noise':10})
 			
-			ctfed_projections.append(prj_n)		
+			if options.noiseproc:
+			
+				prj_r.process_inplace(options.noiseproc[0],options.noiseproc[1])
+				
+				#noise = EMData(prj_r['nx'],prj_r['ny'])
+				#noise.to_one()
+				#noise.process_inplace(options.noiseproc[0],options.noiseproc[1])
+				#prj_n = prj_r + (noise * options.snr)
+			
+			
+			ctfed_projections.append(prj_r)		
+
+			if options.saveprjs:
+				prj_r.write_image(stackname.replace('.hdf', '_prjsctf' + '_ptcl' + str(i).zfill(len(str(nslices))) + '.hdf') , j)	
+			
 						
-			alt += int(parameters['tilt_step'])
+			alt += int(options.tiltstep)
 		
-		box = particles[i].get_xsize()
-		r = Reconstructors.get(parameters['reconstructor'],{'size':(box,box,box),'sym':'c1','mode':'gauss_2','verbose':True})
+		box = ptcls[i].get_xsize()
+		
+		if options.pad:
+			box /= 3
+		
+		r = Reconstructors.get(options.reconstructor[0],{'size':(box,box,box),'sym':'c1','verbose':True,'mode':'gauss_2'})
+		#r = Reconstructors.get(options.reconstructor[0],options.reconstructor[1])
 		r.setup()
 		
 		for p in ctfed_projections:
@@ -271,20 +329,13 @@ def subtomosim(options,ptcls):
 			r.insert_slice(p,p['xform.projection'],1.0)
 		
 		rec = r.finish(True)
-		mname = parameters['model'].split('/')[-1].split('.')[0]
-		name = 'rec_' + mname + '#' + str(i).zfill(len(str(len(particles)))) + '.hdf'
-		rec.write_image(name)
+		#mname = parameters['model'].split('/')[-1].split('.')[0]
+		#name = 'rec_' + mname + '#' + str(i).zfill(len(str(len(particles)))) + '.hdf'
+		
+		rec.write_image(outname,i)
+	
+	
 	return()	
-
-
-
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
 	main()
