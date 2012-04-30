350535e4d44af722fefa9bef4dd1549a91604de7
jgalaz <jgalaz>
2012-04-30 07:53:56 +0000

    Should be functional now.

--- examples/e2spt_resolutionplot.py
+++ examples/e2spt_resolutionplot.py
@@ -37,6 +37,7 @@ import sys
 import time
 import numpy
 import pylab
+#from operator import itemgetter					 
 
 def main():
 	progname = os.path.basename(sys.argv[0])
@@ -45,11 +46,11 @@ def main():
 			
 	parser = EMArgumentParser(usage=usage,version=EMANVERSION)
 	
-	parser.add_argument("--vol1", type=str, help="File for volume 1. Note that volume 2 will be aligned TO volume 1; that is, volume 1 is 'the reference'. The format MUST be '.hdf' or '.mrc' ", default=None)
-	parser.add_argument("--vol2", type=str, help="File for volume 2. Volume 2 to will be 'moved' to find its best alignment to volume 1. The format MUST be '.hdf' or '.mrc' ", default=None)
+	parser.add_argument("--vol1", type=str, help="File for volume 1. Note that volume 1 will be aligned TO volume 2; that is, volume 1 will be 'moved' to find its best fit to volume 2. The format MUST be '.hdf' or '.mrc' ", default=None)
+	parser.add_argument("--vol2", type=str, help="File for volume 2. Volume 2 to will be 'static' (the 'reference' to which volume 1 will be aligned to). The format MUST be '.hdf' or '.mrc' ", default=None)
 	parser.add_argument("--output", type=str, help="Name for the .txt file that will contain the FSC data. If not specified, a default name will be used, vol1_VS_vol2, where vol1 and vol2 are taken from --vol1 and --vol2 without the format.", default=None)
 
-	parser.add_argument("--sym", type=str, default=None, help = "Asymmetric unit to limit the alignment search to. Note that this should only be on IF the reference (--vol1) is ALREADY aligned to the symmetry axis.")
+	parser.add_argument("--sym", type=str, default='c1', help = "Asymmetric unit to limit the alignment search to. Note that this should only be on IF the reference (--vol1) is ALREADY aligned to the symmetry axis.")
 	parser.add_argument("--mask",type=str,help="Mask processor applied to particles before alignment. Default is mask.sharp:outer_radius=-2", default="mask.sharp:outer_radius=-2")
 	parser.add_argument("--normproc",type=str,help="Normalization processor applied to particles before alignment. Default is to use normalize.mask. If normalize.mask is used, results of the mask option will be passed in automatically. If you want to turn this option off specify \'None\'", default="normalize.mask")
 	parser.add_argument("--preprocess",type=str,help="Any processor (as in e2proc3d.py) to be applied to each volume prior to alignment. Not applied to aligned particles before averaging.", default=None)
@@ -73,11 +74,21 @@ def main():
 	
 	parser.add_argument("--ppid", type=int, help="Set the PID of the parent process, used for cross platform PPID",default=-1)
 	parser.add_argument("--verbose", "-v", dest="verbose", action="store", metavar="n",type=int, default=0, help="verbose level [0-9], higner number means higher level of verboseness")
-
+	
+	#print "Before parsing, options are", parser.args()
+	
 	(options, args) = parser.parse_args()
+	
+	print "options are", options
+	
+	logger = E2init(sys.argv, options.ppid)
 
 	vol1 = options.vol1
 	vol2 = options.vol2
+	
+	vol1hdr = EMData(vol1,0,True)
+	apix = vol1hdr['apix_x']
+	boxsize = vol1hdr['nx']
 	
 	vol1ALIname =  vol1.replace('.','_ALI.')
 	
@@ -85,19 +96,31 @@ def main():
 	if not options.output:
 		fscoutputname = 'FSC_' + vol1.split('.')[0] + '_VS_' + vol2.split('.')[0] + '.txt'
 	
-	alicmd = 'e2classaverage3d.py --input=' + vol1ALIname + ' --output=' + fscoutputname + ' --ref=' + vol2 + ' --npeakstorefine=' + str(options.npeakstorefine) + ' --verbose=' + str(options.verbose) + ' --mask=' + options.mask + ' --preprocess=' + options.preprocess + ' --align=' + options.align + ' --parallel=' + options.parallel + ' --ralign=' + options.ralign + ' --aligncmp=' + options.aligncmp + ' --raligncmp=' + options.raligncmp + ' --shrink=' + str(options.shrink) + ' --shrinkrefine=' + str(options.shrinkrefine) + ' --saveali' + ' --normproc=' + options.normproc + ' --sym=' + options.sym + ' --breaksym'
+	alicmd = 'e2classaverage3d.py --path=. --input=' + vol1 + ' --output=' + vol1ALIname + ' --ref=' + vol1 + ' --npeakstorefine=' + str(options.npeakstorefine) + ' --verbose=' + str(options.verbose) + ' --mask=' + options.mask + ' --preprocess=' + options.preprocess + ' --align=' + options.align + ' --parallel=' + options.parallel + ' --ralign=' + options.ralign + ' --aligncmp=' + options.aligncmp + ' --raligncmp=' + options.raligncmp + ' --shrink=' + str(options.shrink) + ' --shrinkrefine=' + str(options.shrinkrefine) + ' --saveali' + ' --normproc=' + options.normproc + ' --sym=' + options.sym + ' --breaksym'
 	
-	fsccmd = 'e2proc3d.py ' + vol2 + ' ' + fscoutputname + ' --calcfsc' + vol1ALIname
+	print "FSCOUTPUTNAME is", fscoutputname
+	print "Because options.output was", options.output
 	
-	#pltcmd = 
+	fsccmd = 'e2proc3d.py ' + vol2 + ' ' + fscoutputname + ' --calcfsc=' + vol1ALIname
 	
-	os.system(alicmd + ' && ' + fsccmd)
+	print "Ali command is\n", alicmd
+	print "\n\nFSC command is\n", fsccmd
+	
+	#cmd = alicmd + ' && ' + 'mv res/' + vol1ALIname + ' . && ' + fsccmd + ' && rm -r res' 
+	
+	cmd = alicmd + ' && ' + fsccmd + ' && rm -r res' 
+
+		
+	print "\n\nThus together they are\n", cmd
+		
+	os.system(cmd)
 	
 	findir=os.listdir(os.getcwd())
 	
 	if fscoutputname not in findir:
 		print "I am waiting for the fsc file to be generated"
 		while fscoutputname not in findir:
+			print "Can't find it yet!"
 			findir=os.listdir(os.getcwd())
 			time.sleep=(10)
 	else:
@@ -109,13 +132,51 @@ def main():
 		k=0
 		x = []
 		values = []
+		inversefreqs = []
 		for line in lines:
 			x.append(float(k))
-			values.append(float( line.split()[-1] ) )
+			values.append( float( line.split()[-1] ) )
+			inversefreqs.append( float( line.split()[0] ))
 			k += 1
 		
-		polycoeffs = numpy.polyfit(x, values, 3)
+		polycoeffs = numpy.polyfit(x, values, 10)
 		yfit = numpy.polyval(polycoeffs, x)
+		
+		#print "\n\n\n\n\n Y FIT IS!!!\n", yfit
+		
+		plot_name = fscoutputname.replace('.txt','_PLOT.png')
+		
+		fullinfo = {}
+		#fullinfo2 = {}
+
+		difs = []
+		
+		for i in range(len(yfit)):
+			dif = abs(yfit[i] - 0.5)
+			difs.append(dif)
+			fullinfo.update({dif:i})
+			#fullinfo2.update({inversefreqs[i]:dif})
+		
+		difsmin1=min(difs)
+		difs.remove(difsmin1)
+		difsmin2=min(difs)
+		
+		
+		minpixel1 = fullinfo[difsmin1]
+		minpixel2 = fullinfo[difsmin2]
+		
+		fsc0p5pixel = (minpixel1+minpixel2)/2
+		fscfreq1 = inversefreqs[minpixel1]
+		fscfreq2 = inversefreqs[minpixel2]
+		
+		fscfreqavg = (fscfreq1 + fscfreq2)/2.0
+		
+		resolution1 = (float(apix) * float(boxsize)) / float(fsc0p5pixel)
+		
+		resolution2 = 1/fscfreq2
+		
+		print "The resolution calculations 1 and 2 are", resolution1, resolution2
+		#difs = sorted(bestfinal, key=itemgetter('score'))
 		
 		pylab.plot(x, values, 'k.')
 		pylab.plot(x, yfit, 'r-')
@@ -125,8 +186,6 @@ def main():
 		pylab.xlabel('pixel number')
 		
 		pylab.savefig(plot_name)
-
-		plot_name = fscoutputname.replace('.txt','_PLOT.png')
 		
 		#pylab.plot(x, values, linewidth=1, marker='o', linestyle='--', color='r')	
 		
@@ -134,6 +193,7 @@ def main():
 		#a.set_xlim(1,mults[-1])
 		#a.set_ylim(0,max(x))
 
-
+	E2end(logger)
+	
 if __name__ == "__main__":
 	main()
