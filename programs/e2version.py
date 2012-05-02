385e235630e465a8c0efc488938b24438b4434af
gtang <gtang>
2012-05-02 16:01:25 +0000

    change popen4 to subprocess call

--- programs/e2version.py
+++ programs/e2version.py
@@ -1,10 +1,42 @@
 #!/usr/bin/env python
 
+#
+# Author: Grant Tang  
+# Copyright (c) 2000-2006 Baylor College of Medicine
+#
+# This software is issued under a joint BSD/GNU license. You may use the
+# source code in this file under either license. However, note that the
+# complete EMAN2 and SPARX software packages have some GPL dependencies,
+# so you are responsible for compliance with the licenses of these packages
+# if you opt to use BSD licensing. The warranty disclaimer below holds
+# in either instance.
+#
+# This complete copyright notice must be included in any revised version of the
+# source code. Additional authorship citations may be added, but existing
+# author citations must be preserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  2111-1307 USA
+#
+#
+
 import os
 import sys
 import platform
+from subprocess import *
 
-EMANVERSION="EMAN 2.0.4"
+EMANVERSION="EMAN 2.0.5"
 CVSDATESTAMP="$Date$"
 
 def main():
@@ -12,7 +44,8 @@ def main():
     
     if sys.platform=='linux2':
     	cmd = 'cat /etc/system-release'
-    	fin,fout = os.popen4(cmd)
+        p = Popen(cmd, shell=True, bufsize=1024, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+        (fin, fout) = (p.stdin, p.stdout)
     	result = fout.read().strip()
     	print 'Your EMAN2 is running on: ', result, os.uname()[2], os.uname()[-1]
     elif sys.platform=='darwin':
