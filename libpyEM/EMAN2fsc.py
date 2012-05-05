ed98b6cdcc515a3f3df4c223028eabb792056f86
stevel <stevel>
2012-05-05 16:47:01 +0000

    Fix missing import

--- libpyEM/EMAN2fsc.py
+++ libpyEM/EMAN2fsc.py
@@ -30,6 +30,8 @@
 #
 #
 
+from EMAN2db import db_open_dict,db_close_dict
+
 # This module contains function that implement python level FSC manipulation, so as to have code in only one place, once
 def db_compute_fsc(a, b, apix, pathname, dbname):
 	fsc = a.calc_fourier_shell_correlation(b)
@@ -107,3 +109,4 @@ def get_convergence_results_list(keys):
 		i += 1
 
 	return solns
+
