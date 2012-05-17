52c40370e91f4906d2e7fe7bf0b07ffc2c0e71e1
jgalaz <jgalaz>
2012-05-17 18:42:59 +0000

    Multiple improvements.

--- examples/e2spt_meanintensityplot.py
+++ examples/e2spt_meanintensityplot.py
@@ -19,33 +19,27 @@ def main():
 			
 	parser = EMArgumentParser(usage=usage,version=EMANVERSION)
 	
-	parser.add_argument("--input",type=str,default=None,help="Image you want to apply the cylindrical mask to.")
-	parser.add_argument("--output",type=str,default=None,help="File where you want to save the masked image.")
+	parser.add_argument("--input",type=str,default=None,help="Stack you want to analyze.")
+	parser.add_argument("--output",type=str,default=None,help="Stack to save the edited volumes (masked, lowpassed, etc).")
 	
 	parser.add_argument("--shrink", type=int,default=1,help="Optionally shrink the input volumes by an integer amount.")
-	parser.add_argument("--bins", type=int,default=5,help="Number of bins for histogram.")
+	#parser.add_argument("--bins", type=int,default=5,help="Number of bins for histogram.")
 	
-	parser.add_argument("--sym", type=str, default='c1', help = "Asymmetric unit to limit the alignment search to. Note that this should only be on IF the reference (--vol1) is ALREADY aligned to the symmetry axis.")
+	parser.add_argument("--sym", type=str, default='c1', help = "Symmetry to enforce before computing mean intensity in the box. Note that this should only be used if the particles are properly aligned to the symmetry axis.")
 	parser.add_argument("--mask",type=str,help="Mask processor applied to particles before alignment. Default is mask.sharp:outer_radius=-2", default="mask.sharp:outer_radius=-2")
+	parser.add_argument("--maskfile",type=str,default=None,help="A maskfile that will be multiplied by the image.")
 	
 	#parser.add_argument("--normproc",type=str,help="Normalization processor applied to particles before alignment. Default is to use normalize.mask. If normalize.mask is used, results of the mask option will be passed in automatically. If you want to turn this option off specify \'None\'", default="normalize.mask")
 	
 	parser.add_argument("--preprocess",type=str,help="Any processor (as in e2proc3d.py) to be applied to each volume prior to alignment. Not applied to aligned particles before averaging.", default=None)
 	parser.add_argument("--lowpass",type=str,help="A lowpass filtering processor (as in e2proc3d.py) to be applied to each volume prior to alignment. Not applied to aligned particles before averaging.", default=None)
 	parser.add_argument("--highpass",type=str,help="A highpass filtering processor (as in e2proc3d.py) to be applied to each volume prior to alignment. Not applied to aligned particles before averaging.", default=None)
-	parser.add_argument("--saveeditedvol",action="store_true",help="Saves the edited stack, mean that it is masked, normalized, lowpass filtered and symmetrized, and whatever other processing option was selected", default=False)
+
 	parser.add_argument("--ppid", type=int, help="Set the PID of the parent process, used for cross platform PPID",default=-1)
 
 	(options, args) = parser.parse_args()
 	
 	logger = E2init(sys.argv, options.ppid)
-	
-	#params_dict = parameters.__dict__
-	#parameters = params_dict
-	#cavity_scan(parameters)
-	
-	#if options.normproc: 
-	#	options.normproc=parsemodopt(options.normproc)
 	
 	if options.mask: 
 		options.mask=parsemodopt(options.mask)
@@ -65,9 +59,7 @@ def main():
 
 def cavity_scan(options):
 	print "I am in the intensity scanner"
-	x = []
-	y = []
-	#k = 0
+	intensities = []
 	data = options.input
 	
 	n = EMUtil.get_image_count(data)
@@ -93,6 +85,10 @@ def cavity_scan(options):
 		
 		a.mult(mask)
 		
+		if options.maskfile:
+			m=EMData(options.maskfile,0)
+			a.mult(m)		
+
 		# preprocess
 		if options.preprocess != None:
 			a.process_inplace(options.preprocess[0],options.preprocess[1])
@@ -108,25 +104,63 @@ def cavity_scan(options):
 		# Shrink
 		if options.shrink !=None and options.shrink>1 :
 			a=a.process("math.meanshrink",{"n":options.shrink})
-	
-		#b=b.process('xform.scale',{'scale':1,'clip':clipf})
-		
-		#b.process_inplace('testimage.cylinder',{'radius':r,'height':parameters['height']})
 
-		y.append(a['mean_nonzero'])
+		intensities.append(a['mean_nonzero']*1000)
 		
-		if options.saveeditedvol:
-			a.write_image(data.replace('.','_EDITED.'),i)
+		if options.output:
+			if options.output == 'same':
+				a.write_image(data.replace('.','_EDITED.'),i)
+			else:
+				a.write_image(options.output,i)
 	
-	msktag = str(options.mask[1]).split(':')[-1].replace(' ','').replace('}','').zfill(3)
+	msktag=''
+	if options.mask:	
+		msktag = str(options.mask[1]).replace(':','_').replace(' ','').replace('}','').replace('{','').replace("'",'').replace(',','')
+	
+	if options.maskfile:
+		msktag = options.maskfile
+	
+	msktag=msktag.replace('.hdf','')
+	msktag=msktag.split('.')[0]
+
 	print "\n\n\n\n\n\n\nMSK TAG is\n", msktag
 	plot_name = data.replace('.hdf', '_MEANintensityPLOT_MSK' + msktag + '.png')
 
-	print "The total number of particles is", len(y)
+	print "The total number of particles is", len(intensities)
 
 	#plt.hist(y, bins=5)
-	n, bins, patches = plt.hist(y, options.bins, normed=1, facecolor='blue', alpha=0.75)
 
+	std = numpy.std(intensities)
+	mean = numpy.mean(intensities)
+	statistics = 'mean='+str(mean) + ' std='+str(std) + '\n'
+	
+	statsfile = plot_name.replace('.png','_STATS.txt')
+	f=open(statsfile,'w')
+	f.writelines(statistics)
+	f.close()
+
+	intensitiesfile = plot_name.replace('.png','_INTENSITIES.txt')
+	
+	intensitieslines = []
+	for i in intensities:	
+		intensitieslines.append(str(i)+'\n')
+
+	g=open(intensitiesfile,'w')
+	g.writelines(statistics)
+	g.close()
+
+	print "The standard deviation is", std
+	cuberoot = numpy.power(len(intensities),1.0/3.0)
+	print "The cuberoot of n is", cuberoot
+	width = (3.5*std)/cuberoot
+	print "Therefore the bin width according to Scott's normal reference rule is", width
+	calcbins = (max(intensities) - min(intensities)) / width
+	print "And the number of bins should be", calcbins
+
+	plt.hist(intensities, calcbins, facecolor='blue', alpha=0.75)	
+
+	'''
+	FITTING A CURVE
 	binmids = []
 	
 	for i in range( len(bins) ):
@@ -136,6 +170,7 @@ def cavity_scan(options):
 			#print "i was smaller, because len bins is", len(bins)
 			binmids.append( (bins[i] + bins[i+1])/2 )
 	
+	
 	polycoeffs = numpy.polyfit(binmids, n, 3)
 	yfit = numpy.polyval(polycoeffs, binmids)
 	
@@ -143,25 +178,15 @@ def cavity_scan(options):
 	print "\n\nThe length of binmids is\n", len(binmids)
 	
 	#hist, bins = numpy.histogram(y, options.bins)
+	'''
 
-	plt.title("Cavity densities for " + str(len(y)) + " particles")
+	plt.title("Mean density for " + str(len(intensities)) + " particles")
 	plt.ylabel("Number of particles")
 	plt.xlabel("Density")
+	#plt.axis([min(intensities), max(intensities)])	
+	#plt.tick_params(axis='both',which='both',direction='out',length=1,width=2)
 	
-	#print "\n\nBins are\n", bins
-	#print "Bin length is", len(bins)
-	#print "N length is", len(n)
-	
-	print "\n\n n is\n", n
-	y = [4]*11
-	print "\n\ny is\n", y
-	print "Y length is", len(y)
-	
-	#width=0.7*(bins[1]-bins[0])
-	#center=(bins[:-1]+bins[1:])/2
-	#plt.bar(center,hist,align='center',width=width)
-
-	l = plt.plot(binmids, yfit, 'r--', linewidth=1)
+	#plt.plot(binmids, yfit, 'r--', linewidth=1)
 
 	plt.grid(True)
 	#plt.show()
