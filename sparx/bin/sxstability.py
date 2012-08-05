2682a790e23776920553bd9c16ea9e951e67556d
pawel <pawel>
2012-08-05 22:31:11 +0000

    sxstab OK

--- sparx/bin/sxstability.py
+++ sparx/bin/sxstability.py
@@ -135,9 +135,9 @@ def main():
 				stable_set, mir_stab_rate, pix_err = multi_align_stability_new(all_ali_params, 0.0, 10000.0, options.thld_err, options.verbose, 2*ou+1)
 				print "Average %4d : %20.3f %20.3f %20d %20d"%(i, mir_stab_rate, pix_err, len(stable_set), len(mem))
 				if options.stab_part and len(stable_set) >= options.thld_grp:
-					stab_mem = [0,0.0,0]*len(stable_set)
+					stab_mem = [[0,0.0,0] for j in xrange(len(stable_set))]
 					for j in xrange(len(stable_set)): stab_mem[j] = [mem[int(stable_set[j][1])], stable_set[j][0], j]
-					write_text_row(stab_mem, "stab_part_%03d.txt"%i)
+					write_text_row(stab_mem, "stab_part_%06d.txt"%i)
 
 		global_def.BATCH = False
 
