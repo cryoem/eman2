7eb01edebb3e2d38ce9f8833eb9880c852482023
fjia <fjia>
2012-11-06 01:20:19 +0000

    *** empty log message ***

--- sparx/bin/sxihrsr.py
+++ sparx/bin/sxihrsr.py
@@ -226,8 +226,8 @@ def main():
 		else:
 			from applications import ihrsr
 			global_def.BATCH = True
-			if len(args) < 4:  mask3d = None
-			else:               mask3d = args[3]
+			if len(args) < 4:  mask = None
+			else:               mask = args[3]
 			ihrsr(args[0], args[1], args[2], mask, irp, oup, options.rs, xrp, options.ynumber, txsp, options.delta, options.initial_theta, options.delta_theta, options.an, options.maxit, options.CTF, options.snr, options.dp, options.ndp, options.dp_step, options.dphi, options.ndphi, options.dphi_step, options.psi_max, rminp, rmaxp, options.fract, options.nise, options.npad,options.sym, options.function, options.datasym, options.apix, options.debug, options.MPI, options.WRAP, y_restrict2) 
 			global_def.BATCH = False
 		
