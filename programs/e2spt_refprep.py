b330b6cba6791a0fd31831644939e0373d56badc
jgalaz <jgalaz>
2012-11-14 21:37:51 +0000

    Bug fixes.

--- programs/e2spt_refprep.py
+++ programs/e2spt_refprep.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #
-# Author: Jesus G. Galaz 9/1/2010 - Last Update 11/10/2012 
+# Author: Jesus G. Galaz 9/1/2010 - Last Update 11/14/2012 
 # Copyright (c) 2011- Baylor College of Medicine
 #
 # This software is issued under a joint BSD/GNU license. You may use the
@@ -45,9 +45,9 @@ def main():
 	parser.add_argument('--ref', type=str, default='', help="Path to the reference that needs to be processed to match the data.")
 	parser.add_argument('--input', type=str, default='', help="Path to the image or stack of images which the reference will match after processing.")
 	parser.add_argument('--output', type=str, default='', help="Specify the name of the file to which the edited reference will be written.")
-	parser.add_argument("--boxsize",type=int,help="If NOT specipied, the reference's box size will match that of the data. If specified, both the refernece and the data will be resized.",default=None)
-	parser.add_argument("--refsym", trype=str, default='', help='Will apply the specified symmetry to the edited reference.')
-	parser.add_argument("--apix",type=float,default=None,help="If specified, the program will assume this is the correct apix for the DATA, which will be written to the data's header, and will also be used to scale the reference.")
+	parser.add_argument("--boxsize",type=int,help="If NOT specified, the reference's box size will match that of the data. If specified, both the reference and the data will be resized.",default=None)
+	parser.add_argument("--refsym", type=str, default='', help='Will apply the specified symmetry to the edited reference.')
+	parser.add_argument("--apix",type=float,default=None,help="If specified, the program will assume this is the correct apix for the DATA, which will be written to the data's header if the data is in .hdf format, and will also be used to scale the reference.")
 	parser.add_argument("--verbose", "-v", dest="verbose", action="store", metavar="n", type=int, default=0, help="verbose level [0-9], higner number means higher level of verboseness")
 	parser.add_argument("--preprocess",type=str,help="Any processor (as in e2proc3d.py) to be applied to the edited reference.", default=None)
 	parser.add_argument("--lowpass",type=str,help="A lowpass filtering processor (as in e2proc3d.py) be applied to the edited reference.", default=None)
@@ -55,6 +55,7 @@ def main():
 	parser.add_argument("--mask",type=str,help="Mask processor applied to particles before alignment. Default is mask.sharp:outer_radius=-2", returnNone=True, default="mask.sharp:outer_radius=-2", guitype='comboparambox', choicelist='re_filter_list(dump_processors_list(),\'mask\')', row=11, col=0, rowspan=1, colspan=3, mode='alignment,breaksym')
 	parser.add_argument("--normproc",type=str,help="Normalization processor applied to particles before alignment. Default is to use normalize.mask. If normalize.mask is used, results of the mask option will be passed in automatically. If you want to turn this option off specify \'None\'", default="normalize.mask")
 	parser.add_argument("--mirrorref", action="store_true", help="Will generate a mirrored copy of the reference.", default=False)
+	parser.add_argument("--sharplowpassresolution",type=float,default=None,help="If specified, the reference will be sharply filtered to this resolution.")
 	parser.add_argument("--ppid", type=int, help="Set the PID of the parent process, used for cross platform PPID",default=-1)
 
 	(options, args) = parser.parse_args()
@@ -79,6 +80,9 @@ def main():
 	if options.highpass: 
 		options.highpass=parsemodopt(options.highpass)
 	
+	if options.verbose:
+		print "I have parsed the processing options."
+	
 	'''
 	Check for sane formats for ref and output
 	'''
@@ -100,8 +104,10 @@ def main():
 		os.system('e2pdb2mrc.py ' + pdbmodel + ' ' + mrcmodel)
 		options.ref = mrcmodel
 		check=1
+		if options.verbose:
+			print "I've converted the .pdf reference to .mrc"
 	
-	if '.mrc' in options.ref:
+	if '.mrc' in options.ref and '.hdf' in options.output:
 		mrcmodel = options.ref
 		if check==0:
 			#os.system('cp ' + mrcmodel + ' ' + options.path)
@@ -110,8 +116,13 @@ def main():
 		os.system('e2proc3d.py ' + mrcmodel + ' ' + hdfmodel + ' && rm ' + mrcmodel)
 		options.ref = hdfmodel
 		check=1
+		if options.verbose:
+			print "I've converted the .mrc reference to .hdf"
 		
 	ref = EMData(options.ref,0)
+	
+	if options.verbose:
+		print "I have loaded the reference."
 	
 	'''
 	Make all sides of the reference box equally sized and even, if they're not
@@ -127,8 +138,13 @@ def main():
 		
 		R = Region((x0 - side)/2, (y0 - side)/2, (z0 - side)/2, side, side, side)
 		ref.clip_inplace(R)
+		
+		if options.verbose:
+			print "I have clipped the reference to have a cubical and even-sided box size."
 
 	ref_apix = ref['apix_x']
+	if options.verbose:
+		print "I've read the reference's apix, which is", ref_apix
 
 	'''
 	Get the target apix for the reference. If options.apix is supplied, first change the apix of the data the reference is supposed to match.
@@ -137,17 +153,14 @@ def main():
 		target_apix = EMData(options.input,0,True)['apix_x']
 	
 	if options.apix:
-		if options.input:
-			nptcls = 1
-			if '.hdf' in options.input:
-				nptcls = EMUtil.get_imag_count(data)
-		
+		if options.input and '.hdf' in options.input:
+			nptcls = EMUtil.get_image_count(options.input)
 			for i in range(nptcls):
-				a =EMData(data,i)
+				a =EMData(options.input,i)
 				a['apix_x'] = options.apix
 				a['apix_y'] = options.apix
 				a['apix_z'] = options.apix
-				a.write_image(data,i)
+				a.write_image(options.input.replace('.hdf','_ed.hdf'),i)
 		
 		target_apix = options.apix
 	
@@ -160,34 +173,66 @@ def main():
 	Get the target boxsize for the reference. If options.boxsize is supplied, first change the boxsize of the data the reference is supposed to match.
 	'''	
 	if options.input:
-		target_boxsize = EMData(data,0,True)['nx']
+		target_boxsize = EMData(options.input,0,True)['nx']
 	
 	if options.boxsize:
 		if options.input:
 			nptcls = 1
 			if '.hdf' in options.input:
-				nptcls = EMUtil.get_imag_count(data)
-		
+				nptcls = EMUtil.get_image_count(options.input)
+				aux=0
+				if options.apix:
+					options.input=options.input.replace('.hdf','_ed.hdf')
+					aux=1
+					
 			for i in range(nptcls):
-				a =EMData(data,i)
-				a['nx'] = options.boxsize
-				a['ny'] = options.boxsize
-				a['nz'] = options.boxsize
-				a.write_image(data,i)
-		
+				print "\n\nOPTIONSINPUT is!!!!!!!\n", options.input
+				
+				a = EMData(options.input,i)
+				a.process_inplace('xform.scale',{'scale':1,'clip':options.boxsize})
+				outname=options.input
+				if aux==0:
+					outname=options.input.replace('.hdf','_ed.hdf')
+				a.write_image(outname,i)
+
 		target_boxsize = options.boxsize
+	
+	if not options.input and not options.boxsize:
+		print """ERROR: You must supply an image through --input= to get the target boxsize for the edited reference, or specify this number through --boxsize= . If all you want to do
+		is scale the box of the reference, you ought to use e2proc3d.py <input> <output> --process=math.meanshrink:n="""
+		sys.exit()
 	
 	'''
 	Calculate the scale between the reference model and the data, round to the nearest integer to be able to use math.meanshrink (which preserves features),
 	then calculate the scale factor again and use xform.scale, to get the exact scaling, even if features might become a bit distorted by xform.scale
 	'''	
-	scalefactor = ref_apix/target_apix
-	scalefactor_int = round(scalefactor)
-	ref = ref.process("math.meanshrink",{"n":scalefactor_int})
+	meanshrinkfactor = target_apix/ref_apix
+	meanshrinkfactor_int = int(round(meanshrinkfactor))
 	
-	ref_apix = ref['apix_x']
+	if options.verbose:
+		print "The ref's apix is", ref_apix
+		print "And the target apix is", target_apix
+		print "Therefore, the meanshrink factor is", meanshrinkfactor
+		print "Which, for the first step of shrinking (using math.meanshrink), will be rounded to", meanshrinkfactor_int
+	
+	if meanshrinkfactor_int > 1:
+		print "bout to shrink"
+		print "The type of ref is", type(ref)
+		ref.process_inplace("math.meanshrink",{"n":meanshrinkfactor_int})
+		if options.verbose:
+			print "The reference was shrunk, to a first approximation."
+		
+		ref_apix = ref['apix_x']
+		
 	scalefactor = ref_apix/target_apix
-	ref = ref.process("xform.scale",{"scale":scalefactor,"clip":target_boxsize})
+	
+	if options.verbose:
+		print "The finer scale factor to apply is", scalefactor
+	
+	ref.process_inplace("xform.scale",{"scale":scalefactor,"clip":target_boxsize})
+
+	if options.verbose:
+		print "The reference has now been precisely shrunk."
 
 	''' 
 	Apply processing options. Make the mask first (if specified), use it to normalize (optionally), then apply it. Then apply any specified filters.
@@ -238,7 +283,7 @@ def main():
 		
 		if options.verbose:
 			print "The sharp lowpass filter will be actually applied at this resolution", actual_res
-			print "Becuase these many pixels in Fourier space will be zeroed out", ref_box/2) - npixels
+			print "Becuase these many pixels in Fourier space will be zeroed out", ref_box/2 - npixels
 		
 		ref_table = [1.0] * npixels + [0.0] * (( ref_box/2) - npixels )
 		
