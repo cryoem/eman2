bd1a51cc90093d0450a25601fac926cc5b3b7a90
jgalaz <jgalaz>
2012-11-16 09:35:37 +0000

    In development.

--- programs/e2spt_autoboxer.py
+++ programs/e2spt_autoboxer.py
@@ -47,7 +47,9 @@ import numpy
 def main():
 	
 	progname = os.path.basename(sys.argv[0])
-	usage = """Autoboxes globular particles from tomograms."""
+	usage = """WARNING:  **PRELIMINARY** program, still heavily under development.
+	
+	Autoboxes globular particles from tomograms."""
 			
 	parser = EMArgumentParser(usage=usage,version=EMANVERSION)
 	
@@ -80,16 +82,27 @@ def main():
 	parser.add_argument("--path",type=str,default=None,help="""Directory to store results in. The default is a numbered series of directories containing the prefix 'sptautobox'; 
 															for example, sptautobox_01 will be the directory by default if 'sptautobox' already exists.""")
 	
+	parser.add_argument("--yshort",action="store_true",default=False,help="This means you have provided a --tomogram in which 'y' is the short axis.")
+	
 	(options, args) = parser.parse_args()
 	
-	print "options are", options
-	
+	rootpath = os.getcwd()
+		
 	logger = E2init(sys.argv, options.ppid)
 	
 	'''
+	Check that the output and tomogam formats are sane, to prevent crashes later.
+	'''
+	if '.hdf' not in options.output and '.mrc' not in options.output:
+		print "ERROR: The output reference must be written to either an mrc or an hdf file."
+		sys.exit() 
+	
+	if '.hdf' not in options.tomogram and '.mrc' not in options.tomogram and '.rec' not in options.tomogram:
+			print "ERROR: The tomogram must be in .mrc, .rec (which is also just a .mrc file) or .hdf format."
+			
+	'''
 	Make the directory where to create the database where the results will be stored
 	'''
-	
 	if options.path and ("/" in options.path or "#" in options.path) :
 		print "Path specifier should be the name of a subdirectory to use in the current directory. Neither '/' or '#' can be included. "
 		sys.exit(1)
@@ -121,79 +134,170 @@ def main():
 	if options.path not in files:
 		
 		print "I will make the path", options.path
-		os.system('mkdir ' + options.path)
+		os.system('mkdir ' + options.path)	
+	
+	options.path = rootpath + '/' + options.path
 	
 	'''
-	Tomogram processing options, if specified. First, parse those which need to be parsed, then apply them.
+	Shrink tomogram (if required), then load it.
 	'''
-	if options.normproc: 
-		options.normproc=parsemodopt(options.normproc)
-	
-	if options.mask: 
-		options.mask=parsemodopt(options.mask)
-	
-	if options.preprocess: 
-		options.preprocess=parsemodopt(options.preprocess)
-		
-	if options.lowpass: 
-		options.lowpass=parsemodopt(options.lowpass)
-	
-	if options.highpass: 
-		options.highpass=parsemodopt(options.highpass)
-	
-	if options.verbose:
-		print "I have parsed the processing options."
-	
-	tomogramfile=options.tomgram
 	if options.shrink:
-		binnedname = ''
-		if '.rec' in options.tomogram:
-			binnedname = options.tomogram.replace('.rec','_' str(options.shrink) + '.rec')
-		elif '.mrc' in options.tomogram:
-			binnedname = options.tomogram.replace('.mrc','_' str(options.shrink) + '.mrc')
-		elif '.hdf' in options.tomogram:
-			binnedname = options.tomogram.replace('.hdf','_' str(options.shrink) + '.hdf')
-		else:
-			print "ERROR: The tomogram must be in .mrc, .rec (which is also just a .mrc file) or .hdf format."
-			
-		if binnedname:
-			os.system('e2proc3d.py ' + options.tomogram + ' ' + binnedname + ' --process=math.meanshrink:n=' + options.shrink)
-			tomogramfile = binnedname
-			
-	if options.mask and options.gridradius:
-		tomohdr = EMData(tomogramfile,0,True)
-		mask=EMData(tomohdr["nx"],tomohdr["ny"],tomohdr["nz"])
-		height = tomohdr['nz']
-		if options.yshort:
-				mask=EMData(tomohdr["nx"],tomohdr["nz"],tomohdr["ny"])			
-				height = tomohdr['ny']
-		mask.to_one()
-		mask.process_inplace('testimage.cylinder',{'radius':options.gridradius,'height':height})
-		
-		if options.gridoffset:
-			x=options.gridoffset.split(',')[0]
-			y=options.gridoffset.split(',')[-1]
-			mask.translate(x,y)	
-		
-		tomo=EMData(tomogramfile,0)
-		tomo.mult(mask)
-		
-		
+		binnedname = options.path + '/' + options.tomogram.split('/')[-1].replace('.','_bin' str(options.shrink) + '.')
+		os.system('e2proc3d.py ' + options.tomogram + ' ' + binnedname + ' --process=math.meanshrink:n=' + options.shrink)
+		options.tomogram = binnedname
+	else:
+		tomogramfile = options.path + '/' + options.tomogram		
 		
 	'''
 	Apply any specified filters (preprocess, lowpass and/or highpass)
 	'''
 	if options.preprocess != None:
-		ref.process_inplace(options.preprocess[0],options.preprocess[1])
-			
-	if options.lowpass != None:
-		ref.process_inplace(options.lowpass[0],options.lowpass[1])
-			
-	if options.highpass != None:
-		ref.process_inplace(options.highpass[0],options.highpass[1])
-	
-	
+		print "Patience. Applying this processor to tomogram:", options.preprocess
+		tomogramfile = tomogramfile.replace('.','_pr.')
+		os.system('e2proc3d.py ' + options.tomogram + ' ' + tomogramfile + ' --process=' + options.preprocess)
+		options.tomogram = tomogramfile
 		
+	if options.lowpass != None:
+		print "Patience. Applying lowpass filter to tomogram:", options.lowpass
+		tomogramfile = tomogramfile.replace('.','_lp.')
+		os.system('e2proc3d.py ' + options.tomogram + ' ' + tomogramfile + ' --process=' + options.lowpass)
+		options.tomogram = tomogramfile
+
+	if options.highpass != None:
+		print "Patience. Applying highpass filter to tomogram:", options.highpass
+		tomogramfile = tomogramfile.replace('.','_hp.')
+		os.system('e2proc3d.py ' + options.tomogram + ' ' + tomogramfile + ' --process=' + options.highpass)
+		options.tomogram = tomogramfile
+	
+	'''
+	Apply any masks if there are big carbon regions beyond the grid-hole and masking is specified.
+	'''
+	xo=0
+	yo=0		
+	if options.mask and options.gridradius:
+		tomohdr = EMData(tomogramfile,0,True)
+		height = tomohdr['nz']
+		#mask=EMData(options.gridradius*2,options.gridradius*2,height])
+		mask=EMData(tomohdr['nx'],tomohdr['ny'],height])
+
+		if options.yshort:
+				height = tomohdr['ny']
+				#mask=EMData(options.gridradius*2,height,options.gridradius*2)			
+				mask=EMData(tomohdr['nx'],tomohdr['nz'],height])
+		mask.to_one()
+		mask.process_inplace('testimage.cylinder',{'radius':options.gridradius,'height':height})
+		
+		if options.gridoffset:
+			xo=options.gridoffset.split(',')[0]
+			yo=options.gridoffset.split(',')[-1]
+			mask.translate(xo,yo)	
+		
+		tomo=EMData(tomogramfile)
+		tomo.mult(mask)
+		
+		tomogramfile=tomogramfile.replace('.','_msk.')
+				
+		tomo.write_image(tomogramfile,0)
+		options.tomogram=tomogramfile
+
+	'''
+	If a template is provided, check that it is sane. If not, fix it.	
+	'''
+	if options.template:
+		if '.hdf' not in options.template and '.pdb' not in options.template and '.mrc' not in options.template:
+			print "ERROR: The format of the template to use must be .pdb, .mrc or .hdf"
+			sys.exit()
+		else:
+			check=0
+			if '.pdb' in options.template:
+				pdbmodel = options.template
+				#os.system('cp ' + pdbmodel + ' ' + options.path)
+				pdbmodel = pdbmodel.split('/')[-1]
+				mrcmodel = pdbmodel.replace('.pdb','.mrc')
+				os.system('e2pdb2mrc.py ' + pdbmodel + ' ' + mrcmodel)
+				options.template = mrcmodel
+				check=1
+				if options.verbose:
+					print "I've converted the .pdf template to .mrc"
+		
+			if '.mrc' in options.template and '.hdf' in options.output:
+				mrcmodel = options.template
+				if check==0:
+					#os.system('cp ' + mrcmodel + ' ' + options.path)
+					mrcmodel = mrcmodel.split('/')[-1]
+				hdfmodel = mrcmodel.replace('.mrc','.hdf')
+				os.system('e2proc3d.py ' + mrcmodel + ' ' + hdfmodel + ' && rm ' + mrcmodel)
+				options.template = hdfmodel
+				check=1
+				if options.verbose:
+					print "I've converted the .mrc template to .hdf"
+		
+			template = EMData(options.template,0)
+			
+			if options.verbose:
+				print "I have loaded the template."
+	
+			'''
+			Make all sides of the template box equally sized and even, if they're not
+			'''
+			if template['nx'] != template['ny'] or template['nx'] != template['nz'] or template['ny'] != template['nz']:	
+				x0 = template['nx']
+				y0 = template['ny']
+				z0 = template['nz']
+	
+				side = max(x0,y0,z0)
+				if side % 2:
+					side += 1
+		
+				R = Region((x0 - side)/2, (y0 - side)/2, (z0 - side)/2, side, side, side)
+				template.clip_inplace(R)
+		
+				if options.verbose:
+					print "I have clipped the template to have a cubical and even-sided box size."
+				
+				options.template = options.template.replace('.','_fixed.')
+				template.write_image(options.path + '/' + options.template,0)
+		
+	elif options.particlestack:
+		options = generateref(options)
+			
+	'''
+	Scan the tomogram to extract subvolumes to compare against the template, only if they are inside the grid-hole (in this case, in side the mask if there is any)
+	'''
+	x = tomohdr['nx']
+	y = tomohdr['ny']
+	z = tomohdr['nz']
+	if options.yshort:
+		aux=z
+		z=y
+		y=aux
+	
+	for i in xrange(boxsize/2, x - boxsize/2):
+		for j in xrange(boxsize/2 , y - boxsize/2):
+			for k in xrange(boxsize/2 , z - boxsize/2):
+				if options.mask:
+					if (x - xo)*(x - xo) + (y - yo)*(y - yo) < options.gridradius * options.gridradius
+						scanposition(options,i,j,k)
+				else:
+					scanposition(options,i,j,k)
+	return()
+	
+	
+def generateref(options):
+	ptclhdr = EMData(options.particlestack,0,True)
+	boxsize = ptclhdr['nx']		
+	alicmd = "e2spt_classaverage.py --path=" + options.path + "/generatereference" + " --input=" + options.input + " --output=" + options.input.replace('.hdf','_avg.hdf') + " --npeakstorefine=10 -v 0 --mask=mask.sharp:outer_radius=-2 --lowpass=filter.lowpass.gauss:cutoff_freq=.02 --align=rotate_translate_3d:search=" +str( int(boxsize/4) ) + ":delta=15:dphi=15:verbose=0 --parallel=thread:7 --ralign=refine_3d_grid:delta=5:range=15:search=2 --averager=mean.tomo --aligncmp=ccc.tomo --raligncmp=ccc.tomo --savesteps --saveali --normproc=normalize.mask"
+	os.system(cmd)
+	options.template = options.path + "/generatereference/" + options.input.replace('.hdf','_avg.hdf')
+	return(options)
+	
+
+def scanposition(options,x,y,z):
+	boxsize = EMData(options.template,0,True)['nx']
+	r = Region((2*x-boxsize)/2,(2*y-boxsize)/2, (2*z-boxsize)/2, boxsize, boxsize, boxsize)
+	e = EMData()
+	e.read_image(tomogram,0,False,r)
+	return(e)
 			 
 
 if '__main__' == __name__:
