a161f3d8c08cf893d8f8334c474369979b7786a6
fjia <fjia>
2013-01-04 22:22:36 +0000

    *** empty log message ***

--- sparx/bin/sxihrsr.py
+++ sparx/bin/sxihrsr.py
@@ -277,7 +277,7 @@ def main():
 				global_def.BATCH = True
 				if len(args) < 4:  mask = None
 				else:               mask = args[3]
-				ihrsrlocalcons_MPI(args[0], args[1], args[2], mask, options.cons, irp, oup, options.rs, xrp, options.ynumber, txsp, options.delta, options.initial_theta, options.delta_theta, options.an, options.maxit, options.CTF, options.snr, options.dp, options.ndp, options.dp_step, options.dphi, options.ndphi, options.dphi_step, options.psi_max, rminp, rmaxp, options.fract, options.nise, options.npad,options.sym, options.function, options.datasym, options.apix, options.debug, options.MPI, options.WRAP, y_restrict2) 
+				ihrsrlocalcons_MPI(args[0], args[1], args[2], mask, options.cons, irp, oup, options.rs, xrp, options.ynumber, txsp, options.delta, options.initial_theta, options.delta_theta, options.an, options.maxit, options.CTF, options.snr, options.dp, options.ndp, options.dp_step, options.dphi, options.ndphi, options.dphi_step, options.psi_max, rminp, rmaxp, options.fract, options.nise, options.npad,options.sym, options.function, options.datasym, options.apix, options.debug, y_restrict2, options.WRAP) 
 				global_def.BATCH = False
 			else:
 				from applications import ihrsr
