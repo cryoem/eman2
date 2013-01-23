fab107d8a3b183af74ce6d5d85969c2a82880965
jgalaz <jgalaz>
2013-01-23 21:27:31 +0000

    Capacities largely extenxed to deal with particle stacks, and to plot rotational correlation agreement in pseudo-3D if both alt and az are varied continously, and to produce plots of rotational agreement of one particle against another (except of only producing plots to see the agreement of one particle with itself).

--- examples/e2spt_rotationalplot.py
+++ examples/e2spt_rotationalplot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #
-# Author: Jesus Galaz, 04/28/2012 - using code and concepts drawn from Jesus Galaz's scripts
+# Author: Jesus Galaz, 04/28/2012 - Last update 12/07/2012
 # Copyright (c) 2011 Baylor College of Medicine
 #
 # This software is issued under a joint BSD/GNU license. You may use the
@@ -36,7 +36,11 @@ import os
 import sys
 import time
 import numpy
+from pylab import *
 import pylab
+import colorsys
+
+pylab.rcParams['legend.loc'] = 'best'
 #from operator import itemgetter					 
 
 def main():
@@ -46,7 +50,9 @@ def main():
 			
 	parser = EMArgumentParser(usage=usage,version=EMANVERSION)
 	
-	parser.add_argument("--input", type=str, help="Filename of the .hdf volume whose rotational correlation plot you want to compute.", default=None)
+	parser.add_argument("--vols1", type=str, help="Comma-separated filenames of the .hdf volumes whose self-rotational correlation plot you want to compute.", default=None)
+	parser.add_argument("--vols2", type=str, help="Comma-separated filenames of the .hdf volumes whose rotational correlation plot you want to compute against the volumes provided through --vols1.", default=None)
+
 	parser.add_argument("--output", type=str, help="Name for the .txt file with the results and the corresponding .png plot")
 	
 	parser.add_argument("--mask",type=str,help="Mask processor applied to particles before alignment. Default is mask.sharp:outer_radius=-2", default="mask.sharp:outer_radius=-2")
@@ -59,7 +65,8 @@ def main():
 	parser.add_argument("--shrinkrefine", type=int,default=1,help="Optionally shrink the input volumes by an integer amount for refine alignment.")
 	
 	parser.add_argument("--daz", type=int,default=3,help="Step size to vary azimuth.")
-	parser.add_argument("--dalt", type=int,default=0,help="Step size to vary altitude.")
+	parser.add_argument("--icosvertices", action="store_true",help="Will produce an azimutal plot at each vertex of an icosahedron.", default=False)
+	parser.add_argument("--dalt", type=int,default=181,help="Step size to vary altitude.")
 	parser.add_argument("--alti", type=int,default=0,help="""Initial position to check in altitude. For example, for a D symmetric chaperonin, 
 															if you want to check alt=0 ONLY, provide --alti=0 and --dalt=181 as options.
 															if you want to check alt=180 ONLY, provide --alti=180, --dalt=1 or greater.
@@ -71,90 +78,294 @@ def main():
 	parser.add_argument("--verbose", "-v", dest="verbose", action="store", metavar="n",type=int, default=0, help="verbose level [0-9], higner number means higher level of verboseness.")
 	parser.add_argument("--plotonly",action="store_true", help="Assumes vol1 and vol2 are already aligned with respect to each other and thus skips alignment.", default=False)
 	parser.add_argument("--normalizeplot",action="store_true", help="Make maximum correlation value on plot equal to 1 and scale all other values accordingly.", default=False)
+	parser.add_argument("--singleplot",action="store_true", help="Plot all alts, or each vertex of --icosvertices is on, in a single .png file.", default=False)
+	parser.add_argument("--only2dplot",action="store_true", help="Skips all plots, except 2dplot.", default=False)
+	parser.add_argument("--savetxt",action="store_true", help="Will save the values for each plot into .txt files.", default=False)
 
 	
 	(options, args) = parser.parse_args()
 		
 	logger = E2init(sys.argv, options.ppid)
+	
+	if options.icosvertices and options.vols2:
+			print "ERROR: You can only use --icosvertices for volumes in --vols1. You must NOT supply --vols2."
+			sys.exit()
+	
 
-	'''
-	Check that the input format is sane, to prevent crashes later.
-	'''
+	vols1=[]
+	if options.vols1:
+		vols1 = options.vols1.split(',')
 	
-	if '.hdf' not in options.input and '.mrc' not in options.input:
-		print "ERROR: The input image must be in .mrc, .rec (which is also just a .mrc file) or .hdf format."
+	vols = vols1
+
+	vols2=[]
+	if options.vols2:
+		vols2 = options.vols2.split(',')
+		vols = vols1 + vols2
+		
+		for v in vols:
+			if '.hdf' not in v and '.mrc' not in v:
+				print "ERROR: The input volumes must all be either .hdf, .mrc or .rec (which is also just a .mrc file)."
+				sys.exit()
+					
+			rotcccplot(v,v,options)
 	
-	vol1hdr = EMData(options.input,0,True)
-	apix = vol1hdr['apix_x']
-	boxsize = vol1hdr['nx']
+		for v1 in vols1:
+			for v2 in vols2:
+				rotcccplot(v1,v2,options)
+				
+	else:
+		for v in vols:
+			if '.hdf' not in v and '.mrc' not in v:
+				print "ERROR: The input volumes must all be either .hdf, .mrc or .rec (which is also just a .mrc file)."
+				sys.exit()
 	
+			rotcccplot(v,v,options)
+	
+	E2end(logger)
+			
+	return()
+			
+
+def rotcccplot(v1,v2,options):	
+	
+	nimg1 = EMUtil.get_image_count()
+	print "The first file actually has this many images in it", nimg1
+	nimg2 = nimg1
+	
+	
+	
+	vol2 = EMData()
+	if v1 != v2:
+		nimg2 = EMUtil.get_image_count()
+		if nimg2 > 1:
+			
+			print "The second file actually has this many images in it", nimg2
+			print """Error: You cannot provide a stack for both --vol1 and --vol2.
+				 To compare multiple volumes in an all vs all fashion, supply individual files separated by a comma.
+				 You can compare multiple files in a stack against a single volume, if you provide the stack through --vol1,
+				 and the single volume through --vol2."""
+			sys.exit()
+
+
+	for ni in range(nimg1):
+		vol1 = EMData(v1,ni)
+		vol1 = preprocess(vol1,options)
+		title = v1
+		
+		if v1 != v2:
+			vol2 = EMData(v2,0)
+			vol2 = preprocess(vol2,options)
+			if nimg1 > 1:
+				title = v1 + '_ptcl' + str(ni).zfill(len(str(nimg1)))+'_VS_' + v2
+			else:
+				title = v1 + ' VS ' + v2
+				
+		else:
+			vol2 = vol1.copy()
+	
+		loops = 1
+		ts=[]
+	
+		if options.icosvertices:
+			ts = genicosvertices()
+			loops = len(ts)
+	
+		for loop in range(loops):
+			print "looping over the stack %s; img number %d" %(v1,ni) 
+			if ts:
+				vol2 = vol1.copy()
+				vol2.transform(ts[loop])
+				print "I have gone to vertex %d by applying this transform" %(loop)
+				print ts[loop]
+						
+			ret = azimuthalccc(vol1,vol2,options)
+			azs = ret[0]
+			values = ret[1]
+		
+			print "I have acquired azs and values and will proceed to plot."
+	
+			if options.normalizeplot:
+				for ele in values:
+					#val = values[ele]
+				
+					minv = min(values[ele])
+					maxv1 = max(values[ele])
+					#print "Min max before normalization was", minv,maxv1
+					for k in range(len(values[ele])):
+						values[ele][k] = values[ele][k] - minv 
+				
+					minv2 = min(values[ele])
+					maxv = max(values[ele])
+					#print "After subtracting min, the are", minv2,maxv
+					#print "Max before normalization was", maxv
+					for k in range(len(values[ele])):
+						values[ele][k] = values[ele][k] / maxv
+				
+					#print "after norm they are", min(values[ele]), max(values[ele])
+		
+			#print "Len of azs and values is", len(azs), len(values)
+			#print "therefore the az,values to plot are"
+		
+			vertextag=''
+		
+			for ele in values:
+				plotname = title.replace('.hdf','').replace('.mrc','') + '.png'
+				#alt = ele
+				#print "There's a plot for this alt", ele
+				#print "BASE plot name will be", plotname
+				#print "because title is", title
+			
+				lines=[]
+		
+				vertextag = '_alt' + str(ele).zfill(3)
+				if ts:
+					vertextag = vertextag + '_' + str(loop).zfill(2)
+		
+				print "Vertextag is", vertextag
+			
+				txtname = plotname.replace('.png',vertextag + '.txt')
+				if options.savetxt:
+			
+					print "txtname is", txtname
+				
+					f = open(txtname,'w')
+					for i in range(len(values[ele])):
+						line = str(azs[i]) + ' ' + str(values[ele][i]) + '\n'
+						lines.append(line)
+						#print "Line to write is", line
+					f.writelines(lines)
+					f.close()
+
+				lab = 'alt='+str(ele).zfill(3)
+				if options.icosvertices:
+					lab = 'vertex' + str(loop).zfill(2)
+				if not options.only2dplot:
+					pylab.plot(azs, values[ele], linewidth=2, label=lab)
+					legend()
+			
+				if options.normalizeplot:
+					print "Normalize plot is on"
+					ylim([0,1.5])
+					if options.icosvertices:
+						print "icosvertices is on"
+						if options.singleplot:
+							print "Single plot is on"
+							ylim([0,6])
+						else:
+							print "Single plot is NOT on"
+							ylim([0,1.5])
+						
+				else:
+					ylim([0,1.5*max(values[ele])])
+			
+				if not options.singleplot:
+					plotname = txtname.replace('.txt','.png')
+					pylab.title(title)
+					pylab.ylabel('Correlation')
+					pylab.xlabel('Azimuth')
+					pylab.savefig(plotname)
+					clf()
+		
+		if options.singleplot:	
+			if not options.only2dplot:
+				pylab.savefig(plotname)	
+				pylab.title(title)
+				pylab.ylabel('Correlation')
+				pylab.xlabel('Azimuth')	
+				pylab.savefig(plotname)
+				clf()
+		
+			if not options.icosvertices:
+				print "I will call 2dplot"
+				twoD_plot(plotname,values,options)
+	return()
+	
+	
+def preprocess(vol,options):
+	print "Entering preprocessing function"
 	if options.mask: 
-		options.mask=parsemodopt(options.mask)
+		options.mask = parsemodopt(options.mask)
+		print "mask is", options.mask
 	
 	if options.preprocess: 
-		options.preprocess=parsemodopt(options.preprocess)
-		
-	if options.lowpass: 
-		options.lowpass=parsemodopt(options.lowpass)
+		options.preprocess = parsemodopt(options.preprocess)
+		print "Preprocessor is", options.preprocess
 	
-	if options.highpass: 
-		options.highpass=parsemodopt(options.highpass)
+	if options.lowpass: 
+		options.lowpass = parsemodopt(options.lowpass)
+		print "lowpass is", options.lowpass
 		
-	vol1 = EMData(options.input,0)
+	if options.highpass: 
+		options.highpass = parsemodopt(options.highpass)
+		print "Highpass is", options.highpass
+		
+	apix = vol['apix_x']
+	boxsize = vol['nx']
+	
+	#vol1 = EMData(options.input,0)
 	
 	# Make the mask first, use it to normalize (optionally), then apply it 
-	mask = EMData(vol1["nx"],vol1["ny"],vol1["nz"])
+	mask = EMData(vol["nx"],vol["ny"],vol["nz"])
 	mask.to_one()
 	if options.mask:
 		#print "This is the mask I will apply: mask.process_inplace(%s,%s)" %(options["mask"][0],options["mask"][1]) 
 		mask.process_inplace(options.mask[0],options.mask[1])
 
 	# normalize
-	vol1.process_inplace('normalize')
-	vol1.mult(mask)
-	vol1.process_inplace('normalize')
-	vol1.mult(mask)
+	print "Normalizing and masking"
+	vol.process_inplace('normalize')
+	vol.mult(mask)
+	vol.process_inplace('normalize')
+	vol.mult(mask)
 
 	# preprocess
 	if options.preprocess != None:
-		vol1.process_inplace(options.preprocess[0],options.preprocess[1])
+		print "Preprocessing"
+		vol.process_inplace(options.preprocess[0],options.preprocess[1])
 
 	# lowpass
 	if options.lowpass != None:
-		vol1.process_inplace(options.lowpass[0],options.lowpass[1])
+		print "Lowpassing"
+		vol.process_inplace(options.lowpass[0],options.lowpass[1])
 
 	# highpass
 	if options.highpass != None:
-		vol1.process_inplace(options.highpass[0],options.highpass[1])
+		print "Highpassing"
+		vol.process_inplace(options.highpass[0],options.highpass[1])
 
 	# Shrinking both for initial alignment and reference
 	if options.shrink!=None and options.shrink > 1 :
-		vol1=vol1.process("math.meanshrink",{"n":options.shrink})
-
-	alt=options.alti
+		print "Shrinking"
+		vol=vol.process("math.meanshrink",{"n":options.shrink})
+	print "Leaving preprocessing function"
+	return(vol)
 	
-	values = []
+
+def azimuthalccc(vol1,vol2,options):
+	print "In azimuthalccc function"
+	alt=options.alti
+	values = {}
 	azs=[]
 	
-	if options.dalt == 180:
-		maxaz = 360 * 2
-		
+	if options.icosvertices:
+		options.dalt = 181
+	k=0	
 	while alt <= 180:
 		#if not options.dalt:
 		#	alt = 180
 		az=0
-		azlist=[]
-		valueslist=[]
+		valuesforthisalt=[]
 		while az <= 360:
-			vol2 = EMData()
-			vol2 = vol1.copy()
+			moving = EMData()
+			moving = vol2.copy()
 			
-			print "I will rotate by this alt and az", alt, az
-			vol2.rotate(az,alt,0)
-			if alt == 180:
-				print "In theory, I have flipped the molecule now!!!!", alt, az
+			#print "I will rotate by this alt and az", alt, az
+			moving.rotate(az,alt,0)
+			#if alt == 180:
+				#print "In theory, I have flipped the molecule now!!!!", alt, az
 			
-			ccf = vol1.calc_ccf(vol2)
+			ccf = vol1.calc_ccf(moving)
 			ccf.process_inplace("xform.phaseorigin.tocorner") 
 			ccf.process_inplace('normalize')
 				
@@ -169,76 +380,113 @@ def main():
 			#zbest = loc_sub[2]
 
 			best_value = ccf.get_value_at(loc_sub[0],loc_sub[1],loc_sub[2])
-			valueslist.append(best_value)
-			azlist.append(az)
-
+			valuesforthisalt.append(best_value)
+			print "Done with ccc=%f for this alt=%d,az=%d" % ( best_value, alt, az)
+			if k == 0:
+				azs.append(az)
 			az += options.daz
-		azs.append(azlist)
-		values.append(valueslist)
 		
-		if not options.dalt:
-			alt = 179
-			options.dalt=2
+		#azs.append(azlist)
+		values.update({alt:valuesforthisalt})
+		k+=1
+		
+		#if not options.dalt:
+		#	alt = 179
+		#	options.dalt=2
 			
 		alt = alt + options.dalt
-		print "ALT now is!!!", alt
-	
-		#if not options.dalt:
-		#	alt+=181
+		#print "ALT now is!!!", alt
+	print "LEAVING azimuthalccc function"
 
-	fileoutputname = options.output
-	
-	lines=[]
-	
-	fileoutputname.replace('.png','.txt')
-	
-	if '.txt' not in fileoutputname:
-		fileoutputname += '.txt'
-		
-	f = open(fileoutputname,'w')
-	for i in range(len(values)):
-		line = str(azs[i]) + ' ' + str(values[i]) + '\n'
-		lines.append(line)
-	f.writelines(lines)
-	f.close()
-	
-	#polycoeffs = numpy.polyfit(x, values, 30)
-	#yfit = numpy.polyval(polycoeffs, x)
+	return(azs,values)
 
-	plot_name = fileoutputname.replace('.txt','_PLOT.png')
+def color(value):
+	color =	colorsys.hsv_to_rgb( float(value) / 180.0 / (1.1), 1, 1)
+	return(color)
+
+
+def twoD_plot(plotname,values,options):
+	print "I am in 2d plot"
 	
-	"""
-	Scale the correlation values so that the maximum correlation in the curve is 1
-	"""
+	azs = set([])
+	alts = set([])
+	cccs = []
 	
-	#values = values/ max(values)
+	widthy = options.dalt
+	widthx = options.daz
+	print "The step in alt is", options.dalt
+	print "The step in az is", options.daz
+	widths = [widthx,widthy]
+	#markerwidth = max(widths)
+	markerwidth = sum(widths)/2
+	print "marker width is the average of the two", markerwidth
 	
-	for i in range(len(values)):
-		if options.normalizeplot:
-			for j in range(len(values[i])):
-				values[i][j]= values[i][j] / max(values[i])
-		pylab.plot(azs[i], values[i], linewidth=2)
-	#fit = pylab.plot(x, yfit, 'r-')
+	for ele in values:
+		for k in range(len(values[ele])):
+			ccc = values[ele][k]
+			pylab.plot(k*options.daz,ele,marker='o',markersize=markerwidth*2,color=color((ccc*ccc)*180),alpha=0.5, markeredgecolor=None)
+			cccs.append(ccc)
+			azs.add(k*options.daz)
+			alts.add(ele)				
+	
+	plotname = plotname.replace('.png','_2D.png')
 
-	pylab.title(plot_name)
-	pylab.ylabel('Correlation')
-	pylab.xlabel('Azimuth')
-	#pylab.grid(True)
-	#pylab.legend( (curve, fit), ('Values', 'Fit'))
-	#pylab.yaxis([0,boxsize/2 + 10,0,1.2])
+	pylab.title(plotname.replace('.png',''))
+	pylab.xlabel('az')
+	pylab.ylabel('alt')
+	
+	pylab.xlim([-widthx,max(azs) + markerwidth*2])
+	pylab.ylim([-widthy,max(alts)+ markerwidth*2])
+	#for i in range(len(finalpoints)):
+	#	plt.plot(*zip(*[finalpoints[i]]),marker='o',markersize=4,color=color(ang_errors[i]))	
+	plt.savefig(plotname,bbox_inches=0)
+	clf()
+	return()
 
-	#pylab.annotate("FSC0.5 = "+str(final0p5)+" A", xy=(0, 1), xytext=(300, -30), xycoords='data', textcoords='offset points',bbox=dict(boxstyle="round", fc="0.8"))
-	#pylab.annotate("FSC0.143 = "+str(final0p143)+" A", xy=(0, 1), xytext=(300, -55), xycoords='data', textcoords='offset points',bbox=dict(boxstyle="round", fc="0.8"))
-	#pylab.annotate("Sampling ="+"%.2f"%(apix) + " A/pixel", xy=(0, 1), xytext=(300, -80), xycoords='data', textcoords='offset points',bbox=dict(boxstyle="round", fc="0.8"))
+def genicosvertices():
+	t=Transform()
 
-	pylab.savefig(plot_name)
+	syms = []
+	for i in range(60):
+		syms.append(t.get_sym('icos',i))
+	
+	ts = []
+	alts = set([])
+	phis = set([])
+	for s in syms:
+		rot=s.get_rotation()
+		az=rot['az']
+		alt=rot['alt']
+		phi=rot['phi']
+	
+		if az != 0.0 and phi != 0.0:
+			alts.add(round(alt,3))
+			phis.add(round(phi,3))
 
-	#pylab.plot(x, values, linewidth=1, marker='o', linestyle='--', color='r')	
-	#a = plt.gca()
-	#a.set_xlim(1,mults[-1])
-	#a.set_ylim(0,max(x))
+	angles = {}		
+	for a in alts:
+		ps =set()
+		for s in syms:
+			rot=s.get_rotation()
+			#az=rot['az']
+			alt=round(rot['alt'],3)
+			phi=round(rot['phi'],3)
+			if a == alt:
+				ps.add(phi)
+		angles.update({a:ps})
 
-	E2end(logger)
+	#print "angles is", angles
+	for n in angles:
+		alt = n
+		ps = angles[n]
+		for phi in ps:	
+			ts.append( Transform({'type':'eman','alt':alt,'phi':phi}) )
+
+	ts.append(Transform())
+	ts.append(Transform({'type':'eman','alt':180}))
+
+	return(ts)
+
 	
 if __name__ == "__main__":
 	main()
