f911caae78531f4f0de512d3875e38b94d86678f
fjia <fjia>
2013-02-26 23:14:07 +0000

    remove old comments

--- libEM/sparx/util_sparx.cpp
+++ libEM/sparx/util_sparx.cpp
@@ -3903,7 +3903,6 @@ Dict Util::Crosrng_sm_psi(EMData* circ1p, EMData* circ2p, vector<int> numr, floa
 	const int psi_range = int(psi_max/360.0*maxrin + 0.5);
 
 	for (k=-psi_range; k<=psi_range; k++) {
-		//j = ( k + psi_pos + maxrin -1 )%maxrin+1;
 		j = ( k + psi_pos + maxrin )%maxrin+1;
 		if (q(j) >= qn) {
 			qn  = q(j);
@@ -4004,7 +4003,6 @@ Dict Util::Crosrng_psi(EMData* circ1p, EMData* circ2p, vector<int> numr, float p
 	const int psi_range = int(psi_max/360.0*maxrin + 0.5);
 
 	for (k=-psi_range; k<=psi_range; k++) {
-		//j = (k+psi_pos+maxrin-1)%maxrin+1;
 		j = (k+psi_pos+maxrin)%maxrin+1;
 		if (q(j) >= qn) {
 			qn  = q(j);
