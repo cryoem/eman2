6329d0e7c2845fc539bf88e35109201bb3335813
fjia <fjia>
2013-04-01 19:31:01 +0000

    remove mirror_only flag from proj_ali_helical_local

--- sparx/libpy/alignment.py
+++ sparx/libpy/alignment.py
@@ -1329,40 +1329,8 @@ def proj_ali_helical_local(data, refrings, numr, xrng, yrng, stepx,ynumber, an, 
 		finfo.write("Old parameters: %9.4f %9.4f %9.4f %9.4f %9.4f\n"%(phi, theta, psi, tx, ty))
 		finfo.flush()
 
-	sn = int(sym_string[1:])
-
-	k0 =   0.0
-	k2 = 180.0
-
-	if( abs( t1.at(2,2) )<1.0e-6 ):
-		if (sym_string[0] =="c") or (sym_string[0] =="C"):
-			if sn%2 == 0:  k1=360.0/sn
-			else:          k1=360.0/2/sn
-		elif (sym_string[0] =="d") or (sym_string[0] =="D"):
-			if sn%2 == 0:  k1=360.0/2/sn
-			else:          k1=360.0/4/sn
-	else:
-		if   (sym_string[0] =="c")   or (sym_string[0] =="C"):    k1=360.0/sn
-		elif (sym_string[0] =="d")   or (sym_string[0] =="D"):    k1=360.0/2/sn
-
-	k3 = k1 +180.0
-	from numpy import float32
-	dpphi = float32(phi)
-	mirror_only = False
-	if sn%2 == 1:
-		if (abs(cos(theta*pi/180.0)) < 1.0e-6):
-			if  ( dpphi >= float(k2) and dpphi < float(k3) ): mirror_only = True
-			# temporary hack to deal with parameters whose phi angles may land exactly on k3 since the parameters were generated with old
-			# even_angles code.	
-			if dpphi == k3: mirror_only = True
-		else:
-			if( cos( pi*float( theta )/180.0 ) < 0.0 ): mirror_only = True  # theta > 90.0
-	else:
-		# sn is even
-		if theta > 90.0: mirror_only = True # assumes that when theta is allowed to vary, it only varies from theta1 to 90 where theta1<90.			
-
 	[ang, sxs, sys, mirror, iref, peak] = \
-	Util.multiref_polar_ali_helical_local(data, refrings, xrng, yrng, stepx, ant, psi_max, mode, numr, cnx-tx, cny-ty, int(ynumber), mirror_only, yrnglocal)
+	Util.multiref_polar_ali_helical_local(data, refrings, xrng, yrng, stepx, ant, psi_max, mode, numr, cnx-tx, cny-ty, int(ynumber), yrnglocal)
 
 	iref = int(iref)
 
@@ -1382,8 +1350,6 @@ def proj_ali_helical_local(data, refrings, numr, xrng, yrng, stepx,ynumber, an, 
 		s2y   = syb + ty
 
 		if finfo:
-			if mirror_only:
-				finfo.write("mirror only\n")
 			finfo.write("ref phi: %9.4f\n"%(refrings[iref].get_attr("phi")))
 			finfo.write( "New parameters: %9.4f %9.4f %9.4f %9.4f %9.4f %10.5f \n\n" %(phi, theta, psi, s2x, s2y, peak))
 			finfo.flush()
