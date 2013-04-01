d0939e5083c46368b054ae813ad4cdbb39e5a1a8
fjia <fjia>
2013-04-01 22:59:02 +0000

    add input argument to multiref_polar_ali_helical_90_local

--- libEM/sparx/util_sparx.h
+++ libEM/sparx/util_sparx.h
@@ -935,7 +935,7 @@ public:
                 vector< int >numr, float cnx, float cny, int ynumber=-1);
 	static vector<float> multiref_polar_ali_helical_90_local(EMData* image, const vector< EMData* >& crefim,
                 float xrng, float yrng, float step, float ant, float psi_max, string mode,
-                vector< int >numr, float cnx, float cny, int ynumber=-1);
+                vector< int >numr, float cnx, float cny, int ynumber=-1, float yrnglocal=-1.0);
 
 	/** formerly known as apmq
 	 * Determine shift and rotation between image and many reference
