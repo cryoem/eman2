6fdec9a4715a6ff25c3b11e6e9093e4a98dafbdd
fjia <fjia>
2013-04-22 18:58:19 +0000

    *** empty log message ***

--- sparx/bin/sxhelical_demo.py
+++ sparx/bin/sxhelical_demo.py
@@ -50,11 +50,11 @@ def main():
 	
 		sxhelical_demo.py 3MFP_1SU.pdb rnew.pdb --heli --dp=27.6 --dphi=166.5 \n
 	
-	Generate micrographs of helical filament from reference volume. 
+	Generate projections of helical filament from reference volume. 
 	Input: Reference Volume, output directory 
-	Output: Three micrographs containing helical filaments stored in output directory		
+	Output: Three micrographs stored in output directory		
 				 
-		sxhelical_demo.py tmp.hdf  mic --CTF --apix=1.84	
+		sxhelical_demo.py tmp.hdf  mic --generate_micrograph --CTF --apix=1.84	
 	"""
 	parser = OptionParser(usage,version=SPARXVERSION)
 	
@@ -67,6 +67,7 @@ def main():
 	parser.add_option("--generate_micrograph",    action="store_true",      default=False,      		  	 help="Generate micrograph of helical filament from reference volume. Input: Reference Volume, output directory, Output: Three micrographs containing helical filaments stored in output directory")
 	parser.add_option("--CTF",              	  action="store_true",  	default=False,   				 help="Use CTF correction")
 	parser.add_option("--apix",               	  type="float",			 	default= -1,               	     help="pixel size in Angstroms")   
+	parser.add_option("--rand_seed",              type="int",			    default=14567,              	 help="the seed used for generating random numbers (default 14567) for adding noise to the generated micrographs.")
 	
 	(options, args) = parser.parse_args()
 	if len(args) != 2:
@@ -83,8 +84,8 @@ def main():
 			if options.apix <= 0:
 				print "Please enter pixel size."
 				sys.exit()
-			generate_helimic(args[0], args[1], options.dp, options.dphi, options.apix, options.CTF)
-			sys.exit()
+			generate_helimic(args[0], args[1], options.dp, options.dphi, options.apix, options.CTF, options.rand_seed)
+		
 			
 def helicise_pdb(inpdb, outpdb, dp, dphi):
 	from math import cos, sin, pi
@@ -97,7 +98,6 @@ def helicise_pdb(inpdb, outpdb, dp, dphi):
 	infile.close()
 
 	p = []
-
 
 	pos = []
 	for i in xrange( len(pall) ):
@@ -146,6 +146,78 @@ def helicise_pdb(inpdb, outpdb, dp, dphi):
 	outfile.writelines(pnew)
 	outfile.writelines(pall[n-1:len(pall)])
 	outfile.close()
+
+def generate_helimic(refvol, outdir, dp, dphi, apix, CTF=False, rand_seed=14567):
+
+	seed(14567)
+	angles =[]
+	for i in xrange(3):
+		angles.append( [0.0+60.0*i, 90.0-i*5, 90.0, 0.0, 0.0] )
+	
+	nangle = len(angles)
+	modelvol = EMData()
+	modelvol.read_image(refvol)
+	
+	nx = modelvol.get_xsize()
+	ny = modelvol.get_ysize()
+	nz = modelvol.get_zsize()
+	
+	
+	
+	Cs      = 2.0
+	pixel   = options.apix
+	voltage = 200.0
+	ampcont = 10.0
+	
+	iprj    = 0
+	width = 500
+	xstart = 0
+	ystart = 0
+	
+	params = []
+	for idef in xrange(3,6):
+	
+	
+		mic = model_blank(2048, 2048)
+		defocus = idef*0.2
+		if options.CTF :
+			ctf = EMAN2Ctf()
+			ctf.from_dict( {"defocus":defocus, "cs":Cs, "voltage":voltage, "apix":pixel, "ampcont":ampcont, "bfactor":0.0} )
+	
+		sigma = 1.5 + random() # 1.5-2.5
+		addon = model_gauss(sigma, nx, ny, nz, sigma, sigma, nx/2, ny/2, nz/2 )
+		scale = 2500 * (0.5+random())
+		model = modelvol + scale*addon
+		volfts, kbx, kby, kbz = prep_vol( model )
+		i = idef - 3
+		for k in xrange(1):
+			dphi = 0.0 #8.0*(random()-0.5)
+			dtht = 0.0 #6.0*(random()-0.5)
+			psi  = 90+10*( i-1 )
+	
+			phi = angles[idef-3][0]+dphi
+			tht = angles[idef-3][1]-dtht
+	
+			s2x = 2.5*(i-1)
+			s2y = 2.5*(i-1)
+	
+	
+			params.append( [phi, tht, psi, s2x, s2y])
+	
+			proj = prgs(volfts, kbz, [phi, tht, psi, -s2x, -s2y], kbx, kby)
+			proj = Util.window(proj, 320, nz)		
+			mic += pad(proj, 2048, 2048, 1, 0.0, int(750*(i-1)), int(20*(i-1)), 0)
+	
+				
+	
+		mic += model_gauss_noise(30.0,2048,2048)
+		if options.CTF :
+			#apply CTF
+			mic = filt_ctf(mic, ctf)
+	
+		mic += filt_gaussl(model_gauss_noise(17.5,2048,2048), 0.3)
+		
+		mic.write_image("%s/mic%1d.hdf"%(outdir, idef-3),0)
 	
 if __name__ == "__main__":
 	main()
