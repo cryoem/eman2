7bd82772a917069db79f774872bbf0a66a7b3a51
fjia <fjia>
2013-05-03 15:41:29 +0000

    *** empty log message ***

--- sparx/bin/sxshiftali.py
+++ sparx/bin/sxshiftali.py
@@ -480,9 +480,9 @@ def helicalshiftali_MPI(stack, maskfile=None, maxit=100, CTF=False, snr=1.0, Fou
 		st = Util.infomask(data[im], mask, False)
 		data[im] -= st[0]
 		if CTF:
+			ctf_params = data[im].get_attr("ctf")
 			qctf = data[im].get_attr("ctf_applied")
 			if qctf == 0:
-				ctf_params = data[im].get_attr("ctf")
 				data[im] = filt_ctf(fft(data[im]), ctf_params)
 				data[im].set_attr('ctf_applied', 1)
 			elif qctf != 1:
