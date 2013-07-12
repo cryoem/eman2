82d2fe8efd675c6ede4f00c56c7bc1c506d633c1
cailin <cailin>
2013-07-12 19:42:43 +0000

    calculate the box size according to the atom locations.

--- programs/e2pdb2mrc.py
+++ programs/e2pdb2mrc.py
@@ -67,7 +67,7 @@ def main():
 	parser.add_argument("--apix", "-A", type=float, help="A/voxel", default=1.0)
 	parser.add_argument("--res", "-R", type=float, help="Resolution in A, equivalent to Gaussian lowpass with 1/e width at 1/res",default=2.8)
 	parser.add_argument("--box", "-B", type=str, help="Box size in pixels, <xyz> or <x>,<y>,<z>")
-	parser.add_argument("--het", action="store_true", help="Include HET atoms in the map", default=None)
+	parser.add_argument("--het", action="store_true", help="Include HET atoms in the map", default=False)
 	parser.add_argument("--chains",type=str,help="String list of chain identifiers to include, eg 'ABEFG'")
 	parser.add_argument("--quiet",action="store_true",default=False,help="Verbose is the default")
 	parser.add_argument("--ppid", type=int, help="Set the PID of the parent process, used for cross platform PPID",default=-1)
@@ -83,29 +83,95 @@ def main():
 	try: box=options.box
 	except: box=None	
 		
-	if (options.usenufft):		
-		if (options.box==None):
-			print "Must input the box size, <x>,<y>,<z>"
-			sys.exit(1)
-						
+	if (options.usenufft):
+		
 		try: boxs=options.box.split(',')
 		except:
-			raise Exception,"Invalid Input: %s"%optstr 
-		try: boxs=[int(i) for i in boxs]
+			pass 
+		try: 
+			boxs=[int(i) for i in boxs]
+			boxsizeinput=max(boxs)
 		except:
-			raise Exception,"Invalid Input: %s"%optstr				
-				
-		boxsize=max(boxs)
-				
+			pass				
+		
 		if options.addpdbbfactor==False:
 			addpdbbfactor=-1
 		else:
 			addpdbbfactor=1
+		
+		try : infile=open(args[0],"r")
+		except : raise IOError("%s is an invalid file name" %file_name)
+	
+	
+		#if res<=apix : print "Warning: res<=apix. Generally res should be 2x apix or more"
+
+		aavg=[0,0,0]	# calculate atomic center
+		amin=[1.0e20,1.0e20,1.0e20]		# coords of lower left front corner of bounding box
+		amax=[-1.0e20,-1.0e20,-1.0e20]	# coords
+		natm=0
+		atoms=[]		# we store a list of atoms to process to avoid multiple passes
+		nelec=0
+		mass=0
+
+	# parse the pdb file and pull out relevant atoms
+		for line in infile:
+			if (line[:4]=='ATOM' or (line[:6]=='HETATM' and options.het)) :
+				if chains and not (line[21] in chains) : continue
 			
+				try:
+					a=line[12:14].strip()
+					aseq=int(line[6:11].strip())
+					resol=int(line[22:26].strip())
+	
+					x=float(line[30:38])
+					y=float(line[38:46])
+					z=float(line[46:54])
+				except:
+					print "PDB Parse error:\n%s\n'%s','%s','%s'  '%s','%s','%s'\n"%(
+						line,line[12:14],line[6:11],line[22:26],line[30:38],line[38:46],line[46:54])
+					print a,aseq,resol,x,y,z
+
+				atoms.append((a,x,y,z))
+						
+				aavg[0]+=x
+				aavg[1]+=y
+				aavg[2]+=z
+				natm+=1
+				
+				amin[0]=min(x,amin[0])
+				amin[1]=min(y,amin[1])
+				amin[2]=min(z,amin[2])
+				amax[0]=max(x,amax[0])
+				amax[1]=max(y,amax[1])
+				amax[2]=max(z,amax[2])
+	
+				try:
+					nelec+=atomdefs[a.upper()][0]
+					mass+=atomdefs[a.upper()][1]
+				except:
+					print("Unknown atom %s ignored at %d"%(a,aseq))
+							
+		infile.close()
+		
+		print "%d atoms used with a total charge of %d e- and a mass of %d kDa"%(natm,nelec,mass/1000)
+		print "atomic center at %1.1f,%1.1f,%1.1f (center of volume at 0,0,0)"%(aavg[0]/natm,aavg[1]/natm,aavg[2]/natm)
+		print "Bounding box: x: %7.2f - %7.2f"%(amin[0],amax[0])
+		print "              y: %7.2f - %7.2f"%(amin[1],amax[1])
+		print "              z: %7.2f - %7.2f"%(amin[2],amax[2])
+	
+		#print "boxsize=", boxsize
+		try:
+			boxsize=boxsizeinput
+		except:
+			boxsize=max([(amax[0]-amin[0]),(amax[1]-amin[1]),(amax[2]-amin[2])])
+			boxsize=int(1.9*boxsize+(2.0*options.res/options.apix))
+		 
 		pa=PointArray()
 		pa.read_from_pdb(args[0])
 		out=pa.pdb2mrc_by_summation(boxsize,options.apix,options.res,addpdbbfactor)
 		out.write_image(args[1])
+		
+		print '\r   %d\nConversion complete'%len(atoms)
 
 	else:
 		outmap = pdb_2_mrc(args[0],options.apix,options.res,options.het,box,chains,options.quiet)
