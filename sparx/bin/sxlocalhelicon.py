9e4a8fc9e282b22eb55b64d1bed5ad69a4d8f3d3
fjia <fjia>
2013-08-13 16:43:29 +0000

    import from applications

--- sparx/bin/sxlocalhelicon.py
+++ sparx/bin/sxlocalhelicon.py
@@ -75,7 +75,6 @@ def main():
 	parser.add_option("--debug",              action="store_true",   default=False,               help="debug")
 	parser.add_option("--initial_theta",      type="float",		     default=90.0,                help="intial theta for reference projection")
 	parser.add_option("--delta_theta",        type="float",		     default=1.0,                 help="delta theta for reference projection")
-	parser.add_option("--test",               action="store_true",   default=False,      		  help="test")
 	parser.add_option("--boundaryavg",        action="store_true",   default=False,      		  help="boundaryavg")
 	parser.add_option("--MA_WRAP",            type="int",            default= 0,                  help="do wrapping in MA if MA_WRAP=1, else no wrapping in MA. Default is 0.")
 	parser.add_option("--seg_ny",             type="int",            default= 256,                help="y dimension of desired segment size, should be related to fract in that fract ~ seg_ny/ny, where ny is dimension of input projections.")
@@ -126,11 +125,11 @@ def main():
 			disable_bdb_cache()
 
 		
-		from development import localhelicon_MPI
+		from applications import localhelicon_MPI
 		global_def.BATCH = True
 		if len(args) < 4:  mask = None
 		else:               mask = args[3]
-		localhelicon_MPI(args[0], args[1], args[2], options.seg_ny, mask, irp, oup, options.rs, xrp, options.ynumber, txsp, options.delta, options.initial_theta, options.delta_theta, options.an, options.maxit, options.CTF, options.snr, options.dp, options.dphi, options.psi_max, rminp, rmaxp, options.fract, options.npad,options.sym, options.function, options.apix, options.debug, y_restrict2, options.MA, options.searchit,options.test, options.boundaryavg, options.MA_WRAP)
+		localhelicon_MPI(args[0], args[1], args[2], options.seg_ny, mask, irp, oup, options.rs, xrp, options.ynumber, txsp, options.delta, options.initial_theta, options.delta_theta, options.an, options.maxit, options.CTF, options.snr, options.dp, options.dphi, options.psi_max, rminp, rmaxp, options.fract, options.npad,options.sym, options.function, options.apix, options.debug, y_restrict2, options.MA, options.searchit, options.boundaryavg, options.MA_WRAP)
 		global_def.BATCH = False
 			
 		if options.MPI:
