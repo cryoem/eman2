d03808bbeb8d37094b9b4055915533a6be2205f3
scmurray <scmurray>
2014-01-14 19:02:27 +0000

    Changed to work with EMAN 2.1

--- programs/e2refinetofrealign.py
+++ programs/e2refinetofrealign.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Author: Stephen Murray (scmurray@bcm.edu), 2/14/11
-# Copyright (c) 2000-2011 Baylor Colelge of Medicine
+# Copyright (c) 2000-2013 Baylor College of Medicine
 
 # Official copyright notice. EMAN2 is distributed under a joint GPL/BSD license. Please copy
 # this statement from one of the other programs. You must agree to use this license if your
@@ -17,14 +17,19 @@ import sys
 from subprocess import *
 
 SEP = 1                                # First is always best
-CLS = 6                                # Number of images in the cls_result_## bdb
+CLS = 7                               # Number of images in the cls_result_## bdb
+CLASS = 0
+DX = 2
+DY = 3
 DALPHA = 4                             # index of the dalpha image
+FLIP = 5
 APERPIX = 100000                       # set as 10 microns
 ANGAST = 0                             # eman2 throws out astygmatic particles so the angle is always 0
 PRESA = 0                              # default
 SPACE = ' '                            # to clean up the string manipulation later
 FBEAUT = 'F'
-FCREF = 'F'
+FFILT = 'F'
+FBFACT = 'F'
 IFSC = '0'
 FSTAT = 'F'
 IBLOW = '2'
@@ -95,81 +100,126 @@ mcrscp = {}
 high = args[1]
 if int(high) < 10:
    high = "0" + high
-dir = "bdb:" + dir
 
-db = db_open_dict(dir + "#projections_" + high)                     # Projections
 
-num_images = EMUtil.get_image_count(dir + "#projections_" + high)   # Number of classes
+s = "e2proc2d.py " + dir + "/classes_" + high +"_even.hdf class_stack_temp.hdf --interlv=" + dir + "/classes_" + high +"_odd.hdf --verbose=0"
+call(s,shell=True)
+class_list = {}
+class_list = EMData.read_images("class_stack_temp.hdf")
+num_classes = len(class_list)   # Number of classes
+az_list = {}
+alt_list = {}
+exc = 0
+command_dict = js_open_dict(dir+"/0_refine_parms.json")
+for i in range(num_classes):
+   for index in class_list[i]['class_ptcl_idxs']:
+      if i%2 == 0:
+         az_list[2*index] = class_list[i]['xform.projection'].get_rotation("eman")['az']
+         alt_list[2*index] = class_list[i]['xform.projection'].get_rotation("eman")['alt']
+         if 'exc_class_ptcl_idxs' in class_list[i].get_attr_dict():
+            if isinstance(class_list[i]['exc_class_ptcl_idxs'], ( int, long )):
+               az_list[2*int(class_list[i]['exc_class_ptcl_idxs'])] = class_list[i]['xform.projection'].get_rotation("eman")['az']
+               alt_list[2*int(class_list[i]['exc_class_ptcl_idxs'])] = class_list[i]['xform.projection'].get_rotation("eman")['alt']
+            else:
+               for exc_index in class_list[i]['exc_class_ptcl_idxs']:
+                  az_list[2*exc_index] = class_list[i]['xform.projection'].get_rotation("eman")['az']
+                  alt_list[2*exc_index] = class_list[i]['xform.projection'].get_rotation("eman")['alt']
+      else:
+         az_list[2*index+1] = class_list[i]['xform.projection'].get_rotation("eman")['az']
+         alt_list[2*index+1] = class_list[i]['xform.projection'].get_rotation("eman")['alt']
+         if 'exc_class_ptcl_idxs' in class_list[i].get_attr_dict():
+            if isinstance(class_list[i]['exc_class_ptcl_idxs'], ( int, long )):
+               az_list[2*int(class_list[i]['exc_class_ptcl_idxs'])+1] = class_list[i]['xform.projection'].get_rotation("eman")['az']
+               alt_list[2*int(class_list[i]['exc_class_ptcl_idxs'])+1] = class_list[i]['xform.projection'].get_rotation("eman")['alt']
+            else:
+               for exc_index in class_list[i]['exc_class_ptcl_idxs']:
+                  az_list[2*exc_index+1] = class_list[i]['xform.projection'].get_rotation("eman")['az']
+                  alt_list[2*exc_index+1] = class_list[i]['xform.projection'].get_rotation("eman")['alt']
+   
+total_len = len(az_list)
+even_set_name = class_list[0].get_attr_dict()['class_ptcl_src']
+odd_set_name = class_list[1].get_attr_dict()['class_ptcl_src']
+all_set_name = even_set_name.replace("_even",'')
+all_set_data = EMData.read_images(all_set_name)
 
-cmd = db_open_dict(dir + "#register")                               # Register containing parameters used in the refinement
-cmd = cmd['cmd_dict']
-inpt1 = cmd['input']						    # Input filename for refinement
-inpt=inpt1.split("_ctf")[0]
-part2 = db_open_dict(inpt1)
-if not 'ctf' in part2[0].get_attr_dict():
-   print "This command cannot be run with data that has not been CTF corrected"
-   exit(-1)
-part = db_open_dict(inpt)
-tmp = part2[0]['ctf'].to_dict()                                      # CTF has things like Defocus, apix, etc
-
-for i in range(num_images):
-   classes[i]= db[i]['xform.projection'].get_rotation("eman")        # Has the angles needed to get the Euler Angles
-
-nx = db[0]['nx']
-cls_dir = dir + "#cls_result_" + high                               # CLS results. 
-
-s = "e2proc2d.py " + inpt + " " + E2FA + "/particlestack.mrc --twod2threed --process=normalize.edgemean --verbose=0"
+s = "e2proc2d.py " + all_set_name + " " + E2FA + "/particlestack.mrc --twod2threed --process=normalize.edgemean --verbose=0"
 call(s, shell=True)
-s = "e2proc3d.py " + dir + "#threed_filt_" + high + " " + E2FA + "/3DMapInOut.mrc --process=normalize.edgemean --verbose=0"
+s = "e2proc3d.py " + dir + "/threed_" + high + ".hdf " + E2FA + "/3DMapInOut.mrc --process=normalize.edgemean --verbose=0"
 call(s, shell=True)
-# Retrieve the pixel values of each image in the CLS stack.
-for i in range(CLS):
-   img = EMData(cls_dir, i)
-   ny = img['ny']
-   ptcl = {}
-   for j in range(ny):
-       ptcl[j] = img[SEP-1,j]        
-   ptcl_data[i] = ptcl
+s = "e2proc2d.py " + dir + "/cls_result_" + high + "_even.hdf cls_stack_temp.hdf --interlv=" +  dir + "/cls_result_" + high + "_odd.hdf --writejunk --verbose=0"
+call(s, shell=True)
 
-# Write output file of particle meta data for FreAlign into the E2FA subdirectory created above
+
+#if not 'ctf' in part2[0].get_attr_dict():
+   #print "This command cannot be run with data that has not been CTF corrected"
+   #exit(-1)
+#part = EMData.read_images(str(inpt))
+#tmp = part2[0]['ctf'].to_dict()                                      # CTF has things like Defocus, apix, etc
+
+
+### Retrieve the pixel values of each image in the CLS stack.
+cls_stack = EMData.read_images("cls_stack_temp.hdf")
+ny = cls_stack[0]['ny']
+dx_list = {}
+dy_list = {}
+dalpha_list = {}
+flip_list = {}
+cls_class_list = {}
+for i in range(ny):
+   cls_class_list[i] = cls_stack[2*CLASS][i]
+   cls_class_list[i+ny] = cls_stack[2*CLASS+1][i]
+   dx_list[i] = cls_stack[2*DX][i]
+   dx_list[i+ny] = cls_stack[2*DX+1][i]
+   dy_list[i] = cls_stack[2*DY][i]
+   dy_list[i+ny] = cls_stack[2*DY+1][i]   
+   dalpha_list[i] = cls_stack[2*DALPHA][i]
+   dalpha_list[i+ny] = cls_stack[2*DALPHA+1][i]
+   flip_list[i] = cls_stack[2*FLIP][i]
+   flip_list[i+ny] = cls_stack[2*FLIP+1][i]
+
+   
+## Write output file of particle meta data for FreAlign into the E2FA subdirectory created above
 f = open(OUTFILE1,'w')
 film = 0
 bool_found = 0
 film_dict = {}
-for i in range(ny):
-   ctf_dict = part2[i]['ctf'].to_dict()
+ctf_dict = {}
+
+for i in range(len(all_set_data)):
+   ptcl = all_set_data[i]
+   ctf_dict = ptcl['ctf'].to_dict()
    defocus = ctf_dict['defocus'] * 10000
    for item in film_dict:
       if ctf_dict == film_dict[item]:
          film = item
          bool_found = 1
-
    if not bool_found:
       film = len(film_dict)
       film_dict[film] = ctf_dict
    bool_found = 0
    mag = APERPIX / ctf_dict['apix']
-   class_num = int(ptcl_data[0][i])
-   if ptcl_data[5][i]:
-      t = Transform({"type":"eman","az":classes[class_num]['az']+180,"alt":180-classes[class_num]['alt'],"phi":ptcl_data[DALPHA][i]*-1,"tx":ptcl_data[2][i],"ty":ptcl_data[3][i]})
+   class_num = cls_class_list[i]   
+   #t = Transform({"type":"eman","az":az_list[i],"alt":alt_list[i],"phi":dalpha_list[i],"tx":dx_list[i],"ty":dy_list[i]})
+   if flip_list[i] == 1:
+      t = Transform({"type":"eman","az":az_list[i]+180,"alt":180-alt_list[i],"phi":dalpha_list[i]*-1,"tx":dx_list[i],"ty":dy_list[i]})
    else:
-      t = Transform({"type":"eman","az":classes[class_num]['az'],"alt":classes[class_num]['alt'],"phi":ptcl_data[DALPHA][i],"tx":ptcl_data[2][i],"ty":ptcl_data[3][i]})
+      t = Transform({"type":"eman","az":az_list[i],"alt":alt_list[i],"phi":dalpha_list[i],"tx":dx_list[i],"ty":dy_list[i]})
    t = t.inverse()
    s = '{0:7d}{1:8.2f}{2:8.2f}{3:8.2f}{4:8.2f}{5:8.2f}{6:7.0f}.{7:6d}{8:9.1f}{9:9.1f}{10:8.2f}{11:7.2f}{12:6.2f}\n'.format(i+1, t.get_rotation("mrc")['phi'], t.get_rotation("mrc")['theta'], t.get_rotation("mrc")['omega'], t.get_trans()[0], t.get_trans()[1], mag, film + 1, defocus, defocus, ANGAST, PRESA, PRESA)
+   #s = '{0:7d}{1:8.2f}{2:8.2f}{3:8.2f}{4:8.2f}{5:8.2f}{6:7.0f}.{7:6d}{8:9.1f}{9:9.1f}{10:8.2f}{11:7.2f}{12:6.2f}\n'.format(i+1, t.get_rotation("eman")['az'], t.get_rotation("eman")['alt'], t.get_rotation("eman")['phi'], t.get_trans()[0], t.get_trans()[1], mag, film + 1, defocus, defocus, ANGAST, PRESA, PRESA)
    f.write(s)
+s = "rm class_stack_temp.hdf cls_stack_temp.hdf "
+call(s,shell=True)
 f.close()
 
+#####################################################################################################################
+##														   #
+## Card Creation For FreAlign. Please see the FreAlign README.TXT file for more information about each Card or Flag #
+## 											 			   #
+#####################################################################################################################
 
 
-####################################################################################################################
-#														   #
-# Card Creation For FreAlign. Please see the FreAlign README.TXT file for more information about each Card or Flag #
-# 											 			   #
-####################################################################################################################
-
-
-RO = str(tmp['apix']*.375*(part[0]['nx']))
-
+RO = str(command_dict['apix']*.375*(all_set_data[0]['nx']))
 for option1 in optionList:
 	if option1 == "fbeaut":
 		FBEAUT = 'T'
@@ -184,8 +234,8 @@ for option1 in optionList:
 	elif option1 == "fstat":
 		FSTAT = 'T'
 	elif option1 == "randomizemodel":
-		if options.randomizemodel != 0:
-			s1 = "e2proc3d.py " + E2FA + "/3DMapInOut.mrc " + E2FA + "/3DMapInOut.mrc --process=filter.lowpass.randomphase:apix=" + str(tmp['apix']) + ":cutoff_freq=" + str(1/options.randomizemodel)
+		if float(options.randomizemodel) != 0.0:
+			s1 = "e2proc3d.py " + E2FA + "/3DMapInOut.mrc " + E2FA + "/3DMapInOut.mrc --process=filter.lowpass.randomphase:apix=" + str(command_dict['apix']) + ":cutoff_freq=" + str(1/float(options.randomizemodel))
 			call(s1, shell=True)
 
 OUTFILE2 = E2FA + "/card.txt"          # Cards required by FA
@@ -197,7 +247,7 @@ CFORM = 'M'
 IFLAG = '1'
 FMAG = FDEF = FASTIG = FPART = FMATCH = 'F'
 IEWALD = '0'
-s = CFORM + SPACE + IFLAG + SPACE + FMAG + SPACE + FDEF + SPACE + FASTIG + SPACE + FPART + SPACE + IEWALD + SPACE + FBEAUT + SPACE + FCREF + SPACE + FMATCH + SPACE + IFSC + SPACE + FSTAT + SPACE + IBLOW + '\n'
+s = CFORM + SPACE + IFLAG + SPACE + FMAG + SPACE + FDEF + SPACE + FASTIG + SPACE + FPART + SPACE + IEWALD + SPACE + FBEAUT + SPACE + FFILT + SPACE + FBFACT + SPACE + FMATCH + SPACE + IFSC + SPACE + FSTAT + SPACE + IBLOW + '\n'
 f.write(s)
 
 # Card 2
@@ -206,8 +256,8 @@ XSTD = '1'
 PBC = '100'
 BOFF = '60'
 IPMAX = '10' 
-PSIZE = str(tmp['apix']) 
-WGH = str(tmp['ampcont']/100)
+PSIZE = str(command_dict['apix']) 
+WGH = str(ctf_dict['ampcont']/100)
 s = RO + SPACE + RI + SPACE + PSIZE + SPACE + WGH + SPACE + XSTD + SPACE + PBC + SPACE + BOFF + SPACE + DANG + SPACE + ITMAX + SPACE + IPMAX + '\n'
 f.write(s)
  
@@ -217,11 +267,11 @@ f.write(MASK + '\n')
 
 # Card 4
 IFIRST = '1'
-ILAST = str(ny)
+ILAST = str(ny*2)
 f.write(IFIRST + SPACE + ILAST + '\n')
 
 # Card 5
-sym = cmd['sym']
+sym = command_dict['sym']
 if sym[0] == 'c':      # Convert from EMAN2 symmetry conventions to FreAlign symmetry conventions
    ASYM = 'C' + sym[1]
 elif sym[0] == 'd':
@@ -243,8 +293,8 @@ RELMAG = 1.0
 DSTEP = 10 
 TARGET = 15
 THRESH = 90
-CS = tmp['cs']
-AKV = tmp['voltage'] 
+CS = ctf_dict['cs']
+AKV = ctf_dict['voltage'] 
 TX = TY = 0.0
 s = str(RELMAG) + SPACE + str(DSTEP) + SPACE + str(TARGET) + SPACE + str(THRESH) + SPACE + str(CS) + SPACE + str(AKV) + SPACE + str(TX) + SPACE + str(TY) + "\n"
 f.write(s)
