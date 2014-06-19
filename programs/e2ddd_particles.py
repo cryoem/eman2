1abd03f7ed3297d0b717adbe43e30d422cc3857f
stevel <stevel>
2014-06-19 17:39:45 +0000

    debugging

--- programs/e2ddd_particles.py
+++ programs/e2ddd_particles.py
@@ -161,25 +161,37 @@ def main():
 
 			# write the aligned, clipped average to the output file
 			newpt.process_inplace("normalize.edgemean")
-			newpt.write_image("{}/{}_ptcls.hdf".format(newproj,base_name(ptloc[1])),ptloc[0])
+			newpt.write_image("{}/particles/{}_ptcls.hdf".format(newproj,base_name(ptloc[1])),ptloc[0])
 			
 			if options.verbose>1 : print i,movie,ptloc[0],int(cls[0][0,i])
 			
 	E2end(pid)
 
-def alignstack(ref,stack,verbose=0):
+def alignstack(refo,stack,verbose=0):
 	"""aligns a movie stack to a reference image, with some constraints forcing the relative alignments to follow a pattern
 	Returns the aligned average."""
 	
-	nx=ref["nx"]
-	ny=ref["ny"]
-
-	outim2=[]
+	global cnt
+	nx=refo["nx"]
+	ny=refo["ny"]
 	
-	outim=stack
+	# a little preprocessing on the stack
+	outim=[i.get_clip(Region(-nx/2,-ny/2,nx*2,ny*2)) for i in stack]
+	
+	for i in outim:
+		i.scale(2.0)
+		i.process_inplace("filter.highpass.gauss",{"cutoff_abs":.002})
+		i.process_inplace("filter.lowpass.gauss",{"cutoff_abs":.1})
+	
+	ref=refo.get_clip(Region(-nx/2,-ny/2,nx*2,ny*2))
+	ref.scale(2.0)
+
+	nx*=2
+	ny*=2
+	
 	xali=XYData()			# this will contain the alignments which are hierarchically estimated and improved
 	yali=XYData()			# x is time in both cases, y is x or y
-	for it in xrange(2):
+	for it in xrange(1):
 		step=len(outim)
 		
 		while step>1:
@@ -190,55 +202,106 @@ def alignstack(ref,stack,verbose=0):
 				if i1-i0<step/2 : 
 					i0+=step
 					continue
-				av=sum([outim[i].process("xform.translate.int",{"trans":(int(xali.get_yatx_smooth(i,1)),int(yali.get_yatx_smooth(i,1)))}) for i in xrange(i0,i1)])
-				
+#				av=sum([outim[i].process("xform.translate.int",{"trans":(int(xali.get_yatx_smooth(i,1)),int(yali.get_yatx_smooth(i,1)))}) for i in xrange(i0,i1)])
+				av=sum(outim[i0:i1])
+
 				tloc=(i0+i1-1)/2.0		# the "time" of the current average
 				if step>=len(outim)/2 : lrange=nx/4
 				else: lrange=hypot(xali.get_yatx_smooth(i1,1)-xali.get_yatx_smooth(i0,1),yali.get_yatx_smooth(i1,1)-yali.get_yatx_smooth(i0,1))
-				if lrange<8 : lrange=8		
+				if lrange<6 : lrange=6	
 				
 				guess=(xali.get_yatx_smooth(tloc,1),yali.get_yatx_smooth(tloc,1))
 				
-				if verbose :print step,i0,xali.get_yatx_smooth(tloc,1),yali.get_yatx_smooth(tloc,1),lrange
+				if verbose :print step,i0,guess,lrange,
 
 				ccf=av.calc_ccf(ref,fp_flag.CIRCULANT,1)		# centered CCF
 				ccf.process_inplace("normalize.edgemean")
-				ccf.clip_inplace(Region(nx/2-lrange,ny/2-lrange,lrange*2,lrange*2))
+				csig=ccf["sigma"]
+				cmean=ccf["mean"]
+#				ccf.write_image("xyz.hdf",-1)
+				ccf.clip_inplace(Region(nx/2-lrange-guess[0],ny/2-lrange-guess[1],lrange*2,lrange*2))
 				dx,dy,dz=ccf.calc_max_location()
-				try: Z=ccf[dx,dy]/ccf["sigma"]
+				try: Z=(ccf[dx,dy]-cmean)/csig
 				except: 
-					print "Bad alignment {},{} on {} to {}".format(dx,dy,i0,i1)
+					print "\nBad alignment {},{} on {} to {}".format(dx,dy,i0,i1)
 					i0+=step
 					continue
-
+				
+				if verbose: print "###",dx-lrange,dy-lrange,
+				
+				if dx>0 and dy>0 and dx<lrange*2-1 and dy<lrange*2-1 :
+					wx=(ccf[dx-1,dy-1]+ccf[dx-1,dy]+ccf[dx-1,dy+1],ccf[dx,dy-1]+ccf[dx,dy]+ccf[dx,dy+1],ccf[dx+1,dy-1]+ccf[dx+1,dy]+ccf[dx+1,dy+1]) # peak weights in x around peak
+					wy=(ccf[dx-1,dy-1]+ccf[dx,dy-1]+ccf[dx+1,dy-1],ccf[dx-1,dy-1]+ccf[dx,dy-1]+ccf[dx+1,dy-1],ccf[dx-1,dy-1]+ccf[dx,dy-1]+ccf[dx+1,dy-1]) # peak weights in y around peak
+					dx=((dx-1)*wx[0]+dx*wx[1]+(dx+1)*wx[2])/sum(wx)
+					dy=((dy-1)*wy[0]+dy*wy[1]+(dy+1)*wy[2])/sum(wy)
+				
+				
+				
 				dx-=lrange
 				dy-=lrange
+				if verbose: print dx,dy,Z
 				
-				xali.insort(tloc,xali.get_yatx_smooth(tloc,1)-dx)
-				yali.insort(tloc,yali.get_yatx_smooth(tloc,1)-dy)
-				xali.dedupx()
-				yali.dedupx()
+				# we only include points where the alignment seems reliable
+				if Z>5.0 :
+					xali.insort(tloc,guess[0]-dx)
+					yali.insort(tloc,guess[1]-dy)
+					xali.dedupx()
+					yali.dedupx()
 									
 				i0+=step
 			
 			# Smoothing
-			# we should have all possible x-values at this point, so we just do a very simplistic smoothing
+			# we just do a very simplistic smoothing at each step
 
 			for i in xrange(xali.get_size()-2):
 				xali.set_y(i+1,(xali.get_y(i)+xali.get_y(i+1)*2.0+xali.get_y(i+2))/4.0)
 				yali.set_y(i+1,(yali.get_y(i)+yali.get_y(i+1)*2.0+yali.get_y(i+2))/4.0)
+				
+			if xali.get_size()>=4:
+				# smooth first point
+				lp=xali.get_size()-1
+				sx0=xali.get_x(1)-xali.get_x(0)
+				sx1=xali.get_x(lp-1)-xali.get_x(1)
+				sl=(xali.get_y(lp-1)-xali.get_y(1))/sx1
+				xali.set_y(0,(xali.get_y(1)-sl*sx0+xali.get_y(0))/2.0)
+				sl=(yali.get_y(lp-1)-yali.get_y(1))/sx1
+				yali.set_y(0,(yali.get_y(1)-sl*sx0+yali.get_y(0))/2.0)
 
-			if verbose :
+				# smooth last point
+				print xali.get_y(lp),yali.get_y(lp),"->",
+				sx1=xali.get_x(lp-1)-xali.get_x(1)
+				sx0=xali.get_x(lp)-xali.get_x(lp-1)
+				sl=(xali.get_y(lp-1)-xali.get_y(1))/sx1
+				xali.set_y(lp,(xali.get_y(lp-1)+sl*sx0+xali.get_y(lp))/2.0)
+				sl=(yali.get_y(lp-1)-yali.get_y(1))/sx1
+				yali.set_y(lp,(yali.get_y(lp-1)+sl*sx0+yali.get_y(lp))/2.0)
+				print xali.get_y(lp),yali.get_y(lp)
+			   
+
+			if verbose>1 :
 				print ["%6.1f"%i for i in xali.get_xlist()]
 				print ["%6.2f"%i for i in xali.get_ylist()]
 				print ["%6.2f"%i for i in yali.get_ylist()]
+
+			out=file("path_{}_{}.txt".format(cnt,step),"w")
+			for i in xrange(len(outim)): out.write("{}\t{}\t{}\n".format(i,xali.get_yatx_smooth(i,1),yali.get_yatx_smooth(i,1)))
+
+	cnt+=1
 			
 		
-	av=sum([outim[i].process("xform.translate.int",{"trans":(int(xali.get_yatx_smooth(i,1)),int(yali.get_yatx_smooth(i,1)))}) for i in xrange(len(outim))])
+	av=sum([stack[i].process("xform.translate.int",{"trans":(int(xali.get_yatx_smooth(i,1)/2.0),int(yali.get_yatx_smooth(i,1)/2.0))}) for i in xrange(len(outim))])
 	av.mult(1.0/len(outim))
+	
+	
+	if verbose :
+		print ["%6.1f"%i for i in xali.get_xlist()]
+		print ["%6.2f"%i for i in xali.get_ylist()]
+		print ["%6.2f"%i for i in yali.get_ylist()]
+	
 
 	return av
 	
+cnt=1
 
 if __name__ == "__main__":
 	main()
