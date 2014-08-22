52b6c05abaef2826592875c28d9a26d811cc2551
stevel <stevel>
2014-08-22 13:56:48 +0000

    Possible fix for define issues with int min/max

--- libEM/hdfio.cpp
+++ libEM/hdfio.cpp
@@ -50,6 +50,23 @@
 	#define  MAXPATHLEN (MAX_PATH * 4)
 #endif	//_WIN32
 
+#ifndef UINT_16_MAX
+/* Minimum of signed integral types.  */
+# define INT8_MIN               (-128)
+# define INT16_MIN              (-32767-1)
+# define INT32_MIN              (-2147483647-1)
+
+/* Maximum of signed integral types.  */
+# define INT8_MAX               (127)
+# define INT16_MAX              (32767)
+# define INT32_MAX              (2147483647)
+
+/* Maximum of unsigned integral types.  */
+# define UINT8_MAX              (255)
+# define UINT16_MAX             (65535)
+# define UINT32_MAX             (4294967295U)
+#endif
+
 using namespace EMAN;
 
 hid_t HdfIO::mapinfo_type = -1;
