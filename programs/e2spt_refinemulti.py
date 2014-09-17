b40eaba5d8c846476dd26894efd3d05316f1dfc8
jgalaz <jgalaz>
2014-09-17 09:05:52 +0000

    .

--- programs/e2spt_refinemulti.py
+++ programs/e2spt_refinemulti.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #
-# Author: Jesus Galaz-Montoya  November/21/2013
+# Author: Jesus Galaz-Montoya  November/21/2013; modified Sep/16/2014
 # Copyright (c) 2011- Baylor College of Medicine
 #
 # This software is issued under a joint BSD/GNU license. You may use the
