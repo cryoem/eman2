699e8f905c8cd125e084b85a5e1028d903b476a3
pawel <pawel>
2014-09-26 21:48:27 +0000

    *** empty log message ***

--- sparx/bin/sxconsistency.py
+++ sparx/bin/sxconsistency.py
@@ -30,10 +30,10 @@
 #
 #
 
-#  VERSION 2  08/29/2014
+#  VERSION 2  09/25/2014
 
-from EMAN2 import *
-from sparx import *
+from   EMAN2 import *
+from   sparx import *
 import os
 import global_def
 from   global_def import *
@@ -41,7 +41,6 @@ from   optparse import OptionParser
 import sys
 from   random import shuffle
 import subprocess
-
 
 
 
@@ -177,7 +176,7 @@ def apply_rotation_gaps(angleset1, qrot):
 			angleset1[i] = [d["phi"], d["theta"], d["psi"], -d["tx"], -d["ty"]]
 
 
-def errors_per_image(nn, qt, asi, avgtrans, thresherr=1.0, radius = 1.0):
+def errors_per_image_original(nn, qt, asi, avgtrans, thresherr=1.0, radius = 1.0):
 	#print "errors per image"
 	#  We have average parameters and transformed parameters
 	#  Find errors per image
@@ -217,6 +216,40 @@ def errors_per_image(nn, qt, asi, avgtrans, thresherr=1.0, radius = 1.0):
 	perr = [perr[k][1] <= thresherr for k in xrange(nn)]
 	return perr
 
+def errors_per_image(params, avgtrans, thresherr=1.0, radius = 1.0):
+	#print "errors per image"
+	#  We have average parameters and transformed parameters
+	#  Find errors per image
+	ll = len(params)
+	nn = len(params[0])
+	perr = [None]*nn
+	for k in xrange(nn):
+		#  
+		nas = [0.0,0.0,0.0]
+		r1,r2,r3 = getfvec(avgtrans[k][0],avgtrans[k][1])
+		sacos = 0.0
+		sder = 0.0
+		ser3 = 0.0
+		for i in xrange(ll):
+			#print i,fifi[i]
+			d = max_3D_pixel_error(params[i][k], avgtrans[k], r=radius)
+			#if(d>10.):  print  "  LARGE ERROR",k,i,d,fifi[i], avgtrans[k]
+			ser3 += d
+			n1,n2,n3 = getfvec(params[i][k][0],params[i][k][1])
+			sacos += acos( min(1.0, n1*r1+n2*r2+n3*r3) )
+			sder += pixel_error_2D(params[i][k][2:], avgtrans[k][2:], r = 1.0)
+		# average deviation in radians
+		sacos /= ll
+		sare   = tan(sacos)
+		sder  /= ll
+		ser3  /= ll
+		#print  k, ser3, sare, sder
+		perr[k] = [ser3, sacos, sare, sder]
+
+	#write_text_row(perr, 'per.txt')
+	#perr = [perr[k][1] <= thresherr for k in xrange(nn)]
+	return perr
+
 
 
 def main():
@@ -246,7 +279,7 @@ def main():
 			j,k = MPI_start_end(nn, 4, i)
 			chunks.append(t[j:k])
 			chunks[i].sort()
-			write_text_file(chunks[i],os.path.join(outdir,'chunk%1d.txt'%i))
+			write_text_file(chunks[i],os.path.join(outdir,'chunk%01d.txt'%i))
 
 		del t
 		write_text_file([ len(chunks[i]) for i in xrange(4) ],os.path.join(outdir,'chunklengths.txt'))
@@ -260,9 +293,9 @@ def main():
 				ll+=1
 
 		for i in xrange(6):
-			listfile = os.path.join(outdir,'lili%1d.txt'%i)
+			listfile = os.path.join(outdir,'lili%01d.txt'%i)
 			write_text_file(pt[i],listfile)
-			outbdb = "bdb:"+ os.path.join(outdir,"X%1d"%i)
+			outbdb = "bdb:"+ os.path.join(outdir,"X%01d"%i)
 			cmd = '{} {} {} {}'.format('e2bdb.py', inputbdb, '--makevstack='+outbdb, '--list='+listfile)
 			subprocess.call(cmd, shell=True)
 		"""
@@ -274,7 +307,7 @@ def main():
 		ndigits = options.ndigits  #for chc5 1, for ribo 4#4.0
 		prms = []
 		for i in xrange(6):
-			prms.append( read_text_row(os.path.join(outdir,options.params+"%1d.txt"%i)) )
+			prms.append( read_text_row(os.path.join(outdir,options.params+"%01d.txt"%i)) )
 			for j in xrange(len(prms[-1])):
 				for k in xrange(5):
 					prms[-1][j][k] = round(prms[-1][j][k], ndigits)
@@ -330,12 +363,12 @@ def main():
 		chunks = {}
 		chunklengths = {}
 		for i in xrange(4):
-			chunks[chr(65+i)] = map(int,read_text_file(os.path.join(outdir,"chunk"+"%1d.txt"%i)))
+			chunks[chr(65+i)] = map(int,read_text_file(os.path.join(outdir,"chunk"+"%01d.txt"%i)))
 			chunklengths[chr(65+i)] = len(chunks[chr(65+i)])
 
 
 		for i in xrange(6):
-			prms = read_text_row(os.path.join(outdir,options.params+"%1d.txt"%i))
+			prms = read_text_row(os.path.join(outdir,options.params+"%01d.txt"%i))
 			for q in blocks:
 				if params.has_key(q+chr(48+i)):
 					params[q+chr(48+i)] = prms[:chunklengths[q]]
@@ -386,7 +419,7 @@ def main():
 						nas[1] += n2
 						nas[2] += n3
 						print qnom, n1,n2,n3,nas
-					#  To get the correct pixer phi angle as to be taken from the above!!
+					#  To get the correct pixer phi angle has to be taken from the above!!
 					pixer[q[0][0]][j] = max_3D_pixel_error(fifi[0], fifi[1], r=radius)
 
 				nom = sqrt(nas[0]**2 + nas[1]**2 + nas[2]**2)
@@ -433,8 +466,8 @@ def main():
 				else:
 					bad[i].append(chunks[q][k])
 					#[chunks[q][k],pixer[q][k],deprt, params[pairs[i][0]][k],params[pairs[i][1]][k],params[lefts[i]][k],avgtrans[q][k]])
-			write_text_file( good[i], os.path.join(outdir,"newgood%1d.txt"%i) )
-			write_text_file( bad[i],  os.path.join(outdir,"newbad%1d.txt"%i) )
+			write_text_file( good[i], os.path.join(outdir,"newgood%01d.txt"%i) )
+			write_text_file( bad[i],  os.path.join(outdir,"newbad%01d.txt"%i) )
 		#  Write chunklengths
 		chunklengths = [len(good[i]) for i in xrange(4)]
 		write_text_file(chunklengths, os.path.join(outdir,"chunklengths.txt") )
@@ -442,7 +475,7 @@ def main():
 		ll = 0
 		for i in xrange(3):
 			for j in xrange(i+1,4):
-				write_text_file( good[i] + good[j], os.path.join(outdir,"newlili%1d.txt"%ll) )
+				write_text_file( good[i] + good[j], os.path.join(outdir,"newlili%01d.txt"%ll) )
 				ll += 1
 		#  Generate newx files from newgood, these contain consecutive (with gaps) numbering that allows to generate truncated X files from the previous X files
 		ll = 0
@@ -458,7 +491,7 @@ def main():
 					if  perr[blocks[j]][k]:
 						secondblock.append(k+l)
 
-				write_text_file( firstblock + secondblock, os.path.join(outdir,"goodX%1d.txt"%ll) )
+				write_text_file( firstblock + secondblock, os.path.join(outdir,"goodX%01d.txt"%ll) )
 				ll += 1
 		del good,bad,firstblock,secondblock,perr
 		#  write out parameters, for those in pairs write out average, for leftouts leave them as they were
@@ -471,7 +504,7 @@ def main():
 						prms += params[q+chr(48+i)]
 					except:
 						prms += avgtrans[q]
-			write_text_row(prms, os.path.join(outdir,outgrouparms+"%1d.txt"%i))
+			write_text_row(prms, os.path.join(outdir,outgrouparms+"%01d.txt"%i))
 
 		"""
 		0   1   2   3   4   5
@@ -482,6 +515,89 @@ def main():
 		=   =   D   =   D   D
 
 		"""
+
+	elif options.phase == 4 and len(args) == 1:
+		outdir      = args[0]
+		bp = 'badparams'
+		#outgrouparms= args[1]
+		radius = options.ou
+		thresherr = options.thresherr
+		sym = int(options.sym[1:])
+		qsym = 360.0/sym
+		#params = [[None for i in xrange(3)] for j in xrange(4)]
+		ll = 3 # this is hardwired as we have three groups.  however, I would like to keep the code general.
+		for jj in xrange(4):
+			params = [None for ii in xrange(ll)]
+			newbad = map(int, read_text_file(options.params+"%01d.txt"%jj) )
+			nn = len(newbad)
+			for ii in xrange(ll):
+				params[ii] = read_text_row(os.path.join(outdir,bp+"%01d%01d.txt"%(jj,ii)))
+				assert(nn == len(params[ii]) )
+
+			#  Compute average projection params and pixel errors
+			avgtrans = [None]*nn
+			pixer   = [0.0]*nn
+			for j in xrange(nn):
+				nas = [0.0,0.0,0.0]
+				if( sym == 1):
+					#pixer[q[0][0]][j] = max_3D_pixel_error(fifi[0], fifi[1], r=radius)
+					for i in xrange(ll):
+						n1,n2,n3 = getfvec(params[i][j][0],params[i][j][1])
+						nas[0] += n1
+						nas[1] += n2
+						nas[2] += n3
+				else:
+					m1,m2,m3 = getfvec(params[0][j][0],params[0][j][1])
+					nas[0] = m1
+					nas[1] = m2
+					nas[2] = m3
+					for i in xrange(1,ll):
+						qnom = -1.e10
+						for j in xrange(-1,2,1):
+							t1,t2,t3 = getfvec(params[i][j][0]+j*qsym,params[i][j][1])
+							nom = t1*m1 + t2*m2 + t3*m3
+							if(nom > qnom):
+								qnom = nom
+								n1=t1
+								n2=t2
+								n3=t3
+							#if(k == 2):
+							#	print '  t1,t2,t3 ',fifi[i][0]+j*qsym,fifi[i][1],t1,t2,t3,nom,qnom
+						nas[0] += n1
+						nas[1] += n2
+						nas[2] += n3
+						print qnom, n1,n2,n3,nas
+					#  To get the correct pixer phi angle has to be taken from the above!!
+
+				nom = sqrt(nas[0]**2 + nas[1]**2 + nas[2]**2)
+
+				if(nom < 1.e-6):
+					nphi   = 0.0
+					ntheta = 0.0
+				else:
+					ntheta = degrees(acos(nas[2]/nom))%360.0
+					if(sym>1 and ntheta>90.0):  nphi   = (degrees(atan2( nas[1], nas[0] ))-180.0)%qsym + 180.0
+					else:                       nphi   =  degrees(atan2( nas[1], nas[0] ))%qsym
+
+				#print   "FIFI     %4d     %7.2f     %7.2f    %7.2f    %7.2f     %7.2f     %7.2f    %7.2f    %7.2f"%(k,fifi[0][0],fifi[0][1],fifi[1][0],fifi[1][1],fifi[2][0],fifi[2][1],nphi,ntheta)
+				twod = average2dtransform([params[ii][j][2:] for ii in xrange(ll)])
+				avgtrans[j] = [nphi, ntheta, twod[0], twod[1], twod[2]]
+
+			perr =  errors_per_image(params, avgtrans, thresherr, radius )
+			rescued = []
+			rejects = []
+			for j in xrange(nn):
+				#print  chr(65+jj),j,perr[j][0],[[params[m][j][i] for i in xrange(2)] for m in xrange(ll)]
+				if( perr[j][0] <= thresherr ):  rescued.append([newbad[j],j])
+				else:							rejects.append([newbad[j],j])
+			if( len(rescued) == 0 ):    write_text_row([-1,-1],  os.path.join(outdir,'rescued'+"%01d.txt"%jj))
+			else:                  		write_text_row(rescued,  os.path.join(outdir,'rescued'+"%01d.txt"%jj))
+			if( len(rejects) == 0 ):    write_text_row([-1,-1],  os.path.join(outdir,'rejects'+"%01d.txt"%jj))
+			else:                  		write_text_row(rejects,  os.path.join(outdir,'rejects'+"%01d.txt"%jj))
+			hi = hist_list([perr[j][0]  for j in xrange(nn)  ], 16)
+			print  "Pixel errors for BAD GROUP  ",chr(65+jj)
+			for ii in xrange(len(hi[0])):
+				print  "%4d   %12.3f    %12.0f "%(ii,hi[0][ii],hi[1][ii])
 
 
 	else:
@@ -513,6 +629,16 @@ def main():
                    The next two files contain the original image numbers refering to the top bdb.
 			  outdir/newgood.txt    - Text file with indices of images whose orientation parameters arrors are below specified thresherr (to be kept)
 			  outdir/bad.txt        - Text file with indices of images whose orientation parameters arrors are above specified thresherr (to be rejected)
+
+		Phase 4:   sxconsistency.py --phase=4 outdir --ou=133 --thresherr=3.0 --params=master/main001/newbad
+
+			input files:
+			  outdir - directory containing files badparamsij.txt i= 0,3, j=0,2, which resulted from three alignments of four badchunk images
+			  --params= - Root of four files with original indices of bad images, they will be read and a subset corresponding to rescued ones will be outputed
+			output files:
+			  rescued*.txt - four files with indices of accepted images from badchunk.
+			  rejects*.txt - four files with indices of rejected images from badchunk.
+			  					There are two columns: [number in original bdb:chunk, number in bdb:newbad] 
 		"""
 		print "Please run '" + progname + " -h' for detailed options"
 
