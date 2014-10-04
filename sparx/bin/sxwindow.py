7d20c6b48df955d880e18ff0fa7e0183ed010cb9
durmaz <durmaz>
2014-10-04 01:07:59 +0000

    Corrected and tested

--- sparx/bin/sxwindow.py
+++ sparx/bin/sxwindow.py
@@ -48,80 +48,103 @@ This program is used to window particles from a micrograph. The coordinates of t
 
 def main():
 	progname = os.path.basename(sys.argv[0])
-	usage = progname + " [micrographs list] ... --coords_dir=coords_dir  --importctf=ctf_file  --topdir=topdir  --input_pixel=input_pixel  --new_apix=new_apix --box_size=box_size --outdir=outdir --outstack=outstack --micsuffix=micsuffix --invert=invert"
+	usage = progname + " [micrographs list] ...  --coords_dir=coords_dir  --importctf=ctf_file  --indir=input_dir" + \
+	                                          "  --input_pixel=input_pixel  --new_apix=new_apix --box_size=box_size" + \
+	                                          "  --outdir=outdir  --outsuffix=outsuffix  --micsuffix=micsuffix" + \
+	                                          "  --nameroot=nameroot  --invert=invert"
 
 	parser = OptionParser(usage, version=SPARXVERSION)
 
-	parser.add_option('--coords_dir',  dest='coordsdir', help='Directory containing particle coordinates')
-	parser.add_option('--importctf',   help='File name with CTF parameters produced by sxcter.')
- 	parser.add_option('--topdir',      default='./', help='Path name of directory containing relevant micrograph directories')
-	parser.add_option('--input_pixel', type=float,  default=1.0,  help='input pixel size')
-	parser.add_option("--new_apix",    type=float, 	default=-1.0, help="New target pixel size to which the micrograph should be resampled. Default is -1, in which case there is no resampling.")
-	parser.add_option('--box_size',    type=int,   help='x and y dimension in pixels of square area to be windowed. Pixel size is assumed to be new_pixel_size.')
-	parser.add_option('--outdir',      help='Output directory')
-	parser.add_option('--outstack',    help='Output stack name')	
-	parser.add_option("--micsuffix",   type=str,	default="hdf", help="A string denoting micrograph type. Currently only handles suffix types, e.g. 'hdf', 'ser' etc.")
-	parser.add_option("--invert",      help="If writing output inverts pixel intensities",default=False)
-	
-	parser.add_option("--defocuserror",       type="float",  	default=1000000.0,        help="Exclude micrographs whose relative defocus error as estimated by sxcter is larger than defocuserror percent.  The error is computed as (std dev defocus)/defocus*100%")
-	parser.add_option("--astigmatismerror",   type="float",  	default=360.0,            help="Set to zero astigmatism for micrographs whose astigmatism angular error as estimated by sxcter is larger than astigmatismerror degrees.")
+	parser.add_option('--coords_dir',       dest='coordsdir',                 help='Directory containing files with particle coordinates')
+	parser.add_option('--importctf',                                          help='File name with CTF parameters produced by sxcter.')
+	parser.add_option("--indir",            type="string", default= ".",      help="Directory containing micrographs to be processed.")
+	parser.add_option('--input_pixel',      type=float,    default=1.0,       help='input pixel size')
+	parser.add_option("--new_apix",         type=float,    default=-1.0,      help="New target pixel size to which the micrograph should be resampled. Default is -1, in which case there is no resampling.")
+	parser.add_option('--box_size',         type=int,      default=256,       help='x and y dimension in pixels of square area to be windowed. Pixel size is assumed to be new_pixel_size.')
+	parser.add_option('--outdir',                                             help='Output directory')
+	parser.add_option('--outsuffix',        type=str,      default="",        help="Suffix for output stack, e.g. '_ptcls' etc.")	
+	parser.add_option("--micsuffix",        type=str,      default="hdf",     help="A string denoting micrograph type. For example 'mrc', 'hdf', 'ser' ...")
+	parser.add_option("--nameroot",         type="string", default="",        help="Prefix of micrographs to be processed.")
+	parser.add_option("--invert",                          default=False,     help="If writing output inverts pixel intensities")
+	                                        
+	parser.add_option("--defocuserror",     type="float",  default=1000000.0, help="Exclude micrographs whose relative defocus error as estimated by sxcter is larger than defocuserror percent.  The error is computed as (std dev defocus)/defocus*100%")
+	parser.add_option("--astigmatismerror", type="float",  default=360.0,     help="Set to zero astigmatism for micrographs whose astigmatism angular error as estimated by sxcter is larger than astigmatismerror degrees.")
 
 
 	(options, args) = parser.parse_args()
+	
 	box_size = options.box_size
-	suffix    = "_ptcls"
 	extension = "." + options.micsuffix
 	cterr = [options.defocuserror/100.0, options.astigmatismerror]
 	
 	new_pixel_size = options.new_apix
 	if new_pixel_size < 0: new_pixel_size = options.input_pixel
 
+# 	Build micrograph basename list
 	micnames = []
 	if len(args) > 0:
-		micnames = args
+		for s in args:
+			micnames.append(s.split('.')[0])
 	else:
 		import glob
-		for f in glob.glob(os.path.join(options.topdir, "*" + extension)):
-			micnames.append(f)
+		for f in glob.glob(os.path.join(options.indir, options.nameroot + "*" + extension)):
+			mic = f.split('/')[-1]
+			bmic=mic.split('.')[0]
+			micnames.append(bmic)
 
-	micnames = [base_name(f) for f in micnames]
+# 	If there is no micrographs, exit
+	if len(micnames) == 0:
+		print usage
+		sys.exit()
 	
-	
+# 	Load CTFs
+	if options.importctf:
+		ctfs0 = read_text_row(options.importctf)
+
+		ctfs={}
+		for i in xrange(len(ctfs0)):
+			ctf=ctfs0[i]
+			fmic = ctf[-1].split('/')
+			basemic = fmic[-1].split('.')[0]
+
+			if(ctf[8]/ctf[0] > cterr[0]):
+				print_msg('Defocus error %f exceeds the threshold. Micrograph %s rejected.\n'%(ctf[8]/ctf[0], basemic))
+			else:
+				if(ctf[10] > cterr[1] ):
+					ctf[6] = 0.0
+					ctf[7] = 0.0
+				ctfs[basemic] = ctf
+
 	mask = model_circle(box_size//2, box_size, box_size)
 	
+# 	Loop over micrographs
 	for k in range(len(micnames)):
 		# basename is name of micrograph minus the path and extension
 		basename = micnames[k]
-		f_mic = os.path.join(os.path.abspath(options.topdir), basename + extension)
-		f_info = info_name(f_mic)
+		f_mic    = os.path.join(os.path.abspath(options.indir), basename + extension)
+		f_info   = os.path.join('info', basename + '_info.json')
 		
-		print "\nProcessing micrograph %s... Path: %s... Coordinates file %s" % (base_name(f_mic), f_mic, f_info)
+# 		CHECKS: BEGIN
+# 		IF micrograph exists
 		if not os.path.exists(f_mic):
-			print "\n    Cannot read %s. Skipping..." % f_mic
+			print "\n    Cannot read %s. Skipping %s ..." % (f_mic, basename)
 			continue
 		
-		otcl_images  = "bdb:%s/"%options.outdir + basename + suffix
-
-		if options.importctf:
-			ctfs = read_text_row(options.importctf)
-			nx = True
-			for i in xrange(len(ctfs)):
-				smic = ctfs[i][-1].split('/')
-				ctfilename = (smic[-1].split('.'))[0]
-				if(ctfilename == basename):
-					ctfs = ctfs[i]
-					nx = False
-					break
-			if nx:
-				print "Micrograph %s"%filename,"  not listed in CTER results, skipping ...."
-				break
-			if(ctfs[8]/ctfs[0] > cterr[0]):
-				print_msg('Defocus error %f exceeds the threshold. Micrograph %s rejected.\n'%(ctfs[8]/ctfs[0], filename))
-				break
-			if(ctfs[10] > cterr[1] ):
-				ctfs[6] = 0.0
-				ctfs[7] = 0.0
+# 		IF coordinates file exists
+		if not os.path.exists(f_info):
+			print "\n    Cannot read %s. Skipping %s ..." % (f_info, basename)
+			continue
 		
+# 		IF micrograph is in CTER results
+		if basename not in ctfs:
+			print "\nMicrograph %s not listed in CTER results, skipping ....\n" % basename
+			continue
+		else:
+			ctf = ctfs[basename]
+# 		CHECKS: END
+
+		print "\nProcessing micrograph %s... Path: %s... Coordinates file %s" % (basename, f_mic, f_info)
+	
 		coords = js_open_dict(f_info)["boxes"]
 		
 		immic = get_im(f_mic)
@@ -137,7 +160,7 @@ def main():
 			nx = immic.get_xsize()
 			ny = immic.get_ysize()
 			immic = resample(immic, resample_ratio)
-			if options.importctf: ctfs[3] = new_pixel_size
+			if options.importctf: ctf[3] = new_pixel_size
 			# New coords
 			for i in range(len(coords)):
 				coords[i][0] *= resample_ratio
@@ -152,11 +175,12 @@ def main():
 		
 		if options.importctf:
 			from utilities import generate_ctf
-			ctfs = generate_ctf(ctfs)
+			ctf = generate_ctf(ctf)
 
 		x0 = immic.get_xsize()//2
 		y0 = immic.get_ysize()//2
 
+		otcl_images  = "bdb:%s/" % options.outdir + basename + options.outsuffix
 		for i in range(len(coords)):
 
 			x = int(coords[i][0])
@@ -169,7 +193,7 @@ def main():
 			imw /= stat[1]
 			
 			if options.importctf:
-				imw.set_attr("ctf",ctfs)
+				imw.set_attr("ctf",ctf)
 				imw.set_attr("ctf_applied", 0)
 			
 			imw.set_attr("ptcl_source_coord", [int(round(x/resample_ratio)),int(round(y/resample_ratio))])
