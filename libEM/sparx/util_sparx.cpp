d17a91711475fbd2c1d7708f70a0f51a0cbb5ae6
pawel <pawel>
2014-10-24 14:18:02 +0000

    *** empty log message ***

--- libEM/sparx/util_sparx.cpp
+++ libEM/sparx/util_sparx.cpp
@@ -20954,7 +20954,7 @@ EMData* Util::move_points(EMData* img, float qprob, int ri, int ro)
 	if (!img) {
 		throw NullPointerException("NULL input image");
 	}
-	cout <<"  VERSION  05/25/2014  08:00 pm"<<endl;
+	cout <<"  VERSION  10/24/2014  09:00 am"<<endl;
 	float dummy;
 	dummy = ri;
 	cout <<  "   fmod   "<<fmod(qprob,dummy)<<endl;
