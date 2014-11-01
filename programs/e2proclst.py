15e748fd21b5e04f86682d59fe62fff6879cb4c4
stevel <stevel>
2014-11-01 15:00:56 +0000

    New filtering options for proclst

--- programs/e2proclst.py
+++ programs/e2proclst.py
@@ -46,6 +46,8 @@ sort of virtual stack represented by .lst files, use e2proc2d.py or e2proc3d.py 
 	parser.add_argument("--merge",type=str,help="Specify the output name here. This will concatenate all of the input .lst files into a single output",default=None)
 	parser.add_argument("--mergesort",type=str,help="Specify the output name here. This will merge all of the input .lst files into a single (resorted) output",default=None)
 	parser.add_argument("--retype",type=str,help="If a lst file is referencing a set of particles from particles/imgname__oldtype.hdf, this will change oldtype to the specified string in-place (modifies input files)",default=None)
+	parser.add_argument("--minlosnr",type=float,help="Integrated SNR from 1/200-1/20 1/A must be larger than this",default=0,guitype='floatbox', row=8, col=0)
+	parser.add_argument("--minhisnr",type=float,help="Integrated SNR from 1/10-1/4 1/A must be larger than this",default=0,guitype='floatbox', row=8, col=1)
 	parser.add_argument("--verbose", "-v", dest="verbose", action="store", metavar="n", type=int, help="verbose level [0-9], higner number means higher level of verboseness",default=1)
 
 	(options, args) = parser.parse_args()
@@ -55,6 +57,9 @@ sort of virtual stack represented by .lst files, use e2proc2d.py or e2proc3d.py 
 		sys.exit(1)
 
 	if options.retype != None:
+		if options.minlosnr>0 or options.minhisnr>0 :
+			print "ERROR: --minlosnr and --minhisnr not compatible with --retype"
+			sys.exit(1)
 		
 		# if the user provided the leading __ for us, we strip it off and add it back later
 		if options.retype[:2]=="__" : 
@@ -86,6 +91,10 @@ sort of virtual stack represented by .lst files, use e2proc2d.py or e2proc3d.py 
 
 	if options.merge!=None:
 		
+		if options.minlosnr>0 or options.minhisnr>0 :
+			print "ERROR: --minlosnr and --minhisnr not compatible with --merge. Please use --mergesort instead."
+			sys.exit(1)
+			
 		# create/update output lst
 		lsto=LSXFile(options.merge)
 		ntot=0
@@ -108,6 +117,7 @@ sort of virtual stack represented by .lst files, use e2proc2d.py or e2proc3d.py 
 		
 		# loop over input files
 		ptcls=[]
+		pfiles=set()
 		for f in args:
 			lst=LSXFile(f,True)
 			ntot+=len(lst)
@@ -115,13 +125,36 @@ sort of virtual stack represented by .lst files, use e2proc2d.py or e2proc3d.py 
 			for i in xrange(len(lst)):
 				im=lst.read(i)
 				ptcls.append((im[1],im[0],im[2]))
-			ptcls.sort()
-			
+				pfiles.add(im[1])
+				
+		ptcls.sort()
+		
+		# remove particles in files not meeting our criteria
+		if options.minlosnr>0 or options.minhisnr>0 :
+			# the list conversion here is so we are iterating over a copy and not modifying the set while we iterate over it
+			for pfile in list(pfiles):
+				js=js_open_dict(info_name(pfile))
+				ctf=js["ctf"][0]
+				js.close()
+				r1=int(floor(1.0/(200.0*ctf.dsbg)))		# lowsnr is 200-20 A
+				r2=int(ceil(1.0/(20.0*ctf.dsbg)))
+				r3=int(floor(1.0/(10.0*ctf.dsbg)))		# hisnr is 10 to 4 A
+				r4=int(ceil(1.0/(4.0*ctf.dsbg)))
+				losnr=sum(ctf.snr[r1:r2])/(r2-r1)
+				hisnr=sum(ctf.snr[r3:r4])/(r4-r3)
+				if losnr<options.minlosnr or hisnr<options.minhisnr:
+					pfiles.remove(pfile)
+					if options.verbose: print pfile," removed due to SNR criteria"
+		
+		nwrt=0
 		for i in ptcls:
-			lsto.write(-1,i[1],i[0],i[2])
+			if i[0] in pfiles : 
+				lsto.write(-1,i[1],i[0],i[2])
+				nwrt+=1
 
-		if options.verbose : print "{} particles in {}".format(ntot,options.mergesort)
-
+		if options.verbose : 
+			if nwrt==ntot : print "{} particles in {}".format(ntot,options.mergesort)
+			else : print "{} of {} particles written to {}".format(nwrt,ntot,options.mergesort)
 
 if __name__ == "__main__":
 	main()
