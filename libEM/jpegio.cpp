092f24f342a06fb6f5810e818f0ef3ed7e03e2ef
vern <vern>
2014-12-02 01:42:47 +0000

    Called new routine getRenderLimits to clean up integer scaling for output files, cleaned up a little.

--- libEM/jpegio.cpp
+++ libEM/jpegio.cpp
@@ -44,14 +44,15 @@
 
 using namespace EMAN;
 
-JpegIO::JpegIO(const string & file, IOMode rw):	filename(file), rw_mode(rw), jpeg_file(0), initialized(false),rendermin(0),rendermax(0)
+JpegIO::JpegIO(const string & file, IOMode rw):	filename(file), rw_mode(rw),
+				jpeg_file(0), initialized(false), rendermin(0.0), rendermax(0.0)
 {}
 
 JpegIO::~JpegIO()
 {
 	if (jpeg_file) {
 		fclose(jpeg_file);
-		jpeg_file = 0;
+		jpeg_file = NULL;
 	}
 
 }
@@ -59,6 +60,7 @@ JpegIO::~JpegIO()
 void JpegIO::init()
 {
 	ENTERFUNC;
+
 	if (initialized) {
 		return;
 	}
@@ -68,7 +70,7 @@ void JpegIO::init()
 	bool is_new_file = false;
 	jpeg_file = sfopen(filename, rw_mode, &is_new_file, true);
 
-	if (!is_new_file) {
+	if (! is_new_file) {
 		throw ImageReadException(filename, "JPEG reading not supported");
 	}
 	else {
@@ -77,18 +79,24 @@ void JpegIO::init()
 		jpeg_stdio_dest(&cinfo, jpeg_file);
 	}
 
-	rendermin=rendermax=0;
-	jpegqual=75;
+	rendermin = 0.0;
+	rendermax = 0.0;
+
+	jpegqual  = 75;
+
 	EXITFUNC;
 }
 
 bool JpegIO::is_valid(const void *)
 {
 //	ENTERFUNC;
+
 	return false;
 }
 
-//int JpegIO::read_header(Dict & dict, int image_index, const Region * area, bool)
+// int JpegIO::read_header(Dict & dict, int image_index,
+// const Region * area, bool)
+
 int JpegIO::read_header(Dict &, int, const Region *, bool)
 {
 	ENTERFUNC;
@@ -96,45 +104,58 @@ int JpegIO::read_header(Dict &, int, const Region *, bool)
 	throw ImageReadException(filename, "JPEG reading not supported");
 
 	EXITFUNC;
+
 	return 0;
 }
 
-int JpegIO::write_header(const Dict & dict, int image_index, const Region* area,
-						EMUtil::EMDataType, bool)
+int JpegIO::write_header(const Dict & dict, int image_index,
+						const Region* area, EMUtil::EMDataType, bool)
 {
 	ENTERFUNC;
 
-	//single image format, index can only be zero
-	if(image_index == -1) {
+	// single image format, index can only be zero
+
+	if (image_index == -1) {
 		image_index = 0;
 	}
-	if(image_index != 0) {
-		throw ImageWriteException(filename, "JPEG file does not support stack.");
+
+	if (image_index != 0) {
+		throw ImageWriteException(filename,
+			"JPEG file does not support stack.");
 	}
+
 	check_write_access(rw_mode, image_index);
 
 	if ((int) dict["nz"] != 1) {
 		LOGERR("Only support 2D JPEG file write");
+
 		return 1;
 	}
 
-	rendermin=(float)dict["render_min"];	// float value representing black in the output
-	rendermax=(float)dict["render_max"];	// float value representign white in the output
-	jpegqual=(int)dict["jpeg_quality"];
-	if (jpegqual==0) jpegqual=75;
+//	rendermin = (float)dict["render_min"];	// float value representing black in the output
+//	rendermax = (float)dict["render_max"];	// float value representign white in the output
 
-	cinfo.image_width = (int) dict["nx"];      /* image width and height, in pixels */
+	EMUtil::getRenderLimits(dict, rendermin, rendermax);
+
+	jpegqual = (int)dict["jpeg_quality"];
+	if (jpegqual == 0) jpegqual = 75;
+
+	cinfo.image_width  = (int) dict["nx"];	/* image width and height, in pixels */
 	cinfo.image_height = (int) dict["ny"];
+
 	if (area) {
-		cinfo.image_width = (int) area->size[0];
+		cinfo.image_width  = (int) area->size[0];
 		cinfo.image_height = (int) area->size[1];
 	}
+
 	cinfo.input_components = 1;     /* # of color components per pixel */
 	cinfo.in_color_space = JCS_GRAYSCALE; /* colorspace of input image */
+
 	jpeg_set_defaults(&cinfo);
-	jpeg_set_quality(&cinfo,jpegqual,true);
+	jpeg_set_quality(&cinfo, jpegqual, true);
 
 	EXITFUNC;
+
 	return 0;
 }
 
@@ -143,6 +164,7 @@ int JpegIO::read_data(float *, int, const Region *, bool)
 	ENTERFUNC;
 
 	EXITFUNC;
+
 	return 0;
 }
 
@@ -151,28 +173,39 @@ int JpegIO::write_data(float *data, int image_index, const Region* area,
 {
 	ENTERFUNC;
 
-	if (image_index>0) throw ImageWriteException("N/A", "JPEG files are single-image only");
-	if (area && (area->size[0]!=cinfo.image_width || area->size[1]!=cinfo.image_height))
-		 throw ImageWriteException("N/A", "No region writing for JPEG images");
-	int nx=cinfo.image_width,ny=cinfo.image_height;
+	if (image_index > 0)
+		 throw ImageWriteException("N/A", "JPEG files are single-image only");
 
-	// If we didn't get any parameters in 'render_min' or 'render_max', we need to find some good ones
+	if (area && (area->size[0] != cinfo.image_width ||
+		 area->size[1] != cinfo.image_height))
+		 throw ImageWriteException("N/A", "No region writing for JPEG images");
+
+	int nx = cinfo.image_width, ny = cinfo.image_height;
+
+	// If we didn't get any parameters in 'render_min' or 'render_max',
+	// we need to find some good ones.
+
 	EMUtil::getRenderMinMax(data, nx, ny, rendermin, rendermax);
 
-	unsigned char *cdata=(unsigned char *)malloc(nx+1);
+	unsigned char *cdata = (unsigned char *)malloc(nx+1);
 
-	/**Flip the image vertically, since EMAN use top-left corner as image origin
-	* But PNG use bottom-left corner as image origin */
+	/* Flip the image vertically, since EMAN use top-left corner as image origin
+	/* But PNG use bottom-left corner as image origin */
 	// convert and write the data 1 scanline at a time
+
 	JSAMPROW rp[1];
-	rp[0]=cdata;
+	rp[0] = cdata;
+
 	jpeg_start_compress(&cinfo, TRUE);
+
 	for (int i=ny-1; i>=0; i--) {
 		for (int j=0; j<nx; j++) {
-			if (data[i*nx+j]<=rendermin) cdata[j]=0;
-			else if (data[i*nx+j]>=rendermax) cdata[j]=255;
-			else cdata[j]=(int)((data[i*nx+j]-rendermin)/(rendermax-rendermin)*256.0);
+			if (data[i*nx+j] <= rendermin) cdata[j] = 0;
+			else if (data[i*nx+j] >= rendermax) cdata[j] = 255;
+			else cdata[j]= (int)((data[i*nx+j]-rendermin)/
+								(rendermax-rendermin)*256.0);
 		}
+
 		jpeg_write_scanlines(&cinfo, rp, 1);
 	}
 
@@ -180,6 +213,7 @@ int JpegIO::write_data(float *data, int image_index, const Region* area,
 	jpeg_destroy_compress(&cinfo);
 
 	free(cdata);
+
 	EXITFUNC;
 
 	return 0;
@@ -203,6 +237,5 @@ bool JpegIO::is_image_big_endian()
 {
 	return true;
 }
-
 
 #endif	//EM_JPEG
