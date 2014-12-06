9c531b0ba9c63e2b9270eb9987814aaa6d877afd
stevel <stevel>
2014-12-06 07:19:52 +0000

    more movietools

--- examples/movietools.py
+++ examples/movietools.py
@@ -3,10 +3,17 @@ import EMAN2
 from math import sqrt
 
 def readstack(n):
-	"read the data for the nth particle. Return ref,ptclframes"
+	"read the data for the nth particle. Return ref,ptclframes,psref"
 	ref=EMAN2.EMData("tmp2.hdf",n*33)
 	ptcls=EMAN2.EMData.read_images("tmp2.hdf",range(n*33+1,(n+1)*33))
-	return ref,ptcls
+	ctfim=ref.do_fft()
+	ctf=ref["ctf"]
+	ctf.compute_2d_complex(ctfim,EMAN2.Ctf.CtfType.CTF_POWEVAL)
+	ctfim.ri2ap()
+	ctfim=ctfim.amplitude()
+#	ctfim.process_inplace("normalize")
+
+	return ref,ptcls,ctfim
 
 def seqavg(stack):
 	"return progressive averages of frames in stack"
@@ -43,4 +50,100 @@ def peaks(stack):
 	pk=[(i[0]-nx/2,i[1]-nx/2) for i in pk]
 	return pk
 
+def localali(im1,im2,psref,maxdx):
+	"""Takes an fft.amplitude() psref to use as a basis for optimal alignment. Exhaustive search +-maxdx pixels.
+	returns (dx,dy) required to bring im2 into alignment with im1"""
 
+	nx=im1["nx"]
+	mask=EMAN2.EMData(nx,nx,1)
+	mask.to_one()
+	mask.process_inplace("mask.gaussian",{"inner_radius":nx/4,"outer_radius":nx/12})
+#	EMAN2.display(mask)
+
+	maxdx=int(maxdx)
+	out=EMAN2.EMData(2*maxdx+1,2*maxdx+1,1)
+	for dx in xrange(-maxdx,maxdx+1):
+		for dy in xrange(-maxdx,maxdx+1):
+			av=im1+im2.process("xform.translate.int",{"trans":(dx,dy,0)})
+			av.mult(mask)	# to prevent edge effects and slightly smooth the powspec
+			avf=av.do_fft()
+			avf.ri2ap()
+			avf=avf.amplitude()
+			out[dx+maxdx,dy+maxdx]=avf.cmp("ccc",psref)
+#			print dx,dy,out[dx+20,dy+20]
+
+	ret=out.calc_max_location()
+	return (int(ret[0]-maxdx),int(ret[1]-maxdx)),out
+
+def stackaliloccor(ref,stack):
+	"""aligns images in stack to ref based on the assumption that movements follow a path and don't jump around much"""
+	nx=ref["nx"]
+	ny=ref["ny"]
+
+	# Should get better results if we filter by the CTF or SNR
+	ctfim=ref.do_fft()
+	ctf=ref["ctf"]
+	ctf.bfactor=50
+	ctf.compute_2d_complex(ctfim,EMAN2.Ctf.CtfType.CTF_INTEN)
+
+	# prefilter ref so we don't have to do it over and over
+	reff=ref.do_fft()
+	reff.mult(ctfim)
+	reff=reff.do_ift()
+
+	# We compute the CCF for the unaligned average, then filter out values close to the max
+	# to define a region of permissible translation for individual frames
+	unaliavg=sum(stack)
+	ccfmask=unaliavg.calc_ccf(reff,EMAN2.fp_flag.CIRCULANT,True)
+	ccfmask.process_inplace("normalize.edgemean")
+	ccfmask.process_inplace("threshold.binary",{"value":ccfmask["maximum"]*.8})
+	ccfmask.process_inplace("mask.addshells",{"nshells":2})
+
+#	EMAN2.display(ccfmask)
+
+	for i,im in enumerate(stack):
+		ccf=im.calc_ccf(reff,EMAN2.fp_flag.CIRCULANT,True)
+		ccf.mult(ccfmask)
+		pk=ccf.calc_max_location()
+		dx=-(pk[0]-nx/2)
+		dy=-(pk[1]-ny/2)
+		print i,dx,dy
+
+		try: avg.add(im.process("xform.translate.int",{"trans":(dx,dy)}))
+		except: avg=im.process("xform.translate.int",{"trans":(dx,dy)})
+		
+	return avg
+
+def stackali(ref,stack,psref,maxdx):
+	"""aligns images in stack to ref, under constraint of maximizing psref"""
+
+	unaliavg=sum(stack)
+	ali=unaliavg.align("translational",ref)
+	
+	# This is the overall alignment of the average without considering psref
+	# just to help us search in the correct neighborhood
+	dx0,dy0=[int(i) for i in ali["xform.align2d"].get_trans_2d()]
+	print "overall: ",dx0,dy0
+
+	# to avoid a lot of gymnastics, and because the edge of the reference is "flat"
+	# we shift the ref off-center for the alignments, then compensate at the end
+	xfref=ref.process("xform.translate.int",{"trans":(-dx0,-dy0,0)})
+	#EMAN2.display((ref,unaliavg,xfref),True)
+
+	# align to the reference under psref
+	for i,im in enumerate(stack):
+		dx,dy=localali(xfref,im,psref,maxdx)
+		try: avg1.add(im.process("xform.translate.int",{"trans":(dx,dy)}))
+		except: avg1=im.process("xform.translate.int",{"trans":(dx,dy)})
+		print i,dx,dy
+
+	print "------"
+
+	# now align to the initial average under psref (in case the reference is bad?)
+	for i,im in enumerate(stack):
+		dx,dy=localali(avg1,im,psref,maxdx)
+		try: avg2.add(im.process("xform.translate.int",{"trans":(dx+dx0,dy+dy0)}))
+		except: avg2=im.process("xform.translate.int",{"trans":(dx+dx0,dy+dy0)})
+		print i,dx,dy
+	
+	return avg1,avg2
