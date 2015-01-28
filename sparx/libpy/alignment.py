406aa01e43b66389aa553b559eb80fbb919c5061
pawel <pawel>
2015-01-28 01:36:27 +0000

    quick fix

--- sparx/libpy/alignment.py
+++ sparx/libpy/alignment.py
@@ -3689,6 +3689,8 @@ def shc(data, refrings, numr, xrng, yrng, step, an = -1.0, sym = "c1", finfo=Non
 				ut = t2*kts
 				# we do not care which position minimizes the error
 				pixel_error = min(max_3D_pixel_error(t1, ut, numr[-3]), pixel_error)
+		else:
+			pixel_error = min(max_3D_pixel_error(t1, t2, numr[-3]), pixel_error)
 		if finfo:
 			finfo.write( "New parameters: %9.4f %9.4f %9.4f %9.4f %9.4f %10.5f  %11.3e\n\n" %(phi, theta, psi, s2x, s2y, peak, pixel_error))
 			finfo.flush()
