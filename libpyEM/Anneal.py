c20320f791fe5a85e44257afc0661dee925595d9
jmbell <jmbell>
2015-06-17 01:57:23 +0000

    Added testing to the executable and cleaned things up for the merger with the other python optmizers in EMAN2. This transition will occur soon.

--- libpyEM/Anneal.py
+++ libpyEM/Anneal.py
@@ -97,26 +97,20 @@ def geodesic_distance(a, b):
 def example_tsp_problem():
     # latitude and longitude for the twenty largest U.S. cities
     cities = {'New York City': (40.72, 74.00),'Los Angeles': (34.05, 118.25),'Chicago': (41.88, 87.63),'Houston': (29.77, 95.38),'Phoenix': (33.45, 112.07),'Philadelphia': (39.95, 75.17),'San Antonio': (29.53, 98.47),'Dallas': (32.78, 96.80),'San Diego': (32.78, 117.15),'San Jose': (37.30, 121.87),'Detroit': (42.33, 83.05),'San Francisco': (37.78, 122.42), 'Jacksonville': (30.32, 81.70),'Indianapolis': (39.78, 86.15),'Austin': (30.27, 97.77),'Columbus': (39.98, 82.98),'Fort Worth': (32.75, 97.33),'Charlotte': (35.23, 80.85),'Memphis': (35.12, 89.97),'Baltimore': (39.28, 76.62)}
-
     # initial state, a randomly-ordered itinerary
     init_state = list(cities.keys())
     np.random.shuffle(init_state)
-
     # create a distance matrix
     distance_matrix = {}
     for ka, va in cities.items():
         distance_matrix[ka] = {}
         for kb, vb in cities.items():
-            if kb == ka:
-                distance_matrix[ka][kb] = 0.0
-            else:
-                distance_matrix[ka][kb] = geodesic_distance(va, vb)
-
+            if kb == ka: distance_matrix[ka][kb] = 0.0
+            else: distance_matrix[ka][kb] = geodesic_distance(va, vb)
     tsps = TSPSolver(init_state, distance_matrix)
     # since our state is just a list, slice is the fastest way to copy
     tsps.copy_strategy = "slice"
     state, e = tsps.anneal()
-
     while state[0] != 'New York City':
         state = state[1:] + state[:1]  # rotate NYC to start
     print("%i mile route:" % e)
@@ -450,30 +444,6 @@ class BaseAnnealer(object):
         print('') # New line after auto() output
         # Don't perform anneal, just return params
         return {'tmax': Tmax, 'tmin': Tmin, 'steps': duration}
-
-class TSPSolver(BaseAnnealer):
-
-    """
-    Test annealer with a travelling salesman problem.
-    """
-
-    # pass extra data (the distance matrix) into the constructor
-    def __init__(self, state, distance_matrix):
-        self.distance_matrix = distance_matrix
-        super(TSPSolver, self).__init__(state)  # important!
-
-    def move(self):
-        """Swaps two cities in the route."""
-        a = np.random.randint(0, len(self.state) - 1)
-        b = np.random.randint(0, len(self.state) - 1)
-        self.state[a], self.state[b] = self.state[b], self.state[a]
-
-    def energy(self):
-        """Calculates the length of the route."""
-        e = 0
-        for i in range(len(self.state)):
-            e += self.distance_matrix[self.state[i-1]][self.state[i]]
-        return e
 
 class TSPSolver(BaseAnnealer):
 
