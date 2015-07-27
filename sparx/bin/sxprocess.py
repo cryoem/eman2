0aee96d8e57962e609a01f936bb7de81c9c70093
pawel <pawel>
2015-07-27 17:16:39 +0000

    added isacselect

--- sparx/bin/sxprocess.py
+++ sparx/bin/sxprocess.py
@@ -300,7 +300,8 @@ def main():
 					action="append",  help="One argument is required: name of key with which the database will be created. Fill in database with parameters specified as follows: --makedb param1=value1:param2=value2, e.g. 'gauss_width'=1.0:'pixel_input'=5.2:'pixel_output'=5.2:'thr_low'=1.0")
 	parser.add_option("--generate_projections", metavar="param1=value1:param2=value2", type="string",
 					action="append", help="Three arguments are required: name of input structure from which to generate projections, desired name of output projection stack, and desired prefix for micrographs (e.g. if prefix is 'mic', then micrographs mic0.hdf, mic1.hdf etc will be generated). Optional arguments specifying format, apix, box size and whether to add CTF effects can be entered as follows after --generate_projections: format='bdb':apix=5.2:CTF=True:boxsize=100, or format='hdf', etc., where format is bdb or hdf, apix (pixel size) is a float, CTF is True or False, and boxsize denotes the dimension of the box (assumed to be a square). If an optional parameter is not specified, it will default as follows: format='bdb', apix=2.5, CTF=False, boxsize=64.")
-	parser.add_option("--isacgroup", 			type="int", 		help="Retrieve original image numbers in the selected ISAC group   See ISAC documentaion for details.", default=-1)
+	parser.add_option("--isacgroup", 			type="int", 		help="Retrieve original image numbers in the selected ISAC group   See ISAC documentation for details.", default=-1)
+	parser.add_option("--isacselect", 			action="store_true", 		help="Retrieve original image numbers of images listed in ISAC output stack of averages   See ISAC documentation for details.", default=False)
 	parser.add_option("--params",	   			type="string",      default=None,    help="Name of header of parameter, which one depends on specific option")
 	parser.add_option("--adjpw", 				action="store_true",	help="Adjust rotationally averaged power spectrum of an image", default=False)
 	parser.add_option("--rotpw", 				type="string",   	default=None,    help="Name of the text file to contain rotationally averaged power spectrum of the input image.")
@@ -405,6 +406,20 @@ def main():
 			l.append(int(get_im(args[0],k).get_attr(options.params)))
 		from utilities import write_text_file
 		write_text_file(l, args[2])
+
+	elif options.isacselect>-1:
+		nargs = len(args)
+		if nargs != 2:
+			ERROR("Two files needed on input!", "isacgroup", 1)
+			return
+		from utilities import get_im
+		nima = EMUtil.get_image_count(args[0])
+		m = []
+		for k in xrange(nima):
+			m += get_im(args[0],k).get_attr("members")
+		m.sort()
+		from utilities import write_text_file
+		write_text_file(m, args[1])
 
 	elif options.pw:
 		nargs = len(args)
