aa92fd7309d6f05c67ed7da9f9250143631f2e4a
jmbell <jmbell>
2015-08-17 22:47:14 +0000

    fixed a minor bug that saved box centers incorrectly

--- examples/e2starfiletoboxfile.py
+++ examples/e2starfiletoboxfile.py
@@ -7,34 +7,34 @@ from EMAN2 import *
 def main():
 
 	usage = """Program to convert an "_autopick.star" file to .box coordinate format."""
-	
+
 	parser = EMArgumentParser(usage=usage,version=EMANVERSION)
-	
+
 	parser.add_argument("--starfile",type=str,default=None,help="""The _autopick.star file you wish to convert to .box format""",required=True)
-	parser.add_argument("--boxsize",help="""Specify the boxsize for each particle.""", required=True, type=int)
-	
+	parser.add_argument("--boxsize",type=int, help="""Specify the boxsize for each particle.""", required=True)
+
 	(options, args) = parser.parse_args()
-	
+
 	bf = options.starfile.replace('_autopick.star','.box')
-	bs = options.boxsize
-	
+	bs = int(options.boxsize)
+
 	if os.path.isfile(bf):
 		print("The file {} already exists. Please move or remove it before running this program.".format(bf))
 		sys.exit(1)
-	
+
 	logger = E2init(sys.argv)
-	
+
 	with open(options.starfile) as sf:
 		lns = filter(None,[l.split()[:2] for l in sf if '_' not in l if l != '\n'])
-	
+
 	with open(bf,'w+') as bf:
 		for ln in lns:
-			x = ln[0] - bs / 2
-			y = ln[1] - bs / 2
+			x = int(float(ln[0])) - bs / 2
+			y = int(float(ln[1])) - bs / 2
 			bf.write("{}\t{}\t{}\t{}\n".format(x,y,bs,bs))
-	
+
 	E2end(logger)
-	
+
 	return
 
 if __name__ == "__main__":
