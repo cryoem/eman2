5f12b79ee72c3e9ffb847da4cb62f32c9addb9e4
jmbell <jmbell>
2015-08-18 16:59:26 +0000

    *** empty log message ***

--- examples/e2starfile2boxfile.py
+++ examples/e2starfile2boxfile.py
@@ -10,27 +10,27 @@ def main():
 	
 	parser = EMArgumentParser(usage=usage,version=EMANVERSION)
 	
-	parser.add_argument("--starfile",type=str,default=None,help="""The _autopick.star file you wish to convert to .box format""",required=True)
-	parser.add_argument("--boxsize",help="""Specify the boxsize for each particle.""", required=True, type=int)
+	parser.add_argument("--starfile",help="""The _autopick.star file you wish to convert to .box format""",required=True,type=str)
+	parser.add_argument("--boxsize",help="""Specify the boxsize for each particle.""",required=True,type=int)
 	
 	(options, args) = parser.parse_args()
 	
 	boxfile = options.starfile.replace('_autopick.star','.box')
-	bs = options.boxsize
-	
-	if os.path.isfile(boxfile):
-		print("The file {} already exists. Please move or remove it before running this program.".format(boxfile))
-		sys.exit(1)
+	bs = int(options.boxsize / 2)
+
+	#if os.path.isfile(boxfile):
+	#	print("The file {} already exists. Please move or remove it before running this program.".format(boxfile))
+	#	sys.exit(1)
 	
 	logger = E2init(sys.argv)
 	
 	with open(options.starfile) as sf:
 		lns = filter(None,[l.split()[:2] for l in sf if '_' not in l if l != '\n'])
-		lns = [l+[bs] for l in lns]
+		lns = [l for l in lns]
 	
 	with open(boxfile,'w+') as bf:
 		for ln in lns:
-			bf.write("{}\t{}\t{}\t{}\n".format(ln[0],ln[1],ln[2],ln[2]))
+			bf.write("{}\t{}\t{}\t{}\n".format(ln[0]-bs,ln[1]-bs,bs,bs))
 	
 	E2end(logger)
 	
