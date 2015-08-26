5647142e7a3257c81e76b7b702abd1c5f3f74493
jmbell <jmbell>
2015-08-26 23:39:04 +0000

    *** empty log message ***

--- examples/e2starfiletoboxfile.py
+++ examples/e2starfiletoboxfile.py
@@ -3,43 +3,86 @@
 import os
 import sys
 from EMAN2 import *
+from EMAN2star import *
+
+from IPython import embed
 
 def main():
 
-	usage = """Program to convert an "_autopick.star" file to .box coordinate format."""
+	usage = """Program to convert an ".star" file to .box coordinate format."""
 
 	parser = EMArgumentParser(usage=usage,version=EMANVERSION)
 
-	parser.add_argument("--starfile",help="""The _autopick.star file you wish to convert to .box format""",required=True,type=str)
+	parser.add_argument("--input",help="""The .star file you wish to convert to .box format""",type=str)
+	parser.add_argument("--output",help="""The name of the .box file to be written""",type=str)
 	parser.add_argument("--boxsize",help="""Specify the boxsize for each particle.""",required=True,type=int)
-	#parser.add_argument("--noclobber",help="""If specified, this program will not overwrite existing box files.""",action="store_true",default=False)
-
 	(options, args) = parser.parse_args()
 
-	boxfile = options.starfile.replace('_autopick.star','.box')
-	bs = int(options.boxsize / 2)
-
-	if os.path.isfile(boxfile):
-		print("The file {} already exists. It will not be overwritten.".format(boxfile))
-		sys.exit(1)
-
-	logger = E2init(sys.argv)
-
-	with open(options.starfile) as sf:
-		lines = sf.readlines()
-
-	hdr = [l.split()[0] for l in lines if '_' in l][2:]
-	xind = [i for i,x in enumerate(hdr) if x == '_rlnCoordinateX'][0]
-	yind = [i for i,x in enumerate(hdr) if x == '_rlnCoordinateY'][0]
+	if len(args) != 2: exit(-1)
 	
-	data = filter(None,[l.split() for l in lines if '_' not in l if l != '\n'])
-	coords = [[l[xind],l[yind]] for l in data]
+	if not options.output: options.output = args[1]
+	if not options.input: options.input = args[0]
+	
+	if os.path.exists(args[0]): starf = StarFile(args[0])
+	else: exit(-1)
+	
+	if options.output.split('.')[-1] != "box":
+		f = options.output.split('.')
+		if len(f) > 2: bn = ".".join(f[:-2])
+		else: bn = f[-2]
+		print("Writing to {base}.box rather than {base}.{ext}".format(base=bn,ext=f[-1]))
+		options.output = bn+".box"
+	boxf = options.output
+	
+	bs = int(options.boxsize / 2)
+	
+	logger = E2init(sys.argv)
+	
+	hdr = starf.keys()
+	
+	print(hdr)
+	
+	mk = [i for i in hdr if "Micrograph" in i]
+	xk = [i for i in hdr if "X" in i]
+	yk = [i for i in hdr if "Y" in i]
+	
+	# resolve correct x and y dictionary keys
+	
+	# case 1: only one X key
+	# case 2: multiple xk
+	# case 3: no xk
+	
+	# case 1: only one Y key
+	# case 2: multiple yk
+	# case 3: no yk
+	
+	embed()
+	exit(-1)
+	
+	# case 1: one file to one micrograph
+	if len(mks) < 1:
 
-	with open(boxfile,'w+') as bf:
-		for coord in coords:
-			x = int(float(coord[0]))
-			y = int(float(coord[1]))
-			bf.write("{}\t{}\t{}\t{}\n".format(x-bs/2,y-bs/2,bs,bs))
+		coords =
+		coords = [[l[xk],l[yk]] for l in data]
+		with open(boxf,'w+') as bf:
+			for coord in coords:
+				x = int(float(coord[0]))
+				y = int(float(coord[1]))
+				bf.write("{}\t{}\t{}\t{}\n".format(x-bs/2,y-bs/2,bs,bs))
+	
+	# case 2: multiple micrographs in file, one header name
+	elif len(mks) == 1:
+		mgs = list(set(starf[mghdr[0]))
+		# make box file for each micrograph
+		# add each particle to box file
+			# store x,y coords
+		# write box files with box size. format is "{}\t{}\t{}\t{}".format(x-bs/2,y-bs/2,bs/2,bs/2)
+			
+	# case 3: multiple micrograph data in header
+	else:
+		print("Sorry, I'm not ready to handle this yet")
+		exit(-1)
+	
 
 	E2end(logger)
 
