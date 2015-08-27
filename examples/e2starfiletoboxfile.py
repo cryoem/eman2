e820535c6123f5b3e56b4b6f6c85d26984e4f7d6
jmbell <jmbell>
2015-08-27 12:57:37 +0000

    adding cases to handle different types of star files

--- examples/e2starfiletoboxfile.py
+++ examples/e2starfiletoboxfile.py
@@ -13,15 +13,14 @@ def main():
 
 	parser = EMArgumentParser(usage=usage,version=EMANVERSION)
 
-	parser.add_argument("--input",help="""The .star file you wish to convert to .box format""",type=str)
+	parser.add_argument("--input",help="""The .star file you wish to convert to .box format""",type=str,required=True)
 	parser.add_argument("--output",help="""The name of the .box file to be written""",type=str)
 	parser.add_argument("--boxsize",help="""Specify the boxsize for each particle.""",required=True,type=int)
+	# need an argument to append word/phrase or dirname to output
+	# need path argument for alternative location
 	(options, args) = parser.parse_args()
 
-	if len(args) != 2: exit(-1)
-	
-	if not options.output: options.output = args[1]
-	if not options.input: options.input = args[0]
+	if len(args) < 1: exit(-1)
 	
 	if os.path.exists(args[0]): starf = StarFile(args[0])
 	else: exit(-1)
@@ -32,7 +31,7 @@ def main():
 		else: bn = f[-2]
 		print("Writing to {base}.box rather than {base}.{ext}".format(base=bn,ext=f[-1]))
 		options.output = bn+".box"
-	boxf = options.output
+		options.output
 	
 	bs = int(options.boxsize / 2)
 	
@@ -42,45 +41,49 @@ def main():
 	
 	print(hdr)
 	
-	mk = [i for i in hdr if "Micrograph" in i]
+	mks = [i for i in hdr if "Micrograph" in i]
 	xk = [i for i in hdr if "X" in i]
 	yk = [i for i in hdr if "Y" in i]
 	
 	# resolve correct x and y dictionary keys
 	
-	# case 1: only one X key
-	# case 2: multiple xk
-	# case 3: no xk
+	if len(xk) == 1: # case 1: only one X key
+		xk = xk[0]
+		if not options.output:
+			print("No output file name was specified. Will use the input basename as output.")
+			options.output = options.input.split('.')[0] + ".box"
+	elif len(xk) > 1: # case 2: multiple xk
+		xk = [i for i in xk if "Coordinate" in i][0]
+	else: # case 3: no xk
+		print("Could not find any keys containing 'X'")
+		exit(-1)
 	
-	# case 1: only one Y key
-	# case 2: multiple yk
-	# case 3: no yk
+	if len(yk) == 1: # case 1: only one Y key
+		yk = yk[0]
+	elif len(yk) > 1: # case 2: multiple yk
+		yk = [i for i in yk if "Coordinate" in i][0]
+	else: # case 3: no xk
+		print("Could not find any keys containing 'Y'")
+		exit(-1)
 	
 	embed()
-	exit(-1)
 	
 	# case 1: one file to one micrograph
 	if len(mks) < 1:
-
-		coords =
-		coords = [[l[xk],l[yk]] for l in data]
-		with open(boxf,'w+') as bf:
-			for coord in coords:
-				x = int(float(coord[0]))
-				y = int(float(coord[1]))
-				bf.write("{}\t{}\t{}\t{}\n".format(x-bs/2,y-bs/2,bs,bs))
-	
+		with open(options.output,'w+') as boxf:
+			for x,y in starf[xk],starf[yk]):
+				boxf.write("{}\t{}\t{}\t{}\n".format(x-bs/2,y-bs/2,bs,bs))
 	# case 2: multiple micrographs in file, one header name
 	elif len(mks) == 1:
-		mgs = list(set(starf[mghdr[0]))
-		# make box file for each micrograph
-		# add each particle to box file
-			# store x,y coords
-		# write box files with box size. format is "{}\t{}\t{}\t{}".format(x-bs/2,y-bs/2,bs/2,bs/2)
-			
+		mgs = list(set(starf[mks[0]]))
+		for mg in mgs:
+			boxfile = mg.split('.')[0] + ".box"
+			with open(boxfile,'w+') as boxf:
+				for x,y in zip(starf[xk],starf[yk]):
+					boxf.write("{}\t{}\t{}\t{}\n".format(x-bs/2,y-bs/2,bs,bs))
 	# case 3: multiple micrograph data in header
 	else:
-		print("Sorry, I'm not ready to handle this yet")
+		print("Sorry, I'm not ready to handle this type of .star file yet")
 		exit(-1)
 	
 
