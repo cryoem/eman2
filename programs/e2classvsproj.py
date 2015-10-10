70d903086c310d8e683ceda216a8e5734b51a58b
stevel <stevel>
2015-10-10 21:38:24 +0000

    more flexibility in e2classvsproj.py

--- programs/e2classvsproj.py
+++ programs/e2classvsproj.py
@@ -39,6 +39,30 @@ import os
 import sys
 import traceback
 
+def simfn(jsd,proj,fsp,i,options,verbose):
+	# Now find the best match for each particle. We could use e2simmx, but more efficient to just do it in place (though not parallel this way)
+	best=None
+	ptcl=EMData(fsp,i)
+	ptcl.process_inplace("normalize.edgemean")
+	vals={}
+	for proj in projs:
+		aligned=proj.align(options.align[0],ptcl,options.align[1],options.aligncmp[0],options.aligncmp[1])
+		if options.ralign != None: # potentially employ refine alignment
+			refine_parms=options.ralign[1]
+			refine_parms["xform.align2d"] = aligned.get_attr("xform.align2d")
+			proj.del_attr("xform.align2d")
+			aligned = proj.align(options.ralign[0],ptcl,refine_parms,options.raligncmp[0],options.raligncmp[1])
+		
+		c=ptcl.cmp(options.cmp[0],aligned,options.cmp[1])
+		if best==None or c<best[0] : best=(c,aligned,proj)
+		vals[projs.index(proj)]=c
+		if options.verbose>2 : print i,projs.index(proj),c
+	
+	if options.verbose>1: print "Class-average {} with projection {}".format(i,str(best[1]["xform.projection"]))
+
+	# return ptcl#, best sim val, aligned ptcl, projection, {per proj sim}
+	jsd.put((i,best[0],best[1],best[2],vals))
+
 def main():
 	progname = os.path.basename(sys.argv[0])
 	usage = """prog [options] <classes> <projection stack or 3Dmap> <output>
@@ -55,8 +79,10 @@ def main():
 	parser.add_argument("--align",type=str,help="The name of an 'aligner' to use prior to comparing the images", default="rotate_translate_flip")
 	parser.add_argument("--aligncmp",type=str,help="Name of the aligner along with its construction arguments",default="ccc")
 	parser.add_argument("--ralign",type=str,help="The name and parameters of the second stage aligner which refines the results of the first alignment", default="refine")
-	parser.add_argument("--raligncmp",type=str,help="The name and parameters of the comparitor used by the second stage aligner. Default is dot.",default="ccc")
+	parser.add_argument("--raligncmp",type=str,help="The name and parameters of the comparitor used by the second stage aligner. Default is ccc.",default="ccc")
 	parser.add_argument("--cmp",type=str,help="The name of a 'cmp' to be used in comparing the aligned images", default="ccc")
+	parser.add_argument("--savesim",type=str,default=None,"Save all of the similarity values to a multicolumn text file.")
+	parser.add_argument("--threads", default=4,type=int,help="Number of alignment threads to run in parallel on a single computer. This is the only parallelism supported by this program at present.", guitype='intbox', row=24, col=2, rowspan=1, colspan=1, mode="refinement")
 	parser.add_argument("--ppid", type=int, help="Set the PID of the parent process, used for cross platform PPID",default=-1)
 	parser.add_argument("--verbose", "-v", dest="verbose", action="store", metavar="n", type=int, default=0, help="verbose level [0-9], higner number means higher level of verboseness")
 
@@ -94,29 +120,40 @@ def main():
 			if options.verbose : print i,euler
 	else:
 		E2n=E2init(sys.argv, options.ppid)
-
-	# Now find the best match for each particle. We could use e2simmx, but more efficient to just do it in place (though not parallel this way)
+	
+	jsd=Queue.Queue(0)
 	nptcl=EMUtil.get_image_count(args[0])
-	for i in xrange(nptcl):
-		best=None
-		ptcl=EMData(args[0],i)
-		ptcl.process_inplace("normalize.edgemean")
-		for proj in projs:
-			aligned=proj.align(options.align[0],ptcl,options.align[1],options.aligncmp[0],options.aligncmp[1])
-			if options.ralign != None: # potentially employ refine alignment
-				refine_parms=options.ralign[1]
-				refine_parms["xform.align2d"] = aligned.get_attr("xform.align2d")
-				proj.del_attr("xform.align2d")
-				aligned = proj.align(options.ralign[0],ptcl,refine_parms,options.raligncmp[0],options.raligncmp[1])
-			
-			c=ptcl.cmp(options.cmp[0],aligned,options.cmp[1])
-			if best==None or c<best[0] : best=(c,aligned)
-			if options.verbose>1 : print i,projs.index(proj),c
-		
-		best[1].write_image(args[2],i*2)
-		ptcl.write_image(args[2],i*2+1)
-		if options.verbose: print "Class-average {} with projection {}".format(i,str(best[1]["xform.projection"]))
-		
+	thrds=[threading.Thread(target=alifn,args=(jsd,projs,args[0],i,options,options.verbose)) for i in xrange(nptcl)]
+
+	if options.savesim!=None : out=file(options.savesim,"w")
+	
+	# here we run the threads and save the results, no actual alignment done here
+	print len(thrds)," threads"
+	thrtolaunch=0
+	while thrtolaunch<len(thrds) or threading.active_count()>1:
+		# If we haven't launched all threads yet, then we wait for an empty slot, and launch another
+		# note that it's ok that we wait here forever, since there can't be new results if an existing
+		# thread hasn't finished.
+		if thrtolaunch<len(thrds) :
+			while (threading.active_count()==NTHREADS ) : time.sleep(.1)
+			if options.verbose : print "Starting thread {}/{}".format(thrtolaunch,len(thrds))
+			thrds[thrtolaunch].start()
+			thrtolaunch+=1
+		else: time.sleep(1)
+	
+		while not jsd.empty():
+	# return ptcl#, best sim val, aligned ptcl, projection, {per proj sim}
+			i,sim,ali,proj,pps=jsd.get()
+			ali.write_image(args[2],i*2)
+			proj.write_image(args[2],i*2+1)
+			if options.savesim:
+				for prj in pps.keys():
+					xf=projs[prj]["xform.projection"]
+					out.write("{}\t{}\t{}\t{}\t{}\n".format(i,prj,xf.get_rotation("eman")["alt"],xf.get_rotation("eman")["az"],sim)
+
+
+	for t in thrds:
+		t.join()
 
 	E2end(E2n)
 
