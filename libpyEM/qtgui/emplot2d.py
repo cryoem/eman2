591bdf906843db1fa9c3eb1ce87201043b53b78d
stevel <stevel>
2015-10-30 00:54:15 +0000

    Minor fixes

--- libpyEM/qtgui/emplot2d.py
+++ libpyEM/qtgui/emplot2d.py
@@ -1977,7 +1977,7 @@ class EMPlot2DInspector(QtGui.QWidget):
 
 	def savePdf(self):
 		"""Saves the contents of the current plot to a pdf"""
-		matplotlib.savefig("plot.pdf")
+		matplotlib.pyplot.savefig("plot.pdf")
 
 	def updPlot(self,s=None):
 		if self.quiet : return
--- programs/e2refine_easy.py
+++ programs/e2refine_easy.py
@@ -259,10 +259,17 @@ not need to specify any of the following other than the ones already listed abov
 	if options.input!=None :
 		xsize3d=EMData(options.model,0,True)["nx"]
 		xsize=EMData(options.input,0,True)["nx"]
-		if ( xsize3d != xsize ) :
+		img1 = EMData(options.input,0,True)
+		img3 = EMData(options.model,0,True)
+		apix1=1.0
+		try: 
+			apix1=img1["apix_x"]
+			apix3=img3["apix_x"]
+		except:
+			apix3=apix1
+		
+		if ( xsize3d != xsize or apix1!=apix3 ) :
 			print "WARNING: the dimensions of the particles (%d) do not match the dimensions of the starting model (%d). I will attempt to adjust the model appropriately."%(xsize,xsize3d)
-			img1 = EMData(options.input,0,True)
-			img3 = EMData(options.model,0,True)
 			try:
 				scale=img3["apix_x"]/img1["apix_x"]
 				print "Reference is {box3} x {box3} x {box3} at {apix3:1.2f} A/pix, particles are {box2} x {box2} at {apix2:1.2f} A/pix. Scaling by {scale:1.3f}".format(box3=img3["nx"],box2=img1["nx"],apix3=img3["apix_x"],apix2=img1["apix_x"],scale=scale)
