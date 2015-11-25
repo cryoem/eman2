eb1e731d0fde3c4185f9ea8168854088cf9a19de
hvoicu <hvoicu>
2015-11-25 23:59:15 +0000

    update sxrviper.py sxviper.py rviper.txt viper.txt

--- sparx/bin/sxviper.py
+++ sparx/bin/sxviper.py
@@ -24,7 +24,7 @@ def main(args):
 	usage += """
 
 stack			2D images in a stack file: (default required string)
-output_directory			directory name into which the results will be written (if it does not exist, it will be created, if it does exist, the results will be written possibly overwriting previous results)
+directory		output directory name: into which the results will be written (if it does not exist, it will be created, if it does exist, the results will be written possibly overwriting previous results) (default required string)
 """
 	
 	parser = OptionParser(usage,version=SPARXVERSION)
