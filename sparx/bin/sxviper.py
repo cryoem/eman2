df9b11df0ef0a1e5f9f6efc8fbb6c14e6e24382f
hvoicu <hvoicu>
2015-11-25 21:48:15 +0000

    update sxrviper.py sxviper.py rviper.txt viper.txt

--- sparx/bin/sxviper.py
+++ sparx/bin/sxviper.py
@@ -7,7 +7,7 @@ import global_def
 from global_def import *
 
 def main(args):
-	from utilities import write_text_row, drop_image, model_gauss_noise, get_im, set_params_proj, wrap_mpi_bcast, model_circle
+	from utilities import if_error_all_processes_quit_program, write_text_row, drop_image, model_gauss_noise, get_im, set_params_proj, wrap_mpi_bcast, model_circle
 	from logger import Logger, BaseLogger_Files
 	from mpi import mpi_init, mpi_finalize, MPI_COMM_WORLD, mpi_comm_rank, mpi_comm_size, mpi_barrier
 	import user_functions
@@ -20,21 +20,19 @@ def main(args):
 	from multi_shc import multi_shc
 
 	progname = os.path.basename(sys.argv[0])
-	usage = progname + " stack  output_directory  radius --ir=inner_radius --rs=ring_step --xr=x_range --yr=y_range  --ts=translational_search_step  --delta=angular_step --center=center_type --maxit1=max_iter1 --maxit2=max_iter2 --L2threshold=0.1 --ref_a=S --sym=c1"
+	usage = progname + " stack  [output_directory] --ir=inner_radius --rs=ring_step --xr=x_range --yr=y_range  --ts=translational_search_step  --delta=angular_step --center=center_type --maxit1=max_iter1 --maxit2=max_iter2 --L2threshold=0.1 --ref_a=S --sym=c1"
 	usage += """
 
 stack			2D images in a stack file: (default required string)
-output_directory			name of directory where files will be generated
-radius			radius of the particle: has to be less than < int(nx/2)-1 (default required int)
+output_directory			directory name into which the results will be written (if it does not exist, it will be created, if it does exist, the results will be written possibly overwriting previous results)
 """
 	
 	parser = OptionParser(usage,version=SPARXVERSION)
-	##### XXXXXXXXXXXXXXXXXXXXXX option does not exist in docs XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-	# parser.add_option("--radius",       type= "int",   default= -1,             help="<outer radius> for rotational correlation < int(nx/2)-1 (set to the radius of the particle)")
+	parser.add_option("--radius", type="int",            help="radius of the particle: has to be less than < int(nx/2)-1 (default required int)")
 
-	parser.add_option("--xr", type="string",  default= "0",            help="range for translation search in x direction: search is +/xr in pixels (default 0)")
-	parser.add_option("--yr", type="string",  default= "-1",            help="range for translation search in y direction: if omitted will be set to xr, search is +/yr in pixels (default 0)")
-	parser.add_option("--mask3D", type="string",  default=None,            help="3D mask file: (default sphere)")
+	parser.add_option("--xr", type="string",  default= '0',            help="range for translation search in x direction: search is +/xr in pixels (default '0')")
+	parser.add_option("--yr", type="string",  default= '0',            help="range for translation search in y direction: if omitted will be set to xr, search is +/yr in pixels (default '0')")
+	parser.add_option("--mask3D", type="string",  default= None,            help="3D mask file: (default sphere)")
 	parser.add_option("--moon_elimination", type="string",  default= "",            help="elimination of disconnected pieces: two arguments: mass in KDa and resolution in px/A separated by comma, no space (default none)")
 	parser.add_option("--ir", type="int",  default= 1,            help="inner radius for rotational search: > 0 (default 1)")
 	
@@ -43,11 +41,11 @@ radius			radius of the particle: has to be less than < int(nx/2)-1 (default requ
 	##### XXXXXXXXXXXXXXXXXXXXXX option does not exist in docs XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 	parser.add_option("--ou",       type= "int",   default= -1,                 help= SUPPRESS_HELP)
 	parser.add_option("--rs", type="int",  default= 1,            help="step between rings in rotational search: >0 (default 1)")
-	parser.add_option("--ts", type="string",  default= "1",            help="step size of the translation search in x-y directions: search is -xr, -xr+ts, 0, xr-ts, xr, can be fractional (default 1.0)")
-	parser.add_option("--delta", type="string",  default= "2",            help="angular step of reference projections: (default 2.0)")
-	parser.add_option("--center", type="float",  default= -1,            help="centering of 3D template: average shift method; 0: no centering; 1: center of gravity (default -1)")
-	parser.add_option("--maxit1", type="float",  default= 400,            help="maximum number of iterations performed for the GA part: (default 400)")
-	parser.add_option("--maxit2", type="float",  default= 50,            help="maximum number of iterations performed for the finishing up part: (default 50)")
+	parser.add_option("--ts", type="string",  default= '1.0',            help="step size of the translation search in x-y directions: search is -xr, -xr+ts, 0, xr-ts, xr, can be fractional (default '1.0')")
+	parser.add_option("--delta", type="string",  default= '2.0',            help="angular step of reference projections: (default '2.0')")
+	parser.add_option("--center", type="float",  default= -1.0,            help="centering of 3D template: average shift method; 0: no centering; 1: center of gravity (default -1.0)")
+	parser.add_option("--maxit1", type="float",  default= 400.0,            help="maximum number of iterations performed for the GA part: (default 400.0)")
+	parser.add_option("--maxit2", type="float",  default= 50.0,            help="maximum number of iterations performed for the finishing up part: (default 50.0)")
 	parser.add_option("--L2threshold", type="float",  default= 0.03,            help="stopping criterion of GA: given as a maximum relative dispersion of volumes' L2 norms: (default 0.03)")
 	parser.add_option("--ref_a", type="string",  default= "S",            help="method for generating the quasi-uniformly distributed projection directions: (default S)")
 	parser.add_option("--sym", type="string",  default= "c1",            help="point-group symmetry of the structure: (default c1)")
@@ -72,6 +70,8 @@ radius			radius of the particle: has to be less than < int(nx/2)-1 (default requ
 	#parser.add_option("--CTF",      action="store_true", default=False,         help="NOT USED Consider CTF correction during the alignment ")
 	#parser.add_option("--snr",      type="float",  default= 1.0,                help="NOT USED Signal-to-Noise Ratio of the data (default 1.0)")
 	# (options, args) = parser.parse_args(sys.argv[1:])
+
+	required_option_list = ['radius']
 	(options, args) = parser.parse_args(args)
 	# option_dict = vars(options)
 	# print parser
@@ -84,8 +84,16 @@ radius			radius of the particle: has to be less than < int(nx/2)-1 (default requ
 	else:
 		options.moon_elimination = map(float, options.moon_elimination.split(","))
 
+	# Making sure all required options appeared.
+	for required_option in required_option_list:
+		if not options.__dict__[required_option]:
+			print "\n ==%s== mandatory option is missing.\n"%required_option
+			print "Please run '" + progname + " -h' for detailed options"
+			return 1
 
-	if len(args) < 3 or len(args) > 4:
+
+
+	if len(args) < 2 or len(args) > 3:
 		print "usage: " + usage
 		print "Please run '" + progname + " -h' for detailed options"
 		return 1
@@ -96,8 +104,7 @@ radius			radius of the particle: has to be less than < int(nx/2)-1 (default requ
 
 	# 'radius' and 'ou' are the same as per Pawel's request; 'ou' is hidden from the user
 	# the 'ou' variable is not changed to 'radius' in the 'sparx' program. This change is at interface level only for sxviper.
-	# options.ou = options.radius 
-	options.ou = int(args[2]) 
+	options.ou = options.radius 
 	runs_count = options.nruns
 	mpi_rank = mpi_comm_rank(MPI_COMM_WORLD)
 	mpi_size = mpi_comm_size(MPI_COMM_WORLD)	# Total number of processes, passed by --np option.
