924edf590b8986a4a36cbb83dbdf0fbff338e6ed
horatio <horatio-fm@gmail.com>
2016-01-19 18:16:26 -0600

    Merged dec15_test_do_cones_01 with master
    Squashed commit of the following:
    
    commit 4ebd3c8f017115189e99e90cf9fbd10c08b31586
    Author: horatio <horatio-fm@gmail.com>
    Date:   Tue Jan 19 18:08:39 2016 -0600
    
        cleaned additional code to make the merge with master
    
    commit 7346c750ebcb08f5620ee9aa6c74b6af05ccced6
    Author: horatio <horatio-fm@gmail.com>
    Date:   Tue Jan 19 16:58:29 2016 -0600
    
        transferred code from development to sparx
    
    commit 30c4b946c45145ebc6fbc4b1ea183387eda505ed
    Author: horatio <horatio-fm@gmail.com>
    Date:   Tue Jan 19 16:15:21 2016 -0600
    
        2016-01-19--16-14-38-964  before starting trimming cone integration
    
    commit 19688648388e036ee1def4c91ecac41240140069
    Author: horatio <horatio-fm@gmail.com>
    Date:   Tue Jan 19 12:03:32 2016 -0600
    
        added
        return None, x3
        after
        print "Bracket did not find a mimimum"
        in
        bracket_def
    
    commit 220c96e298a8fe4a1ecb93bb4362e26a73f2a99a
    Author: horatio <horatio-fm@gmail.com>
    Date:   Thu Jan 14 11:54:45 2016 -0600
    
        found bug for difference in cones and no cones scenarios
    
    commit daade9c228bf9c02ae10e1944514e497ff33d319
    Author: horatio <horatio-fm@gmail.com>
    Date:   Mon Jan 11 16:42:11 2016 -0600
    
        making an <0 and an>=0 the same in generator
    
    commit e0043294a54033265c648ae10ab2d068851d082f
    Author: horatio <horatio-fm@gmail.com>
    Date:   Fri Jan 8 13:07:12 2016 -0600
    
        Brought changes from original remotes/old_eman2_dir/implement_cones02
    
    commit ccce5638373e3b1acfcf9c5dd19c0254156f5779
    Merge: 1ef688f e54a7cc
    Author: horatio <horatio-fm@gmail.com>
    Date:   Wed Jan 6 15:03:08 2016 -0600
    
        Merge branch 'master' into dec15_test_do_cones_01
    
    commit 1ef688ff078a4a28ebee37b962e4a75dadde4f7b
    Author: horatio <horatio-fm@gmail.com>
    Date:   Wed Jan 6 15:00:46 2016 -0600
    
        untracked .idea/vcs.xml
    
    commit 81b7b6add5f56f58d986d7daa0de3db1ce36c4b9
    Author: horatio <horatio-fm@gmail.com>
    Date:   Tue Jan 5 14:31:02 2016 -0600
    
        test src1
    
    commit b31de95eaf0d834c7003abab0627a2948d3cc957
    Merge: b85f20a 4049bc0
    Author: horatio <horatio-fm@gmail.com>
    Date:   Tue Jan 5 14:12:55 2016 -0600
    
        Merge branch 'master' into dec15_test_do_cones_01
    
    commit b85f20ac8b26471093dfc98f238185e27faf4184
    Author: horatio <horatio-fm@gmail.com>
    Date:   Tue Dec 22 14:31:48 2015 -0600
    
        remade the connection with *development.py files

--- sparx/bin/sxmeridien.py
+++ sparx/bin/sxmeridien.py
@@ -1475,6 +1475,7 @@ def main():
 	import time
 	import socket
 
+	# 2016-01-19--16-14-38-964  before starting trimming cone integration
 	
 	# ------------------------------------------------------------------------------------
 	# PARSE COMMAND OPTIONS
