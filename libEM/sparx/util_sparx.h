469bfe46c901a91c17cf0df0eef0cc596920b201
Zhong Huang <zhonghuang@huang.local>
2016-01-21 10:46:28 -0600

    modified:   libEM/sparx/util_sparx.cpp     modified:   libEM/sparx/util_sparx.h     modified:   libpyEM/libpyUtils2.cpp

--- libEM/sparx/util_sparx.h
+++ libEM/sparx/util_sparx.h
@@ -1085,7 +1085,8 @@ public:
 	static float ccc_images_G(EMData* image, EMData* refim, EMData* mask, Util::KaiserBessel& kb, float ang, float sx, float sy);
 
 	static float local_inner_product(EMData* image1, EMData* image2, int lx, int ly, int lz, int w);
-
+	
+	static void version();
 	static EMData* move_points(EMData* img,  float qprob, int ri, int ro);
 
 	static EMData* get_biggest_cluster( EMData* mg );
