6674fc3789369393344a84f36030769c8a6aa8ef
Zhong Huang <zhonghuang@huang.local>
2016-04-14 17:01:35 -0500

    corrections

--- sparx/libpy/utilities.py
+++ sparx/libpy/utilities.py
@@ -6843,7 +6843,7 @@ def split_a_group(workdir,list_of_a_group,Tracker):
 def search_lowpass(fsc):
 	fcutoff =.5
 	for i in xrange(len(fsc[1])):
-		if fsc[0][i]<.5:
+		if fsc[1][i]<.5:
 			break
 	if i<len(fsc[1])-1:
 		fcutoff=fsc[0][i-1]
