db696ac730798f1fe1af7b3b2aa4d9e73a12906d
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2016-05-13 17:58:16 +0200

    Set Tabs to the center

--- sparx/templates/sxgui_template.py
+++ sparx/templates/sxgui_template.py
@@ -394,6 +394,38 @@ class SXCmdWidget(QWidget):
 		tab_widget.insertTab(0, self.sxcmd_tab_main, self.sxcmd_tab_main.name)
 		tab_widget.insertTab(1, self.sxcmd_tab_advance, self.sxcmd_tab_advance.name)
 		tab_widget.setAutoFillBackground(True)
+		tab_widget.setStyleSheet("""QTabWidget::pane {
+			border-top: 2px solid #C2C7CB;
+			position: absolute;
+			top: -0.5em;
+		}
+
+		QTabWidget::tab-bar {
+			alignment: center;
+		}
+
+		QTabBar::tab {
+			background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
+					stop: 0 #E1E1E1, stop: 0.4 #DDDDDD,
+					stop: 0.5 #D8D8D8, stop: 1.0 #D3D3D3);
+			border: 2px solid #C4C4C3;
+			border-bottom-color: #C2C7CB; /* same as the pane color */
+			border-top-left-radius: 4px;
+			border-top-right-radius: 4px;
+			min-width: 8ex;
+			padding: 2px;
+		}
+
+		QTabBar::tab:selected, QTabBar::tab:hover {
+			background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
+					stop: 0 #fafafa, stop: 0.4 #f4f4f4,
+					stop: 0.5 #e7e7e7, stop: 1.0 #fafafa);
+		}
+
+		QTabBar::tab:selected {
+			border-color: #9B9B9B;
+			border-bottom-color: #C2C7CB; /* same as pane color */
+		}""")
 		palette = tab_widget.palette()
 		palette.setBrush(QPalette.Background, QBrush(SXLookFeelConst.sxcmd_widget_bg_color))
 		tab_widget.setPalette(palette)
