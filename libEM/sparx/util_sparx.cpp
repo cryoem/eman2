614032de50f9f491648859e5546845137cd3efec
Pawel <pawelpap@att.net>
2016-06-30 09:41:10 -0500

    VERSION UPDATE

--- libEM/sparx/util_sparx.cpp
+++ libEM/sparx/util_sparx.cpp
@@ -22742,7 +22742,7 @@ float Util::ccc_images_G(EMData* image, EMData* refim, EMData* mask, Util::Kaise
 
 void Util::version()
 {
- cout <<"  VERSION  06/23/2016  06:03 PM "<<endl;
+ cout <<"  VERSION  06/30/2016  09:40 AM "<<endl;
  cout <<"  Compile time of util_sparx.cpp  "<< __DATE__ << "  --  " << __TIME__ <<endl;
 }
 
