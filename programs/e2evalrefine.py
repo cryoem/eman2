99febfc72c2b1611c559b1288187e63bb1ee5d4b
James Michael Bell <jmbell91@gmail.com>
2016-08-17 16:17:10 -0500

    updated evalptclqual

--- programs/e2evalrefine.py
+++ programs/e2evalrefine.py
@@ -309,12 +309,12 @@ def main():
 #		except: pass
 
 #		fout=open("ptclsnr.txt".format(i),"w")
-		ptclfsc = "ptclfsc_{}.txt".format(args[0][-2:])
+		ptclfsc = "ptclfsc_{}.txt".format("_".join(args[0].split("_")[1:]))
 		fout=open(ptclfsc,"w")
 		# generate a projection for each particle so we can compare
 
 		pj = 0
-		pf = "ptclfsc_{}_projections.hdf".format(args[0][-2:])
+		pf = "ptclfsc_{}_projections.hdf".format("_".join(args[0].split("_")[1:]))
 
 		tlast=time()
 		for i in xrange(nref):
@@ -622,143 +622,6 @@ def main():
 				print "\t%s\t%1.2f hours\t%s"%(difftime(ttime),ttime/3600.0,hist[n][0])
 
 			n+=1
-
-	#if options.ptcltrace:
-		#print "Particle trace mode"
-
-		#dirs = sorted(os.listdir(options.refine))
-		#for f in dirs:
-			#path = options.refine+"/"+f
-			#if "classmx" in f: cmx.append(path)
-			#elif "projections" in f: proj.append(path)
-			#elif "classes" in f: classes.append(path)
-
-		#if len(cmx) < 2:
-			#print("ERROR: The specified refinement directory must contain at least two classmx files.")
-			#sys.exit(1)
-
-		#if len(cmx) != len(proj):
-			#print("ERROR: The specified refinement directory must contain one projections file for each classmx file")
-			#sys.exit(1)
-
-		#cls = [] # read all classification matrix data into a list of lists
-		#for i in range(0,len(cmx),2):
-			#eps = EMData.read_images(cmx[i])
-			#ops = EMData.read_images(cmx[i+1])
-			#eops = [ptcl for pair in map(None,eps,ops) for ptcl in pair if ptcl is not None]
-			#cls.append(eops)
-		#nptcl = cls[0][0]['ny'] # particles are along the y axis
-
-		## Create a list of lists of Transforms representing the orientations of the reference projections
-		## for each classmx file and try to get projection orientation information for each class
-
-		#if options.verbose: print("Parsing assigned projection orientations")
-
-		#clsort=[]
-		#for x,p,c in zip(cmx,proj,classes):
-			#ncls=EMUtil.get_image_count(p)
-			#orts = []
-			#for i in xrange(ncls):
-				#if options.verbose:
-					#sys.stdout.write('\r{}\t{}\t{}/{}\t'.format(x,p,i+1,ncls))
-				#orts.append( EMData(p,i,True)["xform.projection"] )
-			#clsort.append(orts)
-			#if options.verbose: print("")
-
-		## Get a list of Transform objects to move to each other asymmetric unit in the symmetry group
-		#syms=parsesym( str(options.sym) ).get_syms()
-
-		#if options.verbose: print("Tracing particles...")
-
-		#with open(options.output,"w") as outf:
-
-			#for p in xrange(nptcl):
-				#if options.verbose: sys.stdout.write('\r{0:.0f} / {1:.0f}\t'.format(p+1,nptcl))
-				#isodd = p%2
-
-				#dat = []
-
-				#for i in xrange(1,len(cls)):
-					#ort1=clsort[i-1][int(cls[i-1][0][0,p])] # orientation of particle in first classmx
-					#ort2=clsort[i][int(cls[i][0][0,p])]		# orientation of particle in second classmx
-
-					#diffs=[] # make a list of the rotation angle to each other symmetry-related point
-					#for t in syms:
-						#ort2p=ort2*t
-						#diffs.append((ort1*ort2p.inverse()).get_rotation("spin")["omega"])
-
-					#diff=min(diffs) # The angular error for the best-agreeing orientation
-
-					#cls1 = int(cls[i-1][0][0,p])
-					#e1 = ort1.get_rotation("eman")
-					#az1 = e1["az"]
-					#alt1 = e1["alt"]
-					#phi1 = e1["phi"]
-
-					#cls2 = int(cls[i][0][0,p])
-					#e2 = ort2.get_rotation("eman")
-					#az2 = e2["az"]
-					#alt2 = e2["alt"]
-					#phi2 = e2["phi"]
-
-					#clsdiff = abs(cls2-cls1)
-					#azdiff = abs(az2-az1)
-					#altdiff = abs(alt2-alt1)
-					#phidiff = abs(phi2-phi1)
-
-					#d = [az1,alt1,phi1,cls1,az2,alt2,phi2,cls2,diff,clsdiff,azdiff,altdiff,phidiff]
-					#dat.append("\t".join([str(i) for i in d]))
-
-					#if i == 0:
-						#try:
-							#if isodd: classes1 = cmx[1].replace("classmx","classes")
-							#else: classes1 = cmx[0].replace("classmx","classes")
-							#hdr1 = EMData(classes1,cls1,True)
-							#idx1 = hdr1["projection_image_idx"]
-							#proj1 = hdr1["projection_image"]
-						#except:
-							#pass
-
-				#data = "\t".join(dat)
-
-				#try:
-					#if isodd: classes2 = cmx[-1].replace("classmx","classes")
-					#else: classes2 = cmx[-2].replace("classmx","classes")
-					#hdr2 = EMData(classes2,cls2,True)
-					#idx2 = hdr2["projection_image_idx"]
-					#proj2 = hdr2["projection_image"]
-					#c = [cls1,classes1,cls2,classes2,idx1,proj1,idx2,proj2,p,hdr2["class_ptcl_src"]]
-					#cmt = ";".join([str(i) for i in c])
-				#except:
-					#cmt = "no particles in class corresponding to this projection"
-
-				#line = data + "\t# " + cmt + "\n"
-				#outf.write(line)
-
-		#if ".txt" in options.output: kf = options.output.replace(".txt",".key")
-		#else: kf = options.output + ".key"
-
-		#with open(kf,"w") as keyfile:
-			#ctr = 0
-			#for i in range(1,len(cls)+1):
-				#k = []
-				#k.append("{}:\taz (iter {})".format(ctr,i-1))
-				#k.append("{}:\talt (iter {})".format(ctr+1,i-1))
-				#k.append("{}:\tphi (iter {})".format(ctr+2,i-1))
-				#k.append("{}:\tclass num (iter {})".format(ctr+3,i-1))
-				#k.append("{}\taz (iter {})".format(ctr+4,i))
-				#k.append("{}:\talt (iter {})".format(ctr+5,i))
-				#k.append("{}:\tphi (iter {})".format(ctr+6,i))
-				#k.append("{}:\tclass num (iter {})".format(ctr+7,i))
-				#k.append("{}:\tangular error (iters {} and {})".format(ctr+8,i-1,i))
-				#k.append("{}:\tclass num diff (iters {} and {})".format(ctr+9,i-1,i))
-				#k.append("{}:\taz diff (iters {} and {})".format(ctr+10,i-1,i))
-				#k.append("{}:\talt diff (iters {} and {})".format(ctr+11,i-1,i))
-				#k.append("{}:\tphi diff (iters {} and {})".format(ctr+12,i-1,i))
-				#keyfile.write("\n".join([x for x in k])+"\n")
-				#ctr+=len(k)
-
-		#print("Particle trace results stored in {}.\nThe file {} describes the contents of each column.".format(options.output,kf))
 
 
 if __name__ == "__main__":
