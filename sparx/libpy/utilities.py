ebe4f58b32f7819bbf5542df6022c195ebb47859
Pawel <pawelpap@att.net>
2016-11-11 12:43:12 -0600

    angles selections

--- sparx/libpy/utilities.py
+++ sparx/libpy/utilities.py
@@ -3648,6 +3648,9 @@ def getfvec( phi, tht ):
 	return (x,y,z)
 
 def nearest_fang( vecs, phi, tht ):
+	"""
+		vecs = [ [x0,y0,z0], [x1,y1,z1], ...]
+	"""
 	from utilities import getfvec
 	vec = getfvec( phi, tht )
 	return  Util.nearest_fang(vecs, vec[0],vec[1],vec[2])[0]
@@ -3803,7 +3806,7 @@ def nearest_full_k_projangles(anormals, refang, howmany = 1, sym="c1"):
 	if( sym == "c1"):
 		refnormal = []
 		for i,q in enumerate(anormals):
-			refnormal += getfvec(q[0],q[1])
+			refnormal.append(getfvec(q[0],q[1]))
 		ref = getfvec(refang[0],refang[1])
 		for i in xrange(howmany):
 			tmp = Util.nearest_fang(refnormal, ref[0],ref[1],ref[2])
@@ -3819,7 +3822,7 @@ def nearest_full_k_projangles(anormals, refang, howmany = 1, sym="c1"):
 
 		refnormal = []
 		for i,q in enumerate(anormals):
-			refnormal += getfvec(q[0]*phin,q[1])
+			refnormal.append(getfvec(q[0]*phin,q[1]))
 		ref = getfvec(refang[0]*phin,refang[1])
 		for i in xrange(howmany):
 			tmp = Util.nearest_fang(refnormal, ref[0],ref[1],ref[2])
@@ -3843,9 +3846,10 @@ def nearest_full_k_projangles(anormals, refang, howmany = 1, sym="c1"):
 
 		refnormal = []
 		for i,q in enumerate(anormals):
-			refnormal += getfvec(q[0],q[1])
+			refnormal.append(getfvec(q[0],q[1]))
 
 		for i in xrange(howmany):
+			lookup = range(len(anormals))
 			best_i = -1
 			best_v = -10000000
 			for l in xrange(nt):
@@ -3870,66 +3874,61 @@ def nearest_full_k_projangles(anormals, refang, howmany = 1, sym="c1"):
 def nearest_many_full_k_projangles(anormals, refangs, howmany = 1, sym="c1"):
 	# We assume refang can be on the list of normals
 	from utilities import getfvec
-	#lookup = range(len(anormals))
-	#refnormal = normals[:]
-	assignments = [-1]*howmany
+	assignments = [-1]*len(refangs)
+	refnormal = []
 
 	if( sym == "c1"):
-		refnormal = []
+
 		for i,q in enumerate(anormals):
 			refnormal.append(getfvec(q[0],q[1]))
-		assignments = [-1]*len(refangs)
 		for i,q in enumerate(refangs):
 			ref = getfvec(q[0],q[1])
 			assignments[i] = Util.nearest_fang_select(refnormal, ref[0],ref[1],ref[2], howmany)
 
 	elif( sym[:1] == "c" ):
-		from utilities import get_symt, getfvec
+		from utilities import get_symt
 		from EMAN2 import Vec2f, Transform
 		phin = int(sym[1:])
 
-		refnormal = []
 		for i,q in enumerate(anormals):
 			refnormal.append(getfvec(q[0],q[1]))
-		ref = getfvec(refang[0]*phin,refang[1])
-		for i in xrange(howmany):
-			tmp = Util.nearest_fang(refnormal, ref[0],ref[1],ref[2])
-			k = tmp[0]
-			assignments[i] = lookup[k]
-			for l in xrange(3): del refnormal[3*k+2-l]
-			del lookup[k]
+		for i,q in enumerate(refangs):
+			ref = getfvec(q[0],q[1])
+			assignments[i] = Util.nearest_fang_select(refnormal, ref[0],ref[1],ref[2], howmany)
 
 	elif( sym[:1] == "d" ):
-		from utilities import get_symt, getfvec
+		from utilities import get_symt
 		from EMAN2 import Vec2f, Transform
 		t = get_symt(sym)
 		nt = len(t)
-		a = Transform({"type":"spider","phi":refang[0], "theta":refang[1]})
-		refvec = [None]*nt
-		for i in xrange(nt):
-			qt = a*(t[i].inverse())
-			qt = qt.get_params("spider")
-			refvec[i] = getfvec(qt["phi"], qt["theta"])
-			print i,qt["phi"], qt["theta"],["psi"],refvec[i]
+		refvec = []
+		for i,q in enumerate(refangs):
+			a = Transform({"type":"spider","phi":q[0], "theta":q[1]})
+			temp = [None]*nt
+			for i in xrange(nt):
+				qt = a*(t[i].inverse())
+				qt = qt.get_params("spider")
+				temp[i] = getfvec(qt["phi"], qt["theta"])
+			refvec.append(temp)
 
-		refnormal = []
 		for i,q in enumerate(anormals):
-			refnormal += getfvec(q[0],q[1])
-
-		for i in xrange(howmany):
-			best_i = -1
-			best_v = -10000000
-			for l in xrange(nt):
-				tmp = Util.nearest_fang(refnormal, refvec[l][0],refvec[l][1],refvec[l][2])
-				if(tmp[1] > best_v):
-					best_i = tmp[0]
-					best_v = tmp[1]
-					print i,l,best_i,best_v
-
-			assignments[i] = lookup[best_i]
-			for l in xrange(3): del refnormal[3*best_i+2-l]
-			del lookup[best_i]
-
+			refnormal.append(getfvec(q[0],q[1]))
+		#  This should be pushed to C
+		for i,q in enumerate(refangs):
+			lookup = range(len(refnormal))
+			tass = [0]*howmany
+			for k in xrange(howmany):
+				temp = [refnormal[m] for m in lookup]
+				best_i = -1
+				best_v = -10000000
+				for l in xrange(nt):
+					tmp = Util.nearest_fang(temp, refvec[i][l][0],refvec[i][l][1],refvec[i][l][2])
+					if(tmp[1] > best_v):
+						best_i = tmp[0]
+						best_v = tmp[1]
+				tass[k] = lookup[best_i]
+				del lookup[best_i]
+			assignments[i] = tass[:]
 
 	else:
 		ERROR("  ERROR:  symmetry not supported  "+sym,"nearest_full_k_projangles",1)
