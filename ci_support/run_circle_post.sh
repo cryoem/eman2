802b390f90d6c83b925d9fc004028ce817cdf374
shadow_walker <shadowwalkersb@gmail.com>
2017-02-15 09:28:45 -0600

    configure CircleCI tests to build and install on two containers

---
+++ ci_support/run_circle_post.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Run tests
+export PATH="$HOME/miniconda2/bin/:$HOME/EMAN2/bin:$PATH"
+export PYTHONPATH="$HOME/EMAN2/lib:$HOME/EMAN2/bin:$PYTHONPATH"
+export EMAN2DIR=$HOME/EMAN2
+
+e2version.py
