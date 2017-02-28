0845120938f465ccbcfbcba3f2a075706996b06e
shadow_walker <shadowwalkersb@gmail.com>
2017-02-27 21:35:03 -0600

    refactor activating conda

--- ci_support/pre_build.sh
+++ ci_support/pre_build.sh
@@ -9,12 +9,13 @@ curl -L -O "${MINICONDA_URL}/${MINICONDA_FILE}"
 bash $MINICONDA_FILE -b
 
 # Conda-install packages
-$HOME/miniconda2/bin/conda config --set show_channel_urls true
+source ${HOME}/miniconda2/bin/activate root
+conda config --set show_channel_urls true
 
-$HOME/miniconda2/bin/conda install --yes --quiet qt=4 pyqt=4
-$HOME/miniconda2/bin/conda install --yes --quiet -c conda-forge boost boost-cpp fftw
-$HOME/miniconda2/bin/conda install --yes --quiet -c jmbell ftgl
-$HOME/miniconda2/bin/conda install --yes --quiet bsddb freetype gsl hdf5 ipython jpeg libpng libtiff matplotlib numpy=1.11 pyopengl scikit-learn scipy theano tk cmake
+conda install --yes --quiet qt=4 pyqt=4
+conda install --yes --quiet -c conda-forge boost boost-cpp fftw
+conda install --yes --quiet -c jmbell ftgl
+conda install --yes --quiet bsddb freetype gsl hdf5 ipython jpeg libpng libtiff matplotlib numpy=1.11 pyopengl scikit-learn scipy theano tk cmake
 
 # Build and install eman2
 export build_dir=$HOME/build_eman
