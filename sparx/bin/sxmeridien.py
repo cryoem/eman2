18d297bbc77c33fb7facd12edcee20ebcdfd5e08
Pawel <pawelpap@att.net>
2017-03-13 11:46:23 -0500

    make sure nxinit it increased when fsc is large at Nyquist

--- sparx/bin/sxmeridien.py
+++ sparx/bin/sxmeridien.py
@@ -215,9 +215,12 @@ def AI( fff, anger, shifter, chout = False):
 		if( shifter < Tracker["shifter"] ):		Tracker["shifter"] = shifter
 
 		inc = Tracker["currentres"]
-		if Tracker["large_at_Nyquist"]:	inc += int(0.25 * Tracker["constants"]["nnxo"]/2 +0.5)
-		else:							inc += Tracker["nxstep"]
-		tmp = min(2*inc, Tracker["constants"]["nnxo"] )  #  Cannot exceed image size
+		if Tracker["large_at_Nyquist"]:	
+			inc += int(0.25 * Tracker["constants"]["nnxo"]/2 +0.5)
+			tmp = min(max(2*inc,int(Tracker["nxinit"]*1.09), Tracker["constants"]["nnxo"] )
+		else:
+			inc += Tracker["nxstep"]
+			tmp = min(2*inc, Tracker["constants"]["nnxo"] )  #  Cannot exceed image size
 
 		if( chout ): print("  IN AI nxstep, large at Nyq, outcoming current res, adjusted current, estimated image size",Tracker["nxstep"],Tracker["large_at_Nyquist"],Tracker["currentres"],inc,tmp)
 
