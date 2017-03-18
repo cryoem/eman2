87e78b3693d1d509e50edec2dd56c8822a8ffaf5
shadow_walker <shadowwalkersb@gmail.com>
2017-03-18 12:37:48 -0500

    download and install pydusa into $SP_DIR
    http://blake.bcm.edu/emanwiki/EMAN2/Parallel/PyDusa

--- recipes/eman/build.sh
+++ recipes/eman/build.sh
@@ -10,3 +10,9 @@ cmake $SRC_DIR
 
 make
 make install
+
+curl -v -L -O http://ncmi.bcm.edu/ncmi/software/counter_222/software_121/pydusa-1.15es-fftmpi-6__2016_09_07.tgz
+tar xzvf pydusa-1.15es-fftmpi-6__2016_09_07.tgz
+cd pydusa-1.15es-fftmpi-6
+export EMAN2DIR=$SP_DIR
+./install_mpi.py --force
