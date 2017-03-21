724271f1022aa302e3f41af9a58522a557254a1c
shadow_walker <shadowwalkersb@gmail.com>
2017-03-21 13:33:15 -0500

    make clean && make distclean

--- recipes/eman/build.sh
+++ recipes/eman/build.sh
@@ -16,6 +16,9 @@ tar xzvf pydusa-1.15es-fftmpi-6__2016_09_07.tgz
 cd pydusa-1.15es-fftmpi-6
 export EMAN2DIR=$SP_DIR
 
+make clean
+make distclean
+
 patch Makefile.am "${RECIPE_DIR}"/patch1.diff
 patch Makefile.in "${RECIPE_DIR}"/patch2.diff
 
