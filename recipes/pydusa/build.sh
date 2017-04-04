df7a7a38b20ed25b995e0df4274d6e7424c764be
shadow_walker <shadowwalkersb@gmail.com>
2017-04-04 13:03:28 -0500

    add recipes: pydusa from pydusa-feedstock, fftw-mpi from fftw-mpi-feedstock [ci skip]

---
+++ recipes/pydusa/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export CFLAGS="-I${PREFIX}/include"
+
+./configure --prefix=${SP_DIR}
+
+sed -i.bak 's~\(^LDFLAGS.*$\)~\1 -L/'"${PREFIX}"'/lib -lfftw3_mpi -lfftw3~' src/Makefile
+
+make
+make install
