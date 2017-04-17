f13b67610ee9a2999513dea17cad33dda6181299
Steve Ludtke <sludtke42@gmail.com>
2017-04-16 19:57:08 -0500

    modify root in weight

--- libEM/averager.cpp
+++ libEM/averager.cpp
@@ -535,7 +535,8 @@ EMData * LocalWeightAverager::finish()
 		imc->mult(*stg1);
 		imc->process_inplace("filter.lowpass.gauss",Dict("cutoff_freq",0.02f));
 		imc->process_inplace("threshold.belowtozero",Dict("minval",0.0f));
-		imc->process_inplace("math.sqrt");
+#		imc->process_inplace("math.sqrt");
+		imc->process_inplace("math.pow",{"pow":0.25});
 		(*im)->mult(*imc);
 		result->add(**im);
 		normimage->add(*imc);
