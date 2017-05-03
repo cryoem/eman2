25175f949dbf6d5867a3ef0287af39aee234a819
shadow_walker <shadowwalkersb@gmail.com>
2017-05-03 17:47:14 -0500

    fftw-mpi: conda-remove and conda-install

--- utils/install_pydusa.sh
+++ utils/install_pydusa.sh
@@ -6,6 +6,8 @@ source activate
 
 RECIPE_DIR="${CONDA_PREFIX}/recipes"
 conda build ${RECIPE_DIR}/pydusa
+conda remove fftw-mpi --force --yes
 conda install pydusa --use-local --yes
+conda install fftw-mpi --use-local --yes
 
 conda inspect linkages pydusa
