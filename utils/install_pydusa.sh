ebf74804259dbc6496e940351f8205c9b7fcc1f7
shadow_walker <shadowwalkersb@gmail.com>
2017-05-11 21:10:44 -0500

    change RECIPE_DIR to be relative to script, not environment that may not be EMAN2

--- utils/install_pydusa.sh
+++ utils/install_pydusa.sh
@@ -4,8 +4,8 @@ set -xe
 
 source activate root
 
-RECIPE_DIR="${CONDA_PREFIX}/recipes"
-conda build ${RECIPE_DIR}/pydusa
+RECIPES_DIR=$(cd $(dirname $0)/../recipes && pwd -P)
+conda build ${RECIPES_DIR}/pydusa
 conda remove fftw-mpi --force --yes
 conda install pydusa --use-local --yes
 conda install fftw-mpi --use-local --yes
