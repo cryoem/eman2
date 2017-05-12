111922828cee03e09b89dacb97832267b6ee2f29
muyuanc <g5v991x@gmail.com>
2017-05-11 20:17:19 -0500

    initmodel_sgd.py : do gradient in fourier space. and some parameter tweaking. and some documentation

--- examples/initmodel_sgd.py
+++ examples/initmodel_sgd.py
@@ -24,15 +24,17 @@ def make3d(aptcls, sym="c1"):
 	threed.process_inplace("xform.applysym", {"sym":sym})
 	threed.process_inplace("xform.centerofmass")
 	threed=threed.get_clip(Region((pad-boxsize)/2,(pad-boxsize)/2,(pad-boxsize)/2,boxsize,boxsize,boxsize))
-	threed.process_inplace("normalize.edgemean")
+	threed.process_inplace("normalize")
 	threed.process_inplace("mask.gaussian",{"inner_radius":boxsize/3.0,"outer_radius":boxsize/12.0})
 
-	threed.process_inplace("mask.auto3d",{"radius":boxsize/6,"threshold":threed["sigma_nonzero"]*.85,"nmaxseed":30,"nshells":boxsize/20,"nshellsgauss":boxsize/20})
+	
+	
+	threed["apix_x"]=threed["apix_y"]=threed["apix_z"]=aptcls[0]["apix_x"]
 	return threed
 
 def makeprojs(threed, sym):
 	origen=parsesym(sym)
-	oris=origen.gen_orientations('eman',{"delta":10})
+	oris=origen.gen_orientations('eman',{"delta":9., "perturb":True})
 	projs=[threed.project("standard",ort) for ort in oris]
 	return projs
 
@@ -44,9 +46,8 @@ def do_ali(ptcls, projs):
 	bss=.0
 	pjs=[(p, None) for p in projs]
 	for i in range(len(ptcls)):
-		sim=cmponetomany(pjs,ptcls[i],align=("rotate_translate_flip",{"maxshift":boxsize/5}),alicmp=("ccc",{}),ralign=("refine",{}),cmp=("frc",{"minres":80,"maxres":20}))
-#		 sim=cmponetomany(pjs,ptcls[i],align=("rotate_translate_tree",{"maxshift":boxsize/5, "maxres":15}),alicmp=("ccc",{}),ralign=("refine",{}),cmp=("frc",{"minres":80,"maxres":20}))
-
+		#sim=cmponetomany(pjs,ptcls[i],align=("rotate_translate_flip",{"maxshift":boxsize/5}),alicmp=("ccc",{}),ralign=("refine",{}),cmp=("frc",{"minres":80,"maxres":20}))
+		sim=cmponetomany(pjs,ptcls[i],align=("rotate_translate_tree",{"maxshift":boxsize/5, "maxres":15}),alicmp=("ccc",{}),ralign=("refine",{}),cmp=("frc",{"minres":80,"maxres":10}))
 		bs=min(sim)
 		
 		bss+=bs[0]
@@ -64,8 +65,9 @@ def do_ali(ptcls, projs):
 	for i in range(len(ptcls)):
 		n=ptcls[bslst[i][1]]["match_n"]
 		quals.append(ptcls[bslst[i][1]]["match_qual"])
-		aptcls.append(ptcls[bslst[i][1]].align("rotate_translate_flip",projs[n],{},"ccc",{}))
+		aptcls.append(ptcls[bslst[i][1]].align("rotate_translate_tree",projs[n],{"maxshift":boxsize/5, "maxres":10},"frc",{}))
 		aptcls[-1].process_inplace("normalize.toimage",{"to":projs[n]})
+		#aptcls[-1].process_inplace("normalize")
 		aptcls[-1].add(-aptcls[-1]["mean"])
 	return aptcls
 
@@ -74,8 +76,7 @@ def random_sample(n,k):
 	np.random.shuffle(a)
 	return a[:int(k)]
 
-
-def make_model(jsd, options):
+def make_model(jsd,myid, options):
 	ptclname=options.ptcls
 	num=EMUtil.get_image_count(ptclname)
 	sym=options.sym
@@ -92,8 +93,9 @@ def make_model(jsd, options):
 		
 	map0=make3d(aptcls,sym)
 	map0.process_inplace("filter.lowpass.gauss",{"cutoff_abs":.1})
+	map0.process_inplace("filter.lowpass.randomphase",{"cutoff_abs":.05})
 	#map0.write_image("{}/model_00.hdf".format(path))
-
+	boxsize=map0["nx"]
 	niter=options.niter
 
 	ncopy=(niter+1)*nsample/num+1
@@ -105,61 +107,96 @@ def make_model(jsd, options):
 	scr=[]
 	for it in range(1,niter):
 		
+		pjs=None
+		if options.writetmp: map0.write_image("{}/model_tmp_{:02d}.hdf".format(path,myid), it-1)
 		pjs=makeprojs(map0, sym)
 			
-		ptcls=[EMData(ptclname, i) for i in samples[kk:kk+nsample]]
+		if it==niter-1:
+			if num<32:
+				ptcls=[EMData(ptclname, i) for i in range(num)]
+			else:
+				smp=random_sample(num, 32)
+				ptcls=[EMData(ptclname, i) for i in smp]
+		else:
+			ptcls=[EMData(ptclname, i) for i in samples[kk:kk+nsample]]
+			
+		for p in ptcls:
+			if options.addnoise>0:
+				p.process_inplace("math.addsignoise",{"noise":options.addnoise})
+			p.process_inplace("filter.lowpass.gauss",{"cutoff_freq":.1})
+			p.process_inplace("normalize.edgemean")
+			
+			
 		kk+=nsample
 	#	 print len(ptcls)
 		aptcls=do_ali(ptcls, pjs)
 		
-		if it==niter-1:
-			compares=[]
-			for i, p in enumerate(aptcls):
-				compares.append(p)
-				compares.append(pjs[p["match_n"]])
 		
 			
 		for i, p in enumerate(aptcls):
-			scr.append(p.cmp("ccc", pjs[p["match_n"]]))
+			scr.append(p.cmp("frc", pjs[p["match_n"]]))
+		map1=make3d(aptcls,sym)	
 			
-		if it>9:
-	#		 csym=sym
-	#		 learnrate=.2
-	#		 print "applying symmetry"
-			map1=make3d(aptcls,sym)
-			map1.process_inplace("filter.lowpass.gauss",{"cutoff_abs":.3})
-		else:
-			map1=make3d(aptcls,sym)
-			map1.process_inplace("filter.lowpass.gauss",{"cutoff_abs":.15})
-			
-		dmap=map1-map0
-		ddm=dmap*dmap
+		map0ft=map0.do_fft()
+		map1ft=map1.do_fft()
+		map1ft.process_inplace("mask.wedgefill",{"fillsource":map0ft, "thresh_sigma":.9})
+		dmap=map1ft-map0ft
+		
+		
+		#ddm=dmap*dmap
 		#print "{:d}\t{:.3f}\t{:.3f}".format(it, ddm["mean_nonzero"], np.mean(scr))
-		mapnew=map0+learnrate*dmap
+		mapnewft=map0ft+learnrate*dmap
+		mapnew=mapnewft.do_ift()
+		#if it<niter/2:
+			
+			#mapnew.process_inplace("filter.lowpass.gauss",{"cutoff_abs":.15})
+		#else:
+			#mapnew.process_inplace("filter.lowpass.gauss",{"cutoff_abs":.3})
 		learnrate*=lrmult
 		
-		#mapnew.write_image("{}/model_{:02d}.hdf".format(path,it))
+		
+		mapnew.process_inplace("filter.lowpass.gauss",{"cutoff_freq":.1})
+		#mapnew.process_inplace("filter.lowpass.gauss",{"cutoff_abs":.3})
+		mapnew.process_inplace("mask.auto3d",{"radius":boxsize/6,"threshold":map0["sigma_nonzero"]*.85,"nmaxseed":30,"nshells":boxsize/20,"nshellsgauss":boxsize/20})
+		ddmap=mapnew-map0
+		ddmap.mult(ddmap)
+		if options.verbose>0:print myid, it, ddmap["mean"]
+		
+		
 		map0=mapnew
-		pjs=None
+		#if ddmap["mean"]<.005:
+			#break
 	
+	compares=[]
+	for i, p in enumerate(aptcls):
+		compares.append(p)
+		compares.append(pjs[p["match_n"]])
 	scr=np.array(scr)
-	jsd.put((map0, ddm["mean_nonzero"], np.mean(scr[-num:]), compares))
+	jsd.put((map0, ddmap["mean"], np.mean(scr[-num:]), compares))
+	pjs=None
 	return 
 	#launch_childprocess("e2proclst.py {}/model_*.hdf --create {}/models.lst".format(path, path));
 
 def main():
 	
-	usage=" "
+	usage="""
+	This program makes initial models using a (kind of) stochastic gradient descent approach.
+	[prog] --ptcls <particle stack> 
+	
+	"""
 	parser = EMArgumentParser(usage=usage,version=EMANVERSION)
-	parser.add_argument("--path", type=str,help="initial model path", default="initmodel_00")
-	parser.add_argument("--ptcls", type=str,help="particles input", default=None)
-	parser.add_argument("--sym", type=str,help="symmetry", default='c1')
-	parser.add_argument("--batchsize", type=int,help="batch size", default=6)
-	parser.add_argument("--learnrate", type=float, help="learning rate", default=.1)
-	parser.add_argument("--lrdecay", type=float, help="learning rate multiplier after each iteratiions", default=1.)
-	parser.add_argument("--ntry", type=int,help="number of tries", default=10)
+	parser.add_argument("--path", type=str,help="Path to write initial model output. Default is initmodel_XX", default=None)
+	parser.add_argument("--ptcls", type=str,help="Class average or particles input.", default=None)
+	parser.add_argument("--sym", type=str,help="Symmetry", default='c1')
+	parser.add_argument("--batchsize", type=int,help="Batch size of stochastic gradient desent. N particles are randomly selected to generate an initial model at each step.", default=5)
+	parser.add_argument("--learnrate", type=float, help="Learning rate. i.e. how much the initial model changes toward the gradient direction in each iteration. Range from 0.0~1.0. Default is 0.3", default=.3)
+	parser.add_argument("--lrdecay", type=float, help="Learning rate multiplier after each iteration.", default=1.)
+	parser.add_argument("--addnoise", type=float, help="Add noise on particles at each iteration. Stablize convergence for some reason.", default=3.)
+	parser.add_argument("--ntry", type=int,help="Number of tries.", default=10)
+	parser.add_argument("--writetmp", action="store_true", default=False ,help="Write output for each iteration")
 	parser.add_argument("--threads", type=int,help="threads", default=10)
 	parser.add_argument("--niter", type=int,help="Number of iterations", default=20)
+	parser.add_argument("--verbose", type=int,help="Verbose", default=0)
 	(options, args) = parser.parse_args()
 	logid=E2init(sys.argv)
 
@@ -167,13 +204,25 @@ def main():
 	tt0=time.time()
 
 	path=options.path
-	try:os.mkdir(path)
-	except: 
-		os.system("rm  {}/*".format(path))
+	if path==None:
+		for i in range(100):
+			try:
+				path="initmodel_{:02d}".format(i)
+				os.mkdir(path)
+				options.path=path
+				print "Writing initial model in {}..".format(path)
+				break
+			except:
+				continue
+		else:
+			print "Too many initmodel folders in the project, or something odd happened....Exit."
+			exit()
 		
+	
+	
 	jsd=Queue.Queue(0)	
 	NTHREADS=max(options.threads+1,2)
-	thrds=[threading.Thread(target=make_model,args=(jsd, options)) for i in range(options.ntry)]	
+	thrds=[threading.Thread(target=make_model,args=(jsd, i, options)) for i in range(options.ntry)]	
 	thrtolaunch=0
 	models=[]
 	scrs=[]
@@ -203,7 +252,7 @@ def main():
 	
 	sid=np.argsort(scrs)
 	for i,si in enumerate(sid):
-		print i,grads[si],scrs[si]
+		print i,si,grads[si],scrs[si]
 		models[si].write_image("{}/model_{:02d}.hdf".format(path, i))
 		for c in cmps[si]:
 			c.write_image("{}/model_{:02d}_aptcl.hdf".format(path, i), -1)
