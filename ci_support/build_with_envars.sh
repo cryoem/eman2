e0a513ca6b795898eea098d0890113d887166dc1
shadow_walker <shadowwalkersb@gmail.com>
2017-05-12 20:48:05 -0500

    set PREFIX and SP_DIR programmatically, no hard-coded values

--- ci_support/build_with_envars.sh
+++ ci_support/build_with_envars.sh
@@ -3,8 +3,8 @@
 source ci_support/pre_build.sh
 
 export CONDA_BUILD_STATE=BUILD
-export PREFIX=$HOME/miniconda2/
-export SP_DIR=$PREFIX/lib/python2.7/site-packages
+export PREFIX=$CONDA_PREFIX
+export SP_DIR=$(python -c "import site; print site.getsitepackages()[0]")
 
 cmake $src_dir
 make
