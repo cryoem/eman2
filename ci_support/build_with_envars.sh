1987466ecf6d0b8100b98ddc626eab786bb44a11
shadow_walker <shadowwalkersb@gmail.com>
2017-05-15 17:01:58 -0500

    post-build needs PREFIX and SP_DIR

--- ci_support/build_with_envars.sh
+++ ci_support/build_with_envars.sh
@@ -6,4 +6,7 @@ cmake $src_dir
 make
 make install
 
+export PREFIX=$CONDA_PREFIX
+export SP_DIR=$(python -c "import site; print site.getsitepackages()[0]")
+
 source $src_dir/ci_support/post_build.sh
