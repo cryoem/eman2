c51f0cd7b22833109b3b85e198c3467e0b7b922f
shadow_walker <shadowwalkersb@gmail.com>
2017-05-15 16:27:51 -0500

    remove export statements, already set in cmake

--- ci_support/build_with_envars.sh
+++ ci_support/build_with_envars.sh
@@ -2,10 +2,6 @@
 
 source ci_support/pre_build.sh
 
-export CONDA_BUILD_STATE=BUILD
-export PREFIX=$CONDA_PREFIX
-export SP_DIR=$(python -c "import site; print site.getsitepackages()[0]")
-
 cmake $src_dir
 make
 make install
