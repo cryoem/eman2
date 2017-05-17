22176609ace54a512cd6b6723cb1d42a59181761
shadow_walker <shadowwalkersb@gmail.com>
2017-05-17 16:18:33 -0500

    specify numpy version in pydusa install script

--- utils/install_pydusa_numpy.sh
+++ utils/install_pydusa_numpy.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
+# Installs Pydusa against user-specified NumPy version
+
+if [ $# -ne 1 ];then
+    echo
+    echo -e '\033[35m'"  Usage: $(basename ${0}) [NumPy version]"'\033[0m'
+    echo
+    exit 1
+fi
+
 set -xe
 
 source activate root
 
 RECIPES_DIR=$(cd $(dirname $0)/../recipes && pwd -P)
-conda build ${RECIPES_DIR}/pydusa
+numpy_verison=${1//.}
+
+conda build ${RECIPES_DIR}/pydusa --numpy ${1}
 conda remove fftw-mpi --force --yes
-conda install pydusa --use-local --yes
+conda install pydusa=1.15=np${numpy_verison}_0 --use-local --yes
 conda install fftw-mpi --use-local --yes
 
 conda inspect linkages pydusa
