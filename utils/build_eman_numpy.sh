fc97d9a98471a9518c5e1c1bc4444b454839c701
shadow_walker <shadowwalkersb@gmail.com>
2017-05-17 17:13:03 -0500

    add and install numpy scripts: eman

---
+++ utils/build_eman_numpy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Builds EMAN against user-specified NumPy version(s)
+
+if [ $# -lt 1 ];then
+    echo
+    echo -e '\033[35m'"  Usage: $(basename ${0}) [NumPy version(s)]"'\033[0m'
+    echo
+    exit 1
+fi
+
+set -xe
+
+source activate root
+
+src_dir="${CONDA_PREFIX}/github.com/cryoem/eman2"
+if [ ! -d "${src_dir}/.git" ];then
+    mkdir -p "${src_dir}" && cd "${src_dir}/.."
+    git clone https://github.com/cryoem/eman2.git --branch numpy
+    cd -
+else
+    cd "${src_dir}"
+    git pull --rebase
+    cd -
+fi
+
+for v in ${@};do
+    conda build "${src_dir}/recipes/eman" --numpy ${v} -c cryoem -c defaults -c conda-forge
+done
