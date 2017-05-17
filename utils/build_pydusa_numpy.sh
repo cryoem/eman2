cd8eb25dadd47f35a2087cf6577f7306349f325b
shadow_walker <shadowwalkersb@gmail.com>
2017-05-17 10:43:30 -0500

    parse args and pass to conda-build: build pydusa script

--- utils/build_pydusa_numpy.sh
+++ utils/build_pydusa_numpy.sh
@@ -2,18 +2,29 @@
 
 # Builds numpy version(s), if not specified build all versions, 1.5 - 1.12
 
-set -xe
-
 source activate root
+
+# unset args before using, it is set in activate script above
+unset args
+for elem in ${@};do
+    regex=-*
+    if [[ $elem == $regex ]];then
+        opts=( ${opts[@]} $elem )
+    else
+        args=( ${args[@]} $elem )
+    fi
+done
+
+set -xe
 
 RECIPES_DIR=$(cd $(dirname $0)/../recipes && pwd -P)
 
-if [ $# -lt 1 ];then
+if [ ${#args} -lt 1 ];then
     numpy_versions=( 1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12 )
 else
-    numpy_versions=${@}
+    numpy_versions=${args[@]}
 fi
 
 for v in ${numpy_versions[@]};do
-    conda build "${RECIPES_DIR}/pydusa" --numpy ${v}
+    conda build "${RECIPES_DIR}/pydusa" --numpy ${v} ${opts[@]}
 done
