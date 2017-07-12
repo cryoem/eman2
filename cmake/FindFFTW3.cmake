2d16089320e3a12584bf38a65d820f4f68c7ecb4
shadow_walker <shadowwalkersb@gmail.com>
2017-07-12 17:30:58 -0500

    remove debug prints: findFFTW3

--- cmake/FindFFTW3.cmake
+++ cmake/FindFFTW3.cmake
@@ -1,14 +1,6 @@
 CHECK_REQUIRED_LIB(FFTW3F fftw3f fftw3.h libfftw3f-3 "")
 CHECK_REQUIRED_LIB(FFTW3D fftw3 fftw3.h libfftw3-3  "")
 
-SET(FFTW_LIBRARIES ${FFTW3F_LIBRARY} ${FFTW3D_LIBRARY})
-SET(FFTW_INCLUDE_PATH ${FFTW3_INCLUDE_PATH})
-
-if(ENABLE_CONDA)
-	message("FFTW_LIBRARIES: ${FFTW_LIBRARIES}")
-	message("FFTW_INCLUDE_PATH: ${FFTW_INCLUDE_PATH}")
-endif()
-
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(FFTW3
 		REQUIRED_VARS
