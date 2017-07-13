212074e6c938df50b358910d7301d5b411f636a6
shadow_walker <shadowwalkersb@gmail.com>
2017-07-13 00:32:12 -0500

    find_package: NFFT2

--- cmake/FindNFFT2.cmake
+++ cmake/FindNFFT2.cmake
@@ -1,3 +1,11 @@
 INCLUDE_DIRECTORIES(${NFFT2_INCLUDE_PATH})
 CHECK_OPTIONAL_LIB(NFFT2 nfft nfft.h)
 target_link_libraries(NFFT2 FFTW3D)
+
+message_var(NFFT2_INCLUDE_PATH)
+message_var(NFFT2_LIBRARY)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(NFFT2
+								  REQUIRED_VARS NFFT2_INCLUDE_PATH NFFT2_LIBRARY
+								  )
