6103820456be0bbeec5a40b04d514c6d9001a6d9
shadow_walker <shadowwalkersb@gmail.com>
2017-07-12 23:55:24 -0500

    find_package: DJBFFT

--- cmake/FindDJBFFT.cmake
+++ cmake/FindDJBFFT.cmake
@@ -1,3 +1,11 @@
 SET(DJBFFT_LIBRARIES ${DJBFFT_LIBRARY})
 SET(DJBFFT_INCLUDE_PATH ${DJBFFT_INCLUDE_PATH})
 CHECK_OPTIONAL_LIB(DJBFFT fftc4.h fftr4.h)
+
+message_var(DJBFFT_INCLUDE_PATH)
+message_var(DJBFFT_LIBRARY)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(DJBFFT
+								  REQUIRED_VARS DJBFFT_LIBRARY
+								  )
