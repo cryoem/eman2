1b2f34f4a951f5f85bb4b9b4933afe1d9b83182e
shadow_walker <shadowwalkersb@gmail.com>
2017-07-13 00:32:12 -0500

    interface target: NFFT2

--- cmake/FindNFFT2.cmake
+++ cmake/FindNFFT2.cmake
@@ -1,5 +1,4 @@
-INCLUDE_DIRECTORIES(${NFFT2_INCLUDE_PATH})
-CHECK_OPTIONAL_LIB(NFFT2 nfft nfft.h)
+CHECK_REQUIRED_LIB(NFFT2 nfft nfft.h "" "")
 target_link_libraries(NFFT2 FFTW3D)
 
 message_var(NFFT2_INCLUDE_PATH)
@@ -9,3 +8,12 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(NFFT2
 								  REQUIRED_VARS NFFT2_INCLUDE_PATH NFFT2_LIBRARY
 								  )
+
+if(NFFT2_FOUND AND NOT TARGET NFFT2)
+	add_library(NFFT2 INTERFACE)
+	set_target_properties(NFFT2 PROPERTIES
+						  INTERFACE_INCLUDE_DIRECTORIES "${NFFT2_INCLUDE_PATH}"
+						  INTERFACE_LINK_LIBRARIES      "${NFFT2_LIBRARY}"
+						  INTERFACE_COMPILE_DEFINITIONS USE_NFFT2
+						  )
+endif()
