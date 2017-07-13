1f601592464456ab955fbd54c330e4ea1725be03
shadow_walker <shadowwalkersb@gmail.com>
2017-07-12 20:25:41 -0500

    interface target: FTGL

--- cmake/FindFTGL.cmake
+++ cmake/FindFTGL.cmake
@@ -1,5 +1,4 @@
 CHECK_REQUIRED_LIB(FTGL ftgl FTGL/FTGL.h ftgl FTGL/ftgl.h)
-ADD_DEFINITIONS(-DUSE_FTGL)
 
 if(WIN32)
 	add_definitions(-DFTGL_LIBRARY_STATIC)
@@ -15,3 +14,14 @@ find_package_handle_standard_args(FTGL
 									  FTGL_INCLUDE_PATH
 									  FTGL_LIBRARY
 								  )
+
+if(FTGL_FOUND AND NOT TARGET FTGL)
+	add_library(FTGL INTERFACE)
+	add_library(FTGL::FTGL ALIAS FTGL)
+	set_target_properties(FTGL
+						  PROPERTIES
+						  INTERFACE_INCLUDE_DIRECTORIES "${FTGL_INCLUDE_PATH}"
+						  INTERFACE_LINK_LIBRARIES      "${FTGL_LIBRARY}"
+						  INTERFACE_COMPILE_DEFINITIONS  USE_FTGL
+						  )
+endif()
