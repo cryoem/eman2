9d3251f69c599b25fcfa0613113ba02cfdb35854
shadow_walker <shadowwalkersb@gmail.com>
2017-07-13 00:41:04 -0500

    find_package: OPTPP

--- cmake/FindOPTPP.cmake
+++ cmake/FindOPTPP.cmake
@@ -8,5 +8,19 @@ FIND_PATH(NEWMAT_INCLUDE_PATH newmat.h /usr/include /usr/include/opt++
 FIND_LIBRARY(OPTPP_LIBRARY NAMES opt-linux PATHS /usr/lib /usr/local/lib)
 FIND_LIBRARY(NEWMAT_LIBRARY NAMES newmat-linux PATHS /usr/lib /usr/local/lib)
 
+message_var(OPTPP_INCLUDE_PATH)
+message_var(OPTPP_LIBRARY)
+message_var(NEWMAT_INCLUDE_PATH)
+message_var(NEWMAT_LIBRARY)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(OPTPP
+								  REQUIRED_VARS
+								  	OPTPP_INCLUDE_PATH
+								  	OPTPP_LIBRARY
+								  	NEWMAT_INCLUDE_PATH
+								  	NEWMAT_LIBRARY
+								  )
+
 ADD_DEFINITIONS(-DUSE_OPTPP)
 INCLUDE_DIRECTORIES(${OPTPP_INCLUDE_PATH} ${NEWMAT_INCLUDE_PATH})
