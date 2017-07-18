f6a976a6a70f066848a0130b7800ae559f3a7251
shadow_walker <shadowwalkersb@gmail.com>
2017-07-18 14:55:10 -0500

    find_package: TIFF

--- cmake/TIFF.cmake
+++ cmake/TIFF.cmake
@@ -1,3 +1,10 @@
-CHECK_REQUIRED_LIB(TIFF tiff tiffio.h "" "")
+find_package(TIFF REQUIRED)
 add_definitions(-DUSE_TIFF)
 CHECK_LIB_ONLY(JPEG jpeg)
+
+message_var(TIFF_LIBRARIES)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(TIFF
+		REQUIRED_VARS TIFF_LIBRARIES
+		)
