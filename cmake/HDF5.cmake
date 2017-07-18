85c33095638c8a802a7b7cab3a03add29fe25f36
shadow_walker <shadowwalkersb@gmail.com>
2017-07-18 10:27:55 -0500

    interface target: HDF5

--- cmake/HDF5.cmake
+++ cmake/HDF5.cmake
@@ -10,3 +10,12 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(HDF5
 		REQUIRED_VARS HDF5_LIBRARY
 		)
+
+if(HDF5_FOUND AND NOT TARGET HDF5)
+	add_library(HDF5 INTERFACE)
+	add_library(HDF5::HDF5 ALIAS HDF5)
+	set_target_properties(HDF5 PROPERTIES 
+			INTERFACE_INCLUDE_DIRECTORIES "${HDF5_INCLUDE_PATH}"
+			INTERFACE_LINK_LIBRARIES      "${HDF5_LIBRARY}"
+			)
+endif()
