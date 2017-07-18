bff5f9f3bba6e70a01d4b4974d2bfd9a146b9cde
shadow_walker <shadowwalkersb@gmail.com>
2017-07-18 00:25:14 -0500

    find_package: FTGL

--- cmake/FindFTGL.cmake
+++ cmake/FindFTGL.cmake
@@ -3,3 +3,10 @@ ADD_DEFINITIONS(-DUSE_FTGL)
 IF(EXISTS ${FTGL_INCLUDE_PATH}/FTGL/FTGL.h AND COMMAND IF)
 	ADD_DEFINITIONS(-DOLD_FTGL)
 ENDIF()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(FTGL
+								  REQUIRED_VARS
+									  FTGL_INCLUDE_PATH
+									  FTGL_LIBRARY
+								  )
