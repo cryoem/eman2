326df535e457cdc47f4ed17cc981f276aa001e5b
shadow_walker <shadowwalkersb@gmail.com>
2017-07-18 15:01:46 -0500

    find_package: JPEG

--- cmake/JPEG.cmake
+++ cmake/JPEG.cmake
@@ -1,2 +1,9 @@
-CHECK_REQUIRED_LIB(JPEG jpeg jpeglib.h "" "")
+find_package(JPEG REQUIRED)
 add_definitions(-DUSE_JPEG)
+
+message_var(JPEG_LIBRARIES)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(JPEG
+								  REQUIRED_VARS JPEG_LIBRARIES
+								  )
