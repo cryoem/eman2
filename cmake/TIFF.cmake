87194d84353efff43d05ede323082ad3cc70a8ae
shadow_walker <shadowwalkersb@gmail.com>
2017-07-18 14:58:27 -0500

    interface target: TIFF

--- cmake/TIFF.cmake
+++ cmake/TIFF.cmake
@@ -1,5 +1,4 @@
 find_package(TIFF REQUIRED)
-add_definitions(-DUSE_TIFF)
 CHECK_LIB_ONLY(JPEG jpeg)
 
 message_var(TIFF_LIBRARIES)
@@ -8,3 +7,12 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(TIFF
 		REQUIRED_VARS TIFF_LIBRARIES
 		)
+
+if(TIFF_FOUND AND NOT TARGET TIFF)
+	add_library(TIFF INTERFACE)
+	add_library(TIFF::TIFF ALIAS TIFF)
+	set_target_properties(TIFF PROPERTIES
+			INTERFACE_LINK_LIBRARIES      "${TIFF_LIBRARIES};${JPEG_LIBRARY}"
+			INTERFACE_COMPILE_DEFINITIONS USE_TIFF
+			)
+endif()
