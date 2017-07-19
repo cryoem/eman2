a03f622e58a451882ec71a88bdc421ea0f7602b5
shadow_walker <shadowwalkersb@gmail.com>
2017-07-19 11:51:23 -0500

    interface target: SZLIB

--- cmake/FindSZLIB.cmake
+++ cmake/FindSZLIB.cmake
@@ -4,3 +4,12 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SZLIB
 								  REQUIRED_VARS SZLIB_LIBRARY
 								  )
+
+if(SZLIB_FOUND AND NOT TARGET SZLIB)
+	add_library(SZLIB INTERFACE)
+	add_library(SZLIB::SZLIB ALIAS SZLIB)
+	set_target_properties(SZLIB
+						  PROPERTIES
+						  INTERFACE_LINK_LIBRARIES ${SZLIB_LIBRARY}
+						  )
+endif()
