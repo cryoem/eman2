6f0c43e571f41beb25578d0b32f3c257d10c52cc
shadow_walker <shadowwalkersb@gmail.com>
2017-08-04 00:14:57 -0500

    remove pre_build.sh and post_build.sh

--- ci_support/build_with_envars.sh
+++ ci_support/build_with_envars.sh
@@ -1,6 +1,36 @@
 #!/usr/bin/env bash
 
-source ci_support/pre_build.sh
+set -xe
+
+# Download and install Miniconda
+export MINICONDA_URL="https://repo.continuum.io/miniconda"
+
+curl -L -O "${MINICONDA_URL}/${MINICONDA_FILE}"
+bash $MINICONDA_FILE -b
+
+# Conda-install packages
+source ${HOME}/miniconda2/bin/activate root
+conda config --set show_channel_urls true
+
+conda install --yes --quiet qt=4 pyqt=4
+conda install --yes --quiet -c conda-forge boost=1.63.* boost-cpp=1.63.* fftw cmake>=3.8
+conda install --yes --quiet -c cryoem ftgl
+conda install --yes --quiet bsddb freetype gsl hdf5 ipython jpeg libpng libtiff matplotlib numpy=1.11 pyopengl scikit-learn scipy theano tk
+conda install --yes --quiet -c cryoem pydusa
+
+# Build and install eman2
+export build_dir=$HOME/build_eman
+
+if [ -e ${HOME}/eman2 ];then
+    export src_dir=$HOME/eman2_src
+    mv -v $HOME/eman2 $src_dir
+else
+    export src_dir=${PWD}
+fi
+
+rm -rf ${build_dir}
+mkdir -p ${build_dir}
+cd ${build_dir}
 
 cmake $src_dir -DENABLE_CONDA=ON
 make
@@ -10,4 +40,14 @@ make test-verbose
 export PREFIX=$CONDA_PREFIX
 export SP_DIR=$(python -c "import site; print site.getsitepackages()[0]")
 
-source $src_dir/ci_support/post_build.sh
+set -e
+
+# Run tests
+export PATH="$PREFIX/bin:$PATH"
+
+e2version.py
+e2speedtest.py
+mpirun -n 4 $(which python) ${PREFIX}/examples/mpi_test.py
+cd ${src_dir}
+bash tests/run_prog_tests.sh
+python tests/test_EMAN2DIR.py
