4d850a944f1f841540f31c0f6b6fa5c6da61e24c
shadow_walker <shadowwalkersb@gmail.com>
2017-08-04 08:11:34 -0500

    Revert "unpin conda-build"
    This reverts commit e2aebdd07dfbc7f3092e970b8c0a8b0b6cf4b916.

--- ci_support/build_recipe.sh
+++ ci_support/build_recipe.sh
@@ -4,7 +4,7 @@ source ci_support/setup_conda.sh
 
 export CPU_COUNT=2
 
-conda install conda-build -c defaults --yes --quiet
+conda install conda-build=2 -c defaults --yes --quiet
 
 if [ "$(uname -s)" != "Darwin" ];then
     conda build recipes/eman -c cryoem -c defaults -c conda-forge --numpy 1.8
