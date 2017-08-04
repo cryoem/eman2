969e6be3485136753e5bcb85958522b05ebc67e8
shadow_walker <shadowwalkersb@gmail.com>
2017-08-03 21:02:30 -0500

    add cryoem/label/dev to get dev version of pydusa

--- ci_support/build_recipe.sh
+++ ci_support/build_recipe.sh
@@ -19,5 +19,5 @@ conda install conda-build=2 -c defaults --yes --quiet
 if [ "$(uname -s)" != "Darwin" ];then
     conda build recipes/eman -c cryoem -c defaults -c conda-forge --numpy 1.8
 else
-    conda build recipes/eman -c cryoem -c defaults -c conda-forge
+    conda build recipes/eman -c cryoem/label/dev -c cryoem -c defaults -c conda-forge
 fi
