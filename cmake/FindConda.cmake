38a78b439f5d7a2941162bb18a68498e51e8abe8
shadow_walker <shadowwalkersb@gmail.com>
2017-08-05 23:00:18 -0500

    rename var: CONDA_ROOT -> CONDA_PREFIX

--- cmake/FindConda.cmake
+++ cmake/FindConda.cmake
@@ -1,6 +1,6 @@
 # Assuming the active conda environment is on PATH, this finds the path of bin/ in the environment
 find_program(CONDA_EXECUTABLE conda
-		PATHS ${CONDA_ROOT}/bin ${CONDA_ROOT}/Scripts ENV PATH
+		PATHS ${CONDA_PREFIX}/bin ${CONDA_PREFIX}/Scripts ENV PATH
 		)
 if(CONDA_EXECUTABLE)
 	execute_process(COMMAND ${CONDA_EXECUTABLE} info --root
@@ -13,9 +13,9 @@ else()
 	message("\nNo conda environment found in PATH!\nPATH=$ENV{PATH}\n")
 endif()
 
-set_cache_var_to_var(CONDA_ROOT out_var)
+set_cache_var_to_var(CONDA_PREFIX out_var)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Conda
-		REQUIRED_VARS CONDA_ROOT
+		REQUIRED_VARS CONDA_PREFIX
 		)
