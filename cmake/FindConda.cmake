803332f11060f79bc04deb502c770cc69abf4883
shadow_walker <shadowwalkersb@gmail.com>
2017-08-06 00:21:55 -0500

    update indentation: findConda()

--- cmake/FindConda.cmake
+++ cmake/FindConda.cmake
@@ -1,22 +1,22 @@
 if(DEFINED ENV{CONDA_PREFIX})
 	set(CONDA_PREFIX $ENV{CONDA_PREFIX} CACHE PATH "")
 else()
-# Assuming the active conda environment is on PATH, this finds the path of bin/ in the environment
-find_program(CONDA_EXECUTABLE conda
-		PATHS ${CONDA_PREFIX}/bin ${CONDA_PREFIX}/Scripts ENV PATH
-		)
-if(CONDA_EXECUTABLE)
-	execute_process(COMMAND ${CONDA_EXECUTABLE} info --root
-			OUTPUT_VARIABLE out_var
-			OUTPUT_STRIP_TRAILING_WHITESPACE
+	# Assuming the active conda environment is on PATH, this finds the path of bin/ in the environment
+	find_program(CONDA_EXECUTABLE conda
+			PATHS ${CONDA_PREFIX}/bin ${CONDA_PREFIX}/Scripts ENV PATH
 			)
-	execute_process(COMMAND ${CONDA_EXECUTABLE} info --envs
-			)
-else()
-	message("\nNo conda environment found in PATH!\nPATH=$ENV{PATH}\n")
-endif()
-
-set_cache_var_to_var(CONDA_PREFIX out_var)
+	if(CONDA_EXECUTABLE)
+		execute_process(COMMAND ${CONDA_EXECUTABLE} info --root
+				OUTPUT_VARIABLE out_var
+				OUTPUT_STRIP_TRAILING_WHITESPACE
+				)
+		execute_process(COMMAND ${CONDA_EXECUTABLE} info --envs
+				)
+	else()
+		message("\nNo conda environment found in PATH!\nPATH=$ENV{PATH}\n")
+	endif()
+	
+	set_cache_var_to_var(CONDA_PREFIX out_var)
 endif()
 
 include(FindPackageHandleStandardArgs)
