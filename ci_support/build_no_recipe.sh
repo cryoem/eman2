f8be0f4131433fa5414a037b24362eaa1f1ff257
shadow_walker <shadowwalkersb@gmail.com>
2017-08-22 14:56:34 -0500

    add channel cryoem/label/dev to pull dev version of fftw-mpi

--- ci_support/build_no_recipe.sh
+++ ci_support/build_no_recipe.sh
@@ -5,9 +5,9 @@ source ci_support/setup_conda.sh
 # Following Wiki instructions at
 # http://blake.bcm.edu/emanwiki/EMAN2/COMPILE_EMAN2_ANACONDA
 if [ "$(uname -s)" != "Darwin" ];then
-    conda install --yes --quiet eman-deps="*"="np18*" -c cryoem -c defaults -c conda-forge
+    conda install --yes --quiet eman-deps="*"="np18*" -c cryoem/label/dev -c cryoem -c defaults -c conda-forge
 else
-    conda install --yes --quiet eman-deps -c cryoem -c defaults -c conda-forge
+    conda install --yes --quiet eman-deps -c cryoem/label/dev -c cryoem -c defaults -c conda-forge
 fi
 
 # Build and install eman2
--- ci_support/build_recipe.sh
+++ ci_support/build_recipe.sh
@@ -7,7 +7,7 @@ export CPU_COUNT=2
 conda install conda-build=2 -c defaults --yes --quiet
 
 if [ "$(uname -s)" != "Darwin" ];then
-    conda build recipes/eman -c cryoem -c defaults -c conda-forge --numpy 1.8
+    conda build recipes/eman -c cryoem/label/dev -c cryoem -c defaults -c conda-forge --numpy 1.8
 else
-    conda build recipes/eman -c cryoem -c defaults -c conda-forge
+    conda build recipes/eman -c cryoem/label/dev -c cryoem -c defaults -c conda-forge
 fi
