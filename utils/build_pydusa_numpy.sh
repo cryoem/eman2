87f78c8feb40acf2c068b6ee38c2c2f366ba246e
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2017-08-31 15:28:37 +0200

    Changed pydusa recipe to make other versions available

--- utils/build_pydusa_numpy.sh
+++ utils/build_pydusa_numpy.sh
@@ -2,6 +2,8 @@
 
 # Builds numpy version(s), if not specified build all versions, 1.5 - 1.12
 
+source activate root
+
 # unset args before using, it is set in activate script above
 unset args
 for elem in ${@};do
@@ -17,11 +19,16 @@ set -xe
 
 RECIPES_DIR=$(cd $(dirname $0)/../recipes && pwd -P)
 
-if [ ${#args} -lt 1 ];then
+number_of_words=$(wc -w <<< "${args[@]}")
+if [ ${number_of_words} -le 0 ];then
     numpy_versions=( 1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12 )
+elif [ ${number_of_words} -le 1 ];then
+    numpy_versions=${args[0]}
 else
-    numpy_versions=${args[@]}
+    numpy_versions=${args[0]}
+    export GIT_PYDUSA_VERSION=${args[1]}
 fi
+
 
 for v in ${numpy_versions[@]};do
     conda build "${RECIPES_DIR}/pydusa" --numpy ${v} ${opts[@]}
