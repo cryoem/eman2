94c67e6811b4b68809253ef2901abc0dc18c6991
shadow_walker <shadowwalkersb@gmail.com>
2017-08-31 23:48:56 -0500

    refactor: replace if/else with case statements

--- utils/build_pydusa_numpy.sh
+++ utils/build_pydusa_numpy.sh
@@ -20,15 +20,16 @@ set -xe
 
 RECIPES_DIR=$(cd $(dirname $0)/../recipes && pwd -P)
 
-number_of_words=$(wc -w <<< "${args[@]}")
-if [ ${number_of_words} -le 0 ];then
-    numpy_versions=( 1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12 )
-elif [ ${number_of_words} -le 1 ];then
-    numpy_versions=${args[0]}
-else
-    numpy_versions=${args[0]}
-    export GIT_PYDUSA_VERSION=${args[1]}
-fi
+case ${#args} in
+    1|2) numpy_versions=${args[0]}
+        
+        export GIT_PYDUSA_VERSION=${2:-"master"}
+        ;;
+
+    0) print_usage
+       ;;
+esac
+
 
 
 for v in ${numpy_versions[@]};do
