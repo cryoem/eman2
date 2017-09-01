6d2630dfcacecbbc951a00b5e9df68b9a5802892
shadow_walker <shadowwalkersb@gmail.com>
2017-08-31 23:47:40 -0500

    add print_usage()

--- utils/build_pydusa_numpy.sh
+++ utils/build_pydusa_numpy.sh
@@ -2,6 +2,11 @@
 
 # Builds numpy version(s), if not specified build all versions, 1.5 - 1.12
 
+function print_usage(){
+    printf "\e\033[35m\n  Usage: $(basename ${0}) %s %s %s\n\n\033[0m" "{1.5|...|1.12}" "[branch-to-checkout]" "[--conda-build-arg1] [--conda-build-arg2] ..." >&2
+    exit 64
+}
+
 for elem in ${@};do
     regex=-*
     if [[ $elem == $regex ]];then
