928174fdd802d24275bddbfae0c18c66817f1f2b
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2017-11-17 14:06:15 +0100

    Changed command line to ${@}

--- utils/uninstall_openmpi.sh
+++ utils/uninstall_openmpi.sh
@@ -2,13 +2,4 @@
 
 set -x
 
-for elem in ${@};do
-    regex=-*
-    if [[ $elem == $regex ]];then
-        opts=( ${opts[@]} $elem )
-    else
-        args=( ${args[@]} $elem )
-    fi
-done
-
-conda remove openmpi --force ${opts[@]}
+conda remove openmpi --force ${@}
