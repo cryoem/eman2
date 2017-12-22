15aa4e0a8c1cc26a06b04f59849f276ac879a469
shadow_walker <shadowwalkersb@gmail.com>
2017-12-21 22:01:44 -0600

    update some tests to be called via SRC_DIR

--- ci_support/build_no_recipe.sh
+++ ci_support/build_no_recipe.sh
@@ -18,7 +18,6 @@ bash ${SRC_DIR}/recipes/eman/build.sh
 e2version.py
 e2speedtest.py
 
-cd -
-mpirun -n 4 $(which python) examples/mpi_test.py
-bash tests/run_prog_tests.sh
-python tests/test_EMAN2DIR.py
+mpirun -n 4 $(which python) ${SRC_DIR}/examples/mpi_test.py
+bash ${SRC_DIR}/tests/run_prog_tests.sh
+python ${SRC_DIR}/tests/test_EMAN2DIR.py
