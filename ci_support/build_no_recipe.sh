0f99cd3cc4071571da3875fe30797bc41d1e0ce6
shadow_walker <shadowwalkersb@gmail.com>
2017-12-22 12:39:37 -0600

    no conda install

--- ci_support/build_no_recipe.sh
+++ ci_support/build_no_recipe.sh
@@ -2,19 +2,19 @@
 
 if [ ! -z ${CI} ];then
     source ci_support/setup_conda.sh
-fi
 
-export CPU_COUNT=2
-
-# Following Wiki instructions at
-# http://blake.bcm.edu/emanwiki/EMAN2/COMPILE_EMAN2_ANACONDA
-if [ "$(uname -s)" != "Darwin" ];then
-    conda install --yes --quiet eman-deps="*"="np18*" -c cryoem -c defaults -c conda-forge
-else
-    conda install --yes --quiet eman-deps -c cryoem -c defaults -c conda-forge
+    # Following Wiki instructions at
+    # http://blake.bcm.edu/emanwiki/EMAN2/COMPILE_EMAN2_ANACONDA
+    if [ "$(uname -s)" != "Darwin" ];then
+        conda install --yes --quiet eman-deps="*"="np18*" -c cryoem -c defaults -c conda-forge
+    else
+        conda install --yes --quiet eman-deps -c cryoem -c defaults -c conda-forge
+    fi
 fi
 
 # Build and install eman2
+export CPU_COUNT=2
+
 export SRC_DIR=${PWD}
 bash ${SRC_DIR}/recipes/eman/build.sh
 
