cb045127098c4f5233048feee3adb490de7d420b
shadow_walker <shadowwalkersb@gmail.com>
2018-01-08 17:57:26 -0600

    use PREFIX to run mpi_test.py in tests/run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,5 +7,5 @@ e2speedtest.py
 
 python tests/test_EMAN2DIR.py
 
-mpirun -n 4 $(which python) ${SRC_DIR}/examples/mpi_test.py
+mpirun -n 4 $(which python) ${PREFIX}/examples/mpi_test.py
 bash ${SRC_DIR}/tests/run_prog_tests.sh
