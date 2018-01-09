d5792033956ced320221a46847ee8386daa5c813
shadow_walker <shadowwalkersb@gmail.com>
2018-01-08 19:29:39 -0600

    run mpi test only when user is not root

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,5 +7,7 @@ e2speedtest.py
 
 python tests/test_EMAN2DIR.py
 
-mpirun -n 4 $(which python) ${PREFIX}/examples/mpi_test.py
+if [ $(whoami) != "root" ];then
+    mpirun -n 4 $(which python) ${PREFIX}/examples/mpi_test.py
+fi
 bash ${SRC_DIR}/tests/run_prog_tests.sh
