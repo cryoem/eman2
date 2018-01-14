629aa6ca00bbd7b3290cfdc754d0d2bf01e55ab5
shadow_walker <shadowwalkersb@gmail.com>
2018-01-14 16:34:11 -0600

    remove mpi test [ci skip]

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,7 +7,4 @@ e2speedtest.py
 
 python "${SRC_DIR}/tests/test_EMAN2DIR.py"
 
-if [ $(whoami) != "root" ];then
-    mpirun -n 4 $(which python) ${PREFIX}/examples/mpi_test.py
-fi
 bash "${SRC_DIR}/tests/run_prog_tests.sh"
