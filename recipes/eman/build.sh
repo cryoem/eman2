ac3dfd10ce433390abb472f9d0ec8ee41019f39f
shadow_walker <shadowwalkersb@gmail.com>
2018-01-13 19:19:25 -0600

    unset envars set by/for conda compilers, so system compilers are used [ci skip]

--- recipes/eman/build.sh
+++ recipes/eman/build.sh
@@ -3,6 +3,24 @@
 set -xe
 
 unset MACOSX_DEPLOYMENT_TARGET
+unset CC
+unset CFLAGS
+unset CONDA_BACKUP_HOST
+unset CPP
+unset CPPFLAGS
+unset CXX
+unset CXXFLAGS
+unset DEBUG_CFLAGS
+unset DEBUG_CPPFLAGS
+unset DEBUG_CXXFLAGS
+unset GCC
+unset GCC_AR
+unset GCC_NM
+unset GCC_RANLIB
+unset GXX
+unset HOST
+unset LDFLAGS
+unset _PYTHON_SYSCONFIGDATA_NAME
 
 build_dir="${SRC_DIR}/../build_eman"
 
