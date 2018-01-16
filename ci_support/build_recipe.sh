032be1e1745c3ab30b3572200271f699cbda4e35
shadow_walker <shadowwalkersb@gmail.com>
2018-01-16 12:19:03 -0600

    set numpy envar for cloud CI recipes [ci skip]

--- ci_support/build_recipe.sh
+++ ci_support/build_recipe.sh
@@ -9,7 +9,8 @@ fi
 export CPU_COUNT=2
 
 if [ "$(uname -s)" != "Darwin" ];then
-    conda build recipes/eman -c cryoem -c defaults -c conda-forge --numpy 1.8
+    export NUMPY_VERSION='1.9'
+    conda build recipes/eman -c cryoem -c defaults -c conda-forge
 else
     export EMAN_TEST_SKIP=1
     conda build recipes/eman -c cryoem -c defaults -c conda-forge
