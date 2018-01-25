ba7f87986c5dd4677d7c9d2b9d197f94139f32b1
shadow_walker <shadowwalkersb@gmail.com>
2018-01-24 22:51:26 -0600

    empty test_binary_installation.* [ci skip]

--- tests/test_binary_installation.sh
+++ tests/test_binary_installation.sh
@@ -1,22 +1,3 @@
 #!/usr/bin/env bash
 
 set -xe
-
-MYDIR="$(cd "$(dirname "$0")" && pwd -P)"
-
-for f in ${@};do
-    dir=$(cd $(dirname $f) && pwd -P)
-    fbase=$(basename $f)
-    fbase=${fbase%\.*}
-    conda_loc=${dir}/${fbase}
-    
-    echo "... $fbase ..."
-    
-    rm -rf ${conda_loc}
-    bash $f -bp ${conda_loc}
-    source ${conda_loc}/bin/activate root
-    conda info -a
-    conda list
-    bash "${MYDIR}/run_tests_from_binary.sh"
-    source deactivate
-done
