7de0405c4ea64997cf7521842e0d684eb1858376
muyuanc <g5v991x@gmail.com>
2018-02-01 13:55:13 -0600

    e2boxer: another funny bug in non-gui mode..

--- programs/e2boxer.py
+++ programs/e2boxer.py
@@ -241,8 +241,8 @@ def main():
 			badrefs=EMData.read_images("info/boxrefsbad.hdf")
 		else: badrefs=[]
 
-		if os.path.exists("info/bgrefsbad.hdf"):
-			bgrefs=EMData.read_images("info/bgrefsbad.hdf")
+		if os.path.exists("info/boxrefsbg.hdf"):
+			bgrefs=EMData.read_images("info/boxrefsbg.hdf")
 		else: bgrefs=[]
 
 		
@@ -730,6 +730,7 @@ class boxerConvNet(QtCore.QObject):
 			bgrefs=boxer.bgrefs
 		elif args:
 			goodrefs, badrefs, bgrefs =args
+			
 		else:
 			print("Cannot find boxer window...")
 			
