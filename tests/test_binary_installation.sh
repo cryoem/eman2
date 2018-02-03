bce524381803d23d42def7b2c2e614c9c32f501e
shadow_walker <shadowwalkersb@gmail.com>
2018-02-02 22:43:38 -0600

    run tests on binary installation [ci skip]

--- tests/test_binary_installation.sh
+++ tests/test_binary_installation.sh
@@ -6,6 +6,11 @@ dir="$1"
 fname=$2
 installer_file="${dir}/${fname}"
 binary_loc="${dir}/eman2-binary-test"
+MYDIR="$(cd "$(dirname "$0")" && pwd -P)"
 
 rm -rf ${binary_loc}
 bash "${installer_file}" -bp ${binary_loc}
+source ${binary_loc}/bin/activate root
+conda info -a
+conda list
+bash "${MYDIR}/run_tests_from_binary.sh"
