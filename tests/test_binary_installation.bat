3a92ddfce36429d80ad2502d1055386c1562f7c4
shadow_walker <shadowwalkersb@gmail.com>
2018-02-02 18:20:55 -0600

    install binary [ci skip]

--- tests/test_binary_installation.bat
+++ tests/test_binary_installation.bat
@@ -1,1 +1,10 @@
 @echo on
+
+set "dir=%1"
+set fname=%2
+set "installer_file=%dir%\%fname%"
+set "binary_loc=%dir%\eman2-binary-test"
+
+rmdir /q /s %binary_loc%
+start /wait "" %installer_file% /InstallationType=JustMe /RegisterPython=0 /AddToPath=0 /S /D=%binary_loc%
+if errorlevel 1 exit 1
--- tests/test_binary_installation.sh
+++ tests/test_binary_installation.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 set -xe
+
+dir="$1"
+fname=$2
+installer_file="${dir}/${fname}"
+binary_loc="${dir}/eman2-binary-test"
+
+rm -rf ${binary_loc}
+bash "${installer_file}" -bp ${binary_loc}
