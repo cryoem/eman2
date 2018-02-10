ad37c59265e22a2945816ba55906749f618e90f8
Pawel <pawelpap@att.net>
2018-02-10 13:06:18 -0600

    date

--- libEM/sparx/util_sparx.cpp
+++ libEM/sparx/util_sparx.cpp
@@ -24553,7 +24553,7 @@ float Util::ccc_images_G(EMData* image, EMData* refim, EMData* mask, Util::Kaise
 
 void Util::version()
 {
- cout <<"  Source modification date: 01/29/2018  4:34 PM " <<  endl;
+ cout <<"  Source modification date: 02/05/2018  10:55 AM " <<  endl;
 }
 
 
