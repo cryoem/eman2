db5aba176c5089d1a0c25bee072d63847ebd18b6
shadow_walker <shadowwalkersb@gmail.com>
2018-02-18 02:34:52 -0600

    reconstruct INSTALLERS_DIR in test_wrapper.sh [ci skip]

--- ci_support/test_wrapper.sh
+++ ci_support/test_wrapper.sh
@@ -2,4 +2,6 @@
 
 set -x
 
+INSTALLERS_DIR="${HOME_DIR}/workspace/${STAGE_NAME}-installers"
+
 cmd "/C ${WORKSPACE//\//\\}\\tests\\test_binary_installation.bat ${INSTALLERS_DIR//\//\\} eman2.win.exe "
