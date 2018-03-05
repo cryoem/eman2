dc3467b2d2d270add7a6993915ecf9b54ca23f52
shadow_walker <shadowwalkersb@gmail.com>
2018-03-04 20:30:57 -0600

    unset env vars set by anaconda compilers if conda-build

---
+++ recipes/eman/unset_env_vars.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x
+
+unset MACOSX_DEPLOYMENT_TARGET
+unset CC
+unset CFLAGS
+unset CONDA_BACKUP_HOST
+unset CPP
+unset CPPFLAGS
+unset CXX
+unset CXXFLAGS
+unset DEBUG_CFLAGS
+unset DEBUG_CPPFLAGS
+unset DEBUG_CXXFLAGS
+unset GCC
+unset GCC_AR
+unset GCC_NM
+unset GCC_RANLIB
+unset GXX
+unset HOST
+unset LDFLAGS
+unset _PYTHON_SYSCONFIGDATA_NAME
