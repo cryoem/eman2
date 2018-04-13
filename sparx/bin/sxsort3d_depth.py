98bcdcad8f61dd6c469d0049783efa07c219c100
Zhong Huang <zhonghuang@huang.local>
2018-04-13 11:33:13 -0500

    fix ANOVA in case of single group

--- sparx/bin/sxsort3d_depth.py
+++ sparx/bin/sxsort3d_depth.py
@@ -5622,19 +5622,20 @@ def copy_results(log_file, all_gen_stat_list):
 					copyfile(cluster_file, os.path.join(Tracker["constants"]["masterdir"], "Cluster_%03d.txt"%nclusters))
 					clusters.append(read_text_file(cluster_file))
 					copyfile(os.path.join(Tracker["constants"]["masterdir"], "generation_%03d"%ig, "vol_grp%03d_iter000.hdf"%ic), os.path.join(Tracker["constants"]["masterdir"], "vol_cluster%03d.hdf"%nclusters))
-					cluster = read_text_file(os.path.join(Tracker["constants"]["masterdir"], "generation_%03d"%ig, "Cluster_%03d.txt"%ic))
+					cluster      = read_text_file(os.path.join(Tracker["constants"]["masterdir"], "generation_%03d"%ig, "Cluster_%03d.txt"%ic))
 					cluster_file = "Cluster_%03d.txt"%nclusters
 					vol_file     = "vol_cluster%03d.hdf"%nclusters
-					msg = '{:>8} {:>8}   {:^24}        {:^6}          {:^6}          {:>5}  {:^15} {:^20} '.format(nclusters, len(cluster), ig, round(all_gen_stat_list[ig][ic][0],1), round(all_gen_stat_list[ig][ic][1],1), round(all_gen_stat_list[ig][ic][2],1), cluster_file,  vol_file)
-					nclusters +=1
-					NACC +=len(cluster)
+					msg          = '{:>8} {:>8}   {:^24}        {:^6}          {:^6}          {:>5}  {:^15} {:^20} '.format(nclusters, len(cluster), ig, round(all_gen_stat_list[ig][ic][0],1), round(all_gen_stat_list[ig][ic][1],1), round(all_gen_stat_list[ig][ic][2],1), cluster_file,  vol_file)
+					nclusters   +=1
+					NACC        +=len(cluster)
 					log_file.add(msg)
 			Unaccounted_file = os.path.join(Tracker["constants"]["masterdir"], "generation_%03d"%ig, "Unaccounted.txt")
 			copyfile(Unaccounted_file, os.path.join(Tracker["constants"]["masterdir"], "Unaccounted.txt"))
 			NUACC = Tracker["constants"]["total_stack"] - NACC
 			log_file.add('{:^7} {:^8} {:^22} {:^8} {:^24} {:^8} '.format(' Images', Tracker["constants"]["total_stack"], 'accounted for images: ', NACC, 'unaccounted for images: ', NUACC))
 			log_file.add('Unaccounted images saved in Unaccounted.txt')
-			do_analysis_on_identified_clusters(clusters, log_file)
+			if    len(clusters) >=2: do_analysis_on_identified_clusters(clusters, log_file)
+			else: log_file.add(' ANOVA analysis is skipped ')
 			fout = open(os.path.join(Tracker["constants"]["masterdir"], "Tracker.json"), 'w')
 			json.dump(Tracker, fout)
 			fout.close()
