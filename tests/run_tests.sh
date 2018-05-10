d0b1f791c6ae9cbedf374a32fdc40a8b73d90a2b
shadow_walker <shadowwalkersb@gmail.com>
2018-05-09 20:27:44 -0500

    test git hash [ci skip]

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,6 +7,10 @@ MYDIR="$(cd "$(dirname "$0")"; pwd -P)"
 e2version.py
 e2speedtest.py
 
+if [ ${CONDA_BUILD:-0} -ne 1 ];then
+    bash "${MYDIR}/test_git_hash.sh"
+fi
+
 python "${MYDIR}/test_EMAN2DIR.py"
 
 if [ $(whoami) != "root" ];then
