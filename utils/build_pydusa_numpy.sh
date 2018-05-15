888f3632daef5648fa94c5f12375ee3063f0e040
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2018-05-15 17:16:56 +0200

    Changed default tag of the pydusa branch

--- utils/build_pydusa_numpy.sh
+++ utils/build_pydusa_numpy.sh
@@ -23,7 +23,7 @@ RECIPES_DIR=$(cd $(dirname $0)/../recipes && pwd -P)
 case ${#args[@]} in
     1|2) numpy_versions=${args[0]}
         
-        export GIT_PYDUSA_BRANCH=${args[1]:-"v20180115"}
+        export GIT_PYDUSA_BRANCH=${args[1]:-"v20180515"}
         ;;
 
     *) print_usage
