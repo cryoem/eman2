e599be4a868a0889336a5c91790bb82086fbc9c3
shadow_walker <shadowwalkersb@gmail.com>
2018-05-24 21:19:07 -0500

    cleanup includes, boost/cstdint.hpp

--- libpyEM/libpyTypeConverter2.cpp
+++ libpyEM/libpyTypeConverter2.cpp
@@ -34,7 +34,6 @@
 
 // Boost Includes ==============================================================
 #include <boost/python.hpp>
-#include <boost/cstdint.hpp>
 #include <boost/shared_ptr.hpp>
 
 // Includes ====================================================================
