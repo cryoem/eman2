a0a39e5d2c573c73055dd5f22f603c17fa96524b
James Michael Bell <jmbell91@gmail.com>
2018-06-13 17:13:30 -0500

    updates to evaluation tools in tomography gui

--- programs/e2tomo_eval.py
+++ programs/e2tomo_eval.py
@@ -19,7 +19,11 @@ def main():
 	usage=" "
 	parser = EMArgumentParser(usage=usage,version=EMANVERSION)
 	parser.add_argument("--ppid", type=int,help="", default=None)
-	(options, args) = parser.parse_args()
+
+	parser.add_header(name="orblock1", help='', title="Click launch to evaluate reconstructed tomograms", row=1, col=0, rowspan=1, colspan=2, mode="")
+
+
+(options, args) = parser.parse_args()
 	logid=E2init(sys.argv)
 
 	if not os.path.isdir("tomograms"): os.mkdir("tomograms")
