1050451304c6a84880b9de02bbf6e4c502096fd3
James Michael Bell <jmbell91@gmail.com>
2018-06-15 13:40:53 -0500

    added additional way to call ref option

--- programs/e2spt_sgd.py
+++ programs/e2spt_sgd.py
@@ -30,7 +30,7 @@ def main():
 
 	parser.add_header(name="orblock1", help='Just a visual separation', title="** Initial Model Options **", row=1, col=1, rowspan=1, colspan=1, mode="model")
 
-	parser.add_argument('--ref', type=str, default=None, help="""3D reference for initial model generation. No reference is used by default.""", guitype='filebox', browser="EMBrowserWidget(withmodal=True,multiselect=False)", row=2, col=0,rowspan=1, colspan=3, mode="model")
+	parser.add_argument('--reference','--ref', type=str, default=None, help="""3D reference for initial model generation. No reference is used by default.""", guitype='filebox', browser="EMBrowserWidget(withmodal=True,multiselect=False)", row=2, col=0,rowspan=1, colspan=3, mode="model")
 
 	parser.add_argument("--mask", type=str,help="Mask file to be applied to initial model", default=None, guitype='filebox', browser="EMBrowserWidget(withmodal=True,multiselect=False)", row=3, col=0,rowspan=1, colspan=3, mode="model")
 
