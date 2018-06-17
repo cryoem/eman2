4bec75538d505d8ed7c2825cb6cb141c87c4604f
shadow_walker <shadowwalkersb@gmail.com>
2018-06-16 22:06:52 -0700

    rename signal to avoid name clash with member function: signal_set_scale

--- libpyEM/qtgui/emimagemx.py
+++ libpyEM/qtgui/emimagemx.py
@@ -207,7 +207,7 @@ class EMImageMXWidget(EMGLWidget, EMGLProjectionViewMatrices):
 
 	setsChanged = QtCore.pyqtSignal()
 	mx_boxdeleted = QtCore.pyqtSignal(QtGui.QMouseEvent, list, bool)
-	set_scale = QtCore.pyqtSignal(float, float, bool)
+	signal_set_scale = QtCore.pyqtSignal(float, float, bool)
 	origin_update = QtCore.pyqtSignal(float, float)
 	mx_image_selected = QtCore.pyqtSignal(QtGui.QMouseEvent, tuple)
 	mx_image_double = QtCore.pyqtSignal(QtGui.QMouseEvent, tuple)
@@ -586,7 +586,7 @@ class EMImageMXWidget(EMGLWidget, EMGLProjectionViewMatrices):
 		self.deleteLater()
 
 	def get_emit_signals_and_connections(self):
-		return {"set_origin":self.set_origin,"set_scale":self.set_scale,"origin_update":self.origin_update}
+		return {"set_origin":self.set_origin,"signal_set_scale":self.set_scale,"origin_update":self.origin_update}
 
 	def get_data(self):
 		'''
@@ -941,7 +941,7 @@ class EMImageMXWidget(EMGLWidget, EMGLProjectionViewMatrices):
 			self.draw_scroll = True
 			self.scroll_bar.update_target_ypos()
 
-		if self.emit_events: self.set_scale.emit(self.scale, adjust, update_gl)
+		if self.emit_events: self.signal_set_scale.emit(self.scale, adjust, update_gl)
 		if update_gl: self.updateGL()
 
 	def resize_event(self, width, height):
