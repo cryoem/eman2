97386a7c344c0794ff30628bf62cba006a2fa963
sludtke42 <sludtke42@gmail.com>
2018-06-27 11:49:25 -0500

    Added some new methods to e2msa.py via sklearn. Still experimental. pca and fastica seem to work as intended

--- programs/e2msa.py
+++ programs/e2msa.py
@@ -46,11 +46,15 @@ import sys
 
 def main():
 	progname = os.path.basename(sys.argv[0])
-	usage = """prog [options] <input stack> <output basis>
+	usage = """prog [options] <input stack> <output basis> [input reprojections]
 This too provides a variety of dimensionality reduction methods. This new version
 uses scikit.learn, which provides a greater variety of algorithms, but must load 
 all data into memory. If working with a large file, you may want to consider using
 --step to operate on a limited subset of the data.
+
+If specified, [input reprojections] will contain projections of the full input stack
+(ignoring --step) into the basis subspace represented as a single image. This 
+obviates the need for e2basis.py, and permits use of nonlinear decompositions.
 
 ---
 Performs multivariate statistical analysis on a stack of images. Writes
@@ -61,20 +65,20 @@ classify the data based on the projected vectors. If the
 output file supports arbitrary metadata (like HDF), Eigenvalues
 are stored in the 'eigval' parameter in each image.
 
-Note: The mean value is subtracted from each image prior to Eigenimage
+Note: The mean value is subtracted from each image prior to MSA
 calculation. The mean image is stored as the first image in the output
 file, though it is not part of the orthonormal basis when
 handled this way."""
 
 	parser = EMArgumentParser(usage=usage,version=EMANVERSION)
 
-	parser.add_argument("--mode",type=str,help="Mode should be one of: pca",default="pca")
+	parser.add_argument("--mode",type=str,help="Mode should be one of: pca, fastica, lda",default="pca")
 	parser.add_argument("--nbasis","-n",type=int,help="Number of basis images to generate.",default=20)
 	parser.add_argument("--maskfile","-M",type=str,help="File containing a mask defining the pixels to include in the Eigenimages")
 	parser.add_argument("--mask",type=int,help="Mask radius, negative values imply ny/2+1+mask, --mask=0 disables, --maskfile overrides",default=0)
 	parser.add_argument("--simmx",type=str,help="Will use transformations from simmx on each particle prior to analysis")
 	parser.add_argument("--normalize",action="store_true",help="Perform a careful normalization of input images before MSA. Otherwise normalization is not modified until after mean subtraction.",default=False)
-	parser.add_argument("--step",type=str,default="0,1",help="Specify <init>,<step>. Processes only a subset of the input data. For example, 0,2 would process only the even numbered particles")
+	parser.add_argument("--step",type=str,default="0,1",help="Specify <init>,<step>[,last]. Processes only a subset of the input data. For example, 0,2 would process only the even numbered particles")
 	parser.add_argument("--ppid", type=int, help="Set the PID of the parent process, used for cross platform PPID",default=-1)
 	parser.add_argument("--verbose", "-v", dest="verbose", action="store", metavar="n", type=int, default=0, help="verbose level [0-9], higner number means higher level of verboseness")
 
@@ -89,7 +93,16 @@ handled this way."""
 
 	if options.verbose>0 : print("Beginning MSA")
 
-	try : step = int(options.step.split(",")[0]),int(options.step.split(",")[1])	# convert strings to tuple
+	# Number of image s in the input file
+	nfile=EMUtil.get_image_count(args[0])
+
+	try : 
+		step = [int(i) for i in options.step.split(",")]
+		if len(step)==1: step=(0,step[0],nfile)
+		elif len(step)==2: step.append(nfile)
+		elif len(step)==3:
+			if step[2]<=0 : step[2]+=nfile		# undocumented negative final value permitted
+		else: raise Exception
 	except:
 		print("Invalid --step specification")
 		sys.exit(1)
@@ -110,7 +123,7 @@ handled this way."""
 			mask.process_inplace("mask.sharp",{"outer_radius":options.mask})
 	
 	# Memory usage warning >2G raw data
-	n=(EMUtil.get_image_count(args[0])-step[0])//step[1]
+	n=(step[2]-step[0])//step[1]
 	nval=mask["square_sum"]
 #	print(args[0],n,nval)
 	if options.verbose or n*nval>500000000: print("Estimated memory usage (mb): ",n*nval*4/2**20)
@@ -123,14 +136,25 @@ handled this way."""
 		for i in range(len(data)): data[i]/=np.linalg.norm(data[i])
 
 	# first output image is the mean of the input vectors, which has been subtracted from each vector
+	try: os.unlink(args[1])
+	except: pass
 	mean=np.mean(data,0)
 	for i in range(len(data)): data[i]-=mean
 	from_numpy(mean).process("misc.mask.pack",{"mask":mask,"unpack":1}).write_image(args[1],0)
 	
+	shift=0
 	# This is where the actual action takes place!
 	if options.mode=="pca":
 		msa=skdc.PCA(n_components=options.nbasis)
 #		print(data.shape)
+		msa.fit(data)
+	elif options.mode=="fastica":
+		msa=skdc.FastICA(n_components=options.nbasis,algorithm="parallel",max_iter=500,tol=0.001)
+		msa.fit(data)
+	elif options.mode=="lda":
+		shift=max(-data.min()+data.std()*0.1,data.std()*4.0-data.mean())	# we need positivity
+		data+=shift
+		msa=skdc.LatentDirichletAllocation(n_components=options.nbasis,learning_method="online",verbose=2)
 		msa.fit(data)
 		
 #	print(msa.components_.shape)
@@ -141,20 +165,52 @@ handled this way."""
 	# write other basis vectors
 	for i,v in enumerate(msa.components_):
 		im=from_numpy(v.copy()).process("misc.mask.pack",{"mask":mask,"unpack":1})
-		im["eigval"]=float(msa.singular_values_[i])
-		im["explvarfrac"]=float(msa.explained_variance_ratio_[i])
+		if options.mode=="pca":
+			im["eigval"]=float(msa.singular_values_[i])
+			im["explvarfrac"]=float(msa.explained_variance_ratio_[i])
+			if options.verbose : print("Explained variance: ",im["explvarfrac"],"\tSingular Value: ",im["eigval"])
+		elif options.mode=="fastica":
+			im.mult(1.0/im["sigma"])	# fastica seems to produce very small vector lengths
 		im.write_image(args[1],i+1)
-		if options.verbose : print("Explained variance: ",im["explvarfrac"],"\tSingular Value: ",im["eigval"])
+		
+	# if requested we use the model to generate reprojections of the full set of input images
+	# into the new subspace. This permits use of nonlinear algorithms (the components_ output 
+	# is not directly usable)
+	if args>2:
+		try: os.unlink(args[2])
+		except: pass
+	
+		if options.verbose: print("Reprojecting input data into subspace")
+		chunksize=min(max(2,250000000//nval),step[2])		# limiting memory usage for this step to ~2G
+		out=EMData(options.nbasis,step[2])		# we hold the full set of reprojections in memory, though
+		start=0
+		while (start<step[2]):
+			step2=[start,1,min(step[2],start+chunksize)]
+			if options.verbose: print(step2)
+			
+			# read a chunk of data
+			if options.simmx : chunk=simmx_get(args[0],options.simmx,mask,step2)
+			else : chunk=normal_get(args[0],mask,step2)
+			if shift!=0 : chunk+=shift					# for methods requiring positivity
+			
+			proj=msa.transform(chunk)		# into subspace
+			im=from_numpy(proj.copy())
+			out.insert_clip(im,(0,start,0))
+			start+=chunksize
+			
+		# write results
+		out.write_image(args[2],0)
 
 	E2end(logid)
+	if options.mode not in ("pca","fastica") :
+		print("WARNING: While projection vectors are reliable, use of modes other than PCA or ICA may involve nonlinarities, meaning the 'Eigenimages' may not be interpretable in this way.")
 
 def simmx_get(images,simmxpath,mask,step):
 	"""returns an array of transformed masked images as arrays for PCA"""
 
 	simmx=[EMData(simmxpath,i) for i in range(5)]
 
-
-	n=(EMUtil.get_image_count(images)-step[0])/step[1]
+	n=(step[2]-step[0])/step[1]
 
 	ret=EMData(int(mask["square_sum"]),n)
 	for i in range(n):
@@ -169,7 +225,7 @@ def simmx_get(images,simmxpath,mask,step):
 def normal_get(images,mask,step):
 	"""returns an array of transformed masked images as arrays for PCA"""
 
-	n=(EMUtil.get_image_count(images)-step[0])/step[1]
+	n=(step[2]-step[0])/step[1]
 
 	ret=EMData(int(mask["square_sum"]),n)
 	for i in range(n):
--- programs/e2refine2d_bispec.py
+++ programs/e2refine2d_bispec.py
@@ -111,6 +111,7 @@ def main():
 
 	#options associated with e2basis.py
 
+	msamode="fastica"
 
 	# Database Metadata storage
 	parser.add_argument("--ppid", type=int, help="Set the PID of the parent process, used for cross platform PPID",default=-1)
@@ -174,17 +175,16 @@ def main():
 
 	# MSA on the footprints
 	fpbasis=options.path+"/basis_00.hdf"
+	inputproj=options.path+"/basis_proj_00.hdf"
 	if n>10000 : step="--step 0,{}".format((n+10000)//20000)
 	else: step=""
-	# 5/18/18 removed --normalize. Seems to cause MSA algorithm to screw up (non decreasing eigen vals) with bispectra?
 	#run("e2msa.py %s %s --normalize --nbasis=%0d --scratchfile=%s/msa_scratch.bin %s"%(fpfile,fpbasis,options.nbasisfp,options.path,step))
-	run("e2msa.py %s %s  --nbasis=%0d %s"%(fpfile,fpbasis,options.nbasisfp,step))
+	run("e2msa.py %s %s %s --nbasis %0d %s --mode %s"%(fpfile,fpbasis,inputproj,options.nbasisfp,step,msamode))
 	proc_tally += 1.0
 	if logid : E2progress(logid,proc_tally/total_procs)
 
 	# reproject the particle footprints into the basis subspace
-	inputproj=options.path+"/basis_proj_00.hdf"
-	run("e2basis.py project %s %s %s --oneout --mean1 --verbose=%d"%(fpbasis,fpfile,inputproj,subverbose))
+#	run("e2basis.py project %s %s %s --oneout --mean1 --verbose=%d"%(fpbasis,fpfile,inputproj,subverbose))
 	proc_tally += 1.0
 	if logid : E2progress(logid,proc_tally/total_procs)
 
@@ -209,15 +209,15 @@ def main():
 		# first we sort and align the class-averages from the last step
 
 		# MSA on class-average bispectra
-		run("e2msa.py %s/classes_fp_%02d.hdf %s/basis_%02d.hdf --nbasis=%0d"%(options.path,it-1,options.path,it,options.nbasisfp))
+		run("e2msa {path}/classes_fp_{it1:02d}.hdf {path}/basis_{it:02d}.hdf {path}/basis_proj_{it:02d} --nbasis {nbasis} --mode {mode}".format(path=options.path,it1=it-1,it=it,nbasis=options.nbasisfp,mode=msamode))
 		#run("e2msa.py %s/classes_fp_%02d.hdf %s/basis_%02d.hdf  --normalize --nbasis=%0d --scratchfile=%s/msa_scratch.bin"%(options.path,it-1,options.path,it,options.nbasisfp,options.path))
 		proc_tally += 1.0
 		if logid : E2progress(logid,proc_tally/total_procs)
 
 		# now project original image bispectra into class-average basis space
-		run("e2basis.py project %s/basis_%02d.hdf %s %s/basis_proj_%02d.hdf --oneout --mean1 --verbose=%d"%(options.path,it,fpfile,options.path,it,subverbose))
-		proc_tally += 1.0
-		if logid : E2progress(logid,proc_tally/total_procs)
+		#run("e2basis.py project %s/basis_%02d.hdf %s %s/basis_proj_%02d.hdf --oneout --mean1 --verbose=%d"%(options.path,it,fpfile,options.path,it,subverbose))
+		#proc_tally += 1.0
+		#if logid : E2progress(logid,proc_tally/total_procs)
 
 		# Classification
 		run("e2classifykmeans.py %s/basis_proj_%02d.hdf --original=%s --mininclass=2 --ncls=%d --clsmx=%s/classmx_%02d.hdf --onein --fastseed"%(options.path,it,options.input,options.ncls,options.path,it))
