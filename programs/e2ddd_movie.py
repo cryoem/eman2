d71171b1e61110424ddf42ac4fcbc8c796990141
James Michael Bell <jmbell91@gmail.com>
2018-07-06 14:15:32 -0500

    removed external program options

--- programs/e2ddd_movie.py
+++ programs/e2ddd_movie.py
@@ -567,7 +567,7 @@ def process_movie(options,fsp,dark,gain,first,flast,step,idx):
 	t1 = time()-t
 	print("{:.1f} s".format(time()-t))
 
-	if options.align_frames and not options.realign:
+	elif options.align_frames and not options.realign:
 
 		start = time()
 
