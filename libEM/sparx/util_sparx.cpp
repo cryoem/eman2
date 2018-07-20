86ed1f821febe85221b7a3acd2993147767a697f
Pawel Penczek <pawelpap@att.net>
2018-07-20 13:40:23 -0500

    update version

--- libEM/sparx/util_sparx.cpp
+++ libEM/sparx/util_sparx.cpp
@@ -24553,7 +24553,7 @@ float Util::ccc_images_G(EMData* image, EMData* refim, EMData* mask, Util::Kaise
 
 void Util::version()
 {
- cout <<"  Source modification date: 06/20/2018  12:34 PM " <<  endl;
+ cout <<"  Branch fix-sparx  Source modification date: 06/20/2018  12:34 PM " <<  endl;
 }
 
 
