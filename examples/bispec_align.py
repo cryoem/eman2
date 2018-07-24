6115f5127edce9d156d080f53737fd882e78e9d4
sludtke42 <sludtke42@gmail.com>
2018-07-24 12:52:58 -0500

    Fix future, modify automatic filter in averager

--- examples/bispec_align.py
+++ examples/bispec_align.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from EMAN2 import *
 from sys import argv
 
@@ -18,21 +19,35 @@ b=a[0].process("math.bispectrum.slice",{"rfp":rfp,"size":size})
 c=a[1].process("math.bispectrum.slice",{"rfp":rfp,"size":size})
 #c.process_inplace("normalize.rows",{"unitlen":1})
 
+a.append(a[1].process("xform.flip",{"axis":"x"}))
+cf=a[2].process("math.bispectrum.slice",{"rfp":rfp,"size":size})
+
 cc=b.calc_ccfx(c,0,-1,1)
-display(cc)
+ccf=b.calc_ccfx(cf,0,-1,1)
+display((cc,ccf),1)
 
 cc=b.calc_ccfx(c,0,-1,0)
-display(cc)
+ccf=b.calc_ccfx(cf,0,-1,0)
+display((cc,ccf),0,1)
 
-d=a[1].align("rotate_translate_tree",a[0],{"flip":0})
+d=a[1].align("rotate_translate_tree",a[0],{"flip":1})
 xf=d["xform.align2d"].inverse()
-print("rtt\t{:0.2f}".format(xf.get_rotation("2d")["alpha"]))
+print("rtt\t{:0.2f} {}".format(xf.get_rotation("2d")["alpha"],xf.get_params("2d")["mirror"]))
 
-e=a[1].align("rotate_translate",a[0])
+e=a[1].align("rotate_translate_flip",a[0])
 xf=e["xform.align2d"].inverse()
-print("rt\t{:0.2f}".format(xf.get_rotation("2d")["alpha"]))
+print("rt\t{:0.2f} {}".format(xf.get_rotation("2d")["alpha"],xf.get_params("2d")["mirror"]))
 
-rot=cc.calc_max_index()*360.0/cc["nx"]
-print("bs\t{:0.2f}".format(rot))
-a[1].rotate(-rot,0,0)
-display((a[0],a[1],d,e),1)
+if cc["maximum"]>ccf["maximum"] :
+	rot=cc.calc_max_index()*360.0/cc["nx"]
+	print("bs\t{:0.2f}".format(rot))
+	a[1].rotate(-rot,0,0)
+	f=a[1].align("translational",a[0])
+	display((a[0],f,d,e),1)
+else:
+	rot=ccf.calc_max_index()*360.0/cc["nx"]
+	print("bs\t{:0.2f} 1".format(rot))
+	a[2].rotate(-rot,0,0)
+	f=a[2].align("translational",a[0])
+	display((a[0],f,d,e),1)
+	
