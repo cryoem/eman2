3761ded8bfc7c28511c828362e1330dc0106f3be
shadow_walker <shadowwalkersb@gmail.com>
2018-07-24 22:52:54 -0500

    update post-install-actions for binary builds to use eman-deps=12.1

--- ci_support/post-install-actions.bat
+++ ci_support/post-install-actions.bat
@@ -3,4 +3,4 @@ call %PREFIX%\Scripts\activate.bat
 conda.exe info -a
 conda.exe list
 
-conda.exe install eman-deps=10.1 -c cryoem -c defaults -c conda-forge -y
+conda.exe install eman-deps=12.1 -c cryoem -c defaults -c conda-forge -y
