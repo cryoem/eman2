19ecea2692d0f9d42eebb4e64353a394c1bf770a
jgalaz <jgalaz@gmail.com>
2018-08-10 22:49:50 -0700

    program to automatically prefilter tiltseries and backup the original file.

--- examples/e2tomo_preptiltseries.py
+++ examples/e2tomo_preptiltseries.py
@@ -31,30 +31,71 @@ Author: Jesus Galaz - 10/August/2018, Last update: 10/August/2018
 '''
 
 from EMAN2 import *
-
+from EMAN2_utils import *
+import os
 
 def main():
 
-	usage = """Program to generate a cylindrical mask. It can also create a cylindrical shell if
-			you specify the --height_inner and --radius_inner parameters, in addition to the
-			required outer --height and --radius.
-			"""
+	usage = """Program to automatically prefilter tiltseries to facilitate their alignment. 
+			Should ideally be run after xray correction in IMOD."""
 
 	parser = EMArgumentParser(usage=usage,version=EMANVERSION)	
 	
 	parser.add_argument("--input",type=float,default=None,help="""Default=None. Sampling size (angstroms per pixel) to set on the header of the cylindrical mask.""")
 	
-	parser.add_argument("--highpass",type=str,default='',help="Default=filter.highpass.gauss:cutoff_pixels=4 (zeros out the first four Fourier pixels)_. A highpass filtering processor (as in e2proc3d.py) to be applied to the tiltseries.")
+	parser.add_argument("--highpass",type=str,default='filter.highpass.gauss:cutoff_pixels=4',help="Default=filter.highpass.gauss:cutoff_pixels=4 (zeros out the first four Fourier pixels)_. A highpass filtering processor (as in e2proc3d.py) to be applied to the tiltseries.")
 
-	parser.add_argument("--lowpass",type=str,default='',help="Default=filter.lowpass.tanh:cutoff_freq=0.02 (filters to 50 angstroms resolution). A lowpass filtering processor (as in e2proc3d.py) to be applied to the tiltseries.")
+	parser.add_argument("--lowpass",type=str,default='filter.lowpass.tanh:cutoff_freq=0.05',help="Default=filter.lowpass.tanh:cutoff_freq=0.02 (filters to 50 angstroms resolution). A lowpass filtering processor (as in e2proc3d.py) to be applied to the tiltseries.")
+
+	parser.add_argument("--ppid", type=int, help="""Set the PID of the parent process, used for cross platform PPID""",default=-1)
+
+	parser.add_argument("--replace",action='store_true',default=False,help="""Default=False. This will save the filtered tiltseries to the filename of the original tiltseries. The original tiltseries will be backed up.""")
+	
+	parser.add_argument("--verbose", "-v", help="""verbose level [0-9], higner number means higher level of verboseness. Default=0.""",dest="verbose", action="store", metavar="n",type=int, default=0)
 
 	(options, args) = parser.parse_args()
 
 	tiltseries=args[0]
 
-	logger = E2init(sys.argv, options.ppid)
+	checkinput(options)
 
+	logger = E2init(sys.argv, options.ppid)
+	
+	extension = os.path.splitext(os.path.basename(tiltseries))[-1]
+	if options.verbose:
+		print("\nextension is {}".format(extension))
+	
+	print("\nextension is {}".format(extension))
+	
+	tiltseriesout = tiltseries.replace(extension,'_lp50_hpp4' + extension)
+	
+	lowpass = '--filter.lowpass.tanh:cutoff_freq=0.02'
+	if options.lowpass:
+		lowpass = options.lowpass
+		frequency = float(options.lowpass.split('cutoff_freq=')[-1].split(':')[0])
+		resolution = str(int(round(frequency)))
+		tiltseriesout = tiltseries.replace(extension,'_lp' + resolution + extension)
+	
+	if options.verbose:
+		print("\lowpass filter to apply is {}".format(lowpass))
+
+	highpass = '--filter.highpass.gauss:cutoff_pixels=4'
+	if options.highpass:
+		highpass = options.highpass
+		pixels = options.highpass.split('cutoff_pixels=')[-1].split(':')[0]
+		tiltseriesout = tiltseries.replace(extension,'_hpp' + pixels + extension)
+
+	if options.verbose:
+		print("\highpass filter to apply is {}".format(highpass))
+
+	cmd = 'e2proc2d.py ' + tiltseries + ' ' + tiltseriesout + ' ' + lowpass + ' ' + highpass
+	if options.replace:
+		cmd += ' cp ' + tiltseries +' backup.' + tiltseries + ' && cp ' + tiltseriesout +  ' ' + tiltseries
+	
+	runcmd(options,cmd)
+	
 	E2end(logger)
+	
 	return
 
 
