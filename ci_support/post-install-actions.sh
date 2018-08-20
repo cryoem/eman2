d8347666075a29e86055ed800cc8b01c565623cf
shadow_walker <shadowwalkersb@gmail.com>
2018-08-20 12:19:48 -0500

    eman-deps=13.1 (remove theano, no compilers) in post-install-actions [ci skip]

--- ci_support/post-install-actions.sh
+++ ci_support/post-install-actions.sh
@@ -5,7 +5,7 @@ source ${PREFIX}/bin/activate
 conda info -a
 conda list
 
-conda install eman-deps=12.2 -c cryoem -c defaults -c conda-forge -y
+conda install eman-deps=13.1 -c cryoem -c defaults -c conda-forge -y
 
 cat <<EOF
 
