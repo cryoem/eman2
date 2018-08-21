f4f3952bdefa462c6d05ecf19a6fa3ca0d7e368b
shadow_walker <shadowwalkersb@gmail.com>
2018-08-20 20:30:21 -0500

    skip conda-build tests, eman-deps=13.2 (no compilers) [ci skip]

--- ci_support/post-install-actions.bat
+++ ci_support/post-install-actions.bat
@@ -3,4 +3,4 @@ call %PREFIX%\Scripts\activate.bat
 conda.exe info -a
 conda.exe list
 
-conda.exe install eman-deps=13.1 -c cryoem -c defaults -c conda-forge -y
+conda.exe install eman-deps=13.2 -c cryoem -c defaults -c conda-forge -y
