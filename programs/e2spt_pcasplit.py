3c8416a94d13edfa65c784d3b80f881962ed5c7f
James Michael Bell <jmbell91@gmail.com>
2018-09-11 18:05:51 -0500

    fixed typo

--- programs/e2spt_pcasplit.py
+++ programs/e2spt_pcasplit.py
@@ -52,6 +52,7 @@ def mkpath(base):
 	while os.path.isdir("{}_{:02d}".format(base,num)):
 		num += 1
 	os.mkdir("{}_{:02d}".format(base,num))
+	return "{}_{:02d}".format(base,num)
 
 def main():
 	progname = os.path.basename(sys.argv[0])
@@ -59,15 +60,15 @@ def main():
 
 	parser = EMArgumentParser(usage=usage,version=EMANVERSION)
 	parser.add_argument("--path",type=str,required=True,default=None,help="Path to a folder where results should be stored, following standard naming conventions (default = spt_XX)",guitype='filebox',row=0, col=0, rowspan=1, colspan=2)
-	parser.add_argument("--iter",type=int,required=True,help="Iteration number within path. Default = start a new iteration",default=0,guitype='intbox',row=1, col=0, rowspan=1, colspan=1)
+	parser.add_argument("--iter",type=int,required=True,help="Iteration number within path. Default is the second to last iteration (-2).",default=-2,guitype='intbox',row=1, col=0, rowspan=1, colspan=1)
 	parser.add_argument("--nclass",type=int,required=True,help="Number of classes. Default is 2.",default=2,guitype="intbox",row=1, col=1, rowspan=1, colspan=1)
 	parser.add_header(name="orblock1", help='', title="Optional", row=2, col=0, rowspan=1, colspan=2)
 	parser.add_argument("--res",type=float,help="Filter particles to this resolution (in Angstroms) before classification",default=30.0,guitype="floatbox",row=3, col=0, rowspan=1, colspan=1)
 	parser.add_argument("--sym",type=str,help="Apply this symmetry.",default=None,guitype="intbox",row=3, col=1, rowspan=1, colspan=1)
 	parser.add_argument("--mask",type=str,help="Apply this mask. Default is 'mask_tight.hdf' from <--path>_<--iter>.",default=None,guitype="filebox",row=4, col=0, rowspan=1, colspan=2)
-	parser.add_argument("--nbasis",type=int,required=True,help="Number of PCA basis vectors. Default is 4.",default=4,guitype="intbox",row=5, col=1, rowspan=1, colspan=1)
-	parser.add_argument("--keepthresh",type=float,help="Center PCA outliers beyond this value before performing K-means clustering. Default is 0.2.",default=0.2,guitype="floatbox",row=5, col=0, rowspan=1, colspan=1)
-	parser.add_argument("--nowedgefill",action='store_true',help="Do not fill the missing wedge before classification.",default=False,guitype="boolbox",row=6, col=0, rowspan=1, colspan=1)
+	parser.add_argument("--nbasis",type=int,required=True,help="Number of PCA basis vectors. Default is 4.",default=4,guitype="intbox",row=5, col=0, rowspan=1, colspan=1)
+	# parser.add_argument("--keepthresh",type=float,help="Center PCA outliers beyond this value before performing K-means clustering. Default is no threshold (-1).",default=,guitype="floatbox",row=5, col=0, rowspan=1, colspan=1)
+	parser.add_argument("--nowedgefill",action='store_true',help="Do not fill the missing wedge before classification.",default=False,guitype="boolbox",row=5, col=1, rowspan=1, colspan=1)
 	parser.add_argument("--verbose", "-v", dest="verbose", action="store", metavar="n", type=int, default=0, help="verbose level [0-9], higner number means higher level of verboseness")
 	parser.add_argument("--ppid", type=int, help="Set the PID of the parent process, used for cross platform PPID",default=-1)
 	(options, args) = parser.parse_args()
@@ -79,24 +80,28 @@ def main():
 		print("Could not locate --path {}".format(options.path))
 		sys.exit(1)
 
-	if options.iter<=0 :
+	if options.iter==-2:
 		fls=[int(i[15:17]) for i in os.listdir(options.path) if i[:15]=="particle_parms_" and str.isdigit(i[15:17])]
 		if len(fls)==0 : 
 			print("No subtomogram alignment was complered for this refinement. Please try another")
 			sys.exit(1)
-		options.iter=max(fls)
+		options.iter=max(fls)-1
 
 	options.outpath = mkpath("sptcls")
 
 	logid=E2init(sys.argv, options.ppid)
 
-	threed = EMData("{}/threed_{:.02d}.hdf".format(options.path,options.iter))
+	threed = EMData("{}/threed_{:02d}.hdf".format(options.path,options.iter))
 	
 	if options.mask: msk = EMData(options.msk)
 	else: msk = EMData("{}/mask_tight.hdf".format(options.path))
 
-	refparms=js_open_dict("{}_{:02d}/0_spt_params.json".format(options.path,options.iter))
-	inptcls = refparms["input_ptcls"]
+	refparms=js_open_dict("{}/0_spt_params.json".format(options.path))
+	try:
+		inptcls = refparms["input_ptcls"]
+	except:
+		print("Could not locate input particles. Path and iteration must reference a standard 3D refinement (e2spt_refine.py) and not a subtilt refinement (e2spt_subtilt.py).")
+		sys.exit(1)
 
 	parmsfile = "{}/particle_parms_{:02d}.json".format(options.path,options.iter)
 	js=js_open_dict(parmsfile)
@@ -109,6 +114,7 @@ def main():
 
 	refft = threed.do_fft()
 	imgs00 = []
+
 	for i in range(nptcl):
 
 		if options.verbose:
@@ -117,11 +123,13 @@ def main():
 
 		k="('{}', {})".format(pname, i)
 		xf=js[k]['xform.align3d']
-		e=EMData(pname, i)
+
+		e=EMData(pname, i)		
 		e.transform(xf)
 		e["score"]=js[k]['score']
 		
-		if options.nowedgefill: enew = e
+		if options.nowedgefill: 
+			enew = e
 		else:
 			eft=e.do_fft()
 			eft.process_inplace("mask.wedgefill",{"fillsource":refft, "thresh_sigma":0.0})
@@ -131,8 +139,9 @@ def main():
 		enew.process_inplace("math.meanshrink",{"n":2})
 		enew.process_inplace("filter.lowpass.gauss",{"cutoff_freq":1/options.res})
 
-		if options.sym != 'c1':
+		if options.sym != None:
 			enew.process_inplace("xform.applysym",{"averager":"mean.tomo", "sym":options.sym})
+
 		enew["xform.align3d"]=xf
 
 		enew.write_image("{}/aliptcls.hdf".format(options.outpath), i)
@@ -140,15 +149,11 @@ def main():
 
 	js.close()
 
-	#imgs00=EMData.read_images("{}/aliptcls.hdf".format(options.outpath))
 	nptcl = len(imgs00)
-
 	imgsnp=np.array([m.numpy().copy() for m in imgs00])
 
 	pca=PCA(options.nbasis)
 	pout=pca.fit_transform(imgsnp.reshape((nptcl, -1)))
-
-	pout[abs(pout)>options.keepthresh]=0
 
 	basisfile = "{}/pca_basis.hdf".format(options.outpath)
 	for i,c in enumerate(pca.components_):
@@ -156,7 +161,7 @@ def main():
 		m=from_numpy(egmap.copy())
 		m.write_image(basisfile,i)
 
-	kmeans = KMeans(n_clusters=options.ncls).fit(pout)
+	kmeans = KMeans(n_clusters=options.nclass).fit(pout)
 	lb=kmeans.labels_
 
 	if options.verbose: print("Class: Particle count")
@@ -175,7 +180,6 @@ def main():
 		e.write_image(classfile,i)
 
 	inlst=LSXFile(inptcls, True)
-
 	outlsts=[]
 	for lbl in sorted(np.unique(lb)):
 		outlst = LSXFile(inptcls.replace(".lst","_{}.lst".format(lbl)))
