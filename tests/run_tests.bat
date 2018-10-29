9e34bc5acbaa8817b05162027e2afad9f8552492
shadow_walker <shadowwalkersb@gmail.com>
2018-10-29 00:12:03 -0500

    add fftw threads test

--- tests/run_tests.bat
+++ tests/run_tests.bat
@@ -1,6 +1,7 @@
 :: 1. Run e2version.py and e2speedtest.py
 e2version.py
 e2speedtest.py
+python -c "from EMAN2 import *; Util.version()"
 
 :: 2. Existence tests for data files like images, font files, JSON
 python tests\test_EMAN2DIR.py
