e0897360bd1755559e2d118d496e88dedca64ed2
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2018-12-01 12:41:22 +0100

    Ignore future imports as well from .git

--- tests/future_import_tests.sh
+++ tests/future_import_tests.sh
@@ -5,12 +5,13 @@ future_imports=(
                 division
                )
 
-py_files=$( find . -path ./sphire -prune -o -not -empty -name '*.py' )
+py_files=$( find . -path ./sphire -prune -o -path ./.git -prune -o -not -empty -name '*.py' )
 
 failed_cases=()
 for f in ${py_files[@]};do
+    echo $f
     for imp in ${future_imports[@]};do
-        if [ $f != "./sphire" ];then
+        if [[ $f != "./sphire" && $f != './.git' ]];then
             if ! grep -q "from __future__ import ${imp}" ${f};then
                 failed_cases+=("$f")
                 echo "  ${f} is missing \"from __future__ import ${imp}"\"
