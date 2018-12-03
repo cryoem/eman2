29bfc1966ec10e0efc427ca924bcfb36338d36b9
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2018-12-03 15:19:27 +0100

    Fixed sparx_fundamentals is_in_subunit function

--- sphire/libpy/sparx_fundamentals.py
+++ sphire/libpy/sparx_fundamentals.py
@@ -850,17 +850,39 @@ class symclass(object):
 		        False otherwise.
 		"""
 		pass#IMPORTIMPORTIMPORT from math import degrees, radians, sin, cos, tan, atan, acos, sqrt
-		if( (self.sym[0] == "c")  or  (self.sym[0] == "d" and (self.nsym//2)%2 == 0) ):
-			if((phi>= 0.0 and phi<self.brackets[inc_mirror][0]) and (theta<=self.brackets[inc_mirror][1])):  return True
-			else:  return False
-		elif( self.sym[0] == "d" and (self.nsym//2)%2 == 1 ):
-			if(theta<=self.brackets[inc_mirror][1]):
-				phib = 360.0/self.nsym
-				if( phi>=0.0 and phi<self.brackets[1][0] ):
-					if(inc_mirror==1):  return True
-					elif( (phi>= 0.0 and phi<phib/2) or (phi>= phib and phi<(phib+phib/2)) ): return True
-			return False
-			
+		if self.sym[0] == "c":
+			if phi >= 0.0 and phi < self.brackets[inc_mirror][0] and theta <= self.brackets[inc_mirror][1]:
+				return True
+			elif theta == 180 and inc_mirror:
+				return True
+			elif theta == 0:
+				return True
+			else:
+				return False
+
+		elif self.sym[0] == "d" and (self.nsym//2) % 2 == 0:
+			if phi >= 0.0 and phi < self.brackets[inc_mirror][0] and theta <= self.brackets[inc_mirror][1]:
+				return True
+			elif theta == 0:
+				return True
+			else:
+				return False
+
+		elif self.sym[0] == "d" and (self.nsym//2) % 2 == 1:
+			if theta <= self.brackets[inc_mirror][1]:
+				phib = 360.0 / self.nsym
+				if phi >= 0.0 and phi < self.brackets[1][0]:
+					if inc_mirror == 1:
+						return True
+					elif (phi >= phib / 2 and phi < phib) or (phi >= phib and phi <= phib + phib / 2):
+						return True
+					elif theta == 0:
+						return True
+			if theta == 0:
+				return True
+			else:
+				return False
+
 		elif( (self.sym[:3] == "oct")  or  (self.sym[:4] == "icos") ):
 			if( phi>= 0.0 and phi<self.brackets[inc_mirror][0] and theta<=self.brackets[inc_mirror][3] ):
 				tmphi = min(phi, self.brackets[inc_mirror][2]-phi)
@@ -871,6 +893,8 @@ class symclass(object):
 				#print(  "  baldwin_lower_alt_bound ",self.brackets,baldwin_lower_alt_bound,theta)
 				if(baldwin_lower_alt_bound>theta): return True
 				else: return False
+			elif theta == 0:
+				return True
 			else:
 				#print "phi",self.brackets
 				return False
@@ -893,7 +917,11 @@ class symclass(object):
 						#print(  "  baldwin_upper_alt_bound ",phi,theta,baldwin_upper_alt_bound,self.brackets[inc_mirror])
 						if(baldwin_upper_alt_bound<theta): return False
 						else:  return True
+				elif theta == 0:
+					return True
 				else: return False
+			elif theta == 0:
+				return True
 			else:
 				#print "phi",self.brackets
 				return False
