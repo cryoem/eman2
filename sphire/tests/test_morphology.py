53b76c5d69b89a3654eb204d197a18e884bd80b2
adnan <adnanali244@live.com>
2018-12-14 17:42:20 +0100

    Added more functions in test_morphology but its not finished till now

--- sphire/tests/test_morphology.py
+++ sphire/tests/test_morphology.py
@@ -2,19 +2,24 @@ from __future__ import print_function
 from __future__ import division
 
 import numpy
+import shutil
 import copy
 import math
 import EMAN2_cppwrap as e2cpp
 
 from ..libpy import sparx_morphology as fu
 from .sparx_lib import sparx_morphology as oldfu
+from ..libpy import sparx_filter as ft
 from ..libpy import sparx_utilities as ut
+
+import pickle
+import os
+ABSOLUTE_PATH =  os.path.dirname(os.path.realpath(__file__))
 
 
 import unittest
 
-def get_data(num):
-    dim = 10
+def get_data(num, dim=10):
     data_list = []
     for i in range(num):
         a = e2cpp.EMData(dim, dim)
@@ -24,8 +29,7 @@ def get_data(num):
     return data_list
 
 
-def get_data_3d(num):
-    dim = 10
+def get_data_3d(num, dim=10):
     data_list = []
     for i in range(num):
         a = e2cpp.EMData(dim, dim,dim)
@@ -96,8 +100,323 @@ class MyTestCase(unittest.TestCase):
         return_old = oldfu.square(image)
         self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
 
+    def test_threshold_true_should_return_equal_object(self):
+        image, = get_data(1)
+
+        return_new = fu.threshold(image)
+        return_old = oldfu.threshold(image)
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
+
+    def test_threshold_outside_true_should_return_equal_object(self):
+        image, = get_data(1)
+
+        return_new = fu.threshold_outside(image, 2, 10)
+        return_old = oldfu.threshold_outside(image, 2, 10)
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
 
 
+    def test_notzero_true_should_return_equal_object(self):
+        image, = get_data(1)
+
+        return_new = fu.threshold_outside(image, 2, 10)
+        return_old = oldfu.threshold_outside(image, 2, 10)
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
+
+    def test_rotavg_ctf_true_should_return_equal_object(self):
+        image, = get_data(1)
+        defocus = 1
+        Cs = 2
+        voltage = 300
+        Pixel_size = 1.5
+
+        return_new = fu.rotavg_ctf(image, defocus, Cs, voltage, Pixel_size)
+        return_old = oldfu.rotavg_ctf(image, defocus, Cs, voltage, Pixel_size)
+
+        self.assertTrue(return_new , return_old)
+
+    def test_ctf_1d_true_should_return_equal_object(self):
+        nx = 20
+        defocus = 1
+        cs =  2
+        voltage = 300
+        pixel_size = 1.5
+        bfactor =  0
+        amp_contrast = 0.1
+        ctf = e2cpp.EMAN2Ctf()
+        ctf.from_dict({"defocus": defocus, "cs": cs, "voltage": voltage, "apix": pixel_size, "bfactor": bfactor,
+                       "ampcont": amp_contrast})
+
+        return_new = fu.ctf_1d(nx, ctf)
+        return_old= fu.ctf_1d(nx, ctf)
+
+        self.assertTrue(return_new, return_old)
+
+
+    def test_ctf_2_true_should_return_equal_object(self):
+        nx = 20
+        defocus = 1
+        cs =  2
+        voltage = 300
+        pixel_size = 1.5
+        bfactor =  0
+        amp_contrast = 0.1
+        ctf = e2cpp.EMAN2Ctf()
+        ctf.from_dict({"defocus": defocus, "cs": cs, "voltage": voltage, "apix": pixel_size, "bfactor": bfactor,
+                       "ampcont": amp_contrast})
+
+        return_new = fu.ctf_2(nx, ctf)
+        return_old= fu.ctf_2(nx, ctf)
+
+        self.assertTrue(return_new, return_old)
+
+    def test_ctf_img_true_should_return_equal_object(self):
+        nx = 20
+        defocus = 1
+        cs = 2
+        voltage = 300
+        pixel_size = 1.5
+        bfactor = 0
+        amp_contrast = 0.1
+        ctf = e2cpp.EMAN2Ctf()
+        ctf.from_dict({"defocus": defocus, "cs": cs, "voltage": voltage, "apix": pixel_size, "bfactor": bfactor,
+                       "ampcont": amp_contrast})
+
+        return_new = fu.ctf_img(nx, ctf)
+        return_old = fu.ctf_img(nx, ctf)
+
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
+
+    def test_ctf_img_real_true_should_return_equal_object(self):
+        nx = 20
+        defocus = 1
+        cs = 2
+        voltage = 300
+        pixel_size = 1.5
+        bfactor = 0
+        amp_contrast = 0.1
+        ctf = e2cpp.EMAN2Ctf()
+        ctf.from_dict({"defocus": defocus, "cs": cs, "voltage": voltage, "apix": pixel_size, "bfactor": bfactor,
+                       "ampcont": amp_contrast})
+
+        return_new = fu.ctf_img_real(nx, ctf)
+        return_old = fu.ctf_img_real(nx, ctf)
+
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
+
+
+    def test_ctf_rimg_true_should_return_equal_object(self):
+        nx = 20
+        defocus = 1
+        cs = 2
+        voltage = 300
+        pixel_size = 1.5
+        bfactor = 0
+        amp_contrast = 0.1
+        ctf = e2cpp.EMAN2Ctf()
+        ctf.from_dict({"defocus": defocus, "cs": cs, "voltage": voltage, "apix": pixel_size, "bfactor": bfactor,
+                       "ampcont": amp_contrast})
+
+        return_new = fu.ctf_rimg(nx, ctf)
+        return_old = fu.ctf_rimg(nx, ctf)
+
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
+
+    def test_ctf2_rimg_true_should_return_equal_object(self):
+        nx = 20
+        defocus = 1
+        cs = 2
+        voltage = 300
+        pixel_size = 1.5
+        bfactor = 0
+        amp_contrast = 0.1
+        ctf = e2cpp.EMAN2Ctf()
+        ctf.from_dict({"defocus": defocus, "cs": cs, "voltage": voltage, "apix": pixel_size, "bfactor": bfactor,
+                       "ampcont": amp_contrast})
+
+        return_new = fu.ctf2_rimg(nx, ctf)
+        return_old = fu.ctf2_rimg(nx, ctf)
+
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
+
+    def test_ctflimit_true_should_return_equal_object(self):
+        nx = 30
+        defocus = 1
+        Cs = 2
+        voltage = 300
+        Pixel_size = 1.5
+
+        return_new = fu.ctflimit(nx, defocus, Cs, voltage, Pixel_size)
+        return_old = oldfu.ctflimit(nx, defocus, Cs, voltage, Pixel_size)
+
+        self.assertTrue(return_new , return_old)
+
+
+    def test_imf_params_cl1_true_should_return_equal_object(self):
+        pw = [entry for entry in numpy.arange(0,10).tolist()]
+
+        return_new = fu.imf_params_cl1(pw)
+        return_old = oldfu.imf_params_cl1(pw)
+
+        self.assertTrue(return_new, return_old)
+
+    def test_adaptive_mask_true_should_return_equal_object(self):
+        image, = get_data_3d(1)
+
+        return_new = fu.adaptive_mask(image)
+        return_old = oldfu.adaptive_mask(image)
+
+        self.assertTrue(return_new, return_old)
+
+
+    def test_cosinemask_true_should_return_equal_object(self):
+        image, = get_data_3d(1)
+
+        return_new = fu.cosinemask(image)
+        return_old = oldfu.cosinemask(image)
+
+        self.assertTrue(return_new, return_old)
+
+
+    def test_shrink_3dmask_true_should_return_equal_object(self):
+        nxinit = 4
+        mask_file_name = get_data_3d(1)
+        return_new = fu.get_shrink_3dmask(nxinit,mask_file_name)
+        return_old = oldfu.get_shrink_3dmask(nxinit,mask_file_name)
+
+        self.assertTrue(return_new, return_old)
+
+
+    def test_get_biggest_cluster_true_should_return_equal_object(self):
+        image = ut.model_blank(10, 10)
+
+        return_new = fu.get_biggest_cluster(image)
+        return_old = oldfu.get_biggest_cluster(image)
+
+        self.assertTrue(return_new, return_old)
+
+
+    def test_computer_bfactor_true_should_return_equal_object(self):
+        pw = [entry for entry in numpy.arange(0, 10).tolist()]
+        freq_min = 0.15
+        freq_max = 0.25
+
+        return_new = fu.compute_bfactor(pw, freq_min, freq_max)
+        return_old = oldfu.compute_bfactor(pw, freq_min, freq_max)
+
+        self.assertTrue(return_new, return_old)
+
+    def test_cter_mrk_true_should_return_equal_object(self):
+        defocus = 1
+        cs = 2
+        voltage = 300
+        pixel_size = 1.5
+        bfactor = 0
+        amp_contrast = 0.1
+        ctf = e2cpp.EMAN2Ctf()
+        ctf.from_dict({"defocus": defocus, "cs": cs, "voltage": voltage, "apix": pixel_size, "bfactor": bfactor,
+                       "ampcont": amp_contrast})
+        image1, = get_data(1, 256)
+
+        micrograph_image = ft.filt_ctf(image1,ctf)
+        micrograph_image.write_image('sphire/tests/files/cter_mrk/image.mrc')
+        selection_list = 'image.mrc'
+
+        input_image_path = os.path.join(ABSOLUTE_PATH, "files/cter_mrk/image*.mrc")
+        output_directory = os.path.join(ABSOLUTE_PATH, "files/cter_mrk/results")
+        shutil.rmtree(output_directory)
+
+        wn = 32
+        return_new = fu.cter_mrk(input_image_path, output_directory, wn = wn, selection_list= selection_list,pixel_size = 5)
+        return_old = oldfu.cter_mrk(input_image_path, output_directory, wn = wn, selection_list = selection_list, pixel_size=5)
+
+        self.assertTrue(return_new, return_old)
+
+
+    def test_cter_pep_true_should_return_equal_object(self):
+        defocus = 1
+        cs = 2
+        voltage = 300
+        pixel_size = 1.5
+        bfactor = 0
+        amp_contrast = 0.1
+        ctf = e2cpp.EMAN2Ctf()
+        ctf.from_dict({"defocus": defocus, "cs": cs, "voltage": voltage, "apix": pixel_size, "bfactor": bfactor,
+                       "ampcont": amp_contrast})
+        image1, = get_data(1, 256)
+
+        micrograph_image = ft.filt_ctf(image1,ctf)
+        micrograph_image.write_image('sphire/tests/files/cter_mrk/image.mrc')
+        selection_list = 'image.mrc'
+
+        input_image_path = os.path.join(ABSOLUTE_PATH, "files/cter_mrk/image*.mrc")
+        output_directory = os.path.join(ABSOLUTE_PATH, "files/cter_mrk/results")
+        shutil.rmtree(output_directory)
+
+        wn = 32
+        return_new = fu.cter_pap(input_image_path, output_directory, wn = wn, selection_list= selection_list,pixel_size = 5)
+        return_old = oldfu.cter_pap(input_image_path, output_directory, wn = wn, selection_list = selection_list, pixel_size=5)
+
+        self.assertTrue(return_new, return_old)
+
+
+    def test_ampcont2angle_true_should_return_equal_object(self):
+
+        return_new = fu.ampcont2angle(8)
+        return_old = oldfu.ampcont2angle(8)
+
+        self.assertTrue(return_new, return_old)
+
+    def test_angle2ampcont_true_should_return_equal_object(self):
+
+        return_new = fu.angle2ampcont(0.45)
+        return_old = oldfu.angle2ampcont(0.45)
+
+        self.assertTrue(return_new, return_old)
+
+    def test_bracket_def_true_should_return_equal_object(self):
+
+        def function_test(x1,dat):
+            f = x1 + dat
+            return f
+        dat = 5
+        x1 = 3
+        h  = 3
+
+        return_new = fu.bracket_def(function_test,dat, x1, h)
+        return_old = oldfu.bracket_def(function_test,dat, x1, h)
+
+        self.assertTrue(return_new, return_old)
+
+
+    def test_bracket_true_should_return_equal_object(self):
+
+        def function_test(x1,dat):
+            f = x1 + dat
+            return f
+        dat = 5
+        x1 = 3
+        h  = 3
+
+        return_new = fu.bracket(function_test,dat,h)
+        return_old = oldfu.bracket(function_test,dat,h)
+
+        self.assertTrue(return_new, return_old)
+
+
+    def test_goldsearch_astigmatism_true_should_return_equal_object(self):
+
+        def function_test(x1,dat):
+            f = x1 + dat
+            return f
+        dat = 5
+        x1 = 3
+        a  = 3
+        b = 4
+
+        return_new = fu.goldsearch_astigmatism(function_test,dat,a,b)
+        return_old = oldfu.goldsearch_astigmatism(function_test,dat,a,b)
+
+        self.assertTrue(return_new, return_old)
 
 
 if __name__ == '__main__':
