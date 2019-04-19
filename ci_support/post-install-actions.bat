cb01fcd18102d44f670484640bd8da6f380fffd7
shadow_walker <shadowwalkersb@gmail.com>
2019-04-18 23:13:34 -0500

    set fail on error in post-install scripts

--- ci_support/post-install-actions.bat
+++ ci_support/post-install-actions.bat
@@ -1,8 +1,13 @@
 call %PREFIX%\Scripts\activate.bat
+if errorlevel 1 exit 1
 
 mkdir %PREFIX%\install_logs
+if errorlevel 1 exit 1
 
 conda.exe info -a > %PREFIX%\install_logs\info_log.txt 2>&1
+if errorlevel 1 exit 1
 conda.exe list    > %PREFIX%\install_logs\list_log.txt 2>&1
+if errorlevel 1 exit 1
 
 conda.exe install -v eman-deps=14.1 -c cryoem -c defaults -c conda-forge -y > %PREFIX%\install_logs\install_log.txt 2>&1
+if errorlevel 1 exit 1
