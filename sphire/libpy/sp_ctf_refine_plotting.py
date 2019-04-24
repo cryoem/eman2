730c0f0f7a089378f75bcd11f566f1432e8d8db9
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2019-04-24 18:03:00 +0200

    Adjust license in lib files

--- sphire/libpy/sp_ctf_refine_plotting.py
+++ sphire/libpy/sp_ctf_refine_plotting.py
@@ -2,8 +2,15 @@
 Plotting stuff for CTF Refinement with error assessment in SPHIRE
 
 #
-# Author: Thorsten Wagner 02/18/2019 (thorsten.wagner@mpi-dortmund.mpg.de)
-# Copyright (C) 2019 Max planck institute for molecular physiology, Dortmund
+# Author: Markus Stabrin 2019 (markus.stabrin@mpi-dortmund.mpg.de)
+# Author: Fabian Schoenfeld 2019 (fabian.schoenfeld@mpi-dortmund.mpg.de)
+# Author: Thorsten Wagner 2019 (thorsten.wagner@mpi-dortmund.mpg.de)
+# Author: Tapu Shaikh 2019 (tapu.shaikh@mpi-dortmund.mpg.de)
+# Author: Adnan Ali 2019 (adnan.ali@mpi-dortmund.mpg.de)
+# Author: Luca Lusnig 2019 (luca.lusnig@mpi-dortmund.mpg.de)
+# Author: Toshio Moriya 2019 (toshio.moriya@kek.jp)
+#
+# Copyright (c) 2019 Max Planck Institute of Molecular Physiology
 #
 # This software is issued under a joint BSD/GNU license. You may use the
 # source code in this file under either license. However, note that the
