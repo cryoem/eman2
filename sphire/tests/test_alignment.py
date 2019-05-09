d3c4a7ae5d9ba1020854a1d569c6dbb61b161add
Luca Lusnig <lusnig@rtxr2.client.mpi-dortmund.mpg.de>
2019-05-09 16:57:34 +0200

    restored the Adnan's reference tests

--- sphire/tests/test_alignment.py
+++ sphire/tests/test_alignment.py
@@ -1907,3 +1907,360 @@ class Test_generate_list_of_reference_angles_for_search(unittest.TestCase):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" Adnan helper functions to run the reference tests"""
+import copy
+import os
+import cPickle as pickle
+import EMAN2_cppwrap as e2cpp
+def get_data(num,dim = 10):
+    data_list = []
+    for i in range(num):
+        a = e2cpp.EMData(dim, dim)
+        data_a = a.get_3dview()
+        data_a[...] = numpy.arange(dim * dim, dtype=numpy.float32).reshape(dim, dim) + i
+        data_list.append(a)
+    return data_list
+
+
+def get_data_3d(num, dim=10):
+    data_list = []
+    for i in range(num):
+        a = e2cpp.EMData(dim, dim,dim)
+        data_a = a.get_3dview()
+        data_a[...] = numpy.arange(dim * dim * dim, dtype=numpy.float32).reshape(dim, dim, dim) + i
+        data_list.append(a)
+
+    return data_list
+
+
+
+
+class Test_lib_alignment_compare(unittest.TestCase):
+    """"
+    # data = list of images
+    # numr = tuple or list precalcualte rings
+    # wr = list of weights of numr
+    # cs =  cs = [0.0]*2
+    # tavg = blanck image
+    # cnx = center value x
+    # cny = cnx
+    # xrng =  list of possible shifts
+    # yrng =  list of possible shifts
+    # step = stepsize of the shift
+    """
+    def test_ali2d_single_iter_true_should_return_equal_objects(self):
+        filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.ali2d_single_iter")
+        with open(filepath, 'rb') as rb:
+            argum = pickle.load(rb)
+            print(argum[0])
+        (dataa, numr, wr, cs, tavg, cnx, cny, xrng, yrng, step) = argum[0]
+        (datab, numr, wr, cs, tavg, cnx, cny, xrng, yrng, step) = argum[0]
+
+        dataa = copy.deepcopy(argum[0][0])
+        datab = copy.deepcopy(argum[0][0])
+
+        return_new = fu.ali2d_single_iter(dataa, numr, wr, cs, tavg, cnx, cny, xrng, yrng, step)
+        return_old = fu.ali2d_single_iter(datab, numr, wr, cs, tavg, cnx, cny, xrng, yrng, step)
+        self.assertEqual(return_new,return_old)
+
+
+    def test_ang_n_true_should_return_equal_object(self):
+        return_new = fu.ang_n(2 , 'f' , 3)
+        return_old = oldfu.ang_n(2, 'f' , 3)
+
+        self.assertEqual(return_new, return_old)
+
+    def test_log2_true_should_return_equal_object(self):
+        return_new = fu.log2(10)
+        return_old = oldfu.log2(10)
+
+        self.assertEqual(return_new,return_old)
+
+    def test_Numrinit_true_should_return_equal_object(self):
+        return_new = fu.Numrinit(2 , 5)
+        return_old = oldfu.Numrinit(2, 5)
+
+        self.assertEqual(return_new, return_old)
+
+    def test_ringwe_true_should_return_equal_object(self):
+        filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.ringwe")
+        with open(filepath, 'rb') as rb:
+            argum = pickle.load(rb)
+
+        (numr) = argum[0][0]
+
+        return_new = fu.ringwe(numr)
+        return_old = oldfu.ringwe(numr)
+
+        self.assertEqual(return_new, return_old)
+
+    def test_ornq_true_should_return_equal_object(self):
+
+        filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.ornq")
+        with open(filepath, 'rb') as rb:
+            argum = pickle.load(rb)
+            print(argum)
+
+        (image, crefim, xrng, yrng, step, mode, numr, cnx, cny) = argum[0]
+
+        return_new = fu.ornq(image,crefim,xrng,yrng,step,mode,numr,cnx,cny)
+        return_old = fu.ornq(image,crefim,xrng,yrng,step,mode,numr,cnx,cny)
+        self.assertEqual(return_new, return_old)
+
+    def test_ormq_true_should_return_equal_object(self):
+        filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.ormq")
+        with open(filepath, 'rb') as rb:
+            argum = pickle.load(rb)
+
+        (image, crefim, xrng, yrng, step, mode, numr, cnx, cny, delta) = argum[0]
+
+        return_new = fu.ormq(image,crefim,xrng,yrng,step,mode,numr,cnx,cny,delta)
+        return_old = fu.ormq(image,crefim,xrng,yrng,step,mode,numr,cnx,cny,delta)
+
+        self.assertEqual(return_new, return_old)
+
+
+    """This function does not seem to be use anywhere so I wont be creating a unit test for this function"""
+    # def test_ormq_fast_true_should_return_equal_object(self):
+    #
+    #     filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.ormq")
+    #     with open(filepath, 'rb') as rb:
+    #         arguma= pickle.load(rb)
+    #
+    #     (image, crefim, xrng, yrng, step, mode, numr, cnx, cny, delta) = arguma[0]
+    #
+    #
+    #
+    #     filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.ali2d_single_iter")
+    #     with open(filepath, 'rb') as rb:
+    #         argumb = pickle.load(rb)
+    #
+    #     (dataa, numra, wra, csa, tavga, cnxa, cnya, xrnga, yrnga, stepa) = argumb[0]
+    #     dataa = copy.deepcopy(argumb[0][0])
+    #
+    #
+    #
+    #     return_new = fu.ormq_fast(dataa, crefim, xrnga, yrnga, stepa, numra, mode)
+    #     return_old = fu.ormq_fast(dataa, crefim, xrnga, yrnga, stepa, numra, mode)
+    #
+    #     self.assertTrue(return_new, return_old)
+
+
+    def test_prepref_true_should_return_equal_object(self):
+        filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.ali2d_single_iter")
+        with open(filepath, 'rb') as rb:
+            argum = pickle.load(rb)
+
+        (data, numr, wr, cs, tavg, cnx, cny, xrng, yrng, step) = argum[0]
+
+        mode   = 'f'
+        maxrangex = 4
+        maxrangey = 4
+        maskfile = None
+
+        return_new = fu.prepref(data,maskfile,cnx,cny,numr,mode,maxrangex,maxrangey,step)
+        return_old = oldfu.prepref(data,maskfile,cnx,cny,numr,mode,maxrangex,maxrangey,step)
+
+
+        self.assertTrue(return_old,return_new)
+
+
+    # def test_prepare_refrings_true_should_return_equal_object(self):
+    #     filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.prepare_refrings")
+    #     with open(filepath, 'rb') as rb:
+    #         try:
+    #             argum = pickle.load(rb)
+    #             print(argum)
+    #         except EOFError as exc:
+    #             print(exc)
+    #
+    #         # (volft,kb) = pickle.load(rb)
+    #
+    #     return_new = fu.prepare_refrings(volft,kb)
+    #     # return_old = oldfu.prepare_refrings(volft,kb)
+    #     #
+    #     # self.assertEqual(return_old,return_new)
+
+
+    def test_proj_ali_incore_true_should_return_equal_object(self):
+        filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.shc")
+        with open(filepath, 'rb') as rb:
+            argum = pickle.load(rb)
+
+        print(argum)
+        print(len(argum[0]))
+        print(argum[0][4])
+
+        (data, refrings, list_of_ref_ang, numr, xrng, yrng, step) = argum[0]
+
+        return_new = fu.proj_ali_incore(data, refrings, numr, xrng, yrng, step)
+
+        return_old = oldfu.proj_ali_incore(data, refrings, numr, xrng, yrng, step)
+
+        self.assertTrue(return_old, return_new)
+
+    """ Core dump issue    """
+    def test_proj_ali_incore_local_true_should_return_equal_object(self):
+        filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.shc")
+        with open(filepath, 'rb') as rb:
+            argum = pickle.load(rb)
+
+        (data, refrings, list_of_ref_ang, numr, xrng, yrng, step) = argum[0]
+
+        print(argum)
+
+        an = -1.0
+        xrng = 2.0
+        yrng = 2.0
+        nsym =1
+        symangles = []
+
+        for k in range(len(refrings)):
+            # for i in range(nsym):
+            symangles.append([0.0, 0.0, 1 * 360. / nsym])
+
+        list_of_ref_ang_new = fu.generate_list_of_reference_angles_for_search(symangles, 'c1')
+        list_of_ref_ang_old = oldfu.generate_list_of_reference_angles_for_search(symangles, 'c1')
+
+
+        return_new = fu.proj_ali_incore_local(data, refrings, list_of_ref_ang_new, numr, xrng, yrng, step, an)
+
+        return_old = oldfu.proj_ali_incore_local(data, refrings, list_of_ref_ang_old, numr, xrng, yrng, step, an)
+
+        self.assertTrue(return_old, return_new)
+
+
+    def test_ali_vol_func_true_should_return_equal_object(self):
+        filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.ali_vol_func")
+        with open(filepath, 'rb') as rb:
+            argum = pickle.load(rb)
+            params = argum[0][0]
+            data = argum[1]
+
+
+        return_new = fu.ali_vol_func(params,**data)
+        return_old = oldfu.ali_vol_func(params,**data)
+
+        self.assertEqual(return_old, return_new)
+
+
+    def test_align2d_true_should_return_equal_object(self):
+        filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.align2d_scf")
+        with open(filepath, 'rb') as rb:
+            argum = pickle.load(rb)
+
+        (image,refim, xrng, yrng) = argum[0]
+        (ou) = argum[1]['ou']
+
+        return_new = fu.align2d(image,refim)
+        return_old = oldfu.align2d(image,refim)
+
+        self.assertEqual(return_old, return_new)
+
+
+    def test_align2d_scf_true_should_return_equal_object(self):
+        filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.align2d_scf")
+        with open(filepath, 'rb') as rb:
+            argum = pickle.load(rb)
+
+        (image,refim, xrng, yrng) = argum[0]
+        (ou) = argum[1]['ou']
+
+        return_new = fu.align2d_scf(image,refim,xrng,yrng, ou)
+        return_old = oldfu.align2d_scf(image,refim,xrng,yrng, ou)
+
+        self.assertEqual(return_old, return_new)
+
+
+    def test_multalign2d_scf_true_should_return_equal_object(self):
+        filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.ali2d_single_iter")
+        with open(filepath, 'rb') as rb:
+            argum = pickle.load(rb)
+            # print(argum[0])
+
+        (dataa, numr, wr, cs, tavg, cnx, cny, xrng, yrng, step) = argum[0]
+
+        dataa = copy.deepcopy(argum[0][0])
+
+        mode = "F"
+        ou = 174
+
+        cimage = EMAN2_cppwrap.Util.Polar2Dm(tavg, float(cnx), float(cny), numr, mode)
+        frotim = [sparx_fundamentals.fft(tavg)]
+
+
+        return_new = fu.multalign2d_scf(dataa[0],[cimage],frotim, numr, xrng,yrng, ou)
+        return_old = oldfu.multalign2d_scf(dataa[0],[cimage],frotim, numr, xrng,yrng, ou)
+
+        self.assertEqual(return_old, return_new)
+
+
+    def test_parabl_true_should_return_equal_object(self):
+        filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.parabl")
+        with open(filepath, 'rb') as rb:
+            (Z) = pickle.load(rb)
+
+        return_new = fu.parabl(Z[0][0])
+        return_old = oldfu.parabl(Z[0][0])
+
+        self.assertEqual(return_old, return_new)
+
+    def test_shc_true_should_return_equal_object(self):
+        filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.shc")
+        with open(filepath, 'rb') as rb:
+            argum = pickle.load(rb)
+            print(argum)
+            print(len(argum[0]))
+            print(argum[0][4])
+
+        (data, refrings, list_of_ref_ang, numr, xrng, yrng, step) = argum[0]
+
+        return_new = fu.shc(data, refrings, list_of_ref_ang, numr, xrng, yrng, step)
+
+        return_old = oldfu.shc(data, refrings, list_of_ref_ang, numr, xrng, yrng, step)
+
+        self.assertTrue(return_old, return_new)
+
+
+
+    def test_search_range_true_should_return_equal_object(self):
+        filepath = os.path.join(ABSOLUTE_PATH, "pickle files/alignment.search_range")
+        with open(filepath, 'rb') as rb:
+            argum = pickle.load(rb)
+            print(argum[0])
+            (n, radius, shift, range,location) = argum[0]
+
+        return_new = fu.search_range(n, radius, shift, range)
+        return_old = oldfu.search_range(n, radius, shift, range)
+
+        self.assertEqual(return_old, return_new)
+
+
+    def test_generate_list_true_should_return_equal_object(self):
+        nsym = 5
+        symangles = []
+        for i in range(nsym):
+            symangles.append([0.0, 0.0, i * 360. / nsym])
+
+        return_new = fu.generate_list_of_reference_angles_for_search(symangles , 'c5')
+        return_old = oldfu.generate_list_of_reference_angles_for_search(symangles, 'c5')
+
+
+        self.assertEqual(return_old, return_new)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- sphire/tests/test_morphology.py
+++ sphire/tests/test_morphology.py
@@ -24,8 +24,11 @@ ABSOLUTE_PATH = path.dirname(path.realpath(__file__))
 """
 change it when you run the tests with your path.In this folder I copied 'TcdA1-0010_frames.mrc' got from the sphire tutorial i.e.: 'SphireDemoResults/CorrectedSums/corrsum':
 """
-ABSOLUTE_PATH_TO_MRC_FOLDER= "/home/adnan/PycharmProjects/eman2/sphire/tests/corrsum/"
-ABSOLUTE_PATH_TO_STACK="bdb:/home/adnan/PycharmProjects/eman2/sphire/tests/Class2D/stack_ali2d"
+
+#ABSOLUTE_PATH_TO_MRC_FOLDER= "/home/adnan/PycharmProjects/eman2/sphire/tests/corrsum/"
+#ABSOLUTE_PATH_TO_STACK="bdb:/home/adnan/PycharmProjects/eman2/sphire/tests/Class2D/stack_ali2d"
+ABSOLUTE_PATH_TO_MRC_FOLDER= "/home/lusnig/Downloads/mrc_files_for_unit_test"
+ABSOLUTE_PATH_TO_STACK="bdb:/home/lusnig/Downloads/SphireDemoResults/Class2D/stack_ali2d"
 TOLERANCE = 0.0075
 
 IMAGE_2D, IMAGE_2D_REFERENCE = get_real_data(dim=2)
@@ -63,6 +66,10 @@ There are some opened issues in:
 """
 #todo: Furthemore in the 8,9 in all the cases where the output is 'None' the function saved the results in a huge output file. We'd check them. HOW????
 
+
+"""
+pickle files stored under smb://billy.storage.mpi-dortmund.mpg.de/abt3/group/agraunser/transfer/Adnan/pickle files
+"""
 class Test_binarize(unittest.TestCase):
 
     def test_NoneType_as_img_returns_AttributeError_NoneType_obj_hasnot_attribute_process(self):
@@ -2403,7 +2410,7 @@ class Test_cter_pap(unittest.TestCase):
     i_stop = -1
     selection_list = 'TcdA1-0011_frames_sum.mrc'
     input_image_path = path.join(ABSOLUTE_PATH_TO_MRC_FOLDER, "TcdA1-*_frames_sum.mrc")
-    output_directory = path.join(ABSOLUTE_PATH_TO_MRC_FOLDER, "cter_mrk/results")
+    output_directory = path.join(ABSOLUTE_PATH_TO_MRC_FOLDER, "cter_mrk_results")
 
     def test_all_the_conditions(self,return_new=None,return_old=None, skip=True):
         if skip is False:
@@ -4197,3 +4204,197 @@ class Test_ornq_vpp(unittest.TestCase):
         return_new = fu.ornq_vpp(image, crefim, xrng, yrng, step, mode, numr, cnx, cny, deltapsi = 0.0)
         return_old = oldfu.ornq_vpp(image, crefim, xrng, yrng, step, mode, numr, cnx, cny, deltapsi = 0.0)
         self.assertTrue(numpy.array_equal(return_new, return_old))
+
+
+
+
+
+
+
+
+""" Adnan helper functions to run the reference tests"""
+from ..libpy import sparx_utilities as ut
+import EMAN2_cppwrap as e2cpp
+""" 
+I commented the following lines of codeto avoid conflict when I run the other tests
+from ..libpy_py3 import sphire_filter as fu
+from .sparx_lib import sparx_filter as oldfu
+def get_data(num):
+    dim = 10
+    data_list = []
+    for i in range(num):
+        a = e2cpp.EMData(dim, dim)
+        data_a = a.get_3dview()
+        data_a[...] = numpy.arange(dim * dim, dtype=numpy.float32).reshape(dim, dim) + i
+        data_list.append(a)
+    return data_list
+"""
+
+def get_data_3d(num):
+    dim = 10
+    data_list = []
+    for i in range(num):
+        a = e2cpp.EMData(dim, dim,dim)
+        data_a = a.get_3dview()
+        data_a[...] = numpy.arange(dim * dim * dim, dtype=numpy.float32).reshape(dim, dim, dim) + i
+        data_list.append(a)
+
+    return data_list
+
+def get_data_gauss_noise():
+    dim = 10
+    return ut.model_gauss_noise(0.25 , dim,dim,dim)
+
+
+@unittest.skip("Adnan reference tests")
+class MyTestCase(unittest.TestCase):
+    def test_filt_tophatl_true_should_return_equal_object(self):
+        image, = get_data(1)
+        freq = 0.25
+        return_new = fu.filt_tophatl(image,freq)
+        return_old = oldfu.filt_tophatl(image,freq)
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
+
+    def test_filt_tophatb_true_should_return_equal_object(self):
+        image, = get_data(1)
+        freql = 0.25
+        freqh = 0.35
+        return_new = fu.filt_tophatb(image,freql, freqh)
+        return_old = oldfu.filt_tophatb(image,freql, freqh)
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
+
+    def test_filt_gaussl_true_should_return_equal_object(self):
+        image, = get_data(1)
+        sigma = 0.23
+        return_new = fu.filt_gaussl(image,sigma)
+        return_old = oldfu.filt_gaussl(image,sigma)
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
+
+
+    def test_filt_gaussinv_true_should_return_equal_object(self):
+        image, = get_data(1)
+        sigma = 0.23
+        return_new = fu.filt_gaussinv(image,sigma)
+        return_old = oldfu.filt_gaussinv(image,sigma)
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
+
+    def test_filt_gaussh_true_should_return_equal_object(self):
+        image, = get_data(1)
+        sigma = 0.23
+        return_new = fu.filt_gaussh(image,sigma)
+        return_old = oldfu.filt_gaussh(image,sigma)
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
+
+
+    def test_filt_btwl_true_should_return_equal_object(self):
+        image, = get_data(1)
+        freql = 0.25
+        freqh = 0.35
+        return_new = fu.filt_btwl(image,freql, freqh)
+        return_old = oldfu.filt_btwl(image,freql, freqh)
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
+
+
+    def test_filt_tanl_true_should_return_equal_object(self):
+        image, = get_data(1)
+        freql = 0.25
+        freqh = 0.35
+        return_new = fu.filt_tanl(image,freql, freqh)
+        return_old = oldfu.filt_tanl(image,freql, freqh)
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
+
+
+    def test_filt_table_true_should_return_equal_object(self):
+        image, = get_data(1)
+        table =[entry for entry in numpy.linspace(0, 0.5).tolist()]
+
+        return_new = fu.filt_table(image,table)
+        return_old = oldfu.filt_table(image,table)
+
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
+
+
+    def test_filt_ctf_true_should_return_equal_object(self):
+        image, = get_data(1)
+        defocus = 1
+        cs =  2
+        voltage = 300
+        pixel_size = 1.5
+        bfactor =  0
+        amp_contrast = 0.1
+        ctf = e2cpp.EMAN2Ctf()
+        ctf.from_dict({"defocus": defocus, "cs": cs, "voltage": voltage, "apix": pixel_size, "bfactor": bfactor,
+                       "ampcont": amp_contrast})
+
+        return_new = fu.filt_ctf(image,ctf)
+        return_old = oldfu.filt_ctf(image,ctf)
+
+        self.assertTrue(numpy.array_equal(return_new.get_3dview(), return_old.get_3dview()))
+
+
+    def test_fit_tanh_true_should_return_equal_object(self):
+        dres = []
+        dres.append((0.0,0.05,0,10,0.15,0.20))
+        dres.append((0,0.2,0.4,0.6,0.8,1.0))
+        dres.append((8,9,5,77,98,200))
+
+        return_new = fu.fit_tanh(dres)
+        return_old = oldfu.fit_tanh(dres)
+
+        self.assertTrue(return_new , return_old)
+
+
+    def test_fit_tanh1_true_should_return_equal_object(self):
+        dres = []
+        dres.append((0.0,0.05,0,10,0.15,0.20))
+        dres.append((0,0.2,0.4,0.6,0.8,1.0))
+        dres.append((8,9,5,77,98,200))
+
+        return_new = fu.fit_tanh1(dres)
+        return_old = oldfu.fit_tanh1(dres)
+
+        self.assertTrue(return_new , return_old)
+
+    def test_filt_vols_true_should_return_equal_object(self):
+        vols = []
+        vols.append(get_data_gauss_noise())
+        vols.append(get_data_gauss_noise())
+        vols.append(get_data_gauss_noise())
+
+        dres = []
+        dres.append([0.05, 0.05, 0.05, 0.10, 0.15, 0.20])
+        dres.append([0.05, 0.02, 0.04, 0.06, 0.08, 0.09])
+        dres.append([0.08, 0.09, 0.05, 0.07, 0.05, 0.01])
+        fscs = []
+        fscs.append(dres)
+        fscs.append(dres)
+        fscs.append(dres)
+
+        mask3D, = get_data_3d(1)
+
+        return_new = fu.filt_vols(vols, fscs, mask3D)
+        return_old = oldfu.filt_vols(vols, fscs, mask3D)
+
+        self.assertTrue(return_new, return_old)
+
+    def test_filterlocal_true_should_return_equal_object(self):
+        vols = []
+        vols.append(get_data_gauss_noise())
+        vols.append(get_data_gauss_noise())
+        vols.append(get_data_gauss_noise())
+        ui =  get_data_gauss_noise()     # or use ut.model_blank(1,1,1)
+        vi =  get_data_gauss_noise()
+        m  = "sphire/tests/3d_volume.txt"
+        falloff = 4
+        myid =   1
+        main_node = 0
+        number_of_proc = 6
+
+        return_new = fu.filterlocal(ui, vi, m, falloff, myid, main_node, number_of_proc)
+        return_old = oldfu.filterlocal(ui, vi, m, falloff, myid, main_node, number_of_proc)
+
+        self.assertTrue(return_new, return_old)
+
+
+if __name__ == '__main__':
+    unittest.main()
