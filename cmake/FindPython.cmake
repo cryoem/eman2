c73664960daf1095aa6a2657d28d48f00b684b05
shadow_walker <shadowwalkersb@gmail.com>
2019-06-24 02:04:20 -0500

    query sysconfig for Py_ENABLE_SHARED, link against libpython accordingly

--- cmake/FindPython.cmake
+++ cmake/FindPython.cmake
@@ -26,6 +26,14 @@ else()
 	message("Python site-packages: ${SP_DIR}")
 endif()
 
+execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import sysconfig; print(sysconfig.get_config_var('Py_ENABLE_SHARED'))"
+				OUTPUT_VARIABLE PYTHON_LIB_SHARED
+				OUTPUT_STRIP_TRAILING_WHITESPACE
+				)
+
+cmake_print_variables(PYTHON_LIB_SHARED)
+cmake_print_variables(CMAKE_CXX_COMPILER_ID)
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Python
 		REQUIRED_VARS PYTHON_EXECUTABLE PYTHON_LIBRARIES PYTHON_INCLUDE_DIR SP_DIR
@@ -36,6 +44,10 @@ if(Python_FOUND AND NOT TARGET Python::Python)
 	set_target_properties(Python::Python
 			PROPERTIES
 			INTERFACE_INCLUDE_DIRECTORIES ${PYTHON_INCLUDE_DIRS}
-			INTERFACE_LINK_LIBRARIES      ${PYTHON_LIBRARIES}
+			INTERFACE_LINK_LIBRARIES      $<$<OR:$<CXX_COMPILER_ID:MSVC>,$<BOOL:${PYTHON_LIB_SHARED}>>:${PYTHON_LIBRARIES}>
 			)
+	target_link_options(Python::Python INTERFACE
+						"$<$<AND:$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>,$<NOT:$<BOOL:${PYTHON_LIB_SHARED}>>>:-undefined;dynamic_lookup;-flat_namespace>"
+						"$<$<AND:$<CXX_COMPILER_ID:GNU>,$<NOT:$<BOOL:${PYTHON_LIB_SHARED}>>>:-undefined>"
+						)
 endif()
