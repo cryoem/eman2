5ff6f4a1f5776e9762cc655588e7ec079059f5ae
shadow_walker <shadowwalkersb@gmail.com>
2019-06-26 20:23:34 -0500

    pin conda and conda-build to prevent upgrades to broken conda and/or conda-build

--- ci_support/build_recipe.sh
+++ ci_support/build_recipe.sh
@@ -9,7 +9,7 @@ bash "${MYDIR}/../tests/future_import_tests.sh"
 if [ ! -z ${TRAVIS} ];then
     source ci_support/setup_conda.sh
 
-    conda install conda-build=3 -c defaults --yes
+    conda install conda=4.6.14 conda-build=3.17.8 -c defaults --yes
 fi
 
 if [ ! -z ${CIRCLECI} ];then
