ec4e8c31cbad49555176de7c97cd8005d56c071e
shadow_walker <shadowwalkersb@gmail.com>
2019-07-24 13:23:16 -0500

    redirect output to console and file, use tee

--- ci_support/post-install-actions.sh
+++ ci_support/post-install-actions.sh
@@ -6,16 +6,16 @@ source ${PREFIX}/bin/activate
 
 mkdir ${PREFIX}/install_logs
 
-conda info -a          > ${PREFIX}/install_logs/info_log.txt 2>&1
-conda list             > ${PREFIX}/install_logs/list_log.txt 2>&1
-conda list --explicit >> ${PREFIX}/install_logs/list_log.txt 2>&1
+conda info -a            | tee ${PREFIX}/install_logs/info_log.txt 2>&1
+conda list               | tee ${PREFIX}/install_logs/list_log.txt 2>&1
+conda list --explicit | tee -a ${PREFIX}/install_logs/list_log.txt 2>&1
 
 SP_DIR=$(python -c "import site; print(site.getsitepackages()[0])")
 
 if [ -d site-packages ]; then
     cp -av site-packages/* "${SP_DIR}"
     rm -rv site-packages
-fi > ${PREFIX}/install_logs/install_log.txt 2>&1
+fi | tee ${PREFIX}/install_logs/install_log.txt 2>&1
 
 conda install eman-deps=14.1 -c cryoem -c defaults -c conda-forge -y | tee -a ${PREFIX}/install_logs/install_log.txt 2>&1
 
