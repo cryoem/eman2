e07b8b75a384e85431794cb4356b15ff6cb8b4c3
shadow_walker <shadowwalkersb@gmail.com>
2019-08-30 22:26:58 -0500

    tweak file merge from prev commit

--- tests/test_binary_installation.sh
+++ tests/test_binary_installation.sh
@@ -29,9 +29,7 @@ conda list --explicit
 
 MYDIR="$(cd "$(dirname "$0")" && pwd -P)"
 
-MYDIR="$(cd "$(dirname "$0")" && pwd -P)"
-
-export SRC_DIR="$(cd "$(dirname "$0")"/.. && pwd -P)"
+export SRC_DIR="$(cd "${MYDIR}"/.. && pwd -P)"
 export PREFIX=${SRC_DIR}
 
 bash "${MYDIR}/run_tests.sh"
