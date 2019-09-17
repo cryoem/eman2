f0d9f14ca83cf0d6ed73fce5411b8922b9e7ccb8
Jesus <jgalaz@gmail.com>
2019-09-16 22:55:51 -0700

    .

--- programs/e2tomo_ctfraw.py
+++ programs/e2tomo_ctfraw.py
@@ -57,64 +57,178 @@ def main():
 		
 	parser = EMArgumentParser(usage=usage,version=EMANVERSION)
 	
-	parser.add_argument("--ampcont",type=float,default=0.05,help="""Default=0.05. Amplitude contrast to use for CTF correction phase flipping.""")
+	parser.add_argument("--ampcont",type=float, default=0.05,help="""Default=0.05. Amplitude contrast to use for CTF correction phase flipping.""")
 	
-	parser.add_argument("--apix",type=float,default=None,help="""Default=whatever is on the header of the images. Sampling of the images in angstroms/pixel.""")	
+	parser.add_argument("--apix",type=float, default=None,help="""Default=whatever is on the header of the images. Sampling of the images in angstroms/pixel.""")	
 	
-	parser.add_argument("--bfactor",type=int,default=1000,help="""Default=1000. Bfactor ("temperature factor") to use.""")
+	parser.add_argument("--bfactor",type=int, default=1000,help="""Default=1000. Bfactor ("temperature factor") to use.""")
 
-	parser.add_argument("--cs", type=float,default=2.7,help="""Default=2.7. Cs of the microscope with which the images were collected.""")
+	parser.add_argument("--cs", type=float, default=2.7,help="""Default=2.7. Cs of the microscope with which the images were collected.""")
 	
-	parser.add_argument("--defocus", type=float,default=None,help="""Default=None. Target defocus at the tilt axis. If not provided, a "global defocus" value will be estimated automatially.""")
+	parser.add_argument("--defocus", type=float, default=None,help="""Default=None. Target defocus at the tilt axis. If not provided, a "global defocus" value will be estimated automatially.""")
+	
+	parser.add_argument("--debug", action='store_true', default=False,help="""Default=False. Shows plots as they are being produced -requires user feedback to proceed.""")
 
 	parser.add_argument("--input", type=str, default=None, help="""Default=None. Single 2D image or image stack to calculate CTF for.""")
 				
-	parser.add_argument("--path",type=str,default='sptctf',help="""Directory to store results in. The default is a numbered series of directories containing the prefix 'sptctfraw'; for example, sptctf_02 will be the directory by default if 'sptctf_01' already exists.""")
+	parser.add_argument("--path",type=str, default='tomoctfraw',help="""Directory to store results in. The default is a numbered series of directories containing the prefix 'tomoctfraw'; for example, sptctf_02 will be the directory by default if 'sptctf_01' already exists.""")
+
+	parser.add_argument("--phaseplate", action="store_true", default=False, help="""Default=False. Supply this if the data were collected with hole-free phase plate.""")
+	
+	parser.add_argument("--plots", action='store_true', default=False,help="""Default=False. Turn this option on to generate plots of the background-subtracted 1D power spectrum overlaid with the best CTF fit. Running on a cluster or via ssh remotely might not support plotting.""")
 
 	parser.add_argument("--ppid", type=int, help="Set the PID of the parent process, used for cross platform PPID",default=-1)
 
+	parser.add_argument("--savefft", action="store_true", default=False, help="""Default=False. Saves the average of the ffts of the tiles for an image or its strips.""")
+
+	parser.add_argument("--tilesize",type=int, default=256,help="""Tile size to use for strips when --autofit is provided.""")
+		
+	parser.add_argument("--tiltangle", type=float, default=None, help="""Default=None. Single 2D image or image stack to calculate CTF for.""")
+
+	parser.add_argument("--tltfile", type=str, default=None, help="""File containing a list of tilt angles corresponding to the tilt angles of images 0 to n.""")
+	
 	parser.add_argument("--verbose", "-v", dest="verbose", action="store", metavar="n",type=int, default=0, help="verbose level [0-9], higner number means higher level of verboseness.")
 
-	parser.add_argument("--tilesize",type=int,default=256,help="""Tile size to use for strips when --autofit is provided.""")
-		
-	parser.add_argument("--tiltangle", type=str, default=None, help="""Default=None. Single 2D image or image stack to calculate CTF for.""")
-
-	parser.add_argument("--tltfile",default=None,type=str,help="""File containing a list of tilt angles corresponding to the tilt angles of images 0 to n.""")
-	
-	parser.add_argument("--voltage", type=int,default=300,help="""Default=300. Voltage of the microscope with which the images where collected.""")
+	parser.add_argument("--voltage", type=int, default=300,help="""Default=300. Voltage of the microscope with which the images where collected.""")
 	
 	(options, args) = parser.parse_args()		
 	
 
-	'''
-	Make sure input file and paramters are valid/workable
-	'''
+	#'''
+	#Make sure input file is EMAN2 readable
+	#'''
 	options=checkinput(options)
-	n=EMUtil.get_image_count()
-	check_healthy_input(options,n)
+	n=EMUtil.get_image_count(options.input)
+	hdr=EMData(options.input,0,True)
+	tomo=False
+	
+	#'''
+	#if --input has more than one slice in z, it is likely a tiltseries
+	#'''
+	nz=hdr['nz']	
+	if nz>n:
+		tomo=True
+		n=nz
+	
+	#'''
+	#Check whether paramters are valid, sensible, workable; if yes, load images and tiltangles into a numbered dictionary
+	#'''
+	check_healthy_params(options,n)
 	angles=get_angles(options,n)
 
+	imgs_unstacked = {}
+	if tomo:
+		options = makepath( options, 'tomoctfraw')
+		stem,extension = os.path.splitext(os.path.basename(options.input))
+		out = stem + "_unstacked.hdf"
 
-	'''
-	Log current run of the program ONLY if input parameters are healthy
-	'''
+		cmdunstack = "e2proc2d.py " + options.input + ' ' + out + " --unstacking"
+		imgs_unstacked_tmp = [ out.replace('.hdf', '-' + str(i+1).zfill(len(str(nz)))) + '.hdf' for i in range(nz) ]
+
+		runcmd(options,cmdunstack)
+		for img in imgs_unstacked_tmp:
+			out_final =  options.path + '/' + img.replace('unstacked-','unstacked_')
+			os.rename( img, out_final )
+			num = int(img.split('unstacked-')[-1].replace('.hdf','')) - 1 				#indexes should start from 0
+			imgs_unstacked.update({ num : out_final })
+
+	else:
+		imgs_unstacked.update({0:options.input})
+
+	#'''
+	#Log current run of the program ONLY if input parameters are healthy and images and angles have been loaded without issues
+	#'''
 	logger = E2init(sys.argv, options.ppid)
+	global_defoci = []
+	bad_indexes = []
 	
 	for i in range(n):
 		angle=angles[i]
-		img = EMData(options.input,i)
+		#img = EMData(options.input,i)
+		img_file = imgs_unstacked[i]
+		img = EMData(img_file,0)
+
+		apix = img['apix_x']
+		if options.apix:
+			apix = options.apix
+		else:
+			options.apix = apix
+
 		nx=img['nx']
 		ny=img['ny']
 		
+		#'''
+		#Fit the "global defocus" first, averaging tiles over the entire image, without caring about the defocus gradient direction or the tilt angle
+		#'''
 		defocusmin = 0.5
 		defocusmax = 9.0
 
-		global_defocus = get_global_defocus(options,img,defocusmin,defocusmax)
+		if options.defocus:
+			defocusmin = options.defocus - 1.5
+			defocusmax = options.defocus + 1.5
 
-	if options.defocus:
-		defocusmin = options.defocus - 1.5
-		defocusmax = options.defocus + 1.5
-		#ctfer(options,i,angle,n)	
+		try:
+			global_ctf = get_global_defocus(options,img_file,img,defocusmin,defocusmax)
+			global_defocus = global_ctf.defocus
+			global_defoci.append(global_defocus)
+			maxres = math.sqrt(global_ctf.bfactor/6.0)
+			print("\n(e2tomo_ctfraw)(main) for img {}/{}, global_defocus={}, maxres={}".format(i+1,n,global_defocus,maxres))
+
+		except:
+			print("\nWARNING: img=img_file has a poor fit; retrying")
+			if global_defoci:
+				try:
+					global_defocus_avg = numpy.mean(global_defoci)
+					global_defocus_std = numpy.std(global_defoci)
+					defocusmin = global_defocus_avg - 2*global_defocus_std
+					defocusmax = global_defocus_avg + 2*global_defocus_std
+					print("\nretrying fit with constrained defocusmin={}, defocusmax={}".format(defocusmin,defocusmax))
+					global_ctf = get_global_defocus(options,img_file,img,defocusmin,defocusmax)
+
+				except:
+					print("\nERROR: skipping img={} due to poor fit; I'll try ONE more time after going over all images".format(img_file))
+					bad_indexes.append(i)
+			else:
+				bad_indexes.append(i)
+				continue
+
+
+	for j in bad_indexes:
+		angle=angles[j]
+		#img = EMData(options.input,i)
+		img_file = imgs_unstacked[j]
+		img = EMData(img_file,0)
+
+		apix = img['apix_x']
+		if options.apix:
+			apix = options.apix
+		else:
+			options.apix = apix
+
+		nx=img['nx']
+		ny=img['ny']
+
+		global_defocus_avg = math.mean(global_defoci)
+		global_defocus_std = math.std(global_defoci)
+		defocusmin = global_defocus_avg - 1*global_defocus_std
+		defocusmax = global_defocus_avg + 1*global_defocus_std
+
+		try:
+			global_defocus_avg = math.mean(global_defoci)
+			global_defocus_std = math.std(global_defoci)
+			defocusmin = global_defocus_avg - 2*global_defocus_std
+			defocusmax = global_defocus_avg + 2*global_defocus_std
+			print("\nretrying fit with constrained defocusmin={}, defocusmax={}".format(defocusmin,defocusmax))
+			global_ctf = get_global_defocus(options,img_file,img,defocusmin,defocusmax)
+
+		except:
+			print("\nERROR: skipping img={} PERMANENTLY for global defocus fitting due to poor fit; I'll try ONE more time after going over all images".format(img_file))
+			#bad_indexes.append(i)
+
+		#except:
+		#	print("\nWARNING!!!\n!!!\n!!!: failed to fit CTF for input={}, index={}; the image is likely garbage".format(options.input,i))
+
+			#ctfer(options,i,angle,n)	
 
 	E2end(logger)
 	return
@@ -129,26 +243,43 @@ def main():
 #	return
 
 
-def get_global_defocus(options,img,defocusmin,defocusmax):
+def get_global_defocus(options,img_file,img,defocusmin,defocusmax):
 	#grid_coords=tile_grid(nx,ny,options.tilesize):
-	tiles = get_tiles(img,tilesize,True)
+	tiles = get_tiles(img,options.tilesize,True)
 	tiles_fft_avg = incoherent_sum_from_imglist(tiles,scale=False,checkcorners=True)
-	global_defocus = None
-	#global_defocus = fit_defocus(tiles_fft_avg, options.voltage, options.cs, options.ampcont, options.apix, options.defocus, defocusmin, defocusmax, 0.1)
 
-	return global_defocus
+	if options.savefft:
+		extension = os.path.splitext(os.path.basename( img_file ))[-1]
+		print("\n(get_global_defocus) extension={}".format(extension))
+		out_fft = img_file.replace(extension,'_fft_tile_avg.hdf')
+		print("\n(get_global_defocus) out_fft={}".format(out_fft))
+		if out_fft == img_file:
+			print("\nERROR: risk of overwritting input image")
+			sys.exit(1)
+		tiles_fft_avg.write_image(out_fft,0)
+	
+	tiles_fft_avg.process_inplace('filter.highpass.gauss',{"cutoff_pixels":3})
+	tiles_fft_avg.set_value_at(0,0,0.0)
+	
+	ctf = fit_defocus(options,img_file,tiles_fft_avg, options.voltage, options.cs, options.ampcont, options.phaseplate, options.apix, options.defocus, defocusmin, defocusmax, 0.1)
+
+	return ctf
 
 
-def get_angles( options ):
+def get_angles( options, n ):
 	
 	angles = {}
 	
 	if n >1:
 		with open( options.tltfile, 'r' ) as f:
-			lines = [ int(line.replace('\t','').replace('\n','')) for line in f.readlines()]
+			lines = [ float(line.replace('\t','').replace('\n','')) for line in f.readlines()]
 			angles = { i : lines[i] for i in range(0, len(lines) ) }
 
 		if angles:
+			if len(angles) != n:
+				print("\nERROR: the number of angles na={} does not match the number of images in the tiltseries nt={}".format(len(angles),n))
+				sys.exit(1)
+
 			if options.verbose > 9:
 				print("\n(e2tomo_ctfraw.py)(getangles) angles={}".format(angles) )
 			return angles
@@ -156,17 +287,17 @@ def get_angles( options ):
 			print("\nERROR: failed to read angles from --tltfile={}".format(options.tltfile))
 			sys.exit(1)
 	elif n==1:
-		angles.update({0:int(options.tiltangle)})
+		angles.update({0:options.tiltangle})
 		return angles
 
 	return
 
 
-def check_healthy_input(options,n):
-	if n==1 and not options.tiltangle:
-		print("\nERROR: for single images, --tiltangle is required")
+def check_healthy_params(options,n):
+	if n==1 and options.tiltangle == None:
+		print("\nERROR: for single images, --tiltangle is required; --tiltangle={}".format(options.tiltangle))
 		sys.exit(1)
-	elif not options.tltfile:
+	elif n>1 and not options.tltfile:
 		print("\nERROR: for a full tiltseries, --tltfile is required")
 		sys.exit(1)
 	elif n>1 and options.tltfile:
@@ -185,58 +316,138 @@ def check_healthy_input(options,n):
 					sys.exit(1)
 	return
 
-'''
-def fit_defocus(fft_avg,voltage,cs,ampcont,apix,target_defocus,defocusmin=0.5,defocusmax=9.0,defocusstep=0.1):
+
+def fit_defocus(options,img_file,fft_avg,voltage,cs,ampcont,phaseplate,apix,target_defocus,defocusmin=0.5,defocusmax=9.0,defocusstep=0.1):
 	"""Fit defocus from an incoherent average of tiles 
 	Author: Jesus Montoya, jgalaz@gmail.com, 09/2019
 	"""
 	tilesize = fft_avg['ny']
 	
-	fftbg = fft_avg.process("math.nonconvex")
-	fft1d = fft_avg.calc_radial_dist(old_div(fft_avg.get_ysize(),2),0.0,1.0,1)	# note that this handles the ri2inten averages properly
+	fft_bg = fft_avg.process("math.nonconvex")
+
+	fft_avg_1d = fft_avg.calc_radial_dist(old_div(fft_avg.get_ysize(),2),0.0,1.0,1)	# note that this handles the ri2inten averages properly
 	
+	#print("\n(e2tomo_ctfraw)(fit_defocus) fft_avg_1d={}".format(fft_avg_1d))
+
 	ctf = EMAN2Ctf()
-	try:
 
-		# Compute 1-D curve and background
-		ds = old_div(1.0,( apix * tilesize ))
-		bg_1d = e2ctf.low_bg_curve(fft1d,ds)
+	#print("\n(e2tomo_ctfraw)(fit_defocus) apix={}".format(apix))
+	#print("\n(e2tomo_ctfraw)(fit_defocus) tilesize={}".format(tilesize))
+
+	# Compute 1-D curve and background
+	ds = old_div(1.0,( apix * tilesize ))
+	#print("\n(e2tomo_ctfraw)(fit_defocus) ds={}".format(ds))
+	
+	bg_1d = e2ctf.low_bg_curve(fft_avg_1d,ds)
+	#print("\n(e2tomo_ctfraw)(fit_defocus) bg_1d={}".format(bg_1d))
+	
+	dfhint=( defocusmin, defocusmax, defocusstep )
+	ctf = e2ctf.ctf_fit( fft_avg_1d, bg_1d, bg_1d, fft_avg, fft_bg, float(voltage), float(cs), float(ampcont), phaseplate, float(apix), bgadj=1, autohp=True, dfhint=dfhint, tomo=True )		
+	#print("\n(e2tomo_ctfraw)(fit_defocus) ctf={}".format(ctf))
+
+	#bg_sub = numpy.array(fft_avg_1d) - numpy.array(bg_1d)
+
+	bg_sub,bg_1d = e2ctf.calc_1dfrom2d(ctf, fft_avg, fft_bg)
+
+	fit = ctf_get_fit_curve(ctf, ds, fft_avg_1d, bg_1d, bg_sub)
+	fit/=max(fit)
+
+	
+	#ctf = adjust_ctf_bg(ctf,fft_avg_1d)
+
+	#maxres,bfactor = ctf_fit_bfactor_and_maxres( list(bg_sub),ds,ctf)
+	#print("\ndefocus={}, FIRST trial".format(ctf.defocus))
+
+
+	ctf2 = EMAN2Ctf()
+	dfhint2=(max(dfhint[0],ctf.defocus-0.1),min(dfhint[1],ctf.defocus+0.1),min(old_div(dfhint[2],2.0),0.01))
+	ctf2 = e2ctf.ctf_fit( fft_avg_1d, bg_1d, bg_1d, fft_avg, fft_bg, float(voltage), float(cs), float(ampcont), phaseplate, float(apix), bgadj=1, autohp=True, dfhint=dfhint2 )		
+	bg_sub2,bg_1d2 = e2ctf.calc_1dfrom2d(ctf2, fft_avg, fft_bg)
+	
+	#bg_sub2,bg_1d2 = e2ctf.calc_1dfrom2d(ctf2, fft_avg, fft_bg)
+	fit2 = ctf_get_fit_curve(ctf2, ds, fft_avg_1d, bg_1d2, bg_sub2)
+	fit2/=max(fit2)
+
+	#maxres,bfactor = ctf_fit_bfactor_and_maxres( list(bg_sub2),ds,ctf)
+	#print("\nmaxres calc={}, bfactor={}, defocus={}, SECOND trial".format(maxres,ctf2.bfactor,ctf2.defocus))
+
+	bg_sub_final = numpy.array(bg_sub2) - numpy.array(bg_1d2)
+	bg_sub_final/=max(bg_sub_final)
+	import matplotlib.pyplot as plt
+	
+	if options.plots or options.debug:
+		r = len(ctf.background)
+		s = numpy.arange(0,ds*r,ds)
 		
-		#initial fit, background adjustment, refine fit, final background adjustment
-		#ctf = e2ctf.ctf_fit(fft1d,bg_1d,bg_1d,ffta,fftbg,options.voltage,options.cs,options.ampcont,apix,1,dfhint=( defocusmingradient, defocusmaxgradient, step ))
+		if options.debug:
+			plt.plot(s, fit2, 'k')
+			plt.plot(s, bg_sub_final, 'b')
+			plt.show()
 
-		ctf = e2ctf.ctf_fit( fft1d, bg_1d, bg_1d, ffta, fftbg, options.voltage, options.cs, options.ampcont, apix, 1, dfhint=( defocusmin, defocusmax, defocusstep ) )
-		ctf = bgAdj(ctf,fft1d)
-		return ctf.defocus
-	except:
-		print("ctf fit failed! first try")
-		print("len fft1d is", len(fft1d))
-		print("ffta is", ffta)
-		ctf = EMAN2Ctf()
+		if options.plots:
+			extension = os.path.splitext(os.path.basename( img_file ))[-1]
+
+			print("\n(fit_defocus) should save plots")
+			ps_file = img_file.replace(extension,'_1d_ps.txt')
+			if ps_file == img_file:
+				print("\nERROR: risk of overwritting input image")
+				sys.exit(1)
+			write_txt(s,bg_sub_final,ps_file)
+			#with open(options.path + '/' + pfile,'w') as f:
+			#	lines = [ str(s[i])+'\t'+str(bg_sub_final[i])+'\n' for i in range(len(s-1))]
+			#	lines.append( str(s[-1]) + '\t' + str(bg_sub_final[-1]) )	#the last element does not need a 'return' or 'line break'
 			
-		try:
-			#ctf = e2ctf.ctf_fit(fft1d,ctf.background,ctf.background,ffta,fftbg,options.voltage,options.cs,options.ampcont,apix,1,dfhint=( defocusmingradient, defocusmaxgradient, step))
-			ctf = e2ctf.ctf_fit(fft1d,ctf.background,ctf.background,ffta,fftbg,options.voltage,options.cs,options.ampcont,apix,1,dfhint=( defocusmin, defocusmax, defocusstep ))	
-			ctf = bgAdj(ctf,fft1d)
+			fit_file = img_file.replace(extension,'_1d_fit.txt')
+			if fit_file == img_file:
+				print("\nERROR: risk of overwritting input image")
+				sys.exit(1)
+			#with open(options.path + '/' + pfile,'w') as f:
+			#	lines = [ str(s[i])+'\t'+str(bg_sub_final[i])+'\n' for i in range(len(s-1))]
+			#	lines.append( str(s[-1]) + '\t' + str(bg_sub_final[-1]) )	#the last element does not need a 'return' or 'line break'
+			write_txt(s,fit2,fit_file)
 
-			#if options.astigmatism: 
-			#	e2ctf.ctf_fit_stig(ffta,fftbg,ctf)
-		except:
-			print("ctf fit failed! second adjustment try")
-			print("len fft1d is", len(fft1d))
-			print("ffta is", ffta)
-			return None
+	return ctf
 
-	return 
+
+def write_txt(xdata,ydata,filename):
+	print("\n(write_txt) writing plot txt file for f={}".format(filename))
+	with open(filename,'w') as f:
+		lines = [ str(xdata[i])+'\t'+str(ydata[i])+'\n' for i in range(len(xdata-1)) ]
+		lines.append( str(xdata[-1]) + '\t' + str(ydata[-1]) )	#the last element does not need a 'return' or 'line break'
+		f.writelines(lines)
+
+	return
+
+
+
+def ctf_get_fit_curve(ctf, ds, fft_avg_1d, bg_1d, bg_sub):
+	"""From e2ctf.py"""
+	r = len(ctf.background)
+	s = numpy.arange(0,ds*r,ds)
+	
+	fit = numpy.array(ctf.compute_1d(len(s)*2,ds,Ctf.CtfType.CTF_AMP))		# The fit curve
+	fit = fit*fit			# squared
+
+	# auto-amplitude for b-factor adjustment
+	rto,nrto=0,0
+	for i in range(int(old_div(.04,ds))+1,min(int(old_div(0.15,ds)),len(s)-1)):
+		if bg_sub[i]>0 :
+			rto+=fit[i]
+			nrto+=fabs(bg_sub[i])
+	if nrto==0 : rto=1.0
+	else : rto/=nrto
+	fit=[old_div(fit[i],rto) for i in range(len(s))]
+
+	return fit
+
+
 '''
-
 def adjust_ctf_bg(ctf,fg_1d):
 	"""Smooths the background based on the values of the foreground near the CTF zeroes and puts the
 	smoothed background into the CTF object. From e2ctf.py"""
 	
 	ds=ctf.dsbg
 	#print "\n (bgAdj) ds is", ds
-	ctf=ctf
 	#print "\n (bgAdj) ctf is", ctf
 	bg_1d=list(ctf.background)
 	#print "\n (bgAdj) bg_1d is", bg_1d
@@ -279,6 +490,8 @@ def adjust_ctf_bg(ctf,fg_1d):
 		
 		#bs2=[fg_1d[i]-ctf.background[i] for i in xrange(fz)]
 	return ctf
+'''
+
 
 if '__main__' == __name__:
 	main()
