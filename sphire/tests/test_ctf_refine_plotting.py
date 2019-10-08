cdea8954ac3855c6a9208925682856c376e85adf
luca.lusnig@mpi-dortmund.mpg.de <luca.lusnig@mpi-dortmund.mpg.de>
2019-10-08 13:02:10 +0200

    added structure of the tests for the new files

---
+++ sphire/tests/test_ctf_refine_plotting.py
@@ -0,0 +1,15 @@
+import unittest
+
+from sphire.libpy import sp_ctf_refine_plotting as oldfu
+#from sphire.libpy_py3 import sp_ctf_refine_plotting as fu
+
+
+class Test_create_particles_plot_map(unittest.TestCase):
+    def test_create_particles_plot_map(self):
+        axis = oldfu.create_particles_plot_map(stack_file_path="",indices="",values=None, vmin=None, vmax=None, colmap="viridis", title=None, colorlabel=None, plotpos=())
+        pass
+
+class Test_create_and_save_particle_plots(unittest.TestCase):
+    def test_create_and_save_particle_plots(self):
+        oldfu.create_and_save_particle_plots(path_output_img="", stack_file_path="", refinement_results_per_micrograph="",min_max_error="", min_max_ratio="")
+        pass
