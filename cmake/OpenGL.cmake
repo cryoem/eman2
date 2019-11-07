3c6c484399ec94b5f44ec53b7cd920bdf47402dd
shadow_walker <shadowwalkersb@gmail.com>
2019-11-07 11:55:35 -0600

    debug print opengl target

--- cmake/OpenGL.cmake
+++ cmake/OpenGL.cmake
@@ -22,10 +22,24 @@ endif()
 
 cmake_print_variables(OPENGL_INCLUDE_DIR)
 cmake_print_variables(OPENGL_LIBRARIES)
+cmake_print_variables(OPENGL_glu_LIBRARY)
+cmake_print_variables(OPENGL_opengl_LIBRARY)
+cmake_print_variables(OPENGL_gl_LIBRARY)
+cmake_print_variables(OPENGL_FOUND)
+cmake_print_variables(OPENGL_GLU_FOUND)
+cmake_print_variables(OpenGL_OpenGL_FOUND)
+cmake_print_variables(OPENGL_opengl_LIBRARY)
+cmake_print_variables(OPENGL_gl_LIBRARY)
 
 if(OpenGL_FOUND AND NOT TARGET OpenGL AND NOT TARGET EMAN::OpenGL)
 	add_library(OpenGL INTERFACE)
 	add_library(EMAN::OpenGL ALIAS OpenGL)
+
+	is_target_exist(OpenGL)
+	is_target_exist(OpenGL::OpenGL)
+	is_target_exist(OpenGL::GL)
+	is_target_exist(OpenGL::GLU)
+
 	set_target_properties(OpenGL PROPERTIES
 						  INTERFACE_COMPILE_DEFINITIONS USE_OPENGL
 						  )
