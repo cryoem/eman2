722f371b54717954439fd78586ab9c3b1acceb69
sludtke42 <sludtke42@gmail.com>
2020-02-24 15:38:06 -0600

    trivial

--- programs/e2refinemulti.py
+++ programs/e2refinemulti.py
@@ -680,12 +680,12 @@ Based on your requested resolution and box-size, modified by --speed, I will use
 			if ca<cb :
 				try: ali=a["xform.align3d"]
 				except: ali=Transform()
-				if verbose>0 : print("correct hand detected ",ali)
+				if options.verbose>0 : print("correct hand detected ",ali)
 			else :
 				try: ali=b["xform.align3d"]
 				except: ali=Transform()
 				o.process_inplace("xform.flip",{"axis":"z"})
-				if verbose>0 : print("handedness flip required",ali)
+				if options.verbose>0 : print("handedness flip required",ali)
 			o.transform(ali)
 
 			os.unlink(map2)
