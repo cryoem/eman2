9c5ac6734d9e227e1dddba35885215ea9e434ddb
shadow_walker <shadowwalkersb@gmail.com>
2020-06-11 16:03:31 -0500

    initialize member in initialization list

--- libEM/tifio.cpp
+++ libEM/tifio.cpp
@@ -51,11 +51,9 @@ namespace {
 TiffIO::TiffIO(string tiff_filename, IOMode rw)
 :	filename(tiff_filename), rw_mode(rw), tiff_file(0),
 	bitspersample(0), photometric(0), initialized(false),
-	rendermin(0.0), rendermax(0.0), renderbits(16), nimg(1)
-{
-	is_big_endian = ByteOrder::is_host_big_endian();
-}
-
+	rendermin(0.0), rendermax(0.0), renderbits(16), nimg(1),
+	is_big_endian(ByteOrder::is_host_big_endian())
+{}
 
 TiffIO::~TiffIO()
 {
