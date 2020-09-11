2199602873df228938ef60f8b80db505336f3734
sludtke42 <sludtke42@gmail.com>
2020-09-10 21:37:34 -0500

    More work on goldstandard

--- programs/e2spt_refine.py
+++ programs/e2spt_refine.py
@@ -138,22 +138,26 @@ def main():
 				run("e2proc3d.py {} {}/model_input.hdf --process mask.soft:outer_radius=-1 --first {} --last {}".format(ref, options.path, refn,refn))
 		ref="{}/model_input.hdf".format(options.path)
 		
+	if (len(options.maskalign)>0):
+		maskalign=EMData(options.maskalign,0)
 	
 	for itr in range(startitr,options.niter+startitr):
 
 		# the alignment ref may be masked using a different file, or just copied
 		if options.goldstandard>0 or options.goldcontinue :
-			refe=ref[:-4]+"_even.hdf"
-			refo=ref[:-4]+"_odd.hdf"
+			if itr==1: refe,refo=ref,ref
+			else:
+				refe=ref[:-4]+"_even.hdf"
+				refo=ref[:-4]+"_odd.hdf"
+				
 			ar=EMData(refe,0)
-			if (len(options.maskalign)>0):
-				m=EMData(options.maskalign,0)
-				ar.mult(m)
+			if itr==1 : ar.process_inplace("filter.lowpass.randomphase",{"cutoff_freq":1.0/options.goldstandard})
+			if (len(options.maskalign)>0): ar.mult(maskalign)
 			ar.write_image(f"{options.path}/alignref_even.hdf",0)
+			if itr==0 : 
 			ar=EMData(refo,0)
-			if (len(options.maskalign)>0):
-				m=EMData(options.maskalign,0)
-				ar.mult(m)
+			if itr==1 : ar.process_inplace("filter.lowpass.randomphase",{"cutoff_freq":1.0/options.goldstandard})
+			if (len(options.maskalign)>0): ar.mult(maskalign)
 			ar.write_image(f"{options.path}/alignref_odd.hdf",0)
 		else:
 			ar=EMData(ref,0)
