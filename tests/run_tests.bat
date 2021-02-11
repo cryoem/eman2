31f96512ec35196fb0f92a24650168b594ee01cc
shadow_walker <shadowwalkersb@gmail.com>
2021-02-11 17:58:06 -0600

    remove test: Util.version()

--- tests/run_tests.bat
+++ tests/run_tests.bat
@@ -3,7 +3,6 @@ conda.exe list
 :: 1. Run e2version.py and e2speedtest.py
 e2version.py
 e2speedtest.py
-python -c "from EMAN2 import *; Util.version()"
 
 :: 2. Test imports
 python tests\test_imports.py"
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,7 +9,6 @@ MYDIR="$(cd "$(dirname "$0")"; pwd -P)"
 # 1. Run e2version.py and e2speedtest.py
 e2version.py
 e2speedtest.py
-time python -c "from EMAN2 import *; Util.version()"
 
 # 2. Test git-hash consistency
 if [ ${CONDA_BUILD:-0} -ne 1 ];then
