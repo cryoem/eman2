5fd59e5d8a8261b7f68855c6ee5f84c4088247af
muyuanc <g5v991x@gmail.com>
2021-08-12 17:23:00 -0500

    bug fix. now do classification too

--- programs/e2spt_sgd_new.py
+++ programs/e2spt_sgd_new.py
@@ -2,6 +2,7 @@
 # Muyuan Chen 2021-08
 from EMAN2 import *
 import numpy as np
+from e2spt_refine_new import gather_metadata
 
 def main():
 	
@@ -12,18 +13,24 @@ def main():
 	parser.add_argument("--parallel", type=str,help="parallel", default="thread:12")
 	parser.add_argument("--shrink", type=int,help="shrink", default=1)
 	parser.add_argument("--batch", type=int,help="batch size", default=12)
+	parser.add_argument("--ncls", type=int,help="number of class", default=1)
+	parser.add_argument("--keep", type=float,help="keep fraction of good particles. will actually align more particles and use the number of particles specified by batch", default=.7)
 	parser.add_argument("--learnrate", type=float,help="learning rate", default=.2)
 	parser.add_argument("--res", type=float,help="resolution", default=50)
 	parser.add_argument("--ref", type=str,help="reference", default=None)
+	parser.add_argument("--classify",action="store_true",help="classify particles to the best class. there is the risk that some classes may end up with no particle. by default each class will include the best batch particles, and different classes can overlap.",default=False)
+
 	#parser.add_argument("--sym", type=str,help="sym", default='c1')
 	
 	(options, args) = parser.parse_args()
 	logid=E2init(sys.argv)
 	ptcls=args[0]
-	#ref=args[1]
-			
-	sys.path.insert(0, "/home/muyuanc/eman2source/eman2/programs/")
-	from e2spt_refine_new import gather_metadata
+	
+	res=options.res
+	lr=options.learnrate
+	shrink=options.shrink
+	batch=options.batch
+	ncls=options.ncls	
 	
 	if options.path==None: options.path=num_path_new("sptsgd_")
 	path=options.path
@@ -34,76 +41,43 @@ def main():
 	save_lst_params(info3d, info3dname)
 	save_lst_params(info2d, info2dname)
 
-	res=options.res
-	lr=options.learnrate
-	shrink=options.shrink
-	batch=options.batch
 	
 	e=EMData(info3dname,0)
 	if shrink>1:
 		e.process_inplace("math.meanshrink",{"n":shrink})
-	hdr=e.get_attr_dict()
-	sz=e["nx"]#//shrink
-	pad=EMData(info2dname,0,True)["nx"]//shrink
-	
-	fname=f"{path}/output_all.hdf"
-	if os.path.isfile(fname):
-		os.remove(fname)
-		
-	def make_3d(ali2d):
-		normvol=EMData(pad//2+1, pad, pad)
-		recon=Reconstructors.get("fourier", {"sym":'c1',"size":[pad,pad,pad], "mode":"trilinear", "normout":normvol})
-		recon.setup()
-		for a in ali2d:
-			e=EMData(a["src"],a["idx"])
-			xf=Transform(a["xform.projection"])
-			
-			if shrink>1:
-				e.process_inplace("math.meanshrink",{"n":shrink})
-				xf.set_trans(-xf.get_trans()/shrink)
-			
-			ep=recon.preprocess_slice(e, xf)
-			recon.insert_slice(ep,xf,1)
-
-		threed=recon.finish(False)
-
-		return normvol, threed
-
-	def post_process(threed):
-		avg=threed.do_ift()
-		avg.depad()
-		avg.process_inplace("xform.phaseorigin.tocenter")
-		avg=avg.get_clip(Region(pad//2-sz//2,pad//2-sz//2,pad//2-sz//2, sz,sz,sz), fill=0)
-		avg.set_attr_dict(hdr)
-		avg.process_inplace("filter.lowpass.gauss",{"cutoff_freq":1./res})
-		avg.process_inplace("filter.highpass.gauss",{"cutoff_pixels":2})
-		
-		avg.process_inplace("math.fft.resample",{"n":1/shrink})
-		avg.process_inplace("normalize.edgemean")
-		avg.process_inplace("mask.soft",{"outer_radius":-10,"width":10})
-		return avg
+	options.hdr=hdr=e.get_attr_dict()
+	options.sz=sz=e["nx"]#//shrink
+	options.pad=pad=EMData(info2dname,0,True)["nx"]//shrink
 
 	npt=len(info3d)
 	
+	fnames=[f"{path}/output_all_cls{ic}.hdf" for ic in range(ncls)]
+	for fname in fnames:
+		if os.path.isfile(fname):
+			os.remove(fname)
+	
+	thrd0s=[]
 	if options.ref==None:
-		idx=np.arange(npt)
-		np.random.shuffle(idx)
-		idx=np.sort(idx[:batch])
-		tt=parsesym("c1")
-		xfs=tt.gen_orientations("rand",{"n":batch,"phitoo":True})
-		ali2d=[]
-		for ii,xf in zip(idx,xfs):
-			i2d=info3d[ii]["idx2d"]
-			i2d=[info2d[i] for i in i2d]
-			for i in i2d:
-				d={"src":i["src"],"idx":i["idx"]}
-				d["xform.projection"]=i["xform.projection"]*xf
-				ali2d.append(d)
-			
-		norm0, thrd0=make_3d(ali2d)
-		avg0=post_process(thrd0)
-		avg0.write_image(f"{path}/output.hdf")
-		avg0.write_image(fname, -1)
+		for ic in range(ncls):
+			idx=np.arange(npt)
+			np.random.shuffle(idx)
+			idx=np.sort(idx[:batch])
+			tt=parsesym("c1")
+			xfs=tt.gen_orientations("rand",{"n":batch,"phitoo":True})
+			ali2d=[]
+			for ii,xf in zip(idx,xfs):
+				i2d=info3d[ii]["idx2d"]
+				i2d=[info2d[i] for i in i2d]
+				for i in i2d:
+					d={"src":i["src"],"idx":i["idx"]}
+					d["xform.projection"]=i["xform.projection"]*xf
+					ali2d.append(d)
+				
+			thrd0=make_3d(ali2d, options)
+			avg0=post_process(thrd0, options)
+			avg0.write_image(f"{path}/output_cls{ic}.hdf")
+			avg0.write_compressed(fnames[ic], -1, 12, nooutliers=True)
+			thrd0s.append(thrd0)
 		
 	else:
 		idx=np.arange(npt)
@@ -114,37 +88,105 @@ def main():
 		
 		launch_childprocess(f"e2spt_align_subtlt.py {path}/particle_info_3d.lst {options.ref} --path {path} --maxres {res} --parallel {options.parallel} --fromscratch --iter 0")
 		ali2d=load_lst_params(f"{path}/aliptcls2d_00.lst")
-		norm0, thrd0=make_3d(ali2d)
-		avg0=post_process(thrd0)
-		avg0.write_image(f"{path}/output.hdf")
-		avg0.write_image(fname, -1)
+		thrd0=make_3d(ali2d, options)
+		thrd0s.append(thrd0)
+		avg0=post_process(thrd0, options)
+		avg0.write_image(f"{path}/output_cls0.hdf")
+		avg0.write_compressed(fnames[0], -1, 12, nooutliers=True)
 	
 	for itr in range(options.niter):
-		print(itr)
+		#print(itr)
 		idx=np.arange(npt)
 		np.random.shuffle(idx)
-		idx=np.sort(idx[:batch])
+		nptcl=int(batch*ncls/options.keep)
+		idx=np.sort(idx[:nptcl])
 		ali3d=[info3d[i] for i in idx]
 		save_lst_params(ali3d, info3dname)
 		
-		launch_childprocess(f"e2spt_align_subtlt.py {path}/particle_info_3d.lst {path}/output.hdf --path {path} --maxres {res} --parallel {options.parallel} --fromscratch --iter 0")
-		ali2d=load_lst_params(f"{path}/aliptcls2d_00.lst")
+		a3dout=[]
+		a2dout=[]
+		for ic in range(ncls):
+			print(f"iter {itr}, class {ic}: ")
+			launch_childprocess(f"e2spt_align_subtlt.py {path}/particle_info_3d.lst {path}/output_cls{ic}.hdf --path {path} --maxres {res} --parallel {options.parallel} --fromscratch --iter 0")
+			
+			a3dout.append(load_lst_params(f"{path}/aliptcls3d_00.lst"))
+			a2dout.append(load_lst_params(f"{path}/aliptcls2d_00.lst"))
 		
 		
-		norm1, thrd1=make_3d(ali2d)
-		#avg1=post_process(thrd1)
-		
-		#avg1.write_image(f"{path}/output1.hdf")
-		
-		out=thrd0*(1-lr)+thrd1*lr
-		avg=post_process(out)
-		avg.write_image(f"{path}/output.hdf")
-		avg.write_image(fname, -1)
+		score=[]
+		for ali in a3dout:
+			scr=[a["score"] for a in ali]
+			score.append(scr)
+		score=np.array(score)
+		np.savetxt(f"{path}/score.txt", score.T)
+		clsid=np.argmin(score, axis=0)
+		for ic in range(ncls):
+			if options.classify:
+				scrid=np.where(clsid==ic)[0]
+				print("  class {} - {} particles".format(ic, np.sum(clsid==ic)))
+			else:
+				scr=score[ic].copy()
+				scrid=np.argsort(scr)[:batch]
+			ali2d=[]
+			for a in a2dout[ic]:
+				if a["ptcl3d_id"] in scrid:
+					ali2d.append(a)
+					
+			#print(len(ali2d))
+									
+			thrd1=make_3d(ali2d, options)
+			thrd0=thrd0s[ic]
+			
+			out=thrd0*(1-lr)+thrd1*lr
+			avg=post_process(out, options)
+			avg.write_image(f"{path}/output_cls{ic}.hdf")
+			avg.write_compressed(fnames[ic], -1, 12, nooutliers=True)
 
-		thrd0=out.copy()
-		
+			thrd0s[ic]=out.copy()
+			
 	E2end(logid)
 	
+
+def make_3d(ali2d, options):
+	#normvol=EMData(pad//2+1, pad, pad)
+	pad=options.pad
+	recon=Reconstructors.get("fourier", {"sym":'c1',"size":[pad,pad,pad], "mode":"trilinear"})
+	recon.setup()
+	for a in ali2d:
+		e=EMData(a["src"],a["idx"])
+		xf=Transform(a["xform.projection"])
+		xf.set_trans(-xf.get_trans())
+		
+		if options.shrink>1:
+			e.process_inplace("math.meanshrink",{"n":options.shrink})
+			xf.set_trans(xf.get_trans()/options.shrink)
+		
+		ep=recon.preprocess_slice(e, xf)
+		recon.insert_slice(ep,xf,1)
+
+	threed=recon.finish(False)
+
+	return threed
+
+
+def post_process(threed, options):
+	pad=options.pad
+	sz=options.sz
+	
+	avg=threed.do_ift()
+	avg.depad()
+	avg.process_inplace("xform.phaseorigin.tocenter")
+	avg=avg.get_clip(Region(pad//2-sz//2,pad//2-sz//2,pad//2-sz//2, sz,sz,sz), fill=0)
+	avg.set_attr_dict(options.hdr)
+	avg.process_inplace("filter.lowpass.gauss",{"cutoff_freq":1./options.res})
+	avg.process_inplace("filter.highpass.gauss",{"cutoff_pixels":2})
+	
+	if options.shrink>1:
+		avg.process_inplace("math.fft.resample",{"n":1/options.shrink})
+	avg.process_inplace("normalize.edgemean")
+	avg.process_inplace("mask.soft",{"outer_radius":-10,"width":10})
+	
+	return avg
 	
 	
 if __name__ == '__main__':
