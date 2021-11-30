a6c5137946c45fb005a76ddb2b1aad290716acf7
muyuanc <g5v991x@gmail.com>
2021-11-30 12:06:49 -0600

    deal with empty particles

--- programs/e2spt_extract.py
+++ programs/e2spt_extract.py
@@ -539,7 +539,14 @@ as well as alignment parameters, so use of tomograms from other software is not 
 					pji+=1
 					
 				threed["class_ptcl_src"]=options.output2d
-				threed["class_ptcl_idxs"]=pjids
+				if len(pjids)>1:
+					threed["class_ptcl_idxs"]=pjids
+				elif pid>=0:
+					print("Empty particle exist. Consider use the --append option. Exit.")
+					exit()
+				else:
+					print("Empty particle detected. Skipping particle.")
+					continue
 
 				if options.compressbits<0: threed.write_image(options.output, pid)
 				else: threed.write_compressed(options.output, pid,options.compressbits,nooutliers=True)
