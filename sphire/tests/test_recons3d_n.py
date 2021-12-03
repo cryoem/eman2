d191afedd37057012b44c7fe68f562b7656a4d69
lusnig <luca.lusnig@mpi-dortmund.mpg.de>
2021-12-03 10:35:04 +0100

    test_recons3d_n: uncommented and fix tests BUT it still failing when run it togheter with the oter tests

--- sphire/tests/test_recons3d_n.py
+++ sphire/tests/test_recons3d_n.py
@@ -1,21 +1,13 @@
 from __future__ import print_function
 from __future__ import division
 
-
-
-
 from bin_py3 import sp_recons3d_n as oldfu
 from sphire.bin import sp_recons3d_n as fu
+from sphire.libpy import sp_global_def
 
 from os import path
-### Comment from Adnan......this file has to be modified because now we want to run everything on CI gitlab
-ABSOLUTE_PATH_TO_RESOURCES = "resources_tests/pickles/"
-ABSOLUTE_PATH = path.dirname(path.realpath(__file__))
 
-#Absolute path to the bin folders for compatibility tests purpose.  since in the bin folder we have to set the parameter via cmd I have to run them.
-ABSOLUTE_SPHIRE_PATH= ABSOLUTE_PATH.split("/tests")[0]
-ABSOLUTE_OLDBIN_PATH = path.join(ABSOLUTE_SPHIRE_PATH, "bin_py3")
-ABSOLUTE_BIN_PATH = path.join(ABSOLUTE_SPHIRE_PATH,"bin")
+from tests.test_module import ABSOLUTE_BIN_PATH,ABSOLUTE_OLDBIN_PATH
 import unittest
 
 try:
@@ -30,6 +22,7 @@ except ImportError:
     # python3 case. You will get an error because 'sys.stdout.write(msg)' presents in the library not in the test!!
     from io import StringIO
 import sys
+
 
 #todo: why the new version does not work????
 """
@@ -49,41 +42,45 @@ The errors happen in the "sphire.bin" folder not in  "bin_py3"
         [rtxr2:124665] Local abort after MPI_FINALIZE started completed successfully, but am not able to aggregate error messages, and not able to guarantee that all other processes were killed!
         No data to report.
 """
-
+# in the new version the error outputs have changed slightly
+@unittest.skip("MPI error message ONLY in the sphire.bin version")
 class Test_Error_cases(unittest.TestCase):
-    @unittest.skip("MPI error message ONLY in the sphire.bin version")
     def test_too_few_input_values(self):
         testargs_new = [path.join(ABSOLUTE_BIN_PATH, "sp_recons3d_n.py")]
         testargs_old = [path.join(ABSOLUTE_OLDBIN_PATH, "sp_recons3d_n.py")]
         with patch.object(sys, 'argv', testargs_new):
+            sp_global_def.BATCH = False
             old_stdout = sys.stdout
             print_new = StringIO()
             sys.stdout = print_new
             fu.main()
         with patch.object(sys, 'argv', testargs_old):
+            sp_global_def.BATCH = False
             print_old = StringIO()
             sys.stdout = print_old
             oldfu.main()
         sys.stdout = old_stdout
-        self.assertEqual(print_new.getvalue().split('\n')[1].split("ERROR")[1],' => Incomplete list of arguments')
-        self.assertEqual(print_new.getvalue().split('\n')[1].split("ERROR")[1],print_old.getvalue().split('\n')[1].split("ERROR")[1])
+        self.assertEqual(print_new.getvalue().split('\n')[4].split("ERROR")[1],' => Incomplete list of arguments')
+        self.assertEqual(print_new.getvalue().split('\n')[4].split("ERROR")[1],print_old.getvalue().split('\n')[1].split("ERROR")[1])
 
     @unittest.skip("MPI error message ONLY in the sphire.bin version")
     def test_error_group_and_list_together(self):
         testargs_new = [path.join(ABSOLUTE_BIN_PATH, "sp_recons3d_n.py"), "input_stack","out_volume", "--list='list'", '1','11','1', "--group=1"]
         testargs_old = [path.join(ABSOLUTE_OLDBIN_PATH, "sp_recons3d_n.py"), "input_stack","out_volume", "--list='list'", '1','11','1', "--group=1"]
         with patch.object(sys, 'argv', testargs_new):
+            sp_global_def.BATCH = False
             old_stdout = sys.stdout
             print_new = StringIO()
             sys.stdout = print_new
             fu.main()
         with patch.object(sys, 'argv', testargs_old):
+            sp_global_def.BATCH = False
             print_old = StringIO()
             sys.stdout = print_old
             oldfu.main()
         sys.stdout = old_stdout
-        self.assertEqual(print_new.getvalue().split('\n')[1].split("ERROR")[1],' => options group and list cannot be used together')
-        self.assertEqual(print_new.getvalue().split('\n')[1].split("ERROR")[1],print_old.getvalue().split('\n')[1].split("ERROR")[1])
+        self.assertEqual(print_new.getvalue().split('\n')[4].split("ERROR")[1],' => options group and list cannot be used together')
+        self.assertEqual(print_new.getvalue().split('\n')[4].split("ERROR")[1],print_old.getvalue().split('\n')[1].split("ERROR")[1])
 
     @unittest.skip("segmentatio fault in sphire.bin version ... but in local it works")
     def test_error_interpolation_method(self):
