50d78eec1a53f7caae75cce55e4cd79c06fd212a
shadow_walker <shadowwalkersb@gmail.com>
2022-05-10 21:02:47 -0500

    build and test on github_actions: linux and mac

---
+++ ci_support/build_steps.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# -*- mode: jinja-shell -*-
+
+set -xeuo pipefail
+export REPO_ROOT="/home/conda/repo_root"
+
+
+export PYTHONUNBUFFERED=1
+
+cat >~/.condarc <<CONDARC
+
+conda-build:
+ root-dir: ${REPO_ROOT}/../build_artifacts
+
+CONDARC
+
+
+mamba install --update-specs --yes --quiet --channel conda-forge \
+    conda-build pip boa conda-forge-ci-setup=3
+mamba update --update-specs --yes --quiet --channel conda-forge \
+    conda-build pip boa conda-forge-ci-setup=3
+
+/usr/bin/sudo -n yum install -y mesa-libGL
+
+
+set +e
+conda mambabuild "${REPO_ROOT}/recipe" -c cryoem -c conda-forge
