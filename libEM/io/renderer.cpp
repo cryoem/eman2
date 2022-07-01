e75dd1efb8120a1fa0e5083387e16a50ce411652
shadow_walker <shadowwalkersb@gmail.com>
2022-06-30 19:24:31 -0500

    use switch to enable addition of more customized types

--- libEM/io/renderer.cpp
+++ libEM/io/renderer.cpp
@@ -9,7 +9,14 @@ EMUtil::EMDataType Renderer::rendered_dt(EMUtil::EMDataType dt,
 	if(dt == EMUtil::EM_COMPRESSED) {
 		map<int, decltype(dt)> bits;
 		for (const auto &l: dts) {
-			int key = (l == EMUtil::EM_FLOAT ? 0 : EMDataTypeBits[l]);
+			int key;
+			switch (l) {
+				case EMUtil::EM_FLOAT:
+					key = 0;
+					break;
+				default:
+					key = EMDataTypeBits[l];
+			}
 			bits[key] = l;
 		}
 
