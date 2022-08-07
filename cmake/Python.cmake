74cb852bdbfada0801c2acb45371689e63c6e2d6
shadow_walker <shadowwalkersb@gmail.com>
2022-08-07 18:48:09 -0500

    cmake: Python3_SITELIB

--- cmake/Python.cmake
+++ cmake/Python.cmake
@@ -10,22 +10,7 @@ message("PYTHON_LIBRARIES:    ${PYTHON_LIBRARIES}")
 message("PYTHON_INCLUDE_DIRS: ${PYTHON_INCLUDE_DIRS}")
 message("PYTHON_INCLUDE_PATH: ${PYTHON_INCLUDE_PATH}")
 message("PYTHON_INCLUDE_DIR:  ${PYTHON_INCLUDE_DIR}")
-
-# Set SP_DIR
-if("$ENV{CONDA_BUILD_STATE}" STREQUAL "BUILD" )
-	set(SP_DIR $ENV{SP_DIR})
-else()
-	if(NOT WIN32)
-		set(py_sp_dir_command "import site; print(site.getsitepackages()[0])")
-	else()
-		set(py_sp_dir_command "import site; print(site.getsitepackages()[1])")
-	endif()
-	execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "${py_sp_dir_command}"
-			OUTPUT_VARIABLE SP_DIR
-			OUTPUT_STRIP_TRAILING_WHITESPACE
-			)
-	message("Python site-packages: ${SP_DIR}")
-endif()
+cmake_print_variables(Python3_SITELIB)
 
 execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import sysconfig; print(sysconfig.get_config_var('Py_ENABLE_SHARED'))"
 				OUTPUT_VARIABLE PYTHON_LIB_SHARED
