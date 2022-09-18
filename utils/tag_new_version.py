a84783eb762c4cb962409b75b532143691c33f6b
shadow_walker <shadowwalkersb@gmail.com>
2022-09-18 13:46:21 -0500

    get latest tag from GitHub

--- utils/tag_new_version.py
+++ utils/tag_new_version.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import requests
 
 
 def main():
@@ -9,6 +10,15 @@ def main():
 
 	options = parser.parse_args()
 	print(f"Bumping '{options.bump.upper()}' version...")
+
+	tags = [t['name'] for t in requests.get('https://api.github.com/repos/cryoem/eman2/tags').json()]
+	print(f"Received GitHub tags:\n{tags}")
+
+	tags = sorted([t for t in tags if t.startswith('v')], reverse=True)
+	print(f"Version tags (sorted: latest to oldest):\n{tags}")
+
+	tag = tags[0]
+	print(f"Latest tag:\n{tag}")
 
 
 if __name__ == "__main__":
