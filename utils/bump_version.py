5e1e4e7c0923e7665caef906d2d58dfeac96d701
shadow_walker <shadowwalkersb@gmail.com>
2022-09-18 17:59:32 -0500

    handle files to update, keywords to use and version pairs (old and new)

--- utils/bump_version.py
+++ utils/bump_version.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
 import argparse
+from collections import namedtuple
+
+FileKeyVerPair = namedtuple('FileKeyVerPair', ['fname', 'keyword', 'version_pair'])
+OldNew  = namedtuple('OldNew', ['old', 'new'])
 
 
 def main():
@@ -10,6 +14,20 @@ def main():
 
 	options = parser.parse_args()
 	print(f"Updating version: {options.current_version} -> {options.new_version} ...")
+
+	versions_pair = OldNew(options.current_version, options.new_version)
+
+	eman_versions = (
+		FileKeyVerPair('CMakeLists.txt',                             'VERSION',      versions_pair),
+		FileKeyVerPair('ci_support/set_env_vars.sh',                 'EMAN_VERSION', versions_pair),
+		FileKeyVerPair('ci_support/constructor-mini/construct.yaml', 'eman2',        versions_pair),
+		FileKeyVerPair('ci_support/constructor-huge/construct.yaml', 'eman2',        versions_pair),
+	)
+
+	for f in eman_versions:
+		fname = f.fname
+		keyword = f.keyword
+		version_pair = f.version_pair
 
 
 if __name__ == "__main__":
