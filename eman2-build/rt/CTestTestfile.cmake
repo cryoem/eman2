cc2937755be33ce01985e47bd1887026a7945cfd
landang14 <33734872+landang14@users.noreply.github.com>
2023-03-29 13:53:19 -0500

    Delete eman2-build directory

--- eman2-build/rt/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/landang/Lan_EMAN/eman2/rt
-# Build directory: /Users/landang/Lan_EMAN/eman2/eman2-build/rt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("pyem")
-subdirs("imageio")
--- eman2-build/rt/pyem/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/landang/Lan_EMAN/eman2/rt/pyem
-# Build directory: /Users/landang/Lan_EMAN/eman2/eman2-build/rt/pyem
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
