66b35e15bb83badd9f3d21baa735ed03804b4825
Chen <g5v991x@gmail.com>
2023-06-15 11:59:54 -0700

    better output

--- programs/e2gmm_guess_n.py
+++ programs/e2gmm_guess_n.py
@@ -23,13 +23,10 @@ def main():
 	
 	logid=E2init(sys.argv)
 	fname=args[0]
-	if options.thr<0:
-		tmp="tmp_input.hdf"
-		run(f"e2proc3d.py {fname} {tmp} --process normalize.edgemean")
-		fname=tmp
-		options.thr=4
 		
 	e=EMData(fname)
+	if options.thr<0:
+		options.thr=e["sigma"]*4
 	img=e.numpy().copy()
 	pts=np.array(np.where(img>options.thr)).T
 	
@@ -59,10 +56,13 @@ def main():
 	options.maxn=min(options.maxn, len(pts))
 	nrng=np.arange(options.startn, options.maxn+1, 1000)
 	
+	pdbname=fname[:-4]+"_seg.pdb"
+	gmmname=fname[:-4]+"_gmm.txt"
+	fscname=fname[:-4]+"_fsc.txt"
+	avgname=fname[:-4]+"_gmm.hdf"
+	
 	print("N:    dist")
 	for n in nrng:
-		#launch_childprocess(f"e2segment3d.py {fname} --pdb threed_seg.pdb --process=segment.kmeans:nseg={n}:thr={options.thr}:minsegsep=1")
-		#p=pdb2numpy("threed_seg.pdb")
 		p0=pts.copy()
 		np.random.shuffle(p0)
 		p0=p0[:n]
@@ -76,22 +76,22 @@ def main():
 		dm=np.sort(d)[len(d)//3]
 		#print(np.sort(d))
 		print("{:<5} {:.4f}".format(n, dm))
-		numpy2pdb(p, "threed_seg.pdb")
+		numpy2pdb(p, pdbname)
 		if dm<res:
 			print(f"stop. using N={n}")
 			break
 			
-	run(f"e2project3d.py {fname} --outfile tmp_projection.hdf --orientgen=eman:delta=4 --parallel=thread:16")
-	run(f"e2gmm_refine_new.py --ptclsin tmp_projection.hdf --model threed_seg.pdb --maxres {options.maxres} --minres {options.minres} --modelout model_gmm.txt --niter 50 --trainmodel --evalmodel tmp_model_projections.hdf --learnrate 1e-5")
-	run(f"e2spa_make3d.py --input tmp_model_projections.hdf --output tmp_avg.hdf --thread 32")
-	run(f"e2proc3d.py tmp_avg.hdf model_avg.hdf --process mask.soft:outer_radius=-16 --matchto {fname}")
-	run(f"e2proc3d.py model_avg.hdf model_fsc.txt --calcfsc {fname}")
+	run(f"e2project3d.py {fname} --outfile {fname[:-4]}_tmp_projection.hdf --orientgen=eman:delta=4 --parallel=thread:16")
+	run(f"e2gmm_refine_new.py --ptclsin {fname[:-4]}_tmp_projection.hdf --model {pdbname} --maxres {options.maxres} --minres {options.minres} --modelout {gmmname} --niter 20 --trainmodel --evalmodel {fname[:-4]}_tmp_model_projections.hdf --learnrate 1e-5")
+	run(f"e2spa_make3d.py --input {fname[:-4]}_tmp_model_projections.hdf --output {avgname} --thread 32")
+	run(f"e2proc3d.py {avgname} {avgname} --process mask.soft:outer_radius=-16 --matchto {fname}")
+	run(f"e2proc3d.py {avgname} {fscname} --calcfsc {fname}")
 	
 	
-	print("final pdb model: threed_seg.pdb")
-	print("final GMM in text file: model_gmm.txt")
-	print("final GMM in density map: model_avg.hdf")
-	print("map-model FSC: model_fsc.txt")
+	print("final pdb model: "+ pdbname)
+	print("final GMM in text file: "+ gmmname)
+	print("final GMM in density map: "+avgname)
+	print("map-model FSC: "+fscname)
 	E2end(logid)
 	
 	
