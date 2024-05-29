fa30bef5e69738034209a47612869538ba966e69
Chen <g5v991x@gmail.com>
2024-05-29 16:52:50 -0700

    gmm refinement update

--- programs/e2gmm_heter_refine.py
+++ programs/e2gmm_heter_refine.py
@@ -18,7 +18,9 @@ def main():
 	parser.add_argument("--niter", type=int,help="iterations of focused alignment afterwards.", default=5)
 	parser.add_argument("--expandsym", type=str,help="symmetry. the program does not apply symmetry so always specify symmetry here and the final structure will be in c1", default=None)
 	parser.add_argument("--maxres", type=float,help="maximum resolution for the heterogeneity analysis. This will also be the starting resolution for the following focused alignment.", default=7)
-	parser.add_argument("--minres", type=float,help="min resolution for the heterogeneity analysis.", default=50)
+	parser.add_argument("--minres", type=float,help="min resolution for the heterogeneity analysis.", default=50)	
+	parser.add_argument("--rigidbody", action="store_true", default=False ,help="rigid body movement mode. still testing")
+
 	(options, args) = parser.parse_args()
 	
 	logid=E2init(sys.argv)
@@ -102,15 +104,19 @@ def main():
 			imsk=np.zeros(len(pts), dtype=float)
 			imsk[i]=1
 		
-		pm=pts[imsk>.1]
-		pn=16
-		km=KMeans(pn,max_iter=30)
-		km.fit(pm[:,:3])
-		pc2=km.cluster_centers_
-		
-		pcx=np.vstack([pc, pc2])
-		pp=np.hstack([pcx, np.zeros((len(pcx),1))+np.mean(pts[:,3]), np.zeros((len(pcx),1))+np.mean(pts[:,4])])
-		np.savetxt(f"{path}/model_{it0:02d}_{eo}_anchor.txt", pp)
+		if options.rigidbody:
+			mode="--rigidbody"
+		else:
+			pm=pts[imsk>.1]
+			pn=16
+			km=KMeans(pn,max_iter=30)
+			km.fit(pm[:,:3])
+			pc2=km.cluster_centers_
+			
+			pcx=np.vstack([pc, pc2])
+			pp=np.hstack([pcx, np.zeros((len(pcx),1))+np.mean(pts[:,3]), np.zeros((len(pcx),1))+np.mean(pts[:,4])])
+			np.savetxt(f"{path}/model_{it0:02d}_{eo}_anchor.txt", pp)
+			mode=f"--anchor {path}/model_{it0:02d}_{eo}_anchor.txt"
 		
 		
 		lst=load_lst_params(f"{path}/ptcls_{it0:02d}_{eo}.lst")
@@ -124,14 +130,14 @@ def main():
 		
 		## pretrain from lower res to ensure convergence
 		res0=max(res,7)
-		run(f"e2gmm_refine_new.py --model {path}/model_{it0:02d}_{eo}.txt --anchor {path}/model_{it0:02d}_{eo}_anchor.txt --conv --midout {path}/mid_00_{eo}.txt --maxres {res0} --minres {options.minres} --learnrate 1e-5 --niter 20 --ptclsin {path}/ptcls_{it0:02d}_{eo}_sample.lst --heter --encoderout {path}/enc_{eo}.h5 --decoderout {path}/dec_{eo}.h5 --pas 100 {etc} --maxgradres {res}")
+		run(f"e2gmm_refine_new.py --model {path}/model_{it0:02d}_{eo}.txt {mode} --conv --midout {path}/mid_00_{eo}.txt --maxres {res0} --minres {options.minres} --learnrate 1e-5 --niter 20 --ptclsin {path}/ptcls_{it0:02d}_{eo}_sample.lst --heter --encoderout {path}/enc_{eo}.h5 --decoderout {path}/dec_{eo}.h5 --pas 100 {etc} --maxgradres {res}")
 		# run(f"e2gmm_refine_new.py --model {path}/model_{it0:02d}_{eo}.txt --rigidbody --midout {path}/mid_00_{eo}.txt --maxres {res0} --minres {options.minres} --learnrate 1e-5 --niter 20 --ptclsin {path}/ptcls_{it0:02d}_{eo}_sample.lst --heter --encoderout {path}/enc_{eo}.h5 --decoderout {path}/dec_{eo}.h5 --pas 100 {etc} --maxgradres {res}")
 		
 		run(f"e2gmm_eval.py --pts {path}/mid_00_{eo}.txt --pcaout {path}/mid_pca.txt --ptclsin {path}/ptcls_{it0:02d}_{eo}_sample.lst --ptclsout {path}/ptcls_{eo}_cls_00.lst --mode regress --ncls 4 --nptcl 8000 --axis 0")
 		
 		run(f"e2proc3d.py {path}/ptcls_{eo}_cls_00.hdf {path}/ptcls_{eo}_cls_00.hdf --process filter.lowpass.gauss:cutoff_freq={1./res} --process normalize.edgemean")
 		
-		run(f'e2gmm_batch.py "e2gmm_refine_new.py --model {path}/model_{it0:02d}_{eo}.txt --midout {path}/midall_00_{eo}.txt --maxres {res} --minres {options.minres} --learnrate 1e-5 --niter 10 --ptclsin {path}/ptcls_{it0:02d}_{eo}.lst --heter --encoderout {path}/enc_{eo}.h5 --decoderout {path}/dec_{eo}.h5 --pas 100 {etc}" --load --niter 1 --batch 20000')
+		run(f'e2gmm_batch.py "e2gmm_refine_new.py --model {path}/model_{it0:02d}_{eo}.txt --midout {path}/midall_00_{eo}.txt {mode} --maxres {res} --minres {options.minres} --learnrate 1e-5 --niter 10 --ptclsin {path}/ptcls_{it0:02d}_{eo}.lst --heter --encoderout {path}/enc_{eo}.h5 --decoderout {path}/dec_{eo}.h5 --pas 100 {etc}" --load --niter 1 --batch 20000')
 		
 		run(f"e2gmm_eval.py --pts {path}/midall_00_{eo}.txt --pcaout {path}/mid_pca.txt --ptclsin {path}/ptcls_{it0:02d}_{eo}.lst --ptclsout {path}/ptcls_{eo}_cls_00.lst --mode regress --ncls 4 --nptcl 5000 --axis 0")
 		
