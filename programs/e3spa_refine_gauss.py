I think the large scale translational alignment has the correct sign now. Still a lot of improvements to make.

programs/e3spa_refine_gauss.py
@@ -170,9 +170,9 @@ def main():
 		if options.verbose: print(f"Stage {sn} - {local_datetime()}:")
 		ccache=caches[stage[1]]
 
-#		nliststg=range(sn,nptcl,max(1,nptcl//stage[0]))		# all of the particles to use in the current stage, sn start gives some stochasticity
+		nliststg=range(sn,nptcl,max(1,nptcl//stage[0]))		# all of the particles to use in the current stage, sn start gives some stochasticity
 #		nliststg=range(0,nptcl,max(1,nptcl//stage[0]))		# all of the particles to use in the current stage
-		nliststg=range(0,min(nptcl,stage[0]))		# all of the particles to use in the current stage, bad strategy here for testing
+#		nliststg=range(0,min(nptcl,stage[0]))		# all of the particles to use in the current stage, bad strategy here for testing
 		norm=len(nliststg)//500+1
 
 #	print(ptclsfds.shape,tytx.shape)
@@ -215,14 +215,14 @@ def main():
 				tytx=ccf_step_align(gaus,ptclsfds,orts,tytx)
 				ccache.tytx[nliststg[j:j+500],:2]=tytx
 				dif=(tytx-oldtytx[:,:2])**2
-				print(f"{j}-{j+500}: shift rmsd: {sqrt(tf.math.reduce_mean(dif)):.2f}")
+				print(f"{j}-{j+500}: shift rmsd: {sqrt(tf.math.reduce_mean(dif))*nxraw:.2f}")
 
 			# This is for testing translational alignment only. TODO: remove
-			lsxout=LSXFile(f"{options.path}/tst_ptcls_{sn:02d}.lst")
-			for i in range(len(lsxin)):
-				a,b,c=lsxin[i]
-				lsxout[i]=(a,b,{"xform.projection":Transform({"type":"spinvec","v1":float(ccache.orts[i][0]),"v2":float(ccache.orts[i][1]),"v3":float(ccache.orts[i][2]),"tx":float(ccache.tytx[i][1]*nxraw),"ty":float(ccache.tytx[i][0]*nxraw)}),"frc":float(ccache.frcs[i])})
-			lsxout=None
+			# lsxout=LSXFile(f"{options.path}/tst_ptcls_{sn:02d}.lst")
+			# for i in range(len(lsxin)):
+			# 	a,b,c=lsxin[i]
+			# 	lsxout[i]=(a,b,{"xform.projection":Transform({"type":"spinvec","v1":float(ccache.orts[i][0]),"v2":float(ccache.orts[i][1]),"v3":float(ccache.orts[i][2]),"tx":float(ccache.tytx[i][1]*nxraw),"ty":float(ccache.tytx[i][0]*nxraw)}),"frc":float(ccache.frcs[i])})
+			# lsxout=None
 
 
 