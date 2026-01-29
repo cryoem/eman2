Additional fixes to emplot2d. tweak variance calculation in make3d_gauss

libpyEM/qtgui/emplot2d.py
@@ -87,7 +87,7 @@ from matplotlib.backends.backend_pdf import FigureCanvasPdf
 from matplotlib.figure import Figure
 #matplotlib.use('Agg')
 import numpy as np
-from numpy import ndarray,array
+from numpy import ndarray,array,arange
 
 from .emapplication import EMApp, EMGLWidget
 from .emglobjects import EMOpenGLFlagsAndTools
@@ -987,7 +987,7 @@ lc is the cursor selection point in plot coords"""
 
 		x2,y2=self.plot2scr(x,y)
 
-		r=hypot(x2-evc[0],y2-evc[1])		# distance from cursor
+		r=np.hypot(x2-evc[0],y2-evc[1])		# distance from cursor
 
 		# if the closest point is more than 5 pixels away, nothing is selected
 		if min(r)>5 :
@@ -995,7 +995,7 @@ lc is the cursor selection point in plot coords"""
 			self.del_shapes(("selp0","selp1","selp2","selp3","selp4","selpc"))
 			return
 
-		srt=argsort(r)					# indices which would produce sorted array
+		srt=np.argsort(r)					# indices which would produce sorted array
 
 		# We select one point if it's within 5 pixels, then up to 4 more points within 3 pixels
 		self.selected=[srt[0]]
@@ -3251,7 +3251,8 @@ class EMPlot2DInspector(QtWidgets.QWidget):
 		self.ctrlvls.setValue(pp[9])
 
 		self.hsttog.setChecked(pp[10])
-		self.hststeps.setValue(pp[11])
+		try: self.hststeps.setValue(pp[11])
+		except: pass
 		self.quiet=0
 
 	def newCols(self,val):
@@ -3260,7 +3261,7 @@ class EMPlot2DInspector(QtWidgets.QWidget):
 			names = [str(item.text()) for item in self.setlist.selectedItems()]
 			for name in names:
 				self.target().setAxes(name,self.slidex.value(),self.slidey.value(),self.slidec.value(),self.slides.value(),True)
-			print(self.target())
+			#print(self.target())
 			self.target().needupd=1
 			self.target().updateGL()
 

programs/e3make3d_gauss.py
@@ -402,10 +402,10 @@ def main():
 #				if not options.tomo or sn<2:
 
 				# on the first epoch of each stage we look at the variance of the fsc curve to estimate weighting
-				if i==0 and j==0:
+				if i in (0,8) and j==0:
 					frcs=prj_frcs(gaus.jax,orts,tytx,ptclsfds)
-					print("FRCS ",frcs.shape)
-					frchist.append((np.mean(frcs,1),np.std(frcs,1)))
+					#print("FRCS ",frcs.shape)
+					frchist.append((np.array(np.mean(frcs,0)),np.array(np.std(frcs,0))))
 
 				if options.ctf==0:
 					dsapix=apix*nxraw/ptclsfds.shape[1]
@@ -654,8 +654,9 @@ def main():
 	for i in range(len(frchist[-1][0])):
 		outf.write(f"{i}")
 		for j in range(len(frchist)):
-			try: outf.write(f"\t{frchist[j][0][i]:1.4g}\t{frchist[j][1][i]:1.4g}")
-			except: outf.write("\t0.0\t0.0")
+			try: outf.write(f"\t{frchist[j][0][i]:1.4f}\t{frchist[j][1][i]:1.4f}")
+			except:
+				outf.write("\t0.0\t0.0")
 		outf.write("\n")
 
 	# this is just to save some extra processing steps