bf85ee645a84a039f3b35cfdad5eb52295b626c8
sludtke42 <sludtke42@gmail.com>
2026-08-26 18:43:12 -0500

    No real change [ci build]

--- programs/e2.py
+++ programs/e2.py
@@ -3,6 +3,7 @@ import os
 import platform
 import sys
 
+
 # Change default behavior to execute jupyter-lab
 if len(sys.argv)>1 and sys.argv[1]=="--ipython":
 	e2real = os.path.join(os.path.abspath(os.path.dirname(__file__)), "e2_real.py")
